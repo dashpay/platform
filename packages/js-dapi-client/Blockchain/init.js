9a4a24d8e18b9cf84aa44433586770b3f3556ead
Alex Werner <alex@werner.fr>
2017-04-12 05:03:42 +0200

    Rem: Un-necessary call.

--- packages/js-dapi-client/Blockchain/init.js
+++ packages/js-dapi-client/Blockchain/init.js
@@ -8,9 +8,7 @@ exports.init = function() {
             if(self._config.verbose) console.log('Blockchain - init - try to restore Blockchain state');
             let restored = await self.Blockchain.restore();
             if(self._config.verbose) console.log(`Blockchain - init - Restored ? ${restored}`);
-            let lastHeight = await self.Explorer.API.getLastBlockHeight();
-            if(self._config.verbose) console.log(`Blockchain - Last block height is ${lastHeight}.`);
-            if(self._config.verbose) console.log(`Blockchain - Start fetching missing Blockheaders`);
+            if(self._config.verbose) console.log(`Blockchain - Start background fetching missing Blockheaders`);//TODO fetch and emit event when finished!
             if(self._config.verbose) console.log(`Blockchain ready \n`)
 
             return resolve(true);
