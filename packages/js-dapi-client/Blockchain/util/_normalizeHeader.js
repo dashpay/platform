2465377e71f60dfa200ff669a162b60b6bf530db
Alex Werner <alex@werner.fr>
2017-04-21 18:34:06 +0200

    Impr : Handle genesis block case without a prevHash

--- packages/js-dapi-client/Blockchain/util/_normalizeHeader.js
+++ packages/js-dapi-client/Blockchain/util/_normalizeHeader.js
@@ -16,10 +16,17 @@ module.exports = function(_b){
         _el.hasOwnProperty("bits") &&
         _el.hasOwnProperty("nonce")
     ){
+        if(!_el.previousblockhash){
+            _el.previousblockhash=new Buffer('0000000000000000000000000000000000000000000000000000000000000000', 'hex');
+        }
+        else{
+            _el.previousblockhash=DashUtil.toHash(_el.previousblockhash);
+        }
+        _el.merkleroot=DashUtil.toHash(_el.merkleroot);
         bh = {
             version:_el.version,
-            prevHash:DashUtil.toHash(_el.previousblockhash),
-            merkleRoot:DashUtil.toHash(_el.merkleroot),
+            prevHash:_el.previousblockhash,
+            merkleRoot:_el.merkleroot,
             time:_el.time,
             bits:parseInt(_el.bits,16),
             nonce:_el.nonce
@@ -35,12 +42,17 @@ module.exports = function(_b){
         _el.hasOwnProperty("bits") &&
         _el.hasOwnProperty("nonce")
     ){
+        if(!Buffer.isBuffer(_el.prevHash)){
+            _el.prevHash=DashUtil.toHash(_el.prevHash)||new Buffer('0000000000000000000000000000000000000000000000000000000000000000', 'hex');
+            _el.merkleRoot=DashUtil.toHash(_el.merkleRoot);
+            _el.bits = parseInt(_el.bits,16);
+        }
         bh = {
             version:_el.version,
-            prevHash:DashUtil.toHash(_el.prevHash),
-            merkleRoot:DashUtil.toHash(_el.merkleRoot),
+            prevHash:_el.prevHash,
+            merkleRoot:_el.merkleRoot,
             time:_el.time,
-            bits:parseInt(_el.bits,16),
+            bits:_el.bits,
             nonce:_el.nonce
         };
         return new Bitcore.BlockHeader.fromObject(bh);
