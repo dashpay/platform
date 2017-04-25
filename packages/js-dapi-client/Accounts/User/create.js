e175e305a5553244b77ff4a642e308a102bee9f1
Alex Werner <Alex-Werner@users.noreply.github.com>
2017-04-25 23:23:41 +0200

    Merge pull request #1 from Prtfw/master
    added query + object creation for accounts

--- packages/js-dapi-client/Accounts/User/create.js
+++ packages/js-dapi-client/Accounts/User/create.js
@@ -1,37 +1,53 @@
 const has = require('../../util/has.js');
 const {uuid}=require('khal');
+const ax = require('axios');
 
 exports.create = function() {
     let self = this;
     return async function (_u) {
         return new Promise(function (resolve, reject) {
             let res = {error: null, result: 'success'};
-            if(
-                _u &&
-                has(_u,'username') &&
-                has(_u,'password') &&
-                has(_u,'email')
-            ){
-                let msg = {
-                    type:"user",
-                    action:"create",
-                    user:_u,
-                    _reqId:uuid.generate.v4()
-                };
-                self.emitter.once(msg._reqId, function(data){
-                    if(data.hasOwnProperty('error') && data.error==null){
-                        return resolve(data.message);
-                    }else{
-                        return resolve(data.message);
-                    }
+
+            // comment out client side valdiation for now
+            // if(true ||
+            //     _u &&
+            //     has(_u,'username') &&
+            //     has(_u,'password') &&
+            //     has(_u,'email')
+            // ){
+            //     let msg = {
+            //         type:"user",
+            //         action:"create",
+            //         user:_u,
+            //         _reqId:uuid.generate.v4()
+            //     };
+            //
+                // self.emitter.once(msg._reqId, function(data){
+                //     if(data.hasOwnProperty('error') && data.error==null){
+                //         return resolve(data.message);
+                //     }else{
+                //         return resolve(data.message);
+                //     }
+                // });
+                // self.socket.send(JSON.stringify(msg));
+            // }
+            // else{
+            //     res.error = '100 - Missing Params';
+            //     res.result = 'Missing User';
+            //     return resolve(res);
+            // }
+
+            // console.log(_u.params)
+            //     console.log(_u.returns)
+            //     console.log({"query":"mutation{addRootBase(obj:"+ _u.params+")"+_u.returns+"}"})
+            ax.post('http://localhost:4000/graphql/graphiql',
+                {"query":"mutation{add"+_u.base+"(obj:"+ _u.params+")"+_u.returns+"}"})
+                .then(function (response) {
+                    return resolve(response.data.data)
+                })
+                .catch(function (error) {
+                    console.log(error.response.data.errors);
                 });
-                self.socket.send(JSON.stringify(msg));
-            }
-            else{
-                res.error = '100 - Missing Params';
-                res.result = 'Missing User';
-                return resolve(res);
-            }
         });
     }
 }
--- packages/js-dapi-client/Accounts/User/search.js
+++ packages/js-dapi-client/Accounts/User/search.js
@@ -1,36 +1,47 @@
 const has = require('../../util/has.js');
 const {uuid}=require('khal');
+const ax = require('axios');
+
 
 exports.search = function() {
     let self = this;
     return async function(query){
         return new Promise(function (resolve, reject) {
             let res = {error: null, result: 'success'};
-            if(
-                query &&
-                (has(query,'username') ||
-                has(query,'email'))
-            ){
-                let msg = {
-                    type:"user",
-                    action:"search",
-                    query:query,
-                    _reqId:uuid.generate.v4()
-                };
-                self.emitter.once(msg._reqId, function(data){
-                    if(data.hasOwnProperty('error') && data.error==null){
-                        return resolve(data);
-                    }else{
-                        return resolve(data.message);
-                    }
+            // if(
+            //     query &&
+            //     (has(query,'username') ||
+            //     has(query,'email'))
+            // ){
+            //     let msg = {
+            //         type:"user",
+            //         action:"search",
+            //         query:query,
+            //         _reqId:uuid.generate.v4()
+            //     };
+            //     self.emitter.once(msg._reqId, function(data){
+            //         if(data.hasOwnProperty('error') && data.error==null){
+            //             return resolve(data);
+            //         }else{
+            //             return resolve(data.message);
+            //         }
+            //     });
+            //     self.socket.send(JSON.stringify(msg));
+            // }
+            // else{
+            //     res.error = '100 - Missing Params';
+            //     res.result = 'Missing Query';
+            //     return resolve(res);
+            // }
+
+            ax.get('http://localhost:4000/graphql/graphiql?'+"query={RootBase" +query.returns+"}")
+                .then(function (response) {
+                    return resolve(response.data.data)
+                })
+                .catch(function (error) {
+                    console.log(error.response.data.errors);
                 });
-                self.socket.send(JSON.stringify(msg));
-            }
-            else{
-                res.error = '100 - Missing Params';
-                res.result = 'Missing Query';
-                return resolve(res);
-            }
+
         });
     }
 }
