660665ad8bcf021ab3d369c86d58fd106f624e29
Alex Werner <Alex-Werner@users.noreply.github.com>
2017-05-23 04:18:20 +0200

    Merge pull request #3 from dashevo/authService
    Auth service

---
+++ packages/dapi/lib/authService/authService.js
@@ -0,0 +1,25 @@
+const Message = require('bitcore-message-dash');
+class AuthService {
+    constructor(app) {
+        this.app = app;
+        this.challengeMsg = Math.random().toString(36).substring(7); //pvr: pseudo random only, needs to be updated for production
+    }
+    //Triggered by graphQL ?
+    resolveChallenge(txId, signature) {
+        return this.app.rpc.getTransaction(txId)
+            .then(txData => {
+                if(!txData || !txData.hasOwnProperty('vout'))
+                    throw Error('Empty transaction');
+                //pvr: move to bitcore-lib-dash?
+                let rawData = txData.vout.filter(o => o.scriptPubKey.asm.includes('OP_RETURN'))[0]
+                    .scriptPubKey.asm.replace('OP_RETURN ', '');
+                let data = JSON.parse(new Buffer(rawData, 'hex').toString('utf8'));
+                let pubKey = data.pubKey;
+                ///////////////////////////////////
+                return Message(this.challengeMsg).verify(pubKey, signature);
+            }).catch(function(err){
+                console.error('Error ', err)
+            })
+    }
+}
+module.exports = AuthService;
--- packages/dapi/lib/dapi.js
+++ packages/dapi/lib/dapi.js
@@ -1,3 +1,4 @@
+const AuthService = require('./authService/authService');
 const Server = require('./server/server');
 const RPC = require('./rpc/rpc');
 const ZMQ = require('./zmq/zmq');
@@ -6,6 +7,8 @@ class Dapi {
     constructor(config) {
         this.config = config;
         this.rpc = new RPC(this);
+        this.authService = new AuthService(this);
+                
         this.zmq = new ZMQ(this);
         this.server=new Server(this);
         this.socket = new Socket(this);
