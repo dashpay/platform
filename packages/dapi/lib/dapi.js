3f21e62ae764028b2001752e8ebc87836983482c
Alex Werner <Alex-Werner@users.noreply.github.com>
2017-07-31 02:49:43 +0200

    Merge pull request #10 from dashevo/cleanup
    Cleaned up DAPI

--- packages/dapi/lib/dapi.js
+++ packages/dapi/lib/dapi.js
@@ -1,17 +1,11 @@
 const AuthService = require('./authService/authService');
 const Server = require('./server/server');
-const RPC = require('./rpc/rpc');
-const ZMQ = require('./zmq/zmq');
-const Socket = require('./socket/socket');
 class Dapi {
     constructor(config) {
         this.config = config;
-        this.rpc = new RPC(this);
         this.authService = new AuthService(this);
 
-        this.zmq = new ZMQ(this);
         this.server = new Server(this);
-        this.socket = new Socket(this);
     }
 
 }
