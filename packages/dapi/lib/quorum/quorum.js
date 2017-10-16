60246bf855f9051c406ec6a839ee90ca916b9798
Pierre <pvrooyen@gmail.com>
2017-10-16 07:55:31 +0200

    reformat

--- packages/dapi/lib/quorum/quorum.js
+++ packages/dapi/lib/quorum/quorum.js
@@ -1,12 +1,12 @@
 class Quorum {
-	constructor(app){
+	constructor(app) {
 		this.logger = app.logger;
-		
+
 		this.logger.debug('- Init Quorum');
 	}
-	performAction(type, val){
+	performAction(type, val) {
 		this.logger.debug('Quorum - Received action ', type, val);
-		switch (type){
+		switch (type) {
 			case "add":
 				return this.addObject(val);
 				break;
@@ -29,15 +29,15 @@ class Quorum {
 				return this.authenticate(val);
 				break;
 			default:
-				return "Not Implemented - PerformAction "+type;
+				return "Not Implemented - PerformAction " + type;
 		}
 	}
-	addObject(value){return {"response":"Added"};}
-	commitObject(value){return {"response":"Commited"};}
-	removeObject(value){return {"response":"Removed"};}
-	getState(value){return {"response":"Getted"};}
-	listenForeignKey(value){return {"response":"Listened"};}
-	migrateState(value){return {"response":"Migrated"};}
-	authenticate(value){return {"response":"Authenticated"};}
+	addObject(value) { return { "response": "Added" }; }
+	commitObject(value) { return { "response": "Commited" }; }
+	removeObject(value) { return { "response": "Removed" }; }
+	getState(value) { return { "response": "Getted" }; }
+	listenForeignKey(value) { return { "response": "Listened" }; }
+	migrateState(value) { return { "response": "Migrated" }; }
+	authenticate(value) { return { "response": "Authenticated" }; }
 };
 module.exports = Quorum;
