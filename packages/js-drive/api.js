7605b816ce096a82b666cd5bb017ccbb3532f9d0
Nathan Marley <nathan.marley@gmail.com>
2017-11-15 04:59:45 +0800

    JSONRPC api example

---
+++ packages/js-drive/api.js
@@ -0,0 +1,8 @@
+var jayson = require('jayson')
+var server = jayson.server({
+  add: function(args, callback) {
+    callback(null, args[0] + args[1])
+  }
+})
+
+server.http().listen(3000, '127.0.0.1')
