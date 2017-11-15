6327d025e9605b0482d209d010a1adec3ad5af81
Anton <antouhou@gmail.com>
2017-11-15 21:22:48 +0800

    Added mocked data for rpc user

--- packages/dapi/lib/rpc.js
+++ packages/dapi/lib/rpc.js
@@ -2,7 +2,11 @@ const { User } = require('dash-schema/lib').Consensus;
 
 const mockedData = {
   user: {
-
+    uname: '',
+    regtxid: 'ef6ab42e001144bfbaf4777b05148f56a9705b63cdc320c95171bc600df7088e',
+    pubkey: '024964f06ea5cfec1890d7e526653b083c12360f79164c1e8163327d0849fa7bca',
+    credits: 100000,
+    subtx: [],
   },
 };
 
@@ -13,6 +17,7 @@ const dashrpc = {
       throw new Error('Username is not valid');
     }
     const user = Object.assign({}, mockedData.user);
+    user.uname = username;
     return user;
   },
 };
