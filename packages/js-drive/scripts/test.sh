427171dfe27ade26c62a0b48c40dbd758d065cb9
Nathan Marley <nathan.marley@gmail.com>
2017-11-16 15:35:15 +0800

    test each API call

--- packages/js-drive/scripts/test.sh
+++ packages/js-drive/scripts/test.sh
@@ -1,15 +1,31 @@
-# curl -i -X POST -H "Content-Type: application/json; indent=4" \
-# -d '{
-# "jsonrpc": "2.0",
-# "method": "getBlockchainUser",
-# "params": {"name": "andy"},
-# "id": "1"
-# }' http://localhost:5001;echo
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getBlockchainUser",
+"params": {"name": "andy"},
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getBlockchainUserStateSinceHeight",
+"params": {"name": "andy", "dapid": "1234"},
+"id": "1"
+}' http://localhost:5001;echo
 
 curl -i -X POST -H "Content-Type: application/json; indent=4" \
 -d '{
 "jsonrpc": "2.0",
 "method": "getBlockchainUserState",
+"params": {"name": "andy", "dapid": "1234"},
+"id": "1"
+}' http://localhost:5001;echo
+
+curl -i -X POST -H "Content-Type: application/json; indent=4" \
+-d '{
+"jsonrpc": "2.0",
+"method": "getDapSchema",
 "params": {"name": "andy", "dapid": "1234"},
 "id": "1"
 }' http://localhost:5001;echo
