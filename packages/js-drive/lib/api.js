f07d58e66742103853b922ce5cfb121e97c394a6
Nathan Marley <nathan.marley@gmail.com>
2017-11-16 12:54:45 +0800

    reference api_methods

--- packages/js-drive/lib/api.js
+++ packages/js-drive/lib/api.js
@@ -1,45 +1,16 @@
 const jayson = require('jayson')
+const {
+  getBlockchainUser,
+  getBlockchainUserStateSinceHeight,
+  getBlockchainUserState,
+  getDapSchema
+} = require('../lib/api_methods')
+
 let server = jayson.server({
-
-  /**
-   * Returns a BlockchainUser Schema object derived from SubTXs registered on the blockchain
-   * @param uname {string} BlockchainUser name
-   * @memberof DashDrive
-   */
-  getBlockchainUser(args, callback) {
-    callback(null, args["name"])
-  },
-
-  /**
-   * Get changes to a user's state since height
-   * @param uname {string} BlockchainUser name
-   * @param height {number} Height to check since
-   * @memberof DashDrive
-   */
-  getBlockchainUserStateSinceHeight(args, callback) {
-    callback(null, "name: " + args["name"] + ", height: " + args["height"])
-  },
-
-  /**
-   * Returns the current data state for a BU on a given Dap schema, that is resolved by relpaying packets of a BU up to the last transition on the blockchain
-   * @param uname {string} Blockchain username
-   * @param dapid {string} Hash of the DAP Schema
-   * @memberof DashDrive
-   */
-  getBlockchainUserState(args, callback) {
-    callback(null, "name: " + args["name"] + ", dapid: " + args["dapid"])
-  },
-
-  /**
-   * Returns a DAP Schema registered in DashDrive by id
-   * DAP Schemas have null dapids in their containing DAP Object to signify they are within the Platform DAP (constituting a DAP definition not a certain DAP's data)
-   * @param dapid {string} Hash of the DAP Schema
-   * @memberof DashDrive
-   */
-  getDapSchema(args, callback) {
-    callback(null, "dapid: " + args["dapid"])
-  }
-
+  getBlockchainUser,
+  getBlockchainUserStateSinceHeight,
+  getBlockchainUserState,
+  getDapSchema
 })
 
 var bind_host = process.env.BIND_HOST || '0.0.0.0';
