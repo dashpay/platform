9e393a9b6d1d598b9a929f3bb38f1b270cee7d94
Nathan Marley <nathan.marley@gmail.com>
2018-02-02 11:07:17 -0800

    Merge pull request #5 from dashevo/test
    write an actual passing test

--- packages/js-drive/test/api_methods.test.js
+++ packages/js-drive/test/api_methods.test.js
@@ -10,10 +10,10 @@ const {
 } = require('../lib/api_methods')
 
 describe("API.getBlockchainUser", () => {
-  it("should kick some ass", () => {
+  it("should return a blockchain user", () => {
     getBlockchainUser({"name": "andy"}, (err, name) => {
-      expect(name).to.be.a('string');
-      expect(name).to.be.eql('alice')
+      expect(name).to.be.a("string");
+      expect(name).to.be.eql("Got BU: andy")
     })
   })
 })
