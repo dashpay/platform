a1c912edaeb0ff5286138b77de669ac4c1b8e7e1
Nathan Marley <nathan.marley@gmail.com>
2018-02-02 11:03:29 -0800

    Merge pull request #4 from dashevo/rm-api-name
    rename to packages/js-drive/just dashdrive (remove -api)

--- packages/js-drive/build_docker_image.sh
+++ packages/js-drive/build_docker_image.sh
@@ -6,7 +6,7 @@ eval $(aws ecr get-login --no-include-email)
 # 0.5. set the current version:
 VERSION="0.0.1"
 REPO_URL="103738324493.dkr.ecr.us-west-2.amazonaws.com"
-IMAGE_NAME="dashevo/dashdrive-api"
+IMAGE_NAME="dashevo/dashdrive"
 
 # 1. build image:
 docker build -t "${IMAGE_NAME}:latest" -t "${IMAGE_NAME}:${VERSION}" .
