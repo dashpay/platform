d822c212160480796b7b29ac7736acce7f22d85e
Ivan Shumkov <ivan@shumkov.ru>
2018-03-30 12:53:29 +0300

    [DD-106] `startIPFSInstance` should be able to create multiple instances
    * spawn many IPFS instances for tests
    
    * rename method `callInParallel`
    
    * add tests and refactor for readability

--- packages/js-drive/lib/test/startIPFSInstance.js
+++ packages/js-drive/lib/test/startIPFSInstance.js
@@ -1,45 +1,90 @@
 const util = require('util');
 
 const DaemonFactory = require('ipfsd-ctl');
-const IPFSApi = require('ipfs-api');
 
 const df = DaemonFactory.create();
 
-const spawn = util.promisify(df.spawn).bind(df);
-
-// Workaround for https://github.com/ipfs/js-ipfsd-ctl/issues/202
-process.execArgv = [];
+const spawnIpfs = util.promisify(df.spawn).bind(df);
 
 /**
- * Start and stop IPFS instance for mocha test
+ * Start and stop IPFS instance for mocha tests
  *
- * @return {Promise<IPFSApi>}
+ * @return {Promise<IpfsAPI>}
  */
-module.exports = function startIPFSInstance() {
-  let stop;
-  let cleanup;
+async function startIPFSInstance() {
+  const ipfsAPIs = await startIPFSInstance.many(1);
 
-  return new Promise(((resolve) => {
+  return ipfsAPIs[0];
+}
+
+/**
+ * @param {Node[]} objects
+ * @param {string} method
+ * @return {Promise<[any]>}
+ */
+async function callInParallel(objects, method) {
+  const promises = objects.map(object => object[method]());
+  return Promise.all(promises);
+}
+
+async function spawnAndPromisifyIpfs() {
+  const ipfsd = await spawnIpfs();
+
+  ipfsd.stop = util.promisify(ipfsd.stop).bind(ipfsd);
+  ipfsd.cleanup = util.promisify(ipfsd.cleanup).bind(ipfsd);
+
+  return ipfsd;
+}
+
+/**
+ * Start and stop specified number of IPFS instances for mocha tests
+ *
+ * @param number
+ * @return {Promise<IpfsAPI[]>}
+ */
+startIPFSInstance.many = function many(number) {
+  const ipfsInstances = [];
+
+  if (number < 1) {
+    throw new Error('Invalid number of instances');
+  }
+
+  return new Promise(((resolve, reject) => {
     before(async function before() {
-      this.timeout(25 * 1000); // slow Ctl
+      this.timeout(number * 25 * 1000); // slow IPFS ctl
 
-      const ipfsd = await spawn();
-      stop = util.promisify(ipfsd.stop).bind(ipfsd);
-      cleanup = util.promisify(ipfsd.cleanup).bind(ipfsd);
+      try {
+        const firstInstance = await spawnAndPromisifyIpfs();
+        ipfsInstances.push(firstInstance);
 
-      resolve(new IPFSApi(ipfsd.apiAddr));
+        if (number > 1) {
+          const firstInstanceId = await firstInstance.api.id();
+
+          for (let i = 1; i < number; i++) {
+            const instance = await spawnAndPromisifyIpfs();
+
+            await instance.api.swarm.connect(firstInstanceId.addresses[0]);
+
+            ipfsInstances.push(instance);
+          }
+        }
+      } catch (e) {
+        reject(e);
+
+        throw e;
+      }
+
+      resolve(ipfsInstances.map(instance => instance.api));
     });
 
     afterEach(async () => {
-      if (cleanup) {
-        await cleanup();
-      }
+      await callInParallel(ipfsInstances, 'cleanup');
     });
 
     after(async () => {
-      if (stop) {
-        await stop();
-      }
+      await callInParallel(ipfsInstances, 'stop');
     });
   }));
 };
+
+module.exports = startIPFSInstance;
