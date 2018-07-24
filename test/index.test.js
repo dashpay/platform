6c147939f621d704b66dd078e1fb2a35186504dd
pvrooyen <pierre@dash.org>
2018-07-24 21:13:26 +0200

    Feat/longestchain (dashevo/dash-spv#9)
    * refactor tx proof
    
    * rename param
    
    * abstract generic merkle verification
    
    * add mn proofs
    
    * fn rentame
    
    * change genesis concept to root concept
    
    * interrim commit
    
    * rem forkedChain (replaced by new logic)
    
    * fixed tests
    
    * better getheader
    
    * typo and name change
    
    * rem depricated code
    
    * orphans
    
    * final test
    
    * invalid header
    
    * duplicate blocks
    
    * update confirms logic
    
    * blockstore
    
    * add todo for chain splits
    
    * change class declaration

--- test/index.test.js
+++ test/index.test.js
@@ -12,30 +12,92 @@ describe('SPV-DASH (forks & re-orgs)', () => {
     chain = new Blockchain('testnet');
   });
 
-  it('should get 25 testnet headers', () => {
-    headers.length.should.equal(25);
+  it('should get 26 testnet headers', () => {
+    headers.length.should.equal(26);
   });
 
-  it('should contain 1 fork when chain is initialised with genesis block', () => {
-    chain.forkedChains.length.should.equal(1);
+  it('should contain 1 branch when chain is initialised with genesis block', () => {
+    chain.getAllBranches().length.should.equal(1);
   });
 
   it('should contain genesis hash', () => {
     chain.getTipHash().should.equal('00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c');
+    chain.getLongestChain().length.should.equal(1);
   });
 
-  it('should still contain a fork of 1 when first header is added', () => {
+  it('should still contain a branch of 1 when first header is added', () => {
     chain.addHeader(headers[0]);
-    chain.forkedChains.length.should.equal(1);
+    chain.getAllBranches().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(2);
   });
 
-  it('should contain correct tip and height when remaining headers [1..24] is added', () => {
-    headers.shift();
-    chain.addHeaders(headers);
-    chain.getChainHeight().should.equal(25);
+  it('should discard addding of duplicate block', () => {
+    chain.addHeader(headers[0]);
+    chain.getOrphans().length.should.equal(0);
+    chain.getLongestChain().length.should.equal(2);
+  });
+
+  it('create 1 orphan', () => {
+    chain.addHeader(headers[2]);
+    chain.getOrphans().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(2);
+  });
+
+  it('connect the orphan by adding its parent', () => {
+    chain.addHeader(headers[1]);
+    chain.getOrphans().length.should.equal(0);
+    chain.getAllBranches().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(4);
+  });
+
+  it('add remaining test headers', () => {
+    chain.addHeaders(headers.slice(3));
+    chain.getOrphans().length.should.equal(0);
+    chain.getAllBranches().length.should.equal(1);
+    chain.getLongestChain().length.should.equal(26);
+  });
+
+  it('not add an invalid header', () => {
+    chain.addHeader(headers[25]);
+    chain.getLongestChain().length.should.equal(26);
   });
 });
 
+let genesisHash = null;
+describe('Blockstore', () => {
+  before(() => {
+    headers = chainManager.fetchHeaders();
+    chain = new Blockchain('testnet', 10);
+    genesisHash = chain.getTipHash();
+  });
+
+  it('should add 9 headers', (done) => {
+    chain.addHeaders(headers.slice(0, 9));
+    chain.getLongestChain().length.should.equal(10);
+    chain.getHeader(genesisHash)
+      .then((header) => {
+        header.hash.should.equal(genesisHash);
+        header.should.have.property('children');
+        done();
+      });
+  });
+
+  it('should move 1 block to  the blockstore', (done) => {
+    chain.addHeaders(headers.slice(9, 10));
+    chain.getLongestChain().length.should.equal(10);
+    chain.getHeader(genesisHash)
+      .then((header) => {
+        header.hash.should.equal(genesisHash);
+        header.should.not.have.property('children');
+        done();
+      });
+  });
+});
+
+// TODO:
+// Create scenarios where chain splits occur to form competing brances
+// Difficult with current chain provided by chainmanager as this is actual hardcoded
+// Dash testnet headers which requires significant CPU power to create forked chains from
 
 describe('Difficulty Calculation', () => {
   it('should have difficulty of 1 when target is max', () => {
@@ -48,4 +110,3 @@ describe('Difficulty Calculation', () => {
     utils.getDifficulty(testnetMaxTarget).should.be.greaterThan(1);
   });
 });
-
