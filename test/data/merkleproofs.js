91dcd077e5738ed473773a07dba6c0547c525895
Pierre <pierre@dash.org>
2018-08-07 09:43:29 +0200

    txproof tests

---
+++ test/data/merkleproofs.js
@@ -0,0 +1,50 @@
+const merkleJSON = {
+  merkleBlock: { // Mainnet Block 300100
+    header: {
+      hash: '00000000000051b99f3fa12da6997bb54327c7b81509829467b499a7ce03136a',
+      version: 3,
+      prevHash: '0000000000154ef1f88653a6757f1182680a4d5831d815010d5fa646ae79ce35',
+      merkleRoot: 'a0055d45ad9b35e77fb01c59a4feb9976921493d2557a5ac0798b49e82ea1e99',
+      time: 1436550250,
+      bits: 454590659,
+      nonce: 2601264896,
+    },
+    numTransactions: 12,
+    hashes: [
+      '9d0a368bc9923c6cb966135a4ceda30cc5f259f72c8843ce015056375f8a06ec',
+      '39e5cd533567ac0a8602bcc4c29e2f01a4abb0fe68ffbc7be6c393db188b72e0',
+      'cd75b421157eca03eff664bdc165730f91ef2fa52df19ff415ab5acb30045425',
+      '2ef9795147caaeecee5bc2520704bb372cde06dbd2e871750f31336fd3f02be3',
+      '2241d3448560f8b1d3a07ea5c31e79eb595632984a20f50944809a61fdd9fe0b',
+      '45afbfe270014d5593cb065562f1fed726f767fe334d8b3f4379025cfa5be8c5',
+      '198c03da0ccf871db91fe436e2795908eac5cc7d164232182e9445f7f9db1ab2',
+      'ed07c181ce5ba7cb66d205bc970f43e1ca11996d611aa8e91e305eb8608c543c',
+    ],
+    flags: [219, 63],
+  },
+  mnProof: { // Mainnet Block 300100
+    header: {
+      hash: '00000000000051b99f3fa12da6997bb54327c7b81509829467b499a7ce03136a',
+      version: 3,
+      prevHash: '0000000000154ef1f88653a6757f1182680a4d5831d815010d5fa646ae79ce35',
+      merkleRoot: 'a0055d45ad9b35e77fb01c59a4feb9976921493d2557a5ac0798b49e82ea1e99',
+      time: 1436550250,
+      bits: 454590659,
+      nonce: 2601264896,
+    },
+    numTransactions: 12,
+    hashes: [
+      '9d0a368bc9923c6cb966135a4ceda30cc5f259f72c8843ce015056375f8a06ec',
+      '39e5cd533567ac0a8602bcc4c29e2f01a4abb0fe68ffbc7be6c393db188b72e0',
+      'cd75b421157eca03eff664bdc165730f91ef2fa52df19ff415ab5acb30045425',
+      '2ef9795147caaeecee5bc2520704bb372cde06dbd2e871750f31336fd3f02be3',
+      '2241d3448560f8b1d3a07ea5c31e79eb595632984a20f50944809a61fdd9fe0b',
+      '45afbfe270014d5593cb065562f1fed726f767fe334d8b3f4379025cfa5be8c5',
+      '198c03da0ccf871db91fe436e2795908eac5cc7d164232182e9445f7f9db1ab2',
+      'ed07c181ce5ba7cb66d205bc970f43e1ca11996d611aa8e91e305eb8608c543c',
+    ],
+    flags: [219, 63],
+  },
+};
+
+module.exports = merkleJSON;
