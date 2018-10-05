bf25be3b346145f0f4eb1e1e44bb8a8f060309aa
Ivan Shumkov <ivan@shumkov.ru>
2018-10-05 20:23:42 +0300

    Remove obsolete stubs

--- packages/js-drive/lib/stateView/dapSchema/DAPSchema.js
+++
@@ -1,7 +0,0 @@
-module.exports = class DAPSchema {
-  constructor(dapId, schema, createdAt) {
-    this.dapId = dapId;
-    this.schema = schema;
-    this.createdAt = createdAt;
-  }
-};
--- packages/js-drive/lib/stateView/userObject/UserObjectRevision.js
+++
@@ -1,9 +0,0 @@
-module.exports = class UserObjectRevision {
-  constructor(revision, objectHash, blockHeight, packetId, createdAt) {
-    this.revision = revision;
-    this.objectHash = objectHash;
-    this.blockHeight = blockHeight;
-    this.packetId = packetId;
-    this.createdAt = createdAt;
-  }
-};
