ab3248e296829b841fbb87f69ca23525a9759be6
Ivan Shumkov <ivan@shumkov.ru>
2018-10-31 18:14:35 +0000

    Configure tests

---
+++ packages/js-dpp/system/lib/test/bootstrap.js
@@ -0,0 +1,21 @@
+const { expect, use } = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const dirtyChai = require('dirty-chai');
+const chaiAsPromised = require('chai-as-promised');
+
+use(sinonChai);
+use(chaiAsPromised);
+use(dirtyChai);
+
+process.env.NODE_ENV = 'test';
+
+beforeEach(function beforeEach() {
+  if (!this.sinon) {
+    this.sinon = sinon.createSandbox();
+  } else {
+    this.sinon.restore();
+  }
+});
+
+global.expect = expect;
