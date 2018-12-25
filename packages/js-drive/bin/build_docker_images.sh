139ca9c5504ebeada9c40501ee13778352ca977d
Djavid Gabibiyan <djavidoff@gmail.com>
2018-12-25 19:08:14 +0500

    [DD-461] Introduce Versioning for Drive Builds
    * get version from package file and push additional image with it
    
    * change minor version of Drive to 1

--- packages/js-drive/bin/build_docker_images.sh
+++ packages/js-drive/bin/build_docker_images.sh
@@ -5,9 +5,12 @@ set -e
 REPO_URL="103738324493.dkr.ecr.us-west-2.amazonaws.com"
 IMAGE_NAME="dashevo/dashdrive"
 
+DRIVE_VERSION=$(node -p "require('./package.json').version")
+
 docker build --build-arg NODE_ENV=development \
              --build-arg npm_token=$NPM_TOKEN \
              -t "${REPO_URL}/${IMAGE_NAME}" \
              .
 
-docker push "${REPO_URL}/${IMAGE_NAME}"
+docker push "${REPO_URL}/${IMAGE_NAME}:latest"
+docker push "${REPO_URL}/${IMAGE_NAME}:${DRIVE_VERSION}"
