9a933dc016eb35a39043d14d0c511c3a2d4bb247
Nathan Marley <nathan.marley@gmail.com>
2019-04-02 11:58:25 -0300

    Deploy Docker image to Docker Hub (dashevo/js-drive#251)
    * Deploy Docker image to Docker Hub
    
    * Use "set -x" to show script commands in output
    * Update image name to dashpay/dapi
    * Remove ECR authentication and REPO_URL
    * Add Docker Hub auth
    
    * Remove AWS credentials helper from CI config
    
    * Rename DRIVE_VERSION to VERSION

--- packages/js-drive/bin/build_docker_images.sh
+++ packages/js-drive/bin/build_docker_images.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+# Show script in output, and error if anything fails
+set -xe
 
-# ensure REPO_URL is set externally (e.g. via CI/CD)
-if [ "x$REPO_URL" = "x" ]; then
-  echo "error: REPO_URL is required to be set"
-  exit 1
-fi
-
-IMAGE_NAME="dashevo/drive"
-DRIVE_VERSION=$(node -p "require('./package.json').version")
+IMAGE_NAME="dashpay/drive"
+VERSION=$(node -p "require('./package.json').version")
 
 docker build --build-arg NODE_ENV=development \
              --build-arg npm_token=$NPM_TOKEN \
              -t "${REPO_URL}/${IMAGE_NAME}:latest" \
-             -t "${REPO_URL}/${IMAGE_NAME}:${DRIVE_VERSION}" \
+             -t "${REPO_URL}/${IMAGE_NAME}:${VERSION}" \
              .
 
-docker push "${REPO_URL}/${IMAGE_NAME}:latest"
-docker push "${REPO_URL}/${IMAGE_NAME}:${DRIVE_VERSION}"
+# Login to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Push images to the registry
+docker push "${IMAGE_NAME}:latest"
+docker push "${IMAGE_NAME}:${VERSION}"
