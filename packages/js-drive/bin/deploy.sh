38b613591f2dea53f1265f8d12d97831ca854152
Ivan Shumkov <ivan@shumkov.ru>
2019-04-04 13:09:54 +0300

    Add additional checks before deploy

--- packages/js-drive/bin/deploy.sh
+++ packages/js-drive/bin/deploy.sh
@@ -3,8 +3,16 @@
 # Show script in output, and error if anything fails
 set -xe
 
+# Ensure the tag matches the one in package.json, otherwise abort.
+VERSION="$(jq -r .version package.json)"
+PACKAGE_TAG=v"$VERSION"
+
+if [[ "$PACKAGE_TAG" != "$TRAVIS_TAG" ]]; then
+  echo "Travis tag (\"$TRAVIS_TAG\") is not equal to package.json tag (\"$PACKAGE_TAG\"). Please push a correct tag and try again."
+  exit 1
+fi
+
 IMAGE_NAME="dashpay/drive"
-VERSION=$(node -p "require('./package.json').version")
 
 docker build --build-arg NODE_ENV=development \
              -t "${IMAGE_NAME}:latest" \
