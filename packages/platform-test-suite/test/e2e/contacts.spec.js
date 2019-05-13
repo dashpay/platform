8149eebce1bf43d64d939b32b869044200fd0ac5
Andrei <abvgedeika@gmail.com>
2019-05-13 13:54:37 +0300

    Merge pull request #16 from dashevo/fix_for_meta
    Fix `$meta` assertion

--- packages/platform-test-suite/test/e2e/contacts.spec.js
+++ packages/platform-test-suite/test/e2e/contacts.spec.js
@@ -334,7 +334,8 @@ describe('Contacts app', () => {
       }
 
       expect(actualAliceProfile).to.not.be.null();
-      expect(actualAliceProfile.$meta).to.equal(aliceRegTxId);
+      expect(actualAliceProfile).to.have.property('$meta');
+      expect(actualAliceProfile.$meta.userId).to.equal(aliceRegTxId);
 
       delete actualAliceProfile.$meta;
 
@@ -588,7 +589,7 @@ describe('Contacts app', () => {
 
       expect(contacts).to.have.lengthOf(1);
 
-      const [actualBobContactRequest] = contacts[0];
+      const [actualBobContactRequest] = contacts;
 
       delete actualBobContactRequest.$meta;
 
