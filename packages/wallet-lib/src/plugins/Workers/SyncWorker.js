d2929901f4d6989a01d50fa131ca0aae970fe398
Alex Werner <Alex-Werner@users.noreply.github.com>
2019-08-06 14:53:18 +0200

    impr(SyncWorker): improve tx fetching (dashevo/wallet-lib#68)
    * impr(SyncWorker): improve tx fetching
    
    Introduce three threshold (fast, normal, slow). And determine which address need to be fetch in regards of which threshold
    
    * impr(SyncWorker): move slow threshold to 5 min
    
    * impr(SyncWorker): tweak up the rules
    
    Moved 40000 block to 20000 (30 d), removed tx.len > 10 rules.
    
    * style: lint fix

--- packages/wallet-lib/src/plugins/Workers/SyncWorker.js
+++ packages/wallet-lib/src/plugins/Workers/SyncWorker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 const _ = require('lodash');
 const { Worker } = require('../');
 const { ValidTransportLayerRequired, InvalidTransactionObject } = require('../../errors');
@@ -6,8 +7,17 @@ const { UNCONFIRMED_TRANSACTION_STATUS_CODE, SECURE_TRANSACTION_CONFIRMATIONS_NB
 
 // eslint-disable-next-line no-underscore-dangle
 const _defaultOpts = {
-  fetchThreshold: 10 * 60 * 1000,
-  workerIntervalTime: 1 * 10 * 1000,
+  // Thoses are addresses that were used only once, and long time ago.
+  // Low chance of receiving fund. We still check every ten minutes
+  slowFetchThresold: 5 * 60 * 1000,
+  // Those are addresses that we consider standard, InstantSend promise a one minute time,
+  // That is what we offer here (will be changed with streams)
+  fetchThreshold: 60 * 1000,
+  // Those are special cases, such as the current unusedAddress for instance,
+  // Higher chance of receiving tx, we listen in a quite spammy ways.
+  fastFetchThreshold: 15 * 1000,
+  // Loop will go through every 10 sec
+  workerIntervalTime: 10 * 1000,
 };
 
 class SyncWorker extends Worker {
@@ -20,7 +30,7 @@ class SyncWorker extends Worker {
       workerIntervalTime: defaultOpts.workerIntervalTime,
       fetchThreshold: defaultOpts.fetchThreshold,
       dependencies: [
-        'storage', 'transport', 'fetchStatus', 'fetchAddressInfo', 'fetchTransactionInfo', 'walletId', 'getUnusedAddress',
+        'storage', 'transport', 'fetchStatus', 'getTransaction', 'fetchAddressInfo', 'fetchTransactionInfo', 'walletId', 'getUnusedAddress',
       ],
     }, opts);
     super(params);
@@ -101,42 +111,73 @@ class SyncWorker extends Worker {
 
   async execAddressFetching() {
     const self = this;
-    const { addresses } = this.storage.getStore().wallets[this.walletId];
+    const { addresses, network } = this.storage.getStore().wallets[this.walletId];
+    const currBlockheight = this.storage.getStore().chains[network.toString()].blockheight;
+    const currTime = Date.now();
     const { fetchAddressInfo } = this;
 
     const toFetchAddresses = [];
 
-    let prevWasUsed = false;
-    Object.keys(addresses).forEach((walletType) => {
+    let nbPreviousUsed = 0;
+    for (const walletType of Object.keys(addresses)) {
       const walletAddresses = addresses[walletType];
       const walletPaths = Object.keys(walletAddresses);
-
       if (walletPaths.length > 0) {
-        walletPaths.forEach((path) => {
+        for (const path of walletPaths) {
           const address = walletAddresses[path];
+          let shouldFetch = false;
 
-
-          const hasUnconfirmedBalance = address.unconfirmedBalanceSat > 0;
+          const isUsed = address.used;
           // This will make all last address (the one we got from unconfirmedAddress)
           // to basically check each time
-          const isFirstAndUnused = address.used === false && address.index === 0;
-          const hasMostChanceToReceiveTx = prevWasUsed === true && address.used === false;
-          const hasReachRefreshThreshold = address.fetchedLast < (Date.now() - self.fetchThreshold);
-          if (
-            isFirstAndUnused
+          const isFirstAndUnused = isUsed === false && address.index === 0;
+
+          // The five next unused address
+          const hasMostChanceToReceiveTx = nbPreviousUsed < 5 && isUsed === false;
+          const hasUnconfirmedBalance = address.unconfirmedBalanceSat > 0;
+
+          const hasReachFastThreshold = address.fetchedLast < (currTime - self.fastFetchThreshold);
+          const hasReachStandardThreshold = address.fetchedLast < (currTime - self.fetchThreshold);
+          const hasReachSlowThreshold = address.fetchedLast < (currTime - self.slowFetchThresold);
+          // This is for high refresh rate addresses, they still have a threeshold.
+          if ((isFirstAndUnused
               || hasUnconfirmedBalance
-              || hasReachRefreshThreshold
-              || hasMostChanceToReceiveTx
+              || hasMostChanceToReceiveTx) && hasReachFastThreshold
           ) {
+            shouldFetch = true;
+          } else if (!isUsed
+              && ((nbPreviousUsed <= 10 && hasReachStandardThreshold))
+          ) {
+            // The next 10 unused address got a standard threshold
+            shouldFetch = true;
+          } else if (isUsed && address.transactions.length === 1) {
+            // Tis might be a single used and throw address. Let's figure tx age
+            // eslint-disable-next-line no-await-in-loop
+            const tx = await self.getTransaction(address.transactions[0]);
+            if (currBlockheight - tx.blockheight < 20000 && hasReachStandardThreshold) {
+              shouldFetch = true;
+            }
+          } else if (isUsed && address.transactions.length > 1 && hasReachStandardThreshold) {
+            shouldFetch = true;
+          }
+
+          if (hasReachSlowThreshold) {
+            shouldFetch = true;
+          }
+          if (shouldFetch) {
             toFetchAddresses.push(address);
           }
-          prevWasUsed = address.used;
-        });
+          if (!isUsed) {
+            nbPreviousUsed += 1;
+          }
+        }
       }
-    });
+    }
+
     const promises = [];
     toFetchAddresses.forEach((addressObj) => {
-      // We set at false so we don't autofetch utxos. This part will be done in the tx fetching time
+      // We set at false so we don't autofetch utxos.
+      // This part will be done in the tx fetching time
       // const p = fetchAddressInfo(addressObj, false)
       const p = fetchAddressInfo(addressObj)
         .catch((e) => {
@@ -146,8 +187,6 @@ class SyncWorker extends Worker {
       promises.push(p);
     });
 
-    // console.log('fetching addr', toFetchAddresses);
-    // console.log(promises)
     const responses = await Promise.all(promises);
 
     responses.forEach((addrInfo) => {
@@ -163,7 +202,6 @@ class SyncWorker extends Worker {
       }
     });
     this.events.emit(EVENTS.FETCHED_ADDRESS, responses);
-    return true;
   }
 
   async execTransactionsFetching() {
