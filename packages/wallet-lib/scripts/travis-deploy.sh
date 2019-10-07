910a2e7cb89674391e7ca84cb3b6ba118e3fd658
Anton Suprunchuk <antouhou@gmail.com>
2019-10-07 16:55:22 +0300

    Feat: better deploy script (dashevo/wallet-lib#79)
    * Add deploy task to travis
    
    * Add better deploy script
    
    * Bump patch version

---
+++ packages/wallet-lib/scripts/travis-deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+set -xe
+
+# Update this whenever the latest Node.js LTS version changes (~ every year).
+# Do not forget to add this version to .travis.yml config also.
+LATEST_LTS_VERSION="10"
+
+# We want this command to succeed whether or not the Node.js version is the
+# latest (so that the build does not show as failed), but _only_ the latest
+# should be used to publish the module.
+if [ "$TRAVIS_NODE_VERSION" != "$LATEST_LTS_VERSION" ]; then
+  echo "Node.js v$TRAVIS_NODE_VERSION is not latest LTS version -- will not deploy with this version."
+  exit 0
+fi
+
+# Now that checks have been passed, publish the module
+npm publish
