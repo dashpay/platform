cb21799288b45aaeb2e609a60e2ade9087c9f2d1
Alex Werner <Alex-Werner@users.noreply.github.com>
2019-12-30 02:03:15 +0100

    chore: added travis deployment script (dashevo/js-dash-sdk#7)
    * chore: added travis deployment script
    
    * chore: removed GH_Token
    
    We do not have any private repo anymore
    
    * chore: prepare build production for npm
    
    * tests: modified SDK tests
    
    * fix: tests
    
    * chore: add .npmignore

--- packages/js-dash-sdk/src/DashJS/SDK/SDK.spec.ts
+++ packages/js-dash-sdk/src/DashJS/SDK/SDK.spec.ts
@@ -3,8 +3,8 @@ import {SDK} from "./index";
 import 'mocha';
 import schema from '../../../tests/fixtures/dp1.schema.json'
 const mnemonic = 'agree country attract master mimic ball load beauty join gentle turtle hover';
-describe('DashJS - SDK', () => {
-
+describe('DashJS - SDK', function suite() {
+  this.timeout(10000);
   it('should provide expected class', function () {
     expect(SDK.name).to.be.equal('SDK');
     expect(SDK.constructor.name).to.be.equal('Function');
@@ -19,10 +19,17 @@ describe('DashJS - SDK', () => {
     const sdk = new SDK();
     expect(sdk.wallet).to.be.equal(undefined);
   });
-  it('should initiate wallet-lib with a mnemonic', function () {
+  it('should initiate wallet-lib with a mnemonic', async ()=>{
     const sdk = new SDK({mnemonic});
+    await sdk.isReady();
     expect(sdk.wallet).to.exist;
     expect(sdk.wallet!.offlineMode).to.be.equal(false);
+    // @ts-ignore
+    await sdk.wallet.storage.stopWorker();
+    // @ts-ignore
+    await sdk.wallet.disconnect();
+    // @ts-ignore
+    await sdk.account.disconnect();
   });
   // it('should initiate platform and only set contract when schema provided', function () {
   //   const sdkNoSchema= new SDK();
--- packages/js-dash-sdk/tests/e2e/index.js
+++ packages/js-dash-sdk/tests/e2e/index.js
@@ -1,11 +1,10 @@
 const {expect} = require('chai');
-const DashJS = require('../../build');
+const DashJS = require('../../dist/dash.min');
 
 describe('DashJS', () => {
 
   it('should provide expected class', function () {
     expect(DashJS).to.have.property('SDK');
-    expect(DashJS.SDK.name).to.be.equal('SDK')
     expect(DashJS.SDK.constructor.name).to.be.equal('Function')
   });
 });
