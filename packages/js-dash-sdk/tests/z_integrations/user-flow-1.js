b5357d42d289b6b98159a4755097095241813eef
Ivan Shumkov <ivan@shumkov.ru>
2020-06-10 20:57:37 +0300

    test: run integration tests with a new wallet (dashevo/js-dash-sdk#74)

--- packages/js-dash-sdk/tests/z_integrations/user-flow-1.js
+++ packages/js-dash-sdk/tests/z_integrations/user-flow-1.js
@@ -4,6 +4,58 @@ const fixtures = require('../fixtures/user-flow-1');
 const Chance = require('chance');
 const chance = new Chance();
 
+const dotenvSafe = require('dotenv-safe');
+
+const path = require('path');
+
+dotenvSafe.config({
+  path: path.resolve(__dirname, '..', '..', '.env'),
+});
+
+const {
+  Transaction,
+  PrivateKey
+} = require('@dashevo/dashcore-lib');
+
+function wait(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+/**
+ *
+ * @param {DAPIClient} dapiClient
+ * @param {Address} faucetAddress
+ * @param {PrivateKey} faucetPrivateKey
+ * @param {Address} address
+ * @param {number} amount
+ * @return {Promise<string>}
+ */
+async function fundAddress(dapiClient, faucetAddress, faucetPrivateKey, address, amount) {
+  const { items: inputs } = await dapiClient.getUTXO(faucetAddress);
+
+  const transaction = new Transaction();
+
+  transaction.from(inputs.slice(-1)[0])
+    .to(address, amount)
+    .change(faucetAddress)
+    .fee(668)
+    .sign(faucetPrivateKey);
+
+  let { blocks: currentBlockHeight } = await dapiClient.getStatus();
+
+  const transactionId = await dapiClient.sendTransaction(transaction.toBuffer());
+
+  const desiredBlockHeight = currentBlockHeight + 2;
+
+  do {
+    ({ blocks: currentBlockHeight } = await dapiClient.getStatus());
+    await wait(30000);
+  } while (currentBlockHeight < desiredBlockHeight);
+
+  return transactionId;
+}
+
+
 let clientInstance;
 let hasBalance=false;
 let hasDuplicate=true;
@@ -17,11 +69,11 @@ const username = `test-${firstname}${year}`;
 const clientOpts = {
   network: fixtures.network,
   wallet: {
-    mnemonic: fixtures.mnemonic,
+    mnemonic: null,
   },
 };
 describe('Integration - User flow 1 - Identity, DPNS, Documents', function suite() {
-  this.timeout(240000);
+  this.timeout(450000);
 
   it('should init a Client', async () => {
     clientInstance = new Dash.Client(clientOpts);
@@ -30,15 +82,11 @@ describe('Integration - User flow 1 - Identity, DPNS, Documents', function suite
     expect(clientInstance.apps).to.deep.equal({dpns: {contractId: "7PBvxeGpj7SsWfvDSa31uqEMt58LAiJww7zNcVRP1uEM"}});
     expect(clientInstance.wallet.network).to.equal('testnet');
     expect(clientInstance.wallet.offlineMode).to.equal(false);
-    expect(clientInstance.wallet.mnemonic).to.equal(fixtures.mnemonic);
-    expect(clientInstance.wallet.walletId).to.equal('6afaad2189');
     expect(clientInstance.account.index).to.equal(0);
-    expect(clientInstance.account.walletId).to.equal('6afaad2189');
-    expect(clientInstance.account.getUnusedAddress().address).to.equal('yj8sq7ogzz6JtaxpBQm5Hg9YaB5cKExn5T');
-
     expect(clientInstance.platform.dpp).to.exist;
     expect(clientInstance.platform.client).to.exist;
   });
+
   it('should be ready quickly', (done) => {
     let timer = setTimeout(() => {
       done(new Error('Should have been initialized in time'));
@@ -53,6 +101,22 @@ describe('Integration - User flow 1 - Identity, DPNS, Documents', function suite
       return done();
     })
   });
+
+  it('populate balance with dash', async () => {
+    const faucetPrivateKey = PrivateKey.fromString(process.env.FAUCET_PRIVATE_KEY);
+    const faucetAddress = faucetPrivateKey
+      .toAddress(process.env.NETWORK)
+      .toString();
+
+    await fundAddress(
+      clientInstance.clients.dapi,
+      faucetAddress,
+      faucetPrivateKey,
+      clientInstance.account.getAddress().address,
+      20000
+    )
+  })
+
   it('should have a balance', function (done) {
     const balance = (clientInstance.account.getTotalBalance());
     if(balance<10000){
@@ -61,6 +125,7 @@ describe('Integration - User flow 1 - Identity, DPNS, Documents', function suite
     hasBalance = true;
     return done();
   });
+
   it('should check it\'s DPNS reg is available' , async function () {
     const getDocument = await clientInstance.platform.names.get(username);
     expect(getDocument).to.equal(null);
