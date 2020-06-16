d8ba2c0084da5e6a5b31e335586af527689eef74
Anton Suprunchuk <antouhou@gmail.com>
2020-06-16 16:17:39 +0300

    fix: generate one-time private key for the asset lock transaction (dashevo/js-dash-sdk#86)

--- packages/js-dash-sdk/src/SDK/Client/Platform/createAssetLockTransaction.ts
+++ packages/js-dash-sdk/src/SDK/Client/Platform/createAssetLockTransaction.ts
@@ -2,29 +2,27 @@ import {PrivateKey, Transaction} from "@dashevo/dashcore-lib";
 import {utils} from "@dashevo/wallet-lib";
 import {Platform} from "./Platform";
 
+/**
+ * Creates a funding transaction for the platform identity and returns one-time key to sign the state transition
+ * @param {Platform} platform
+ * @param {number} fundingAmount - amount of dash to fund the identity's credits
+ * @return {{transaction: Transaction, privateKey: PrivateKey}} - transaction and one time private key
+ * that can be used to sign registration/top-up state transition
+ */
 export default async function createAssetLockTransaction(platform : Platform, fundingAmount): Promise<{ transaction: Transaction, privateKey: PrivateKey }> {
     const account = await platform.client.getWalletAccount();
 
-    const identityAddressInfo = account.getUnusedAddress();
-    const [identityHDPrivateKey] = account.getPrivateKeys([identityAddressInfo.address]);
-
     // @ts-ignore
-    const assetLockPrivateKey = identityHDPrivateKey.privateKey;
-    const assetLockPublicKey = assetLockPrivateKey.toPublicKey();
+    const assetLockOneTimePrivateKey = new PrivateKey();
+    const assetLockOneTimePublicKey = assetLockOneTimePrivateKey.toPublicKey();
 
-    const identityAddress = assetLockPublicKey.toAddress(platform.client.network).toString();
+    const identityAddress = assetLockOneTimePublicKey.toAddress(platform.client.network).toString();
     const changeAddress = account.getUnusedAddress('internal').address;
 
     const lockTransaction = new Transaction(undefined);
 
     const output = {
         satoshis: fundingAmount,
-        address: identityAddress
-    };
-
-    // TODO: Find another way to mark the address as used
-    const outputToMarkItUsed = {
-        satoshis: 10000,
         address: identityAddress
     };
 
@@ -35,14 +33,12 @@ export default async function createAssetLockTransaction(platform : Platform, fu
         throw new Error(`Not enough balance (${balance}) to cover burn amount of ${fundingAmount}`);
     }
 
-    const selection = utils.coinSelection(utxos, [output, outputToMarkItUsed]);
+    const selection = utils.coinSelection(utxos, [output]);
 
     lockTransaction
         .from(selection.utxos)
         // @ts-ignore
-        .addBurnOutput(output.satoshis, assetLockPublicKey._getID())
-        // @ts-ignore
-        .to(identityAddressInfo.address, 10000)
+        .addBurnOutput(output.satoshis, assetLockOneTimePublicKey._getID())
         // @ts-ignore
         .change(changeAddress)
 
@@ -58,6 +54,6 @@ export default async function createAssetLockTransaction(platform : Platform, fu
 
     return {
         transaction,
-        privateKey: assetLockPrivateKey
+        privateKey: assetLockOneTimePrivateKey
     };
 }
