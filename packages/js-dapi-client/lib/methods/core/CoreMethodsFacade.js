4711523fd3eb4d375c82f1e2e1cfc4b979653935
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-09-03 11:47:45 +0300

    feat!: remove getUTXO and getAddressSummary core methods (dashevo/js-dapi-client#178)

--- packages/js-dapi-client/lib/methods/core/CoreMethodsFacade.js
+++ packages/js-dapi-client/lib/methods/core/CoreMethodsFacade.js
@@ -1,6 +1,5 @@
 const broadcastTransactionFactory = require('./broadcastTransactionFactory');
 const generateToAddressFactory = require('./generateToAddressFactory');
-const getAddressSummaryFactory = require('./getAddressSummaryFactory');
 const getBestBlockHashFactory = require('./getBestBlockHashFactory');
 const getBlockByHashFactory = require('./getBlockByHashFactory');
 const getBlockByHeightFactory = require('./getBlockByHeightFactory');
@@ -8,7 +7,6 @@ const getBlockHashFactory = require('./getBlockHashFactory');
 const getMnListDiffFactory = require('./getMnListDiffFactory');
 const getStatusFactory = require('./getStatusFactory');
 const getTransactionFactory = require('./getTransactionFactory');
-const getUTXOFactory = require('./getUTXOFactory');
 const subscribeToTransactionsWithProofsFactory = require('./subscribeToTransactionsWithProofsFactory');
 
 class CoreMethodsFacade {
@@ -19,7 +17,6 @@ class CoreMethodsFacade {
   constructor(jsonRpcTransport, grpcTransport) {
     this.broadcastTransaction = broadcastTransactionFactory(grpcTransport);
     this.generateToAddress = generateToAddressFactory(jsonRpcTransport);
-    this.getAddressSummary = getAddressSummaryFactory(jsonRpcTransport);
     this.getBestBlockHash = getBestBlockHashFactory(jsonRpcTransport);
     this.getBlockByHash = getBlockByHashFactory(grpcTransport);
     this.getBlockByHeight = getBlockByHeightFactory(grpcTransport);
@@ -27,7 +24,6 @@ class CoreMethodsFacade {
     this.getMnListDiff = getMnListDiffFactory(jsonRpcTransport);
     this.getStatus = getStatusFactory(grpcTransport);
     this.getTransaction = getTransactionFactory(grpcTransport);
-    this.getUTXO = getUTXOFactory(jsonRpcTransport);
     this.subscribeToTransactionsWithProofs = subscribeToTransactionsWithProofsFactory(
       grpcTransport,
     );
