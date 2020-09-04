6230c159f03a73053cd1064a473acaa61ed66499
Djavid Gabibiyan <djavidoff@gmail.com>
2020-09-04 21:51:05 +0500

    feat: remove pending `subscribeToTransactionsWithProofs` functional tests (dashevo/platform-test-suite#72)

--- packages/platform-test-suite/test/functional/core/subscribeToTransactionsWIthProofs.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Core', () => {
-  describe('subscribeToTransactionsWithProofs', () => {
-    it('should respond with only historical data');
-    it('should respond with both historical and new data');
-    it('should respond with a proper historical and new data in case of reorganization');
-    it('should respond with only new data');
-  });
-});
