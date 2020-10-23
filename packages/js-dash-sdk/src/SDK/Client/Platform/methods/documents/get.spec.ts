9ef68038c70074165ed6702de53fd41bf0440c0f
Ivan Shumkov <ivan@shumkov.ru>
2020-10-23 20:58:30 +0300

    feat: convert string identifiers in `where` conditions (dashevo/js-dash-sdk#145)
    Co-authored-by: Djavid Gabibiyan <djavidoff@gmail.com>

---
+++ packages/js-dash-sdk/src/SDK/Client/Platform/methods/documents/get.spec.ts
@@ -0,0 +1,116 @@
+import getDataContractFixture from '@dashevo/dpp/lib/test/fixtures/getDataContractFixture';
+import generateRandomIdentifier from '@dashevo/dpp/lib/test/utils/generateRandomIdentifier';
+import createDPPMock from '@dashevo/dpp/lib/test/mocks/createDPPMock';
+
+import get from './get';
+import { expect } from 'chai';
+
+describe('Client - Platform - Documents - .get()', () => {
+  let platform;
+  let dataContract;
+  let appDefinition;
+  let getDocumentsMock;
+  let appsGetMock;
+
+  beforeEach(function beforeEach() {
+    dataContract = getDataContractFixture();
+
+    appDefinition = {
+      contractId: dataContract.getId(),
+      contract: dataContract,
+    };
+
+    getDocumentsMock = this.sinon.stub().resolves([]);
+    appsGetMock = this.sinon.stub().returns(appDefinition);
+
+    platform = {
+      dpp: createDPPMock(this.sinon),
+      client: {
+        getApps: () => ({
+          has: this.sinon.stub().returns(true),
+          get: appsGetMock,
+        }),
+        getDAPIClient: () => ({
+          platform: {
+            getDocuments: getDocumentsMock,
+          },
+        })
+      },
+    };
+  });
+
+  it('should convert identifier properties inside where condition', async () => {
+    const id = generateRandomIdentifier();
+    await get.call(platform, 'app.withByteArrays', {
+      where: [
+        ['identifierField', '==', id.toString()],
+      ],
+    });
+
+    expect(getDocumentsMock.getCall(0).args).to.have.deep.members([
+      appDefinition.contractId,
+      'withByteArrays',
+      {
+        where: [
+          ['identifierField', '==', id],
+        ],
+      },
+    ]);
+  });
+
+  it('should convert nested identifier properties inside where condition if `elementMatch` is used', async () => {
+    const id = generateRandomIdentifier();
+
+    dataContract = getDataContractFixture();
+    dataContract.documents.withByteArrays.properties.nestedObject = {
+      type: 'object',
+      properties: {
+        idField: {
+          type: "array",
+          byteArray: true,
+          contentMediaType: "application/x.dash.dpp.identifier",
+          minItems: 32,
+          maxItems: 32,
+        },
+        anotherNested: {
+          type: 'object',
+          properties: {
+            anotherIdField: {
+              type: "array",
+              byteArray: true,
+              contentMediaType: "application/x.dash.dpp.identifier",
+              minItems: 32,
+              maxItems: 32,
+            },
+          },
+        },
+      },
+    };
+
+    appDefinition = {
+      contractId: dataContract.getId(),
+      contract: dataContract,
+    };
+
+    appsGetMock.reset();
+    appsGetMock.returns(appDefinition);
+
+    await get.call(platform, 'app.withByteArrays', {
+      where: [
+        ['nestedObject', 'elementMatch', ['idField', '==', id.toString()]],
+        ['nestedObject', 'elementMatch', ['anotherNested', 'elementMatch', ['anotherIdField', '==', id.toString()]]]
+      ],
+    });
+
+    expect(getDocumentsMock.getCall(0).args).to.have.deep.members([
+      appDefinition.contractId,
+      'withByteArrays',
+      {
+        where: [
+          ['nestedObject', 'elementMatch', ['idField', '==', id]],
+          ['nestedObject', 'elementMatch', ['anotherNested', 'elementMatch', ['anotherIdField', '==', id]]]
+        ],
+      },
+    ]);
+  });
+});
--- packages/js-dash-sdk/src/SDK/Client/Platform/methods/documents/get.ts
+++ packages/js-dash-sdk/src/SDK/Client/Platform/methods/documents/get.ts
@@ -1,19 +1,32 @@
+import Identifier from '@dashevo/dpp/lib/Identifier';
+
 import {Platform} from "../../Platform";
 
 /**
- * @param {any} [where] - where
- * @param {any} [orderBy] - order by
+ * @param {WhereCondition[]} [where] - where
+ * @param {OrderByCondition[]} [orderBy] - order by
  * @param {number} [limit] - limit
  * @param {number} [startAt] - start value (included)
  * @param {number} [startAfter] - start value (not included)
  */
 declare interface fetchOpts {
-    where: any;
-    orderBy: any;
-    limit: number;
-    startAt: number;
-    startAfter: number;
+    where?: WhereCondition[];
+    orderBy?: OrderByCondition[];
+    limit?: number;
+    startAt?: number;
+    startAfter?: number;
 }
+
+type OrderByCondition = [
+    string,
+    'asc' | 'desc',
+];
+
+type WhereCondition = [
+    string,
+    '<' | '<=' | '==' | '>' | '>=' | 'in' | 'startsWith' | 'elementMatch' | 'length' | 'contains',
+    WhereCondition|any,
+]
 
 /**
  * Prefetch contract
@@ -29,6 +42,43 @@ const ensureAppContractFetched = async function (this: Platform, appName) {
             await this.contracts.get(appDefinition.contractId);
         }
     }
+}
+
+/**
+ * Convert where condition identifier properties
+ *
+ * @param {WhereCondition} whereCondition
+ * @param {Object} binaryProperties
+ * @param {null|string} [parentProperty=null]
+ *
+ * @return {WhereCondition}
+ */
+function convertIdentifierProperties(whereCondition: WhereCondition, binaryProperties: Record<string, any>, parentProperty: null|string = null) {
+    const [propertyName, operator, propertyValue] = whereCondition;
+
+    const fullPropertyName = parentProperty ? `${parentProperty}.${propertyName}`: propertyName;
+
+    if (operator === 'elementMatch') {
+        return [
+            propertyName,
+            operator,
+            convertIdentifierProperties(
+                propertyValue,
+                binaryProperties,
+                fullPropertyName,
+            ),
+        ];
+    }
+
+    const property = binaryProperties[fullPropertyName];
+
+    if (property && property.contentMediaType === Identifier.MEDIA_TYPE) {
+        if (typeof propertyValue === 'string') {
+            return [propertyName, operator, Identifier.from(propertyValue)];
+        }
+    }
+
+    return [propertyName, operator, propertyValue];
 }
 
 /**
@@ -58,6 +108,12 @@ export async function get(this: Platform, typeLocator: string, opts: fetchOpts):
 
     // If not present, will fetch contract based on appName and contractId store in this.apps.
     await ensureAppContractFetched.call(this, appName);
+
+    if (opts.where) {
+        const binaryProperties = appDefinition.contract.getBinaryProperties(fieldType);
+
+        opts.where = opts.where.map((whereCondition) => convertIdentifierProperties(whereCondition, binaryProperties));
+    }
 
     // @ts-ignore
     const rawDocuments = await this.client.getDAPIClient().platform.getDocuments(
