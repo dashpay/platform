65eb8abaf702fa1fcf37a17f7db78fad90618e7d
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2020-11-13 18:05:51 +0300

    test: provide proofs for getDataContract endpoint (dashevo/js-drive#424)

--- packages/js-drive/lib/abci/handlers/query/dataContractQueryHandlerFactory.js
+++ packages/js-drive/lib/abci/handlers/query/dataContractQueryHandlerFactory.js
@@ -4,30 +4,50 @@ const {
   },
 } = require('abci/types');
 
+const cbor = require('cbor');
+
 const NotFoundAbciError = require('../../errors/NotFoundAbciError');
 
 /**
  *
  * @param {DataContractStoreRepository} dataContractRepository
+ * @param {RootTree} rootTree
+ * @param {DataContractsStoreRootTreeLeaf} dataContractsStoreRootTreeLeaf
  * @return {dataContractQueryHandler}
  */
-function dataContractQueryHandlerFactory(dataContractRepository) {
+function dataContractQueryHandlerFactory(
+  dataContractRepository,
+  rootTree,
+  dataContractsStoreRootTreeLeaf,
+) {
   /**
    * @typedef dataContractQueryHandler
    * @param {Object} params
    * @param {Object} data
    * @param {Buffer} data.id
+   * @param {Object} request
+   * @param {boolean} [request.prove]
    * @return {Promise<ResponseQuery>}
    */
-  async function dataContractQueryHandler(params, { id }) {
+  async function dataContractQueryHandler(params, { id }, request) {
     const dataContract = await dataContractRepository.fetch(id);
 
     if (!dataContract) {
       throw new NotFoundAbciError('Data Contract not found');
     }
 
+    const includeProof = request.prove === 'true';
+
+    const value = {
+      data: dataContract.toBuffer(),
+    };
+
+    if (includeProof) {
+      value.proof = rootTree.getFullProof(dataContractsStoreRootTreeLeaf, [id]);
+    }
+
     return new ResponseQuery({
-      value: dataContract.toBuffer(),
+      value: cbor.encode(value),
     });
   }
 
--- packages/js-drive/test/unit/abci/handlers/query/dataContractQueryHandlerFactory.spec.js
+++ packages/js-drive/test/unit/abci/handlers/query/dataContractQueryHandlerFactory.spec.js
@@ -3,6 +3,7 @@ const {
     ResponseQuery,
   },
 } = require('abci/types');
+const cbor = require('cbor');
 
 const getDataContractFixture = require('@dashevo/dpp/lib/test/fixtures/getDataContractFixture');
 
@@ -17,6 +18,8 @@ describe('dataContractQueryHandlerFactory', () => {
   let dataContract;
   let params;
   let data;
+  let rootTreeMock;
+  let dataContractsStoreRootTreeLeafMock;
 
   beforeEach(function beforeEach() {
     dataContract = getDataContractFixture();
@@ -25,8 +28,16 @@ describe('dataContractQueryHandlerFactory', () => {
       fetch: this.sinon.stub(),
     };
 
+    rootTreeMock = {
+      getFullProof: this.sinon.stub(),
+    };
+
+    dataContractsStoreRootTreeLeafMock = this.sinon.stub();
+
     dataContractQueryHandler = dataContractQueryHandlerFactory(
       dataContractRepositoryMock,
+      rootTreeMock,
+      dataContractsStoreRootTreeLeafMock,
     );
 
     params = { };
@@ -38,17 +49,49 @@ describe('dataContractQueryHandlerFactory', () => {
   it('should return serialized data contract', async () => {
     dataContractRepositoryMock.fetch.resolves(dataContract);
 
-    const result = await dataContractQueryHandler(params, data);
+    const result = await dataContractQueryHandler(params, data, {});
+
+    const value = {
+      data: dataContract.toBuffer(),
+    };
 
     expect(dataContractRepositoryMock.fetch).to.be.calledOnceWith(data.id);
     expect(result).to.be.an.instanceof(ResponseQuery);
     expect(result.code).to.equal(0);
-    expect(result.value).to.deep.equal(dataContract.toBuffer());
+    expect(result.value).to.deep.equal(cbor.encode(value));
+    expect(rootTreeMock.getFullProof).not.to.be.called();
+  });
+
+  it('should return serialized data contract with proof', async () => {
+    const proof = {
+      rootTreeProof: Buffer.from('0100000001f0faf5f55674905a68eba1be2f946e667c1cb5010101', 'hex'),
+      storeTreeProof: Buffer.from('03046b657931060076616c75653103046b657932060076616c75653210', 'hex'),
+    };
+
+    dataContractRepositoryMock.fetch.resolves(dataContract);
+    rootTreeMock.getFullProof.returns(proof);
+
+    const result = await dataContractQueryHandler(params, data, { prove: 'true' });
+
+    const value = {
+      data: dataContract.toBuffer(),
+      proof,
+    };
+
+    expect(dataContractRepositoryMock.fetch).to.be.calledOnceWith(data.id);
+    expect(result).to.be.an.instanceof(ResponseQuery);
+    expect(result.code).to.equal(0);
+    expect(result.value).to.deep.equal(cbor.encode(value));
+    expect(rootTreeMock.getFullProof).to.be.calledOnce();
+    expect(rootTreeMock.getFullProof.getCall(0).args).to.deep.equal([
+      dataContractsStoreRootTreeLeafMock,
+      [dataContract.getId()],
+    ]);
   });
 
   it('should throw NotFoundAbciError if data contract not found', async () => {
     try {
-      await dataContractQueryHandler(params, data);
+      await dataContractQueryHandler(params, data, {});
 
       expect.fail('should throw NotFoundAbciError');
     } catch (e) {
