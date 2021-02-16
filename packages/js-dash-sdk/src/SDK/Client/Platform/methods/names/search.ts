639fd3996576107cb98e9874e5ba5adaa3d1bddf
thephez <thephez@users.noreply.github.com>
2021-02-16 08:47:47 -0500

    fix: ensure name search uses lowercase label (dashevo/js-dash-sdk#183)

--- packages/js-dash-sdk/src/SDK/Client/Platform/methods/names/search.ts
+++ packages/js-dash-sdk/src/SDK/Client/Platform/methods/names/search.ts
@@ -8,11 +8,12 @@ import {Platform} from "../../Platform";
  */
 export async function search(this: Platform, labelPrefix: string, parentDomainName: string = '') {
     const normalizedParentDomainName = parentDomainName.toLowerCase();
+    const normalizedLabelPrefix = labelPrefix.toLowerCase();
 
     const documents = await this.documents.get('dpns.domain', {
         where: [
             ['normalizedParentDomainName', '==', normalizedParentDomainName],
-            ['normalizedLabel', 'startsWith', labelPrefix],
+            ['normalizedLabel', 'startsWith', normalizedLabelPrefix],
         ],
     });
 
