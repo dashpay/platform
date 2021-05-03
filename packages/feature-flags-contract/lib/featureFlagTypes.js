d0f7234d170da4fccedac9b32c679e24a968aaea
Djavid Gabibiyan <djavidoff@gmail.com>
2021-05-03 17:50:31 +0500

    feat: add feature flag types module (dashevo/feature-flags-contract#6)

---
+++ packages/feature-flags-contract/lib/featureFlagTypes.js
@@ -0,0 +1,15 @@
+/**
+ * @classdesc FeatureFlagTypes interface definition
+ *
+ * @name FeatureFlagTypes
+ * @class
+ *
+ * @property {string} FeatureFlagTypes.FIX_CUMULATIVE_FEES
+ * @property {string} FeatureFlagTypes.UPDATE_CONSENSUS_PARAMS
+ * @property {string} FeatureFlagTypes.VERIFY_LLMQ_SIGS_WITH_CORE
+ */
+module.exports = {
+  FIX_CUMULATIVE_FEES: 'fixCumulativeFeesBug',
+  UPDATE_CONSENSUS_PARAMS: 'updateConsensusParams',
+  VERIFY_LLMQ_SIGS_WITH_CORE: 'verifyLLMQSignaturesWithCore',
+};
