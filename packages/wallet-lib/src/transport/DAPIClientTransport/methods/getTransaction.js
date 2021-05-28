88e6a69a652b78a633ed5e05e3225cc1d72c1f03
Anton Suprunchuk <antouhou@gmail.com>
2021-05-28 12:51:12 +0300

    fix: Identifier expects Buffer (dashevo/wallet-lib#264)

--- packages/wallet-lib/src/transport/DAPIClientTransport/methods/getTransaction.js
+++ packages/wallet-lib/src/transport/DAPIClientTransport/methods/getTransaction.js
@@ -4,6 +4,9 @@ const logger = require('../../../logger');
 
 module.exports = async function getTransaction(txid) {
   logger.silly(`DAPIClient.getTransaction[${txid}]`);
-  if (!is.txid(txid)) throw new Error(`Received an invalid txid to fetch : ${txid}`);
+  if (!is.txid(txid)) {
+    throw new Error(`Received an invalid txid to fetch : ${txid}`);
+  }
+
   return new Transaction(await this.client.core.getTransaction(txid));
 };
