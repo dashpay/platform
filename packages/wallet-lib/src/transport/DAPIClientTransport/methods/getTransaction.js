1749447d2e6507132db7d6a99a85424ffccf25a5
Alex Werner <Alex-Werner@users.noreply.github.com>
2021-09-17 22:56:33 +0200

    chore: bump dependencies and package-lock lockfile (dashevo/wallet-lib#314)
    * chore: update dependencies and package-lock lockfile
    
    * fix: update path for NotFoundError
    
    * chore: using alternative branch for tests

--- packages/wallet-lib/src/transport/DAPIClientTransport/methods/getTransaction.js
+++ packages/wallet-lib/src/transport/DAPIClientTransport/methods/getTransaction.js
@@ -1,5 +1,5 @@
 const { Transaction } = require('@dashevo/dashcore-lib');
-const NotFoundError = require('@dashevo/dapi-client/lib/errors/response/NotFoundError');
+const NotFoundError = require('@dashevo/dapi-client/lib/transport/GrpcTransport/errors/NotFoundError');
 const { is } = require('../../../utils');
 const logger = require('../../../logger');
 
