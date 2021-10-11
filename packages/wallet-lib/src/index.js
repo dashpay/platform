04135df9d6f88800dd6d522ad564c4b35c70d66d
Anton Suprunchuk <antouhou@gmail.com>
2021-10-11 14:10:15 +0300

    feat: add patched Console transport for winston to make it work in the browser (dashevo/wallet-lib#330)

--- packages/wallet-lib/src/index.js
+++ packages/wallet-lib/src/index.js
@@ -1,3 +1,7 @@
+// Default winston transport requires setImmediate to work, so
+// polyfill included here. Making it work with webpack is rather tricky, so it is used as per
+// documentation: https://github.com/YuzuJS/setImmediate#usage
+require('setimmediate');
 const Wallet = require('./types/Wallet/Wallet');
 const Account = require('./types/Account/Account');
 const Identities = require('./types/Identities/Identities');
