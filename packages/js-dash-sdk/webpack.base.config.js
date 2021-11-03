551a0916c8ba05402a51dfda6a251061a3a25c61
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-11-03 18:02:13 +0300

    build(sdk): upgrade to webpack 5 (#6)

--- packages/js-dash-sdk/webpack.base.config.js
+++ packages/js-dash-sdk/webpack.base.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+
 const baseConfig = {
   entry: './src/index.ts',
   module: {
@@ -10,15 +12,30 @@ const baseConfig = {
       },
     ],
   },
-  node: {
-    // Prevent embedded winston to throw error with FS not existing.
-    fs: 'empty',
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    fallback: {
+      fs: false,
+      util: require.resolve('util/'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      url: require.resolve('url/'),
+      assert: require.resolve('assert/'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      zlib: require.resolve('browserify-zlib'),
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
-  resolve: {
-    extensions: ['.ts', '.js', '.json'],
-  }
 }
 module.exports = baseConfig;
