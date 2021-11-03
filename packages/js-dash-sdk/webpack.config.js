9c181900326851bb8077a8b76a354889310dd573
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-11-03 17:58:54 +0300

    chore: update webpack to v.5

--- packages/js-dash-sdk/webpack.config.js
+++ packages/js-dash-sdk/webpack.config.js
@@ -1,13 +1,31 @@
 const path = require('path');
+const webpack = require('webpack');
 
 const webConfig =  {
   entry: './build/src/index.js',
   mode: "production",
-  target: 'web',
-  node: {
-    // Prevent embedded winston to throw error with FS not existing.
-    fs: 'empty',
+  resolve: {
+    extensions: ['.js', '.json'],
+    fallback: {
+      fs: false,
+      util: require.resolve('util/'),
+      crypto: require.resolve('crypto-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      url: require.resolve('url/'),
+      assert: require.resolve('assert/'),
+      stream: require.resolve('stream-browserify'),
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      zlib: require.resolve('browserify-zlib'),
+    },
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+  ],
   output: {
     path: path.resolve(__dirname, 'dist'),
     libraryTarget: 'umd',
@@ -16,9 +34,6 @@ const webConfig =  {
     // fixes ReferenceError: window is not defined
     globalObject: "(typeof self !== 'undefined' ? self : this)"
   },
-  resolve: {
-    extensions: ['.js', '.json'],
-  },
 }
 
-module.exports = webConfig;
+module.exports = [webConfig];
