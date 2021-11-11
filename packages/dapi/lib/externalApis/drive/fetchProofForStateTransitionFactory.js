ab51321dc91420f395ac465dfa9ca9545d702247
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-11-11 17:50:52 +0300

    fix(dapi): Identifier expects Buffer (#28)

--- packages/dapi/lib/externalApis/drive/fetchProofForStateTransitionFactory.js
+++ packages/dapi/lib/externalApis/drive/fetchProofForStateTransitionFactory.js
@@ -15,15 +15,21 @@ function fetchProofForStateTransitionFactory(driveClient) {
     let metadata;
     if (stateTransition.isDocumentStateTransition()) {
       ({ documentsProof: proof, metadata } = await driveClient.fetchProofs(
-        { documentIds: modifiedIds },
+        {
+          documentIds: modifiedIds.map((identifier) => identifier.toBuffer()),
+        },
       ));
     } else if (stateTransition.isIdentityStateTransition()) {
       ({ identitiesProof: proof, metadata } = await driveClient.fetchProofs(
-        { identityIds: modifiedIds },
+        {
+          identityIds: modifiedIds.map((identifier) => identifier.toBuffer()),
+        },
       ));
     } else if (stateTransition.isDataContractStateTransition()) {
       ({ dataContractsProof: proof, metadata } = await driveClient.fetchProofs(
-        { dataContractIds: modifiedIds },
+        {
+          dataContractIds: modifiedIds.map((identifier) => identifier.toBuffer()),
+        },
       ));
     }
 
