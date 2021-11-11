400bbfd7577415d186981bcf7a387a632c8c303e
Ivan Shumkov <ivan@shumkov.ru>
2021-11-11 20:29:33 +0300

    test: remove only wallet

--- packages/platform-test-suite/test/e2e/wallet.spec.js
+++ packages/platform-test-suite/test/e2e/wallet.spec.js
@@ -12,7 +12,7 @@ const waitForBalanceToChange = require('../../lib/test/waitForBalanceToChange');
 const { EVENTS } = Dash.WalletLib;
 
 describe('e2e', () => {
-  describe.only('Wallet', function main() {
+  describe('Wallet', function main() {
     this.timeout(950000);
 
     let failed = false;
