ce0609663f8dc45f77b0d28c76c9bae65a62168e
Ivan Shumkov <ivan@shumkov.ru>
2021-11-29 17:13:47 +0300

    fix(dashmate): RPC error on stopping node (#61)

--- scripts/configure_dashmate.sh
+++ scripts/configure_dashmate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 CONFIG_NAME="local"
 
 full_path=$(realpath "$0")
@@ -10,8 +12,8 @@ packages_path="$root_path/packages"
 DAPI_REPO_PATH="${packages_path}/dapi"
 DRIVE_REPO_PATH="${packages_path}/js-drive"
 
-"${packages_path}"/dashmate/bin/dashmate config:set --config=${CONFIG_NAME} platform.dapi.api.docker.build.path "$DAPI_REPO_PATH"
-"${packages_path}"/dashmate/bin/dashmate config:set --config=${CONFIG_NAME} platform.drive.abci.docker.build.path "$DRIVE_REPO_PATH"
+yarn dashmate config:set --config=${CONFIG_NAME} platform.dapi.api.docker.build.path "$DAPI_REPO_PATH"
+yarn dashmate config:set --config=${CONFIG_NAME} platform.drive.abci.docker.build.path "$DRIVE_REPO_PATH"
 
 # create tenderdash blocks every 10s to speed up test suite
-"${packages_path}"/dashmate/bin/dashmate config:set --config=${CONFIG_NAME} platform.drive.tenderdash.consensus.createEmptyBlocksInterval "10s"
+yarn dashmate config:set --config=${CONFIG_NAME} platform.drive.tenderdash.consensus.createEmptyBlocksInterval "10s"
