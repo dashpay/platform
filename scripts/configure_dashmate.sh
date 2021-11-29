a6891b294c483868f65cc0c900bee6b14c027272
Ivan Shumkov <ivan@shumkov.ru>
2021-11-29 15:13:41 +0300

    chore: uncomment build paths

--- scripts/configure_dashmate.sh
+++ scripts/configure_dashmate.sh
@@ -12,8 +12,8 @@ packages_path="$root_path/packages"
 DAPI_REPO_PATH="${packages_path}/dapi"
 DRIVE_REPO_PATH="${packages_path}/js-drive"
 
-#yarn dashmate config:set --config=${CONFIG_NAME} platform.dapi.api.docker.build.path "$DAPI_REPO_PATH"
-#yarn dashmate config:set --config=${CONFIG_NAME} platform.drive.abci.docker.build.path "$DRIVE_REPO_PATH"
+yarn dashmate config:set --config=${CONFIG_NAME} platform.dapi.api.docker.build.path "$DAPI_REPO_PATH"
+yarn dashmate config:set --config=${CONFIG_NAME} platform.drive.abci.docker.build.path "$DRIVE_REPO_PATH"
 
 # create tenderdash blocks every 10s to speed up test suite
 yarn dashmate config:set --config=${CONFIG_NAME} platform.drive.tenderdash.consensus.createEmptyBlocksInterval "10s"
