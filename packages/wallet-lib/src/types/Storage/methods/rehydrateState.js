5b48845d85efc9241ca069f580b030bd77ab2288
Igor Markin <markin.io210@gmail.com>
2021-12-13 15:40:05 +0300

    fix(wallet-lib): invalid deserialization of persistent storage (#76)

--- packages/wallet-lib/src/types/Storage/methods/rehydrateState.js
+++ packages/wallet-lib/src/types/Storage/methods/rehydrateState.js
@@ -1,9 +1,74 @@
 const { merge } = require('lodash');
-const { InstantLock } = require('@dashevo/dashcore-lib');
+const { InstantLock, Transaction, BlockHeader } = require('@dashevo/dashcore-lib');
 const { hasMethod } = require('../../../utils');
 
 const mergeHelper = (initial = {}, additional = {}) => merge(initial, additional);
 const { REHYDRATE_STATE_FAILED, REHYDRATE_STATE_SUCCESS } = require('../../../EVENTS');
+const logger = require('../../../logger');
+
+const storeTypesSchema = {
+  transactions: {
+    '*': Transaction,
+  },
+  instantLocks: {
+    '*': InstantLock,
+  },
+  wallets: {
+    '*': {
+      addresses: {
+        '*': {
+          '*': {
+            utxos: {
+              // eslint-disable-next-line func-names
+              '*': function (item) {
+                // TODO: resolve type inconsistency for address utxos
+                try {
+                  return new Transaction.UnspentOutput(item);
+                } catch (e) {
+                  return new Transaction.Output(item);
+                }
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  chains: {
+    '*': {
+      blockHeaders: {
+        '*': BlockHeader,
+      },
+    },
+  },
+};
+
+const castItemTypes = (item, schema) => {
+  Object.entries(schema).forEach(([schemaKey, schemaValue]) => {
+    if (schemaValue.constructor.name !== 'Object') {
+      const Clazz = schemaValue;
+      if (schemaKey === '*') {
+        Object.keys(item).forEach((itemKey) => {
+          // eslint-disable-next-line no-param-reassign
+          item[itemKey] = new Clazz(item[itemKey]);
+        });
+      } else {
+        if (!item[schemaKey]) {
+          throw new Error(`No schema key "${schemaKey}" found for item ${JSON.stringify(item)}`);
+        }
+
+        // eslint-disable-next-line no-param-reassign
+        item[schemaKey] = new Clazz(item[schemaKey]);
+      }
+    } else if (schemaKey === '*') {
+      Object.values(item).forEach((itemValue) => castItemTypes(itemValue, schemaValue));
+    } else {
+      castItemTypes(item[schemaKey], schemaValue);
+    }
+  });
+
+  return item;
+};
 
 /**
  * Fetch the state from the persistence adapter
@@ -12,31 +77,39 @@ const { REHYDRATE_STATE_FAILED, REHYDRATE_STATE_SUCCESS } = require('../../../EV
 const rehydrateState = async function rehydrateState() {
   if (this.rehydrate && this.lastRehydrate === null) {
     try {
-      const transactions = (this.adapter && hasMethod(this.adapter, 'getItem'))
-        ? (await this.adapter.getItem('transactions') || this.store.transactions)
-        : this.store.transactions;
-      const wallets = (this.adapter && hasMethod(this.adapter, 'getItem'))
-        ? (await this.adapter.getItem('wallets') || this.store.wallets)
-        : this.store.wallets;
-      const chains = (this.adapter && hasMethod(this.adapter, 'getItem'))
-        ? (await this.adapter.getItem('chains') || this.store.chains)
-        : this.store.chains;
-      const instantLocks = (this.adapter && hasMethod(this.adapter, 'getItem'))
-        ? (await this.adapter.getItem('instantLocks') || this.store.instantLocks)
-        : this.store.instantLocks;
+      const storeItems = {
+        transactions: null,
+        wallets: null,
+        chains: null,
+        instantLocks: null,
+      };
+      const keys = Object.keys(storeItems);
+      // Obtain items from storage adapter
+      for (let i = 0; i < keys.length; i += 1) {
+        const itemKey = keys[i];
 
-      // We need to keep deserialized instant locks
-      Object.keys(instantLocks).forEach((transactionHash) => {
-        const instantLock = instantLocks[transactionHash];
-        if (!(instantLock instanceof InstantLock)) {
-          instantLocks[transactionHash] = new InstantLock(instantLock);
+        let item;
+        if (this.adapter && hasMethod(this.adapter, 'getItem')) {
+          // eslint-disable-next-line no-await-in-loop
+          item = await this.adapter.getItem(itemKey);
         }
+
+        storeItems[itemKey] = item || this.store[itemKey];
+      }
+
+      // Cast store items to correct data types
+      try {
+        castItemTypes(storeItems, storeTypesSchema);
+      } catch (e) {
+        logger.error('Error casting storage items types: possibly data schema mismatch');
+        throw e;
+      }
+
+      // Merge with the current items in store
+      Object.keys(storeItems).forEach((itemKey) => {
+        this.store[itemKey] = mergeHelper(this.store[itemKey], storeItems[itemKey]);
       });
 
-      this.store.transactions = mergeHelper(this.store.transactions, transactions);
-      this.store.wallets = mergeHelper(this.store.wallets, wallets);
-      this.store.chains = mergeHelper(this.store.chains, chains);
-      this.store.instantLocks = mergeHelper(this.store.instantLocks, instantLocks);
       this.lastRehydrate = +new Date();
       this.emit(REHYDRATE_STATE_SUCCESS, { type: REHYDRATE_STATE_SUCCESS, payload: null });
     } catch (e) {
