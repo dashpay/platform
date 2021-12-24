951dd1969b754b9e0b8dcdb627c358bcd19578b6
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2021-12-24 16:22:08 +0300

    fix(drive): `ValidatorSetUpdate` doesn't contain `nodeAddress` (#155)

--- .pnp.cjs
+++ .pnp.cjs
@@ -2236,10 +2236,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["@dashevo/abci", [
-        ["https://github.com/dashevo/js-abci.git#commit=aa3dfe8e13d9ced6cf71f6ab233bd1c7e33d044c", {
-          "packageLocation": "./.yarn/cache/@dashevo-abci-https-6a91ed2e9b-f333af58ac.zip/node_modules/@dashevo/abci/",
+        ["https://github.com/dashevo/js-abci.git#commit=5f18c2cd23ade43878a02ef25dff996a1d9c6537", {
+          "packageLocation": "./.yarn/cache/@dashevo-abci-https-44f89b5a2f-cd37544188.zip/node_modules/@dashevo/abci/",
           "packageDependencies": [
-            ["@dashevo/abci", "https://github.com/dashevo/js-abci.git#commit=aa3dfe8e13d9ced6cf71f6ab233bd1c7e33d044c"],
+            ["@dashevo/abci", "https://github.com/dashevo/js-abci.git#commit=5f18c2cd23ade43878a02ef25dff996a1d9c6537"],
             ["bl", "npm:1.2.3"],
             ["protobufjs", "https://github.com/jawid-h/protobuf.js.git#commit=8b91c72dca68fd6c418078fd2358c4969425dcdc"],
             ["protocol-buffers-encodings", "npm:1.1.1"]
@@ -2576,7 +2576,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./packages/js-drive/",
           "packageDependencies": [
             ["@dashevo/drive", "workspace:packages/js-drive"],
-            ["@dashevo/abci", "https://github.com/dashevo/js-abci.git#commit=aa3dfe8e13d9ced6cf71f6ab233bd1c7e33d044c"],
+            ["@dashevo/abci", "https://github.com/dashevo/js-abci.git#commit=5f18c2cd23ade43878a02ef25dff996a1d9c6537"],
             ["@dashevo/dapi-grpc", "workspace:packages/dapi-grpc"],
             ["@dashevo/dashcore-lib", "npm:0.19.28"],
             ["@dashevo/dashd-rpc", "npm:2.3.1"],
