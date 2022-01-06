b113e7fdcd42e08cc58d663d4a14050a336203be
Wisdom Ogwu <iammadab@gmail.com>
2022-01-06 22:44:01 +0100

    Refactored get_raw_for_contract

--- src/contract/types.rs
+++ src/contract/types.rs
@@ -9,20 +9,21 @@ pub enum DocumentFieldType {
     Float,
 }
 
-fn string_to_field_type(field_type_name: String) -> Option<DocumentFieldType> {
+pub fn string_to_field_type(field_type_name: String) -> Option<DocumentFieldType> {
     return match field_type_name.as_str() {
         "integer" => Some(DocumentFieldType::Integer),
         "string" => Some(DocumentFieldType::String),
         "float" => Some(DocumentFieldType::Float),
+        "array" => Some(DocumentFieldType::String),
         _ => None,
-    }
+    };
 }
 
 // Given a field type this function chooses and executes the right encoding method
-fn encode_document_field_type(
-    field_type: DocumentFieldType,
+pub fn encode_document_field_type(
+    field_type: &DocumentFieldType,
     value: &Value,
-) -> Result<Vec<u8>, Error> {
+) -> Result<Option<Vec<u8>>, Error> {
     let field_type_match_error = Error::CorruptedData(String::from(
         "document field type doesn't match document value",
     ));
@@ -30,18 +31,18 @@ fn encode_document_field_type(
     return match field_type {
         DocumentFieldType::String => {
             let value_as_text = value.as_text().ok_or(field_type_match_error)?;
-            Ok(value_as_text.as_bytes().to_vec())
+            Ok(Some(value_as_text.as_bytes().to_vec()))
         }
         DocumentFieldType::Integer => {
             let value_as_integer = value.as_integer().ok_or(field_type_match_error)?;
             let value_as_u64: u64 = value_as_integer
                 .try_into()
                 .map_err(|_| Error::CorruptedData(String::from("expected integer value")))?;
-            Ok(value_as_u64.to_be_bytes().to_vec())
+            Ok(Some(value_as_u64.to_be_bytes().to_vec()))
         }
         DocumentFieldType::Float => {
             let value_as_float = value.as_float().ok_or(field_type_match_error)?;
-            Ok(value_as_float.to_be_bytes().to_vec())
+            Ok(Some(value_as_float.to_be_bytes().to_vec()))
         }
     };
 }
@@ -59,9 +60,9 @@ mod tests {
         let string1 = Value::Text(String::from("a"));
         let string2 = Value::Text(String::from("b"));
 
-        let encoded_string1 = encode_document_field_type(DocumentFieldType::String, &string1)
+        let encoded_string1 = encode_document_field_type(&DocumentFieldType::String, &string1)
             .expect("should encode: valid parameters");
-        let encoded_string2 = encode_document_field_type(DocumentFieldType::String, &string2)
+        let encoded_string2 = encode_document_field_type(&DocumentFieldType::String, &string2)
             .expect("should encode: valid parameters");
 
         assert_eq!(string1 > string2, encoded_string1 > encoded_string2);
@@ -71,11 +72,11 @@ mod tests {
         let float2 = Value::Float(121.1);
         let float3 = Value::Float(13.0);
 
-        let encoded_float1 = encode_document_field_type(DocumentFieldType::Float, &float1)
+        let encoded_float1 = encode_document_field_type(&DocumentFieldType::Float, &float1)
             .expect("should encode: valid parameters");
-        let encoded_float2 = encode_document_field_type(DocumentFieldType::Float, &float2)
+        let encoded_float2 = encode_document_field_type(&DocumentFieldType::Float, &float2)
             .expect("should encode: valid parameters");
-        let encoded_float3 = encode_document_field_type(DocumentFieldType::Float, &float3)
+        let encoded_float3 = encode_document_field_type(&DocumentFieldType::Float, &float3)
             .expect("should encode: valid parameters");
 
         // 11.0 < 121.1
@@ -90,11 +91,11 @@ mod tests {
         let integer2 = Value::Integer(Integer::from(60));
         let integer3 = Value::Integer(Integer::from(60));
 
-        let encoded_integer1 = encode_document_field_type(DocumentFieldType::Integer, &integer1)
+        let encoded_integer1 = encode_document_field_type(&DocumentFieldType::Integer, &integer1)
             .expect("should encode: valid parameters");
-        let encoded_integer2 = encode_document_field_type(DocumentFieldType::Integer, &integer2)
+        let encoded_integer2 = encode_document_field_type(&DocumentFieldType::Integer, &integer2)
             .expect("should encode: valid parameters");
-        let encoded_integer3 = encode_document_field_type(DocumentFieldType::Integer, &integer3)
+        let encoded_integer3 = encode_document_field_type(&DocumentFieldType::Integer, &integer3)
             .expect("should encode: valid parameters");
 
         assert_eq!(encoded_integer1 < encoded_integer2, true);
