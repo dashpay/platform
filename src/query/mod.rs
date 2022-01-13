29d3686082bc24c5de97c7a6cf850803ceeafcb7
Quantum Explorer <quantum@dash.org>
2022-01-13 19:38:14 +0700

    more work on queries

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -4,8 +4,7 @@ use grovedb::{Element, Error, GroveDb, PathQuery, Query, QueryItem};
 use crate::contract::{Contract, DocumentType, Index};
 use ciborium::value::{Value as CborValue, Value};
 use serde::{Deserialize, Serialize};
-use test::RunIgnored::No;
-use crate::query::WhereOperator::{Equal, GreaterThan, GreaterThanOrEquals, LessThan, LessThanOrEquals, In, StartsWith};
+use crate::query::WhereOperator::{Equal, GreaterThan, GreaterThanOrEquals, LessThan, LessThanOrEquals, In, StartsWith, Between, BetweenExcludeBounds, BetweenExcludeLeft, BetweenExcludeRight};
 
 pub struct DocumentPathQuery<'a> {
     document_type: &'a DocumentType,
@@ -14,12 +13,17 @@ pub struct DocumentPathQuery<'a> {
     path_query : PathQuery<'a>,
 }
 
+#[derive(Copy, Clone)]
 pub enum WhereOperator {
     Equal,
     GreaterThan,
     GreaterThanOrEquals,
     LessThan,
     LessThanOrEquals,
+    Between,
+    BetweenExcludeBounds,
+    BetweenExcludeLeft,
+    BetweenExcludeRight,
     In,
     StartsWith,
 }
@@ -31,20 +35,25 @@ fn operator_from_string(string: &str) -> Option<WhereOperator> {
         ">=" => Some(GreaterThanOrEquals),
         "<" => Some(LessThan),
         "<=" => Some(LessThanOrEquals),
+        "Between" => Some(Between),
+        "BetweenExcludeBounds" => Some(BetweenExcludeBounds),
+        "BetweenExcludeLeft" => Some(BetweenExcludeLeft),
+        "BetweenExcludeRight" => Some(BetweenExcludeRight),
         "In" => Some(In),
         "StartsWith" => Some(StartsWith),
         &_ => None
     }
 }
 
+#[derive(Copy, Clone)]
 pub struct WhereClause<'a> {
     field: &'a str,
     operator: WhereOperator,
     value: &'a Value
 }
 
-impl WhereClause {
-    pub fn from_components(clause_components: &Vec<Value>) -> Result<Self, Error> {
+impl<'a> WhereClause<'a> {
+    pub fn from_components(clause_components: &'a Vec<Value>) -> Result<Self, Error> {
         if clause_components.len() != 3 {
             return Err(Error::CorruptedData(String::from("limit should be a integer from 1 to 100")));
         }
@@ -62,27 +71,90 @@ impl WhereClause {
             value,
         })
     }
+
+    fn group_range_clauses(where_clauses: &'a[WhereClause]) -> Result<Option<Self>, Error> {
+        // In order to group range clauses
+        let groupable_range_clauses : Vec<&WhereClause> = where_clauses.iter().filter(| where_clause| {
+            match where_clause.operator {
+                Equal => false,
+                In => false,
+                GreaterThan => true,
+                GreaterThanOrEquals => true,
+                LessThan => true,
+                LessThanOrEquals => true,
+                StartsWith => false,
+                Between => false,
+                BetweenExcludeBounds => false,
+                BetweenExcludeRight => false,
+                BetweenExcludeLeft => false,
+            }
+        }).collect();
+
+        let non_groupable_range_clauses : Vec<&WhereClause> = where_clauses.iter().filter(| where_clause| {
+            match where_clause.operator {
+                Equal => false,
+                In => false,
+                GreaterThan => false,
+                GreaterThanOrEquals => false,
+                LessThan => false,
+                LessThanOrEquals => false,
+                StartsWith => true,
+                Between => true,
+                BetweenExcludeBounds => true,
+                BetweenExcludeRight => true,
+                BetweenExcludeLeft => true,
+            }
+        }).collect();
+
+        if non_groupable_range_clauses.len() == 0 {
+            if groupable_range_clauses.len() == 0 {
+                return Ok(None);
+            }
+            if groupable_range_clauses.len() == 1 {
+                return Ok(Some(*groupable_range_clauses.get(0).unwrap().clone()))
+            }
+
+            if groupable_range_clauses.len() > 2 {
+                return Err(Error::CorruptedData(String::from("there can only be at most 2 range clauses")));
+            }
+
+            groupable_range_clauses.get(0).unwrap();
+        } else if non_groupable_range_clauses.len() == 1 {
+            let where_clause = non_groupable_range_clauses.get(0).unwrap();
+            return Ok(Some(WhereClause {
+                field: where_clause.field,
+                operator: where_clause.operator,
+                value: where_clause.value,
+            }))
+        } else {
+            // if non_groupable_range_clauses.len() > 1
+            return Err(Error::CorruptedData(String::from("there can not be more than 1 non groupable range clause")));
+        }
+
+        Ok(None)
+    }
 }
 
 pub struct DriveQuery<'a> {
     contract: &'a Contract,
     document_type: &'a DocumentType,
-    where_clauses: Vec<WhereClause<'a>>,
-    range_clause: WhereClause<'a>,
+    equal_clauses: Vec<WhereClause<'a>>,
+    range_clause: Option<WhereClause<'a>>,
+    offset: u32,
     limit: u32,
     order_by: &'a str,
-    start_at: Option<vec<u8>>,
+    start_at: Option<Vec<u8>>,
     start_at_included: bool,
 }
 
-impl DriveQuery {
-    pub fn from_cbor(query_cbor: &[u8], contract: &Contract, document_type: &DocumentType) -> Result<Self, Error> {
+impl<'a> DriveQuery<'a> {
+    pub fn from_cbor(query_cbor: &[u8], contract: &'a Contract, document_type: &'a DocumentType) -> Result<Self, Error> {
         let mut document: HashMap<String, CborValue> = ciborium::de::from_reader(query_cbor)
             .map_err(|_| Error::CorruptedData(String::from("unable to decode query")))?;
 
         let limit: u32 = document.get("limit").map_or( Some(100), |id_cbor| {
             if let CborValue::Integer(b) = id_cbor {
-                Some(b as u32)
+                Some(i128::from(*b) as u32)
             } else {
                 None
             }
@@ -90,39 +162,25 @@ impl DriveQuery {
 
         let all_where_clauses : Vec<WhereClause> = document.get("where").map_or( vec![], |id_cbor| {
             if let CborValue::Array(clauses) = id_cbor {
-                clauses.iter().map( | where_clause| {
+                clauses.iter().filter_map( | where_clause| {
                     if let CborValue::Array(clauses_components) = where_clause {
-                        WhereClause::from_components(clauses_components)
+                        WhereClause::from_components(clauses_components).ok()
                     } else {
-                        Err(Error::CorruptedData(String::from("This must be an array of where clauses")))
+                        None
                     }
-                })?.collect()
+                }).collect()
             } else {
                 vec![]
             }
         });
 
-        let equal_clauses = all_where_clauses.iter().filter(| where_clause| {
+        let range_clause = WhereClause::group_range_clauses(&all_where_clauses)?;
+
+        let equal_clauses = all_where_clauses.into_iter().filter(| where_clause| {
             match where_clause.operator {
                 Equal => true,
-                GreaterThan => false,
-                GreaterThanOrEquals => false,
-                LessThan => false,
-                LessThanOrEquals => false,
                 In => true,
-                StartsWith => false,
-            }
-        }).collect();
-
-        let range_clauses = all_where_clauses.iter().filter(| where_clause| {
-            match where_clause.operator {
-                Equal => false,
-                GreaterThan => true,
-                GreaterThanOrEquals => true,
-                LessThan => true,
-                LessThanOrEquals => true,
-                In => false,
-                StartsWith => true,
+                _ => false,
             }
         }).collect();
 
@@ -132,9 +190,9 @@ impl DriveQuery {
             return Err(Error::CorruptedData(String::from("only one of startAt or startAfter should be provided")));
         }
 
-        let mut start_at_included = false;
+        let mut start_at_included = true;
 
-        let mut start_option: Option<&value> = None;
+        let mut start_option: Option<&Value> = None;
 
         if start_after_option.is_some() {
             start_option = start_after_option;
@@ -144,28 +202,35 @@ impl DriveQuery {
             start_at_included = true;
         }
 
-        let start_at: vec<u8> = start_option.map_or( vec![], |id_cbor| {
+        let mut offset: u32 = start_option.map_or( Some(0), |id_cbor| {
             if let CborValue::Integer(b) = id_cbor {
-                Some(b as u32)
+                Some(i128::from(*b) as u32)
             } else {
                 None
             }
         }).ok_or(Error::CorruptedData(String::from("limit should be a integer from 1 to 100")))?;
 
+        if !start_at_included {
+            offset += 1;
+        }
+
         Ok(DriveQuery {
-            where_clauses,
-            range_clause: &WhereClause {},
+            contract,
+            document_type,
+            equal_clauses,
+            range_clause,
+            offset,
             limit,
             order_by: "",
-            start_at,
-            start_at_included
+            start_at: None,
+            start_at_included,
         })
     }
 }
 
-impl DocumentPathQuery {
+impl<'a> DocumentPathQuery<'a> {
 
-    pub fn construct<'a>(contract: &'a Contract, document_type_name : &str, index: &Index, intermediate_values: Vec<Vec<u8>>, final_query: Query) -> Result<Self, Error> {
+    pub fn construct(contract: &'a Contract, document_type_name : &'a str, index: &'a Index, intermediate_values: Vec<Vec<u8>>, final_query: Query) -> Result<Self, Error> {
         // first let's get the contract path
 
         let mut contract_path = contract.document_type_path(document_type_name);
@@ -193,20 +258,13 @@ impl DocumentPathQuery {
         })
     }
 
-    pub fn index_and_intermediate_values_for
-
-    fn execute(self, mut grove: GroveDb) -> Result<vec<vec<u8>>, Error> {
+    fn execute_no_proof(self, mut grove: GroveDb, transaction: Option<&OptimisticTransactionDBTransaction>) -> Result<(Vec<Vec<u8>>, u16), Error> {
         let path_query = self.path_query;
-        let proof = grove.get_query(&[path_query])?.iter().map(| element | {
-            match element {
-                Element::Item(_) => {}
-                Element::Reference(a) => {}
-                Element::Tree(_) => {
-                    // this should never happen
-                    None
-                }
-            }
-        })
+        grove.get_path_query(&path_query, transaction)
+    }
+
+    fn execute_with_proof(self, mut grove: GroveDb) -> Result<Vec<u8>, Error> {
+        todo!()
     }
 }
 
