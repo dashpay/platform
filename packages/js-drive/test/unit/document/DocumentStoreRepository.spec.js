ab931f8e8e4d9f807f10581852dce685d92940cc
Ivan Shumkov <ivan@shumkov.ru>
2022-01-13 17:35:33 +0700

    feat: implement secondary indices

--- packages/js-drive/test/unit/document/DocumentStoreRepository.spec.js
+++ packages/js-drive/test/unit/document/DocumentStoreRepository.spec.js
@@ -3,7 +3,7 @@ const createDPPMock = require('@dashevo/dpp/lib/test/mocks/createDPPMock');
 
 const StoreMock = require('../../../lib/test/mock/StoreMock');
 
-const DocumentStoreRepository = require('../../../lib/document/DocumentRepository');
+const DocumentStoreRepository = require('../../../lib/document/repository/DocumentRepository');
 
 describe('DocumentStoreRepository', () => {
   let document;
