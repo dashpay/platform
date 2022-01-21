03f5a94518aff7c5e3f0e60625d96bc9f99f5f94
Igor Markin <markin.io210@gmail.com>
2022-01-21 05:36:10 +0200

    Implements regtest (dashevo/dash-spv#30)

--- config/config.js
+++ config/config.js
@@ -35,6 +35,16 @@ module.exports = {
       nonce: 3861367235,
     });
   },
+  getRegtestGenesis() {
+    return utils.normalizeHeader({
+      version: 1,
+      previousblockhash: '0000000000000000000000000000000000000000000000000000000000000000',
+      merkleroot: 'e0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7',
+      time: 1390666206,
+      bits: '1e0ffff0',
+      nonce: 3861367235,
+    });
+  },
   getLivenetGenesis() {
     throw Error('Livenet genesis not yet implemented');
   },
