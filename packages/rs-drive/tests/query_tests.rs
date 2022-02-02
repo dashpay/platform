744d36381b677d19741d3172630edbcfb14433e3
Wisdom Ogwu <40731160+iammadab@users.noreply.github.com>
2022-02-02 21:23:33 +0700

    fix: handle key not found error from grovedb (dashpay/rs-platform#33)
    grovedb returns an error if it cannot find a key at the subtree path
    rs-drive was not handling this error until now, which caused it to
    fail for queries with unknown keys.

--- packages/rs-drive/tests/query_tests.rs
+++ packages/rs-drive/tests/query_tests.rs
@@ -186,7 +186,7 @@ fn test_query() {
         None,
     ).expect("query should be executed");
 
-    assert_eq!(results.len(), 1);
+    assert_eq!(results.len(), 0);
     //
     // let names: Vec<String> = results
     //     .into_iter()
