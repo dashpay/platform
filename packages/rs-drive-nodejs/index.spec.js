1d8d253d26bd399909c4d2f9cb1545ee1f3ebeec
Ivan Shumkov <ivan@shumkov.ru>
2022-02-02 13:31:05 +0700

    fix: contract parsing errors

--- packages/rs-drive-nodejs/index.spec.js
+++ packages/rs-drive-nodejs/index.spec.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const { expect } = require('chai');
 
 const getDataContractFixture = require('@dashevo/dpp/lib/test/fixtures/getDataContractFixture');
+const getDocumentsFixture = require('@dashevo/dpp/lib/test/fixtures/getDocumentsFixture');
 
 const Drive = require('./index');
 
@@ -10,9 +11,14 @@ const TEST_DATA_PATH = './test_data';
 
 describe('Drive', () => {
   let drive;
+  let dataContract;
+  let documents;
 
   beforeEach(() => {
     drive = new Drive(TEST_DATA_PATH);
+
+    dataContract = getDataContractFixture();
+    documents = getDocumentsFixture(dataContract);
   });
 
   afterEach(async () => {
@@ -31,27 +37,20 @@ describe('Drive', () => {
   });
 
   describe('#applyContract', () => {
-    it('should create contract if not exists', async () => {
+    beforeEach(async () => {
       await drive.createRootTree();
+    });
 
-      const encodedContract = getDataContractFixture().toBuffer();
+    it('should create contract if not exists', async () => {
+      const result = await drive.applyContract(dataContract.toBuffer());
 
-      const result = await drive.applyContract(encodedContract);
-
-      expect(result).to.be.an.instanceOf(Number);
-      expect(result).to.be.greaterThan(0);
+      expect(result).to.equals(0);
     });
 
     it('should update existing contract', async () => {
-      await drive.createRootTree();
+      await drive.applyContract(dataContract.toBuffer());
 
-      const contract = getDataContractFixture();
-
-      let encodedContract = contract.toBuffer();
-
-      await drive.applyContract(encodedContract);
-
-      contract.setDocumentSchema('newDocumentType', {
+      dataContract.setDocumentSchema('newDocumentType', {
         type: 'object',
         properties: {
           newProperty: {
@@ -61,12 +60,143 @@ describe('Drive', () => {
         additionalProperties: false,
       });
 
-      encodedContract = contract.toBuffer();
+      const result = await drive.applyContract(dataContract.toBuffer());
 
-      const result = await drive.applyContract(encodedContract);
+      expect(result).to.equals(0);
+    });
+  });
 
-      expect(result).to.be.an.instanceOf(Number);
-      expect(result).to.be.greaterThan(0);
+  describe('documents', () => {
+    beforeEach(async () => {
+      await drive.createRootTree();
+
+      await drive.applyContract(dataContract.toBuffer());
+    });
+
+    context('without indices', () => {
+      it('should create a document', async () => {
+        const documentWithoutIndices = documents[0];
+
+        const result = await drive.createDocument(
+          documentWithoutIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithoutIndices.getType(),
+          documentWithoutIndices.getOwnerId(),
+          true,
+        );
+
+        expect(result).to.equals(0);
+      });
+
+      it('should should update a document', async () => {
+        // Create a document
+        const documentWithoutIndices = documents[0];
+
+        await drive.createDocument(
+          documentWithoutIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithoutIndices.getType(),
+          documentWithoutIndices.getOwnerId(),
+          true,
+        );
+
+        // Update the document
+        documentWithoutIndices.set('name', 'Bob');
+
+        const result = await drive.updateDocument(
+          documentWithoutIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithoutIndices.getType(),
+          documentWithoutIndices.getOwnerId(),
+        );
+
+        expect(result).to.equals(0);
+      });
+
+      it('should should delete the document', async () => {
+        // Create a document
+        const documentWithoutIndices = documents[3];
+
+        await drive.createDocument(
+          documentWithoutIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithoutIndices.getType(),
+          documentWithoutIndices.getOwnerId(),
+          true,
+        );
+
+        const result = await drive.deleteDocument(
+          documentWithoutIndices.getId(),
+          dataContract.toBuffer(),
+          documentWithoutIndices.getType(),
+          documentWithoutIndices.getOwnerId(),
+        );
+
+        expect(result).to.equals(0);
+      });
+    });
+
+    context('with indices', () => {
+      it('should create a document', async () => {
+        const documentWithIndices = documents[3];
+
+        const result = await drive.createDocument(
+          documentWithIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithIndices.getType(),
+          documentWithIndices.getOwnerId(),
+          true,
+        );
+
+        expect(result).to.equals(0);
+      });
+
+      it('should should update the document', async () => {
+        // Create a document
+        const documentWithIndices = documents[3];
+
+        await drive.createDocument(
+          documentWithIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithIndices.getType(),
+          documentWithIndices.getOwnerId(),
+          true,
+        );
+
+        // Update the document
+        documentWithIndices.set('firstName', 'Bob');
+
+        const result = await drive.updateDocument(
+          documentWithIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithIndices.getType(),
+          documentWithIndices.getOwnerId(),
+        );
+
+        expect(result).to.equals(0);
+      });
+
+      it('should should delete the document', async () => {
+        // Create a document
+        const documentWithIndices = documents[3];
+
+        await drive.createDocument(
+          documentWithIndices.toBuffer(),
+          dataContract.toBuffer(),
+          documentWithIndices.getType(),
+          documentWithIndices.getOwnerId(),
+          true,
+        );
+
+        const result = await drive.deleteDocument(
+          documentWithIndices.getId(),
+          dataContract.toBuffer(),
+          documentWithIndices.getType(),
+          documentWithIndices.getOwnerId(),
+        );
+
+        expect(result).to.equals(0);
+      });
     });
   });
 });
