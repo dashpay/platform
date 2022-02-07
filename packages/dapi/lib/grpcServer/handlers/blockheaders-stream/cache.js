c00d289ed8606d1146dfbb948119b9c5a5df9b49
pshenmic <pshenmic@gmail.com>
2022-02-07 16:09:48 +0700

    chore: remove unneeded length function

--- packages/dapi/lib/grpcServer/handlers/blockheaders-stream/cache.js
+++ packages/dapi/lib/grpcServer/handlers/blockheaders-stream/cache.js
@@ -3,9 +3,6 @@ const LRU = require('lru-cache');
 const options = {
   max: 500,
   maxAge: 1000 * 60 * 60,
-  length(n, key) {
-    return n * 2 + key.length;
-  },
 };
 
 const cache = new LRU(options);
