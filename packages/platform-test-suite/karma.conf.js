c2e6f6626f67d4cdba0353ae3a33fee1776100c5
mayoreee <mayowaegbinola@gmail.com>
2022-02-09 00:45:50 +0100

    feat (platform-test-suite): execute tests in browsers

--- packages/platform-test-suite/karma.conf.js
+++ packages/platform-test-suite/karma.conf.js
@@ -1,26 +1,79 @@
-const webpack = require('webpack');
 const dotenvSafe = require('dotenv-safe');
 const path = require('path');
+const webpack = require('webpack');
 
-// Set env variables
+
 const { parsed: envs } = dotenvSafe.config({
   path: path.resolve(__dirname, '.env'),
 });
 
+
+
 module.exports = (config) => {
   config.set({
+    autoWatch: false,
+    autoWatchBatchSelay: 250,
+    basePath: '',
+    browserDisconnectTimeout: 1000000,
+    browserDisconnectTolerance: 10,
+    browserNoActivityTimeout: 1000000,
+    browserSocketTimeout: 100000,
+    browsers: [
+      'ChromeHeadless',
+      'FirefoxHeadless',
+      'IE',
+      'Opera'
+    ],
+    captureTimeout: 90000,
     client: {
       mocha: {
         timeout: 650000,
       },
     },
-    frameworks: ['mocha', 'chai'],
+    colors: true,
+    concurrency: Infinity,
+    crossOriginAttribute: true,
+    customContextFile: null,
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: ['-headless'],
+      },
+    },
+    detached: false,
+    failOnEmptyTestSuite: true,
+    failOnSkippedTests: true,
+    failOnFailingTestSuite: true,
     files: [
-      'lib/test/karma/loader.js',
+      'lib/test/browser/index.js',
     ],
+    frameworks: ['mocha', 'chai'],
+    hostname: 'localhost',
+    logLevel: config.LOG_INFO,
+    path: "/",
+    pingTimeout: 10000,
+    plugins: [
+      'karma-chai',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      "karma-ie-launcher",
+      'karma-mocha',
+      'karma-mocha-reporter',
+      "karma-opera-launcher",
+      "karma-phantomjs-launcher",
+      "karma-safari-launcher",
+      'karma-webpack',
+    ],
+    port: 9876,
+    processKillTimeout: 2000,
     preprocessors: {
       'lib/test/karma/loader.js': ['webpack'],
     },
+    protocol: 'http:',
+    reportSlowerThan: 0,
+    restartOnFileChange: true,
+    retryLimit: 5,
+    singleRun: false,
     webpack: {
       mode: 'development',
       resolve: {
@@ -37,38 +90,17 @@ module.exports = (config) => {
           os: require.resolve('os-browserify/browser'),
           zlib: require.resolve('browserify-zlib'),
         },
+        alias: {
+          process: 'process/browser',
+        },
       },
       plugins: [
         new webpack.ProvidePlugin({
           Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
+          process: 'process/browser'
         }),
         new webpack.EnvironmentPlugin(Object.keys(envs)),
       ],
     },
-    reporters: ['mocha'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: false,
-    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
-    singleRun: false,
-    concurrency: Infinity,
-    browserNoActivityTimeout: 900000,
-    browserDisconnectTimeout: 900000,
-    plugins: [
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-chai',
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-webpack',
-    ],
-    customLaunchers: {
-      FirefoxHeadless: {
-        base: 'Firefox',
-        flags: ['-headless'],
-      },
-    },
-  });
-};
+  })
+}
