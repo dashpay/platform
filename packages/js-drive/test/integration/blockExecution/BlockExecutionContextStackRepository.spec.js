d73e490a34bc15821693f54badf00ac0167144ec
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2022-02-16 19:19:01 +0300

    test(drive): initChainHandlerFactory

--- packages/js-drive/test/integration/blockExecution/BlockExecutionContextStackRepository.spec.js
+++ packages/js-drive/test/integration/blockExecution/BlockExecutionContextStackRepository.spec.js
@@ -1,22 +1,43 @@
+const rimraf = require('rimraf');
+
 const cbor = require('cbor');
 
+const Drive = require('@dashevo/rs-drive');
 const BlockExecutionContextStackRepository = require('../../../lib/blockExecution/BlockExecutionContextStackRepository');
 const BlockExecutionContext = require('../../../lib/blockExecution/BlockExecutionContext');
 
 const getBlockExecutionContextObjectFixture = require('../../../lib/test/fixtures/getBlockExecutionContextObjectFixture');
 const BlockExecutionContextStack = require('../../../lib/blockExecution/BlockExecutionContextStack');
+const GroveDBStore = require('../../../lib/storage/GroveDBStore');
+
+function removeConsensusLogger(blockExecutionContextStack) {
+  const plainContexts = blockExecutionContextStack.getContexts()
+    .map((context) => context.toObject({
+      skipConsensusLogger: true,
+    }));
+
+  return plainContexts.map((rawContext) => {
+    const context = new BlockExecutionContext();
+
+    context.fromObject(rawContext);
+
+    return context;
+  });
+}
 
 describe('BlockExecutionContextStackRepository', () => {
-  let db;
+  let rsDrive;
+  let store;
   let repository;
   let blockExecutionContextObject;
   let blockExecutionContext;
   let blockExecutionContextStack;
 
-  beforeEach(() => {
-    db = level('./db/blockchain-state-test', { keyEncoding: 'binary', valueEncoding: 'binary' });
+  beforeEach(async () => {
+    rsDrive = new Drive('./db/grovedb_test');
+    store = new GroveDBStore(rsDrive, 'blockchainStateTestStore');
 
-    repository = new BlockExecutionContextStackRepository(db);
+    repository = new BlockExecutionContextStackRepository(store);
 
     blockExecutionContextObject = getBlockExecutionContextObjectFixture();
 
@@ -31,8 +52,8 @@ describe('BlockExecutionContextStackRepository', () => {
   });
 
   afterEach(async () => {
-    await db.clear();
-    await db.close();
+    await rsDrive.close();
+    rimraf.sync('./db/grovedb_test');
   });
 
   describe('#store', () => {
@@ -43,13 +64,67 @@ describe('BlockExecutionContextStackRepository', () => {
 
       expect(result).to.equal(repository);
 
-      const storedContextsBuffer = await db.get(
+      const storedContextsBuffer = await store.getAux(
         BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
       );
 
       expect(storedContextsBuffer).to.be.instanceOf(Buffer);
 
       const storedContexts = cbor.decode(storedContextsBuffer);
+
+      expect(storedContexts).to.deep.equals(
+        blockExecutionContextStack.getContexts()
+          .map((context) => context.toObject({
+            skipDBTransaction: true,
+            skipConsensusLogger: true,
+          })),
+      );
+    });
+
+    it('should store block execution context stack using transaction', async () => {
+      await store.startTransaction();
+
+      const result = await repository.store(
+        blockExecutionContextStack,
+        true,
+      );
+
+      expect(result).to.equal(repository);
+
+      const notFoundData = await store.getAux(
+        BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
+        { useTransaction: false },
+      );
+
+      expect(notFoundData).to.be.null();
+
+      const dataFromTransaction = await store.getAux(
+        BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
+        { useTransaction: true },
+      );
+
+      expect(dataFromTransaction).to.be.instanceOf(Buffer);
+
+      let storedContexts = cbor.decode(dataFromTransaction);
+
+      expect(storedContexts).to.deep.equals(
+        blockExecutionContextStack.getContexts()
+          .map((context) => context.toObject({
+            skipDBTransaction: true,
+            skipConsensusLogger: true,
+          })),
+      );
+
+      await store.commitTransaction();
+
+      const commitedData = await store.getAux(
+        BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
+        { useTransaction: true },
+      );
+
+      expect(commitedData).to.be.instanceOf(Buffer);
+
+      storedContexts = cbor.decode(commitedData);
 
       expect(storedContexts).to.deep.equals(
         blockExecutionContextStack.getContexts()
@@ -72,13 +147,12 @@ describe('BlockExecutionContextStackRepository', () => {
     it('should return stored block execution context', async () => {
       const plainContexts = blockExecutionContextStack.getContexts()
         .map((context) => context.toObject({
-          skipDBTransaction: true,
           skipConsensusLogger: true,
         }));
 
       const storedStackBuffer = cbor.encode(plainContexts);
 
-      await db.put(
+      await store.putAux(
         BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
         storedStackBuffer,
       );
@@ -87,7 +161,44 @@ describe('BlockExecutionContextStackRepository', () => {
 
       expect(storedStack).to.be.instanceOf(BlockExecutionContextStack);
 
-      expect(storedStack.getContexts()).to.deep.equals(plainContexts);
+      const blockExecutionContexts = removeConsensusLogger(blockExecutionContextStack);
+
+      expect(storedStack.getContexts()).to.deep.equal(blockExecutionContexts);
+    });
+
+    it('should return stored block execution context using transaction', async () => {
+      await store.startTransaction();
+
+      const plainContexts = blockExecutionContextStack.getContexts()
+        .map((context) => context.toObject({
+          skipConsensusLogger: true,
+        }));
+
+      const storedStackBuffer = cbor.encode(plainContexts);
+
+      await store.putAux(
+        BlockExecutionContextStackRepository.EXTERNAL_STORE_KEY_NAME,
+        storedStackBuffer,
+        { useTransaction: true },
+      );
+
+      let storedStack = await repository.fetch(false);
+
+      expect(storedStack.getContexts()).to.deep.equal([]);
+
+      storedStack = await repository.fetch(true);
+
+      let blockExecutionContexts = removeConsensusLogger(blockExecutionContextStack);
+
+      expect(storedStack.getContexts()).to.deep.equal(blockExecutionContexts);
+
+      await store.commitTransaction();
+
+      storedStack = await repository.fetch(true);
+
+      blockExecutionContexts = removeConsensusLogger(blockExecutionContextStack);
+
+      expect(storedStack.getContexts()).to.deep.equal(blockExecutionContexts);
     });
   });
 });
--- packages/js-drive/test/unit/abci/handlers/beginBlockHandlerFactory.spec.js
+++ packages/js-drive/test/unit/abci/handlers/beginBlockHandlerFactory.spec.js
@@ -10,10 +10,9 @@ const {
 
 const beginBlockHandlerFactory = require('../../../../lib/abci/handlers/beginBlockHandlerFactory');
 
-const BlockExecutionDBTransactionsMock = require('../../../../lib/test/mock/BlockExecutionStoreTransactionsMock');
 const BlockExecutionContextMock = require('../../../../lib/test/mock/BlockExecutionContextMock');
 const LoggerMock = require('../../../../lib/test/mock/LoggerMock');
-const NotSupportedNetworkProtocolVersionError = require('../../../../lib/abci/handlers/errors/NotSupportedProtocolVersionError');
+const NotSupportedNetworkProtocolVersionError = require('../../../../lib/abci/handlers/errors/NotSupportedNetworkProtocolVersionError');
 const NetworkProtocolVersionIsNotSetError = require('../../../../lib/abci/handlers/errors/NetworkProtocolVersionIsNotSetError');
 
 describe('beginBlockHandlerFactory', () => {
@@ -22,9 +21,7 @@ describe('beginBlockHandlerFactory', () => {
   let request;
   let blockHeight;
   let coreChainLockedHeight;
-  let blockExecutionDBTransactionsMock;
   let blockExecutionContextMock;
-  let previousBlockExecutionContextMock;
   let header;
   let updateSimplifiedMasternodeListMock;
   let waitForChainLockedHeightMock;
@@ -33,14 +30,13 @@ describe('beginBlockHandlerFactory', () => {
   let dppMock;
   let transactionalDppMock;
   let synchronizeMasternodeIdentitiesMock;
+  let groveDBStoreMock;
+  let blockExecutionContextStackMock;
 
   beforeEach(function beforeEach() {
     protocolVersion = Long.fromInt(1);
 
-    blockExecutionDBTransactionsMock = new BlockExecutionDBTransactionsMock(this.sinon);
-
     blockExecutionContextMock = new BlockExecutionContextMock(this.sinon);
-    previousBlockExecutionContextMock = new BlockExecutionContextMock(this.sinon);
 
     loggerMock = new LoggerMock(this.sinon);
 
@@ -55,10 +51,23 @@ describe('beginBlockHandlerFactory', () => {
     waitForChainLockedHeightMock = this.sinon.stub();
     synchronizeMasternodeIdentitiesMock = this.sinon.stub();
 
+    groveDBStoreMock = {
+      isTransactionStarted: this.sinon.stub(),
+      abortTransaction: this.sinon.stub(),
+      startTransaction: this.sinon.stub(),
+    };
+
+    blockExecutionContextStackMock = {
+      getLatest: this.sinon.stub().returns({
+        getHeader: this.sinon.stub(),
+      }),
+      removeLatest: this.sinon.stub(),
+    };
+
     beginBlockHandler = beginBlockHandlerFactory(
-      blockExecutionDBTransactionsMock,
+      groveDBStoreMock,
       blockExecutionContextMock,
-      previousBlockExecutionContextMock,
+      blockExecutionContextStackMock,
       protocolVersion,
       dppMock,
       transactionalDppMock,
@@ -92,47 +101,44 @@ describe('beginBlockHandlerFactory', () => {
 
   it('should update height, update masternode identities, start transactions return ResponseBeginBlock', async () => {
     updateSimplifiedMasternodeListMock.resolves(true);
+    groveDBStoreMock.isTransactionStarted.resolves(false);
 
     const response = await beginBlockHandler(request);
 
     expect(response).to.be.an.instanceOf(ResponseBeginBlock);
 
-    expect(blockExecutionDBTransactionsMock.start).to.be.calledOnceWithExactly();
+    expect(waitForChainLockedHeightMock).to.be.calledOnceWithExactly(coreChainLockedHeight);
 
-    expect(previousBlockExecutionContextMock.reset).to.be.calledOnceWithExactly();
-    expect(previousBlockExecutionContextMock.populate).to.be.calledOnceWithExactly(
-      blockExecutionContextMock,
-    );
+    expect(blockExecutionContextMock.getHeader).to.be.calledOnceWithExactly();
 
     expect(blockExecutionContextMock.reset).to.be.calledOnceWithExactly();
     expect(blockExecutionContextMock.setHeader).to.be.calledOnceWithExactly(header);
     expect(blockExecutionContextMock.setLastCommitInfo).to.be.calledOnceWithExactly(lastCommitInfo);
 
-    expect(updateSimplifiedMasternodeListMock).to.be.calledOnceWithExactly(
-      coreChainLockedHeight, { logger: loggerMock },
-    );
-    expect(waitForChainLockedHeightMock).to.be.calledOnceWithExactly(coreChainLockedHeight);
-    expect(blockExecutionDBTransactionsMock.abort).to.be.not.called();
     expect(dppMock.setProtocolVersion).to.have.been.calledOnceWithExactly(
       protocolVersion.toNumber(),
     );
     expect(transactionalDppMock.setProtocolVersion).to.have.been.calledOnceWithExactly(
       protocolVersion.toNumber(),
     );
+    expect(groveDBStoreMock.isTransactionStarted).to.be.calledOnceWithExactly();
+    expect(groveDBStoreMock.startTransaction).to.be.calledOnceWithExactly();
 
+    expect(updateSimplifiedMasternodeListMock).to.be.calledOnceWithExactly(
+      coreChainLockedHeight, { logger: loggerMock },
+    );
     expect(synchronizeMasternodeIdentitiesMock).to.have.been.calledOnceWithExactly(
       coreChainLockedHeight,
-      false,
     );
   });
 
-  it('should throw NotSupportedProtocolVersionError if protocol version is not supported', async () => {
+  it('should throw NotSupportedNetworkProtocolVersionError if protocol version is not supported', async () => {
     request.header.version.app = Long.fromInt(42);
 
     try {
       await beginBlockHandler(request);
 
-      expect.fail('should throw NotSupportedProtocolVersionError');
+      expect.fail('should throw NotSupportedNetworkProtocolVersionError');
     } catch (e) {
       expect(e).to.be.instanceOf(NotSupportedNetworkProtocolVersionError);
       expect(e.getNetworkProtocolVersion()).to.equal(request.header.version.app);
@@ -152,19 +158,30 @@ describe('beginBlockHandlerFactory', () => {
     }
   });
 
-  it('should abort already started transactions', async () => {
-    blockExecutionDBTransactionsMock.isStarted.returns(true);
+  it('should abort already started transactions', async function it() {
+    blockExecutionContextMock.getHeader.returns({
+      height: {
+        equals: this.sinon.stub().returns(true),
+      },
+    });
+
+    blockExecutionContextStackMock.getLatest.returns({
+      getHeader: this.sinon.stub().returns(
+        {
+          height: {
+            equals: this.sinon.stub().returns(true),
+          },
+        },
+      ),
+    });
+    groveDBStoreMock.isTransactionStarted.resolves(true);
 
     const response = await beginBlockHandler(request);
 
     expect(response).to.be.an.instanceOf(ResponseBeginBlock);
 
-    expect(blockExecutionDBTransactionsMock.start).to.be.calledOnceWithExactly();
-
-    expect(previousBlockExecutionContextMock.reset).to.be.calledOnceWithExactly();
-    expect(previousBlockExecutionContextMock.populate).to.be.calledOnceWithExactly(
-      blockExecutionContextMock,
-    );
+    expect(groveDBStoreMock.abortTransaction).to.be.calledOnceWithExactly();
+    expect(blockExecutionContextStackMock.removeLatest).to.be.calledOnceWithExactly();
 
     expect(blockExecutionContextMock.reset).to.be.calledOnceWithExactly();
     expect(blockExecutionContextMock.setHeader).to.be.calledOnceWithExactly(header);
@@ -175,7 +192,6 @@ describe('beginBlockHandlerFactory', () => {
     );
 
     expect(waitForChainLockedHeightMock).to.be.calledOnceWithExactly(coreChainLockedHeight);
-    expect(blockExecutionDBTransactionsMock.abort).to.be.calledOnce();
     expect(synchronizeMasternodeIdentitiesMock).to.have.not.been.called();
   });
 });
