3f8c683133891aecb3a7977d109c3120e0ec0f1f
markin.io <markin.io210@gmail.com>
2022-02-20 20:30:59 +0300

    Fixes identity fixture index

--- packages/js-dash-sdk/src/test/fixtures/createIdentityFixtureInAccount.ts
+++ packages/js-dash-sdk/src/test/fixtures/createIdentityFixtureInAccount.ts
@@ -4,7 +4,7 @@ const getIdentityFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityFi
 
 export function createIdentityFixtureInAccount(account) {
     const identityFixture = getIdentityFixture();
-    const identityFixtureIndex = 10000;
+    const identityFixtureIndex = 0;
     const { privateKey: identityPrivateKey } = account.identities.getIdentityHDKeyByIndex(identityFixtureIndex, 0);
 
     identityFixture.publicKeys[0] = new IdentityPublicKey({
