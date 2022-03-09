2b7f732ecf440dc79b612542a2cf755212a88529
Ivan Shumkov <ivan@shumkov.ru>
2022-03-09 23:49:48 +0700

    test: integration test

--- packages/js-drive/test/integration/identity/masternode/synchronizeMasternodeIdentitiesFactory.spec.js
+++ packages/js-drive/test/integration/identity/masternode/synchronizeMasternodeIdentitiesFactory.spec.js
@@ -24,35 +24,14 @@ describe('synchronizeMasternodeIdentitiesFactory', () => {
   let identityRepository;
   let documentRepository;
   let publicKeyToIdentityIdRepository;
+  let coreRpcClientMock;
 
   beforeEach(async function beforeEach() {
-    // rawDiff = {
-    //   baseBlockHash: '644bd9dcbc0537026af6d31181570f934d868f121c55513009bb36f509ec816e',
-    //   blockHash: '23beac1b700c4a49855a9653e036219384ac2fab7eeba2ec45b3e2d0063d1285',
-    //   cbTxMerkleTree: '03000000032f7f142e19bee0c595dac9f900695d1e428a4db70a805fda6c834cfec0de506a0d39baea39dbbaf9827a1f3b8f381a65ebcf4c2ef415025bc4d20afd372e680d12c226f084a6e28e421fbedff22b13aa1191d6a80744d104fa75ede12332467d0107',
-    //   cbTx: '03000500010000000000000000000000000000000000000000000000000000000000000000ffffffff0502e9030101ffffffff01a2567a76070000001976a914f713c2fa5ef0e7c48f0d1b3ad2a79150037c72d788ac00000000460200e90300003fdbe53b9a4cd0b62284195cbd4f4c1655ebdd70e9117ed3c0e49c37bfce46060000000000000000000000000000000000000000000000000000000000000000',
-    //   deletedMNs: [],
-    //   mnList: [
-    //     {
-    //       proRegTxHash: 'e57402007ca10454d77437d9c1156b1c4ff8af86d699c08e9a31dbd1dfe3c991',
-    //       confirmedHash: '0000000000000000000000000000000000000000000000000000000000000000',
-    //       service: '127.0.0.1:20001',
-    //       pubKeyOperator: '906d84cb88f532145d8838414f777b971c976ffcf8ccfc57413a13cf2f8a7750a92f9b997a5a741f1afa34d989f4312b',
-    //       votingAddress: 'ydC3Qkhq6qc1qgHD8PVSHyAB6t3NYa7aw4',
-    //       isValid: true,
-    //     },
-    //   ],
-    //   deletedQuorums: [],
-    //   newQuorums: [],
-    //   merkleRootMNList: '0646cebf379ce4c0d37e11e970ddeb55164c4fbd5c198422b6d04c9a3be5db3f',
-    //   merkleRootQuorums: '0000000000000000000000000000000000000000000000000000000000000000',
-    // };
-
     coreHeight = 3;
 
     container = await createTestDIContainer();
 
-    // Mock Core
+    // Mock fetchTransaction
 
     fetchTransactionMock = this.sinon.stub();
 
@@ -76,6 +55,7 @@ describe('synchronizeMasternodeIdentitiesFactory', () => {
     container.register('fetchTransaction', asValue(fetchTransactionMock));
 
     // Mock Core RPC
+
     coreRpcClientMock = {
       protx: this.sinon.stub().resolves({
         result: rawDiff,
@@ -303,11 +283,13 @@ describe('synchronizeMasternodeIdentitiesFactory', () => {
   });
 
   it('should sync identities if the gap between coreHeight and lastSyncedCoreHeight > smlMaxListsLimit', async () => {
-    await synchronizeMasternodeIdentities(coreHeight);
-
-    coreHeight = 42;
+    // Sync initial list
 
     await synchronizeMasternodeIdentities(coreHeight);
+
+    // Second call
+
+    await synchronizeMasternodeIdentities(coreHeight + 42);
 
     expect(coreRpcClientMock.protx).to.have.been.calledOnceWithExactly('diff', 1, 3);
   });
@@ -334,7 +316,7 @@ describe('synchronizeMasternodeIdentitiesFactory', () => {
       { mnList: smlFixture.concat(newSmlFixture) },
     );
 
-    // Mock Core
+    // Mock fetchTransaction
 
     const transaction3 = {
       extraPayload: {
