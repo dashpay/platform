f9c57743b676ea00b1ce06f2a329b31da66f0662
Ivan Shumkov <ivan@shumkov.ru>
2022-03-14 22:17:26 +0700

    chore: remove hardcoded nodes

--- packages/platform-test-suite/lib/test/createFaucetClient.js
+++ packages/platform-test-suite/lib/test/createFaucetClient.js
@@ -14,20 +14,7 @@ function createFaucetClient() {
   const seeds = getDAPISeeds();
 
   const clientOpts = {
-    // seeds,
-    dapiAddresses: [
-      '34.210.237.116',
-      '54.69.65.231',
-      // '54.185.90.95',
-      // '54.186.234.0',
-      // '35.87.212.139',
-      // '34.212.52.44',
-      '34.217.47.197',
-      '34.220.79.131',
-      '18.237.212.176',
-      '54.188.17.188',
-      '34.210.1.159',
-    ],
+    seeds,
     network: process.env.NETWORK,
     apps: {
       dpns: {
