cac0f09e2176db4dd6211f132b6be3959eed0f69
strophy <badfunkstripe@gmail.com>
2022-03-21 20:32:50 +1100

    chore: use multiarch protoc package

--- packages/dapi-grpc/scripts/build.sh
+++ packages/dapi-grpc/scripts/build.sh
@@ -91,7 +91,7 @@ rm -rf "$CORE_JAVA_OUT_PATH/*"
 docker run -v "$CORE_PROTO_PATH:$CORE_PROTO_PATH" \
            -v "$CORE_JAVA_OUT_PATH:$CORE_JAVA_OUT_PATH" \
            --rm \
-           znly/protoc \
+           strophy/protoc \
            --plugin=protoc-gen-grpc=/usr/bin/protoc-gen-grpc-java \
            --grpc-java_out="$CORE_JAVA_OUT_PATH" \
            --proto_path="$CORE_PROTO_PATH" \
@@ -107,7 +107,7 @@ rm -rf "$PLATFORM_JAVA_OUT_PATH/*"
 docker run -v "$PLATFORM_PROTO_PATH:$PLATFORM_PROTO_PATH" \
            -v "$PLATFORM_JAVA_OUT_PATH:$PLATFORM_JAVA_OUT_PATH" \
            --rm \
-           znly/protoc \
+           strophy/protoc \
            --plugin=protoc-gen-grpc=/usr/bin/protoc-gen-grpc-java \
            --grpc-java_out="$PLATFORM_JAVA_OUT_PATH" \
            --proto_path="$PLATFORM_PROTO_PATH" \
@@ -123,7 +123,7 @@ rm -rf "$CORE_OBJ_C_OUT_PATH/*"
 docker run -v "$CORE_PROTO_PATH:$CORE_PROTO_PATH" \
            -v "$CORE_OBJ_C_OUT_PATH:$CORE_OBJ_C_OUT_PATH" \
            --rm \
-           znly/protoc \
+           strophy/protoc \
            --plugin=protoc-gen-grpc=/usr/bin/grpc_objective_c_plugin \
            --objc_out="$CORE_OBJ_C_OUT_PATH" \
            --grpc_out="$CORE_OBJ_C_OUT_PATH" \
@@ -140,7 +140,7 @@ rm -rf "$PLATFORM_OBJ_C_OUT_PATH/*"
 docker run -v "$PLATFORM_PROTO_PATH:$PLATFORM_PROTO_PATH" \
            -v "$PLATFORM_OBJ_C_OUT_PATH:$PLATFORM_OBJ_C_OUT_PATH" \
            --rm \
-           znly/protoc \
+           strophy/protoc \
            --plugin=protoc-gen-grpc=/usr/bin/grpc_objective_c_plugin \
            --objc_out="$PLATFORM_OBJ_C_OUT_PATH" \
            --grpc_out="$PLATFORM_OBJ_C_OUT_PATH" \
@@ -157,7 +157,7 @@ rm -rf "$CORE_PYTHON_OUT_PATH/*"
 docker run -v "$CORE_PROTO_PATH:$CORE_PROTO_PATH" \
            -v "$CORE_PYTHON_OUT_PATH:$CORE_PYTHON_OUT_PATH" \
            --rm \
-           znly/protoc \
+           strophy/protoc \
            --plugin=protoc-gen-grpc=/usr/bin/grpc_python_plugin \
            --python_out="$CORE_PYTHON_OUT_PATH" \
            --grpc_out="$CORE_PYTHON_OUT_PATH" \
@@ -174,7 +174,7 @@ rm -rf "$PLATFORM_PYTHON_OUT_PATH/*"
 docker run -v "$PLATFORM_PROTO_PATH:$PLATFORM_PROTO_PATH" \
           -v "$PLATFORM_PYTHON_OUT_PATH:$PLATFORM_PYTHON_OUT_PATH" \
           --rm \
-          znly/protoc \
+          strophy/protoc \
           --plugin=protoc-gen-grpc=/usr/bin/grpc_python_plugin \
           --python_out="$PLATFORM_PYTHON_OUT_PATH" \
           --grpc_out="$PLATFORM_PYTHON_OUT_PATH" \
