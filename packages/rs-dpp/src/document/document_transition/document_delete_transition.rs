b7c553a2d34529e363645b1ebb9135e8214db2e3
Pawel Iwan <pawel.iwan@dash.org>
2022-03-28 10:47:08 +0200

    add document delete transition

---
+++ packages/rs-dpp/src/document/document_transition/document_delete_transition.rs
@@ -0,0 +1,61 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value as JsonValue;
+
+use crate::{data_contract::DataContract, errors::ProtocolError};
+
+use super::{Action, DocumentBaseTransition};
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct DocumentDeleteTransition {
+    #[serde(flatten)]
+    base: DocumentBaseTransition,
+}
+
+impl DocumentDeleteTransition {
+    pub fn from_raw_document(
+        raw_transition: JsonValue,
+        data_contract: DataContract,
+    ) -> Result<Self, ProtocolError> {
+        let mut base = DocumentBaseTransition::from_raw_document(raw_transition, data_contract)?;
+        base.action = Action::Delete;
+        Ok(DocumentDeleteTransition { base })
+    }
+
+    pub fn to_object(&self) -> Result<JsonValue, ProtocolError> {
+        self.base.to_object()
+    }
+
+    pub fn to_json(&self) -> Result<JsonValue, ProtocolError> {
+        let value = serde_json::to_value(&self)?;
+        Ok(value)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::document::document_transition::Action;
+    use log::trace;
+
+    #[test]
+    fn test_deserialize_serialize_to_json() {
+        let transition_json = r#"{
+					"$id": "6oCKUeLVgjr7VZCyn1LdGbrepqKLmoabaff5WQqyTKYP",
+					"$type": "note",
+					"$action": 3,
+					"$dataContractId": "5wpZAEWndYcTeuwZpkmSa8s49cHXU5q2DhdibesxFSu8"
+				}"#;
+
+        let cdt: DocumentDeleteTransition =
+            serde_json::from_str(transition_json).expect("no error");
+        trace!("the parsed Document Delete Transition is {:#?}", cdt);
+
+        assert_eq!(cdt.base.action, Action::Delete);
+        assert_eq!(cdt.base.transition_type, "note");
+
+        let mut json_no_whitespace = transition_json.to_string();
+        json_no_whitespace.retain(|v| !v.is_whitespace());
+
+        assert_eq!(cdt.to_json().unwrap().to_string(), json_no_whitespace);
+    }
+}
