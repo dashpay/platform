d13ad0f05c9adc2e0aa9885a18cf25fc5b27014b
Pawel Iwan <pawel.iwan@dash.org>
2022-03-28 10:48:42 +0200

    update document transtion modules exports

--- packages/rs-dpp/src/document/document_transition/mod.rs
+++ packages/rs-dpp/src/document/document_transition/mod.rs
@@ -1,2 +1,11 @@
 mod document_base_transition;
 pub use document_base_transition::*;
+
+mod document_create_transition;
+pub use document_create_transition::*;
+
+mod document_delete_transition;
+pub use document_delete_transition::*;
+
+mod document_replace_transition;
+pub use document_replace_transition::*;
