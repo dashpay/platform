5cad760c151f55a603f3c32e3f1b6b505600759c
Pawel Iwan <pawel.iwan@dash.org>
2022-03-28 10:45:48 +0200

    add support se/de support for u8 enums

--- packages/rs-dpp/src/document/document_transition/document_base_transition.rs
+++ packages/rs-dpp/src/document/document_transition/document_base_transition.rs
@@ -3,82 +3,12 @@ use crate::{
 };
 use anyhow::bail;
 use serde::{Deserialize, Serialize};
+use serde_repr::*;
 use std::convert::TryFrom;
 
 pub use serde_json::Value as JsonValue;
 
-/**
- * @typedef {Object} RawDocumentTransition
- * @property {Buffer} $id
- * @property {string} $type
- * @property {number} $action
- * @property {Buffer} $dataContractId
- */
-
-/**
- * @typedef {Object} JsonDocumentTransition
- * @property {string} $id
- * @property {string} $type
- * @property {number} $action
- * @property {string} $dataContractId
- */
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct DocumentBaseTransition {
-    #[serde(rename = "$id")]
-    pub id: Identifier,
-    #[serde(rename = "$type")]
-    pub transition_type: String,
-    #[serde(rename = "$action")]
-    pub action: Action,
-    #[serde(rename = "$dataContractId")]
-    pub data_contract_id: Identifier,
-    #[serde(skip)]
-    pub data_contract: DataContract,
-}
-
-impl DocumentBaseTransition {
-    pub fn from_raw_document(
-        mut raw_value: JsonValue,
-        data_contract: DataContract,
-    ) -> Result<DocumentBaseTransition, ProtocolError> {
-        if let JsonValue::Object(ref mut o) = &mut raw_value {
-            deserializer::parse_identities(o, &["$id", "$dataContractId"])?;
-        } else {
-            return Err("the raw_value is not an Json Value object".into());
-        }
-        let mut document: DocumentBaseTransition = serde_json::from_value(raw_value)?;
-        document.data_contract = data_contract;
-
-        Ok(document)
-    }
-
-    pub fn to_object(&self) -> Result<JsonValue, ProtocolError> {
-        let mut json_object = serde_json::to_value(&self)?;
-        if !json_object.is_object() {
-            return Err("the Data Contract isn't a JSON Value Object".into());
-        }
-
-        let id = self.id.to_vec();
-        let data_contract_id = self.data_contract_id.to_vec();
-        if let JsonValue::Object(ref mut o) = json_object {
-            o.insert(String::from("$id"), JsonValue::Array(id));
-            o.insert(
-                String::from("$dataContractId"),
-                JsonValue::Array(data_contract_id),
-            );
-        }
-        Ok(json_object)
-    }
-
-    pub fn to_json(&self) -> Result<JsonValue, ProtocolError> {
-        let value = serde_json::to_value(&self)?;
-        Ok(value)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone, Copy)]
+#[derive(Debug, Serialize_repr, Deserialize_repr, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Action {
     Create = 0,
@@ -87,8 +17,14 @@ pub enum Action {
     Delete = 3,
 }
 
+impl Default for Action {
+    fn default() -> Action {
+        Action::Create
+    }
+}
+
 impl TryFrom<&str> for Action {
-    // TODO
+    // TODO  - use error from https://github.com/dashevo/rust-dpp/pull/6
     type Error = anyhow::Error;
 
     fn try_from(name: &str) -> Result<Action, Self::Error> {
@@ -107,5 +43,87 @@ impl TryFrom<String> for Action {
     type Error = anyhow::Error;
     fn try_from(name: String) -> Result<Action, Self::Error> {
         Action::try_from(name.as_str())
+    }
+}
+
+/**
+ * @typedef {Object} RawDocumentTransition
+ * @property {Buffer} $id
+ * @property {string} $type
+ * @property {number} $action
+ * @property {Buffer} $dataContractId
+ */
+
+/**
+ * @typedef {Object} JsonDocumentTransition
+ * @property {string} $id
+ * @property {string} $type
+ * @property {number} $action
+ * @property {string} $dataContractId
+ */
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct DocumentBaseTransition {
+    #[serde(rename = "$id")]
+    /// The document ID
+    pub id: Identifier,
+    #[serde(rename = "$type")]
+    /// Name of document type found int the data contract associated with the `data_contract_id`
+    pub transition_type: String,
+    #[serde(rename = "$action")]
+    /// Action the platform should take for the associated document
+    pub action: Action,
+    #[serde(rename = "$dataContractId")]
+    /// Data contract ID generated from the data contract's `owner_id` and `entropy`
+    pub data_contract_id: Identifier,
+    #[serde(skip)]
+    pub data_contract: DataContract,
+}
+
+impl DocumentBaseTransition {
+    pub fn from_raw_document(
+        mut raw_transition: JsonValue,
+        data_contract: DataContract,
+    ) -> Result<DocumentBaseTransition, ProtocolError> {
+        Self::identifiers_to_strings(&mut raw_transition)?;
+        let mut document: DocumentBaseTransition = serde_json::from_value(raw_transition)?;
+        document.data_contract = data_contract;
+
+        Ok(document)
+    }
+
+    pub fn to_object(&self) -> Result<JsonValue, ProtocolError> {
+        let mut object = serde_json::to_value(&self)?;
+        if !object.is_object() {
+            return Err("The Document Base Transition isn't an Object".into());
+        }
+
+        let id = self.id.to_vec();
+        let data_contract_id = self.data_contract_id.to_vec();
+        if let JsonValue::Object(ref mut o) = object {
+            o.insert(String::from("$id"), JsonValue::Array(id));
+            o.insert(
+                String::from("$dataContractId"),
+                JsonValue::Array(data_contract_id),
+            );
+        }
+        Ok(object)
+    }
+
+    pub fn to_json(&self) -> Result<JsonValue, ProtocolError> {
+        let value = serde_json::to_value(&self)?;
+        Ok(value)
+    }
+
+    pub fn identifiers_to_strings(
+        raw_document_transition: &mut JsonValue,
+    ) -> Result<(), ProtocolError> {
+        if let JsonValue::Object(ref mut o) = raw_document_transition {
+            deserializer::parse_identities(o, &["$id", "$dataContractId"])?;
+        } else {
+            return Err("The raw_transition isn't an Object".into());
+        }
+        Ok(())
     }
 }
