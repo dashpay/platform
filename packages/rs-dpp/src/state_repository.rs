aa860f55de859df77e80cdb1e94bbf09fe2880fe
Pawel Iwan <pawel.iwan@dash.org>
2022-04-05 14:21:16 +0200

    change the retuned object to serialized form in state repository

--- packages/rs-dpp/src/state_repository.rs
+++ packages/rs-dpp/src/state_repository.rs
@@ -2,7 +2,10 @@ use async_trait::async_trait;
 use serde_json::Value as JsonValue;
 
 use crate::{
-    data_contract::DataContract, document::Document, identifier::Identifier, identity::Identity,
+    data_contract::DataContract,
+    document::Document,
+    identifier::Identifier,
+    identity::{Identity, JsonIdentityPublicKey},
     mocks,
 };
 
@@ -11,7 +14,7 @@ use anyhow::Result as AnyResult;
 #[async_trait]
 pub trait StateRepositoryLike {
     /// Fetch the Data Contract by ID
-    async fn fetch_data_contract(data_contract_id: &Identifier) -> AnyResult<DataContract>;
+    async fn fetch_data_contract(data_contract_id: &Identifier) -> AnyResult<JsonValue>;
 
     /// Store Data Contract
     async fn store_data_contract(data_contract: DataContract) -> AnyResult<()>;
@@ -31,21 +34,11 @@ pub trait StateRepositoryLike {
         data_contract_type: &str,
         document_id: &Identifier,
     ) -> AnyResult<()>;
-    /// Fetch transaction by ID
-    /**
-     * Fetch transaction by ID
-     *
-     * @async
-     * @method
-     * @name StateRepository#fetchTransaction
-     * @param {string} id
-     * @returns {Promise<Object|null>}
-     */
-    // TODO shouldn't we use ['dashcore::Transaction'] instead?
+
     async fn fetch_transaction(id: &str) -> AnyResult<JsonValue>;
 
     /// Fetch Identity by ID
-    async fn fetch_identity(id: &Identifier) -> AnyResult<Identity>;
+    async fn fetch_identity(id: &Identifier) -> AnyResult<JsonValue>;
 
     /// Store Public Key hashes and Identity id pair
     async fn store_identity_public_key_hashes(
@@ -59,7 +52,7 @@ pub trait StateRepositoryLike {
     ) -> AnyResult<Vec<Identifier>>;
 
     /// Fetch latest platform block header
-    async fn fetch_latest_platform_block_header() -> AnyResult<mocks::IHeader>;
+    async fn fetch_latest_platform_block_header() -> AnyResult<JsonValue>;
 
     /// Verify Instant Lock
     async fn verify_instant_lock(instant_lock: &mocks::InstantLock) -> AnyResult<bool>;
@@ -75,5 +68,5 @@ pub trait StateRepositoryLike {
     ) -> AnyResult<()>;
 
     /// Fetch Simplified Masternode List Store
-    async fn fetch_sml_store() -> AnyResult<mocks::SimplifiedSMListStore>;
+    async fn fetch_sml_store() -> AnyResult<JsonValue>;
 }
