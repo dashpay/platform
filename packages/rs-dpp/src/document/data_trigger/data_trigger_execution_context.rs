501dbd2cc9c9a2a50518ab1e82bcfb50354fd8e4
Pawel Iwan <pawel.iwan@dash.org>
2022-04-06 10:27:43 +0200

    add data trigger

---
+++ packages/rs-dpp/src/document/data_trigger/data_trigger_execution_context.rs
@@ -0,0 +1,26 @@
+use crate::{
+    prelude::{DataContract, Identifier},
+    state_repository::StateRepositoryLike,
+};
+
+// TODO consider using the reference to the StateRepository instance
+#[derive(Clone, Debug)]
+pub struct DataTriggerExecutionContext<SR> {
+    pub state_repository: SR,
+    pub owner_id: Identifier,
+    pub data_contract: DataContract,
+}
+
+impl<SR> DataTriggerExecutionContext<SR>
+where
+    SR: StateRepositoryLike,
+{
+    /// Creates a new instance of DataTriggerExecutionContext
+    pub fn new(state_repository: SR, owner_id: Identifier, data_contract: DataContract) -> Self {
+        DataTriggerExecutionContext {
+            state_repository,
+            owner_id,
+            data_contract,
+        }
+    }
+}
