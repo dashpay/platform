8068d0b762933ced73fee1f347f27c0ad7734131
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2022-04-07 17:29:52 +0700

    pass public keys in the test instead of raw ones

--- packages/js-dpp/test/integration/identity/validation/validateIdentityFactory.spec.js
+++ packages/js-dpp/test/integration/identity/validation/validateIdentityFactory.spec.js
@@ -371,7 +371,7 @@ describe('validateIdentityFactory', () => {
   it('should return valid result if an identity model is valid', () => {
     const result = validateIdentity(identity);
 
-    expect(validatePublicKeysMock).to.be.calledOnceWithExactly(rawIdentity.publicKeys);
+    expect(validatePublicKeysMock).to.be.calledOnceWithExactly(identity.publicKeys);
 
     expect(result.isValid()).to.be.true();
   });
