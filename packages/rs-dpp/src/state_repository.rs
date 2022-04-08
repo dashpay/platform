e57f353a01b25fd18bcda100d3c410daadb66c8e
Pawel Iwan <pawel.iwan@dash.org>
2022-04-08 07:52:51 +0200

    add self receiver to state repository

--- packages/rs-dpp/src/state_repository.rs
+++ packages/rs-dpp/src/state_repository.rs
@@ -14,59 +14,65 @@ use anyhow::Result as AnyResult;
 #[async_trait]
 pub trait StateRepositoryLike {
     /// Fetch the Data Contract by ID
-    async fn fetch_data_contract(data_contract_id: &Identifier) -> AnyResult<JsonValue>;
+    async fn fetch_data_contract(&self, data_contract_id: &Identifier) -> AnyResult<JsonValue>;
 
     /// Store Data Contract
-    async fn store_data_contract(data_contract: DataContract) -> AnyResult<()>;
+    async fn store_data_contract(&self, data_contract: DataContract) -> AnyResult<()>;
 
     /// Fetch Documents by Data Contract Id and type
     async fn fetch_documents(
+        &self,
         contract_id: &Identifier,
         data_contract_type: &str,
     ) -> AnyResult<Vec<Document>>;
 
     /// Store Document
-    async fn store_document(document: &Document) -> AnyResult<()>;
+    async fn store_document(&self, document: &Document) -> AnyResult<()>;
 
     /// Remove Document
     async fn remove_document(
+        &self,
         contract_id: &Identifier,
         data_contract_type: &str,
         document_id: &Identifier,
     ) -> AnyResult<()>;
 
-    async fn fetch_transaction(id: &str) -> AnyResult<JsonValue>;
+    async fn fetch_transaction(&self, id: &str) -> AnyResult<JsonValue>;
 
     /// Fetch Identity by ID
-    async fn fetch_identity(id: &Identifier) -> AnyResult<JsonValue>;
+    async fn fetch_identity(&self, id: &Identifier) -> AnyResult<JsonValue>;
 
     /// Store Public Key hashes and Identity id pair
     async fn store_identity_public_key_hashes(
+        &self,
         identity_id: &Identifier,
         public_key_hashes: Vec<Vec<u8>>,
     ) -> AnyResult<()>;
 
     /// Fetch Identity Ids by Public Key hashes
     async fn fetch_identity_by_public_key_hashes(
+        &self,
         public_key_hashed: &[&[u8]],
     ) -> AnyResult<Vec<Identifier>>;
 
     /// Fetch latest platform block header
-    async fn fetch_latest_platform_block_header() -> AnyResult<JsonValue>;
+    async fn fetch_latest_platform_block_header(&self) -> AnyResult<JsonValue>;
 
     /// Verify Instant Lock
-    async fn verify_instant_lock(instant_lock: &mocks::InstantLock) -> AnyResult<bool>;
+    async fn verify_instant_lock(&self, instant_lock: &mocks::InstantLock) -> AnyResult<bool>;
 
     /// Check if AssetLock Transaction outPoint exists in spent list
     async fn is_asset_lock_transaction_out_point_already_used(
+        &self,
         out_point_buffer: &[u8],
     ) -> AnyResult<bool>;
 
     /// Store AssetLock Transaction outPoint in spent list
     async fn mark_asset_lock_transaction_out_point_as_used(
+        &self,
         out_point_buffer: &[u8],
     ) -> AnyResult<()>;
 
     /// Fetch Simplified Masternode List Store
-    async fn fetch_sml_store() -> AnyResult<JsonValue>;
+    async fn fetch_sml_store(&self) -> AnyResult<JsonValue>;
 }
