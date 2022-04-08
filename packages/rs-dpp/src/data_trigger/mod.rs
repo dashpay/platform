8c7c184bd968dfd733fb5556d94b561ac9e05527
Pawel Iwan <pawel.iwan@dash.org>
2022-04-08 08:59:45 +0200

    add dashpay data trigger

--- packages/rs-dpp/src/data_trigger/mod.rs
+++ packages/rs-dpp/src/data_trigger/mod.rs
@@ -2,12 +2,12 @@ mod data_trigger_execution_context;
 use std::future::Future;
 use std::pin::Pin;
 
-pub use data_trigger_execution_context::*;
-
+pub mod dashpay_data_triggers;
 mod data_trigger_execution_result;
-pub use data_trigger_execution_result::*;
-
 mod reject_data_trigger;
+
+pub use data_trigger_execution_context::*;
+pub use data_trigger_execution_result::*;
 pub use reject_data_trigger::*;
 
 use crate::{
@@ -21,18 +21,24 @@ pub type Trigger<SR> =
     fn(
         &DocumentTransition,
         &DataTriggerExecutionContext<SR>,
-        &Identifier,
+        Option<&Identifier>,
     ) -> Pin<Box<dyn Future<Output = Result<DataTriggerExecutionResult, anyhow::Error>>>>;
 
-pub struct DataTrigger<SR> {
+pub struct DataTrigger<SR>
+where
+    SR: StateRepositoryLike,
+{
     pub data_contract_id: Identifier,
     pub document_type: String,
     pub trigger: Trigger<SR>,
     pub transition_action: Action,
-    pub top_level_identity: Identifier,
+    pub top_level_identity: Option<Identifier>,
 }
 
-impl<SR> DataTrigger<SR> {
+impl<SR> DataTrigger<SR>
+where
+    SR: StateRepositoryLike,
+{
     /// Check this trigger is matching for specified data
     pub fn is_matching_trigger_for_data(
         &self,
@@ -56,8 +62,12 @@ impl<SR> DataTrigger<SR> {
         // not sure what the function may contain
         let mut result = DataTriggerExecutionResult::default();
 
-        let execution_result =
-            (self.trigger)(document_transition, context, &self.top_level_identity).await;
+        let execution_result = (self.trigger)(
+            document_transition,
+            context,
+            self.top_level_identity.as_ref(),
+        )
+        .await;
 
         if let Err(err) = execution_result {
             let consensus_error = DataTriggerError::DataTriggerExecutionError {
@@ -65,6 +75,8 @@ impl<SR> DataTrigger<SR> {
                 document_transition_id: get_from_transition!(document_transition, id).clone(),
                 message: err.to_string(),
                 execution_error: err,
+                document_transition: None,
+                owner_id: None,
             };
             result.add_error(consensus_error.into());
             return result;
