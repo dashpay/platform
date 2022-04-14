0469d773ad913aba68ffab8adb1c9718b66678ce
Pawel Iwan <pawel.iwan@dash.org>
2022-04-14 10:19:09 +0200

    removing optional error codes

--- packages/rs-dpp/src/errors/codes.rs
+++ packages/rs-dpp/src/errors/codes.rs
@@ -1,34 +1,34 @@
 use super::{abstract_state_error::StateError, DataTriggerError};
 pub trait ErrorWithCode {
     // Returns the Error Code
-    fn get_code(&self) -> Option<u32>;
+    fn get_code(&self) -> u32;
 }
 
 impl ErrorWithCode for StateError {
-    fn get_code(&self) -> Option<u32> {
+    fn get_code(&self) -> u32 {
         match *self {
             // Document
-            Self::DocumentAlreadyPresentError { .. } => Some(4004),
-            Self::DocumentNotFoundError { .. } => Some(4005),
-            Self::DocumentOwnerMismatchError { .. } => Some(4006),
-            Self::DocumentTimestampMismatchError { .. } => Some(4007),
-            Self::DocumentTimestampWindowViolationError { .. } => Some(4008),
-            Self::DuplicateUniqueIndexError { .. } => Some(4009),
-            Self::InvalidDocumentRevisionError { .. } => Some(4010),
+            Self::DocumentAlreadyPresentError { .. } => 4004,
+            Self::DocumentNotFoundError { .. } => 4005,
+            Self::DocumentOwnerMismatchError { .. } => 4006,
+            Self::DocumentTimestampMismatchError { .. } => 4007,
+            Self::DocumentTimestampWindowViolationError { .. } => 4008,
+            Self::DuplicateUniqueIndexError { .. } => 4009,
+            Self::InvalidDocumentRevisionError { .. } => 4010,
             // Data contract
-            Self::DataContractAlreadyPresentError { .. } => Some(4000),
+            Self::DataContractAlreadyPresentError { .. } => 4000,
             Self::DataTriggerError(ref e) => e.get_code(),
         }
     }
 }
 
 impl ErrorWithCode for DataTriggerError {
-    fn get_code(&self) -> Option<u32> {
+    fn get_code(&self) -> u32 {
         match *self {
             // Data Contract - Data Trigger
-            Self::DataTriggerConditionError { .. } => Some(4001),
-            Self::DataTriggerExecutionError { .. } => Some(4002),
-            Self::DataTriggerInvalidResultError { .. } => Some(4003),
+            Self::DataTriggerConditionError { .. } => 4001,
+            Self::DataTriggerExecutionError { .. } => 4002,
+            Self::DataTriggerInvalidResultError { .. } => 4003,
         }
     }
 }
