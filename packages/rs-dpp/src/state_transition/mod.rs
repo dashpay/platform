901651708c8195e8296f5b352bb5590fd224551d
Pawel Iwan <pawel.iwan@dash.org>
2022-04-15 07:28:10 +0200

    add state transition types

---
+++ packages/rs-dpp/src/state_transition/mod.rs
@@ -0,0 +1,2 @@
+mod state_transition_types;
+pub use state_transition_types::*;
---
+++ packages/rs-dpp/src/state_transition/state_transition_types.rs
@@ -0,0 +1,7 @@
+pub enum Types {
+    DataContractCreate = 0,
+    DocumentsBatch = 1,
+    IdentityCreate = 2,
+    IdentityTopUp = 3,
+    DataContractUpdate = 4,
+}
