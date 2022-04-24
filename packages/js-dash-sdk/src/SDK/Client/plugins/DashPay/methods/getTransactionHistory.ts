fd65543680b966a26c655b8b0e50efd63c38a26f
Alex Werner <obusco@gmail.com>
2022-04-25 00:58:03 +0200

    feat: drafted get transaction history

--- packages/js-dash-sdk/src/SDK/Client/plugins/DashPay/methods/getTransactionHistory.ts
+++ packages/js-dash-sdk/src/SDK/Client/plugins/DashPay/methods/getTransactionHistory.ts
@@ -1,5 +1,37 @@
 export async function getTransactionHistory(this: any){
   const account = await this.platform.client.getWalletAccount();
   const transactionHistory = account.getTransactionHistory();
+
+  const { contacts } = this;
+
+  contacts.forEach((contact)=>{
+    const receivingAddresses = contact.getWatchedAddresses('receiving');
+    const sendingAddresses = contact.getWatchedAddresses('sending');
+
+    // TODO: Not for prod, only for draft purpose.
+    transactionHistory.forEach((transactionItem)=>{
+      transactionItem.from.forEach((fromItem)=>{
+        if(receivingAddresses.includes(fromItem.address)){
+          transactionItem.addressType = 'external'
+          transactionItem.contact = contact;
+        }
+        if(sendingAddresses.includes(fromItem.address)){
+          transactionItem.addressType = 'contact'
+          transactionItem.contact = contact;
+        }
+      })
+      transactionItem.to.forEach((toItem)=>{
+        if(receivingAddresses.includes(toItem.address)){
+          transactionItem.addressType = 'contact'
+          transactionItem.contact = contact;
+        }
+        if(sendingAddresses.includes(toItem.address)){
+          transactionItem.addressType = 'contact'
+          transactionItem.contact = contact;
+        }
+      })
+    })
+  });
+
   return transactionHistory;
 }
