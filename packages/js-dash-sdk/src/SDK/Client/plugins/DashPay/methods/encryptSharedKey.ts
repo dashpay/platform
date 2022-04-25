93e73ea1a0c81cc35bae6a2708ce51da2e9dcdfd
Alex Werner <obusco@gmail.com>
2022-04-25 03:45:45 +0200

    feat: replace secp256k1-native for secp256k1
    Browser support

--- packages/js-dash-sdk/src/SDK/Client/plugins/DashPay/methods/encryptSharedKey.ts
+++ packages/js-dash-sdk/src/SDK/Client/plugins/DashPay/methods/encryptSharedKey.ts
@@ -1,19 +1,12 @@
-import secp256k1 from 'secp256k1-native';
+import secp256k1 from 'secp256k1/elliptic';
 
 export function encryptSharedKey(senderPrivateKeyBuffer, receiverPublicKeyBuffer){
-  const ctx = secp256k1.secp256k1_context_create(secp256k1.secp256k1_context_SIGN);
-
-  const sharedSecrect = Buffer.alloc(32);
-
   const point = receiverPublicKeyBuffer;
-
   const scalar = senderPrivateKeyBuffer;
 
-  const pubkey64 = Buffer.alloc(64);
-
-  secp256k1.secp256k1_ec_pubkey_parse(ctx, pubkey64, point);
-
-  secp256k1.secp256k1_ecdh(ctx, sharedSecrect, pubkey64, scalar, null);
-
-  return sharedSecrect.toString('hex');
+  const sharedSecret = secp256k1.ecdh(
+    point,
+    scalar,
+  );
+  return Buffer.from(sharedSecret).toString('hex');
 };
