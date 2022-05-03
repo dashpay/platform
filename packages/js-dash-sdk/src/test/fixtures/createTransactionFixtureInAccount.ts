3b18b74bb156fed4624db85d3f2b0e86bfe5a28e
Ivan Shumkov <ivan@shumkov.ru>
2022-05-03 18:47:04 +0700

    test: increase balance amount in sdk tests

--- packages/js-dash-sdk/src/test/fixtures/createTransactionFixtureInAccount.ts
+++ packages/js-dash-sdk/src/test/fixtures/createTransactionFixtureInAccount.ts
@@ -4,14 +4,16 @@ export async function createTransactionInAccount(account) {
     // add fake tx to the wallet so it will be able to create transactions
     const walletTransaction = new Transaction(undefined)
         .from([{
-            amount: 150000,
+            amount: 1500000,
             script: '76a914f9996443a7d5e2694560f8715e5e8fe602133c6088ac',
             outputIndex: 0,
             txid: new Transaction(undefined).hash,
         }])
-        .to(account.getAddress(10).address, 100000);
+        .to(account.getAddress(10).address, 1000000);
 
     await account.importTransactions([[walletTransaction.serialize(true)]]);
+
+    console.log(account.getTotalBalance());
 
     return walletTransaction;
 }
