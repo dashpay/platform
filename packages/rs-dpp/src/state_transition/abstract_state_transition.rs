91c423b9c71775b363c07b277d0d92ce11840b5b
Pawel Iwan <pawel.iwan@dash.org>
2022-05-03 09:43:31 +0200

    stateTransition with basic methods - wip

---
+++ packages/rs-dpp/src/state_transition/abstract_state_transition.rs
@@ -0,0 +1,84 @@
+use crate::{
+    prelude::ProtocolError,
+    util::{hash, json_value::ReplaceWith, serializer},
+};
+use serde_json::Value as JsonValue;
+
+use serde::{Deserialize, Serialize};
+
+use super::StateTransitionType;
+use crate::util::json_value::JsonValueExt;
+
+pub const DOCUMENT_TRANSITION_TYPES: [StateTransitionType; 1] =
+    [StateTransitionType::DocumentsBatch];
+
+pub const IDENTITY_TRANSITION_TYPE: [StateTransitionType; 2] = [
+    StateTransitionType::IdentityCreate,
+    StateTransitionType::IdentityTopUp,
+];
+
+pub const DATA_CONTRACT_TRANSITION_TYPES: [StateTransitionType; 2] = [
+    StateTransitionType::DataContractCreate,
+    StateTransitionType::DataContractUpdate,
+];
+
+const PROPERTY_SIGNATURE: &str = "signature";
+const PROPERTY_PROTOCOL_VERSION: &str = "protocolVersion";
+
+/**
+ * @typedef RawStateTransition
+ * @property {number} protocolVersion
+ * @property {number} type
+ * @property {Buffer} [signature]
+ */
+
+/**
+ * @typedef JsonStateTransition
+ * @property {number} protocolVersion
+ * @property {number} type
+ * @property {string} [signature]
+ */
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct StateTransitionBase {
+    pub protocol_version: u32,
+    pub signature: Vec<u8>,
+    pub transition_type: StateTransitionType,
+}
+
+impl StateTransitionBase {
+    /// Object is an [`serde_json::Value`] instance that preserves the `Vec<u8>` representation
+    /// for Identifiers and binary data
+    fn to_object(&self, skip_signature: bool) -> Result<JsonValue, ProtocolError> {
+        let mut json_value: JsonValue = serde_json::to_value(self)?;
+        if skip_signature {
+            if let JsonValue::Object(ref mut o) = json_value {
+                o.remove(PROPERTY_SIGNATURE);
+            }
+        }
+        Ok(json_value)
+    }
+
+    /// Object is an [`serde_json::Value`] instance that replaces the binary data with
+    ///  - base58 string for Identifiers
+    ///  - base64 string for other binary data
+    fn to_json(&self) -> Result<JsonValue, ProtocolError> {
+        let mut json_value: JsonValue = serde_json::to_value(self)?;
+        json_value.replace_binary_paths([PROPERTY_SIGNATURE], ReplaceWith::Base64)?;
+        Ok(json_value)
+    }
+
+    // returns the byte-array representation. It is prefixed by 4 bytes of ProtocolVersion and encoded by CBOR
+    fn to_buffer(&self, skip_signature: bool) -> Result<Vec<u8>, ProtocolError> {
+        let mut json_value = self.to_object(skip_signature)?;
+        let protocol_version = json_value.remove_u32(PROPERTY_PROTOCOL_VERSION)?;
+
+        serializer::value_to_cbor(json_value, Some(protocol_version))
+    }
+
+    // hash function is applied to byte-array representation of structure
+    fn hash(&self) -> Result<Vec<u8>, ProtocolError> {
+        Ok(hash::hash(self.to_buffer(false)?))
+    }
+}
--- packages/rs-dpp/src/state_transition/mod.rs
+++ packages/rs-dpp/src/state_transition/mod.rs
@@ -1,2 +1,5 @@
 mod state_transition_types;
 pub use state_transition_types::*;
+
+mod abstract_state_transition;
+pub use abstract_state_transition::*;
