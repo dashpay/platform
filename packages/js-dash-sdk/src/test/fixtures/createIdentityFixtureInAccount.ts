d3c2f84c55cd3e5cf7dde72125d48b05e26db9ea
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2022-05-10 16:18:02 +0300

    feat: Identity master key can be used only to update identity

--- packages/js-dash-sdk/src/test/fixtures/createIdentityFixtureInAccount.ts
+++ packages/js-dash-sdk/src/test/fixtures/createIdentityFixtureInAccount.ts
@@ -5,15 +5,23 @@ const getIdentityFixture = require('@dashevo/dpp/lib/test/fixtures/getIdentityFi
 export function createIdentityFixtureInAccount(account) {
     const identityFixture = getIdentityFixture();
     const identityFixtureIndex = 0;
-    const { privateKey: identityPrivateKey } = account.identities.getIdentityHDKeyByIndex(identityFixtureIndex, 0);
+    const { privateKey: identityPrivateKeyMaster } = account.identities.getIdentityHDKeyByIndex(identityFixtureIndex, 0);
+    const { privateKey: identityPrivateKeyHigh } = account.identities.getIdentityHDKeyByIndex(identityFixtureIndex, 1);
 
     identityFixture.publicKeys[0] = new IdentityPublicKey({
         id: 0,
         type: IdentityPublicKey.TYPES.ECDSA_SECP256K1,
-        data: identityPrivateKey.toPublicKey().toBuffer(),
+        data: identityPrivateKeyMaster.toPublicKey().toBuffer(),
         purpose: IdentityPublicKey.PURPOSES.AUTHENTICATION,
         securityLevel: IdentityPublicKey.SECURITY_LEVELS.MASTER,
         readOnly: false,
+    }, {
+      id: 1,
+      type: IdentityPublicKey.TYPES.ECDSA_SECP256K1,
+      data: identityPrivateKeyHigh.toPublicKey().toBuffer(),
+      purpose: IdentityPublicKey.PURPOSES.AUTHENTICATION,
+      securityLevel: IdentityPublicKey.SECURITY_LEVELS.HIGH,
+      readOnly: false,
     });
 
     account.storage
