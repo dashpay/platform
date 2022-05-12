2aa5982c76b3fe7aedb541f303dbec8696fc6d22
Ivan Shumkov <ivan@shumkov.ru>
2022-05-12 18:26:19 +0700

    feat(dpp): calculate signature verification costs for fees (#387)

--- packages/js-dpp/lib/stateTransition/validation/validateStateTransitionIdentitySignatureFactory.js
+++ packages/js-dpp/lib/stateTransition/validation/validateStateTransitionIdentitySignatureFactory.js
@@ -2,6 +2,7 @@ const IdentityPublicKey = require('../../identity/IdentityPublicKey');
 const InvalidIdentityPublicKeyTypeError = require('../../errors/consensus/signature/InvalidIdentityPublicKeyTypeError');
 const InvalidStateTransitionSignatureError = require('../../errors/consensus/signature/InvalidStateTransitionSignatureError');
 const MissingPublicKeyError = require('../../errors/consensus/signature/MissingPublicKeyError');
+const SignatureVerificationOperation = require('../fee/operations/SignatureVerificationOperation');
 
 /**
  * Validate state transition signature
@@ -56,6 +57,10 @@ function validateStateTransitionIdentitySignatureFactory(
 
       return result;
     }
+
+    const operation = new SignatureVerificationOperation(publicKey.getType());
+
+    executionContext.addOperation(operation);
 
     const signatureIsValid = await stateTransition.verifySignature(publicKey);
 
