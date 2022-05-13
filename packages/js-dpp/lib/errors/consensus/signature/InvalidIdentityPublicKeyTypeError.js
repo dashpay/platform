58a71cc9dfb0d7e0ef89ad603a714ff36ea912e9
Ivan Shumkov <ivan@shumkov.ru>
2022-05-13 17:26:52 +0700

    feat: better error message

--- packages/js-dpp/lib/errors/consensus/signature/InvalidIdentityPublicKeyTypeError.js
+++ packages/js-dpp/lib/errors/consensus/signature/InvalidIdentityPublicKeyTypeError.js
@@ -6,7 +6,7 @@ class InvalidIdentityPublicKeyTypeError extends AbstractSignatureError {
    * @param {number} publicKeyType
    */
   constructor(publicKeyType) {
-    super('Invalid signature type');
+    super(`Unsupported signature type ${publicKeyType}. Please use type ECDSA (0) or BLS (1) keys to sign the state transition`);
 
     this.publicKeyType = publicKeyType;
   }
