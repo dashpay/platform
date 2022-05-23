a577a33bc972b39abb7730ab39bdaa2e9306c0f7
Pawel Iwan <pawel.iwan@dash.org>
2022-05-23 11:36:05 +0200

    tests for abstract identity signed

--- packages/rs-dpp/src/util/hash.rs
+++ packages/rs-dpp/src/util/hash.rs
@@ -1,5 +1,11 @@
 use sha2::{Digest, Sha256};
+use dashcore::hashes::{ripemd160, sha256, Hash};
 
 pub fn hash(payload: impl AsRef<[u8]>) -> Vec<u8> {
     Sha256::digest(Sha256::digest(payload)).to_vec()
+}
+
+
+pub fn ripemd160_sha256(data : &[u8]) -> Vec<u8> {
+    ripemd160::Hash::hash(&sha256::Hash::hash(data)).to_vec()
 }
