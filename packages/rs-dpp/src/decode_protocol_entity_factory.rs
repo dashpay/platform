3cbad42c09f5a10ae2a434f1d0dee86a69c896e4
Pawel Iwan <pawel.iwan@dash.org>
2022-05-27 07:27:06 +0200

    use static method for data contract factory

--- packages/rs-dpp/src/decode_protocol_entity_factory.rs
+++ packages/rs-dpp/src/decode_protocol_entity_factory.rs
@@ -6,12 +6,11 @@ use crate::{
     util::deserializer::get_protocol_version,
 };
 
-#[derive(Default)]
-pub struct DecodeProtocolEntityFactory {}
+#[derive(Default, Clone, Copy)]
+pub struct DecodeProtocolEntity {}
 
-impl DecodeProtocolEntityFactory {
+impl DecodeProtocolEntity {
     pub fn decode_protocol_entity(
-        &self,
         buffer: impl AsRef<[u8]>,
     ) -> Result<(u32, JsonValue), ProtocolError> {
         let (protocol_bytes, document_bytes) = buffer.as_ref().split_at(4);
