d5d82f567098576b6f29905f6bfb70ddccb7f605
pshenmic <pshenmic@gmail.com>
2022-05-30 16:11:14 +0400

    chore: typo

--- packages/wallet-lib/src/types/Storage/methods/rehydrateState.js
+++ packages/wallet-lib/src/types/Storage/methods/rehydrateState.js
@@ -35,7 +35,7 @@ const rehydrateState = async function rehydrateState() {
               }
             });
           } catch (e) {
-            logger.error('Error importing persistence storage, resyncing from start', e);
+            logger.error('Error importing persistent storage, resyncing from start', e);
 
             this.adapter.setItem(`wallet_${walletId}`, null);
           }
