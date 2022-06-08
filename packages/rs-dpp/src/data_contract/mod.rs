3b9821c6b6dd49487d5f14b32d9b2ea40a917290
Pawel Iwan <pawel.iwan@dash.org>
2022-06-08 10:27:47 +0200

    properties refactor

--- packages/rs-dpp/src/data_contract/mod.rs
+++ packages/rs-dpp/src/data_contract/mod.rs
@@ -13,3 +13,14 @@ pub mod enrich_data_contract_with_base_schema;
 pub mod get_binary_properties_from_schema;
 pub mod get_property_definition_by_path;
 pub mod state_transition;
+
+pub(self) mod properties {
+    pub const PROPERTY_PROTOCOL_VERSION: &str = "protocolVersion";
+    pub const PROPERTY_ID: &str = "$id";
+    pub const PROPERTY_OWNER_ID: &str = "ownerId";
+    pub const PROPERTY_VERSION: &str = "version";
+    pub const PROPERTY_SCHEMA: &str = "$schema";
+    pub const PROPERTY_DOCUMENTS: &str = "documents";
+    pub const PROPERTY_DEFINITIONS: &str = "$defs";
+    pub const PROPERTY_ENTROPY: &str = "entropy";
+}
