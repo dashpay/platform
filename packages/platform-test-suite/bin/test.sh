392633e2e8db6c615b741de82ab45dae27b1ac86
pshenmic <pshenmic@gmail.com>
2022-06-09 22:44:11 +0400

    fix: use last argument to gather seed url

--- packages/platform-test-suite/bin/test.sh
+++ packages/platform-test-suite/bin/test.sh
@@ -34,7 +34,7 @@ Usage: test <seed> [options]
   functional:core
   functional:platform"
 
-DAPI_SEED="$1"
+DAPI_SEED="${@: -1}"
 network="testnet"
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
