219e5fb9fe1a4a2a3b768ace4f9470801ac267c9
Ivan Shumkov <ivan@shumkov.ru>
2022-06-14 01:17:32 +0700

    fix: neon security vulnerability (dashpay/rs-platform#110)

--- packages/rs-drive-nodejs/src/converter.rs
+++ packages/rs-drive-nodejs/src/converter.rs
@@ -1,6 +1,9 @@
 use grovedb::{Element, PathQuery, Query, SizedQuery};
-use neon::{borrow::Borrow, prelude::*};
+use neon::handle::Managed;
+use neon::prelude::*;
+use neon::types::buffer::TypedArray;
 use rs_drive::drive::flags::StorageFlags;
+use std::borrow::Borrow;
 
 fn element_to_string(element: &Element) -> &'static str {
     match element {
@@ -14,23 +17,20 @@ pub fn js_object_to_element<'a, C: Context<'a>>(
     js_object: Handle<JsObject>,
     cx: &mut C,
 ) -> NeonResult<Element> {
-    let js_element_type = js_object.get(cx, "type")?.to_string(cx)?;
-    let js_element_epoch = js_object
-        .get(cx, "epoch")?
-        .downcast_or_throw::<JsNumber, _>(cx)?;
+    let js_element_type: Handle<JsString> = js_object.get(cx, "type")?;
+
+    let element_type: String = js_element_type.value(cx);
+
+    let js_element_epoch: Handle<JsNumber> = js_object.get(cx, "epoch")?;
 
     let epoch = u16::try_from(js_element_epoch.value(cx) as i64)
         .or_else(|_| cx.throw_range_error("`epoch` must fit in u16"))?;
 
     let storage_flags = StorageFlags { epoch };
 
-    let value = js_object.get(cx, "value")?;
-
-    let element_type_string: String = js_element_type.value(cx);
-
-    match element_type_string.as_str() {
+    match element_type.as_str() {
         "item" => {
-            let js_buffer = value.downcast_or_throw::<JsBuffer, _>(cx)?;
+            let js_buffer: Handle<JsBuffer> = js_object.get(cx, "value")?;
             let item = js_buffer_to_vec_u8(js_buffer, cx);
 
             Ok(Element::new_item_with_flags(
@@ -39,7 +39,7 @@ pub fn js_object_to_element<'a, C: Context<'a>>(
             ))
         }
         "reference" => {
-            let js_array = value.downcast_or_throw::<JsArray, _>(cx)?;
+            let js_array: Handle<JsArray> = js_object.get(cx, "value")?;
             let reference = js_array_of_buffers_to_vec(js_array, cx)?;
 
             Ok(Element::new_reference_with_flags(
@@ -48,7 +48,7 @@ pub fn js_object_to_element<'a, C: Context<'a>>(
             ))
         }
         "tree" => {
-            let js_buffer = value.downcast_or_throw::<JsBuffer, _>(cx)?;
+            let js_buffer: Handle<JsBuffer> = js_object.get(cx, "value")?;
             let tree_vec = js_buffer_to_vec_u8(js_buffer, cx);
 
             Ok(Element::new_tree_with_flags(
@@ -61,7 +61,7 @@ pub fn js_object_to_element<'a, C: Context<'a>>(
                 storage_flags.to_element_flags(),
             ))
         }
-        _ => cx.throw_error(format!("Unexpected element type {}", element_type_string)),
+        _ => cx.throw_error(format!("Unexpected element type {}", element_type)),
     }
 }
 
@@ -105,10 +105,13 @@ pub fn nested_vecs_to_js<'a, C: Context<'a>>(
 }
 
 pub fn js_buffer_to_vec_u8<'a, C: Context<'a>>(js_buffer: Handle<JsBuffer>, cx: &mut C) -> Vec<u8> {
-    let guard = cx.lock();
+    // let guard = cx.lock();
+
+    let key_memory_view = js_buffer.borrow();
+
     // let key_buffer = js_buffer.deref();
-    let key_memory_view = js_buffer.borrow(&guard);
-    let key_slice = key_memory_view.as_slice::<u8>();
+    // let key_memory_view = js_buffer.borrow(&guard);
+    let key_slice: &[u8] = key_memory_view.as_slice(cx);
     key_slice.to_vec()
 }
 
@@ -143,30 +146,24 @@ fn js_object_get_vec_u8<'a, C: Context<'a>>(
     field: &str,
     cx: &mut C,
 ) -> NeonResult<Vec<u8>> {
-    Ok(js_buffer_to_vec_u8(
-        js_object
-            .get(cx, field)?
-            .downcast_or_throw::<JsBuffer, _>(cx)?,
-        cx,
-    ))
+    let buffer: Handle<JsBuffer> = js_object.get(cx, field)?;
+
+    Ok(js_buffer_to_vec_u8(buffer, cx))
 }
 
 fn js_object_to_query<'a, C: Context<'a>>(
     js_object: Handle<JsObject>,
     cx: &mut C,
 ) -> NeonResult<Query> {
-    let items = js_object
-        .get(cx, "items")?
-        .downcast_or_throw::<JsArray, _>(cx)?;
+    let items: Handle<JsArray> = js_object.get(cx, "items")?;
     let mut query = Query::new();
     for js_item in items.to_vec(cx)? {
         let item = js_item.downcast_or_throw::<JsObject, _>(cx)?;
-        match item
-            .get(cx, "type")?
-            .downcast_or_throw::<JsString, _>(cx)?
-            .value(cx)
-            .as_ref()
-        {
+
+        let item_type: Handle<JsString> = item.get(cx, "type")?;
+        let item_type = item_type.value(cx);
+
+        match item_type.as_ref() {
             "key" => {
                 query.insert_key(js_object_get_vec_u8(item, "key", cx)?);
             }
@@ -230,12 +227,9 @@ fn js_object_to_sized_query<'a, C: Context<'a>>(
     js_object: Handle<JsObject>,
     cx: &mut C,
 ) -> NeonResult<SizedQuery> {
-    let query = js_object_to_query(
-        js_object
-            .get(cx, "query")?
-            .downcast_or_throw::<JsObject, _>(cx)?,
-        cx,
-    )?;
+    let query: Handle<JsObject> = js_object.get(cx, "query")?;
+    let query = js_object_to_query(query, cx)?;
+
     let limit: Option<u16> = js_value_to_option::<JsNumber, _>(js_object.get(cx, "limit")?, cx)?
         .map(|x| {
             u16::try_from(x.value(cx) as i64)
@@ -248,6 +242,7 @@ fn js_object_to_sized_query<'a, C: Context<'a>>(
                 .or_else(|_| cx.throw_range_error("`offset` must fit in u16"))
         })
         .transpose()?;
+
     Ok(SizedQuery::new(query, limit, offset))
 }
 
@@ -255,9 +250,8 @@ pub fn js_path_query_to_path_query<'a, C: Context<'a>>(
     js_path_query: Handle<JsObject>,
     cx: &mut C,
 ) -> NeonResult<PathQuery> {
-    let path =
-        js_array_of_buffers_to_vec(js_path_query.get(cx, "path")?.downcast_or_throw(cx)?, cx)?;
-    let query =
-        js_object_to_sized_query(js_path_query.get(cx, "query")?.downcast_or_throw(cx)?, cx)?;
+    let path = js_array_of_buffers_to_vec(js_path_query.get(cx, "path")?, cx)?;
+    let query = js_object_to_sized_query(js_path_query.get(cx, "query")?, cx)?;
+
     Ok(PathQuery::new(path, query))
 }
