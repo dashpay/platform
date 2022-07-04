70189656ee341eb0928edf1c7faeda6b32326356
Pawel Iwan <pawel.iwan@dash.org>
2022-07-04 15:59:27 +0200

    document transition fixture

--- packages/rs-dpp/src/tests/fixtures/get_document_transitions_fixture.rs
+++ packages/rs-dpp/src/tests/fixtures/get_document_transitions_fixture.rs
@@ -1,8 +1,39 @@
-use crate::document::document_factory::DocumentFactory;
+use std::collections::HashMap;
 
-use super::{get_data_contract_fixture, get_documents_fixture};
+use crate::{
+    document::{
+        document_factory::DocumentFactory,
+        document_transition::{Action, DocumentTransition},
+        Document,
+    },
+    mocks,
+};
 
-fn get_document_transitions_fixture() {
-    let data_contract = get_data_contract_fixture(None);
-    let documents = get_documents_fixture(data_contract);
+use super::{get_data_contract_fixture, get_document_validator_fixture, get_documents_fixture};
+use crate::version::LATEST_VERSION;
+
+fn get_document_transitions_fixture(
+    mut documents: HashMap<Action, Vec<Document>>,
+) -> Vec<DocumentTransition> {
+    let document_factory = DocumentFactory::new(
+        LATEST_VERSION,
+        get_document_validator_fixture(),
+        mocks::FetchAndValidateDataContract {},
+    );
+
+    let create_documents = documents
+        .remove(&Action::Create)
+        .unwrap_or_else(|| get_documents_fixture(get_data_contract_fixture(None)).unwrap());
+    let replace_documents = documents.remove(&Action::Replace).unwrap_or_default();
+    let delete_documents = documents.remove(&Action::Delete).unwrap_or_default();
+
+    document_factory
+        .create_state_transition([
+            (Action::Create, create_documents),
+            (Action::Replace, replace_documents),
+            (Action::Delete, delete_documents),
+        ])
+        .expect("the transitions should be crated")
+        .get_transitions()
+        .to_owned()
 }
