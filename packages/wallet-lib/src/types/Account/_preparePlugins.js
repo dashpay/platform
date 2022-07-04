d8b97b9e1618a4a2118d1ac7e1d23e9f4dc9ac88
markin.io <markin.io210@gmail.com>
2022-07-04 18:28:56 +0400

    fix: this.emit is not a function

--- packages/wallet-lib/src/types/Account/_preparePlugins.js
+++ packages/wallet-lib/src/types/Account/_preparePlugins.js
@@ -2,6 +2,7 @@ const sortPlugins = require('./_sortPlugins');
 const logger = require('../../logger');
 
 const preparePlugins = function preparePlugins(account, userUnsafePlugins) {
+  const self = this;
   function reducer(accumulatorPromise, [plugin, allowSensitiveOperation, awaitOnInjection]) {
     return accumulatorPromise
       .then(async () => {
@@ -13,7 +14,7 @@ const preparePlugins = function preparePlugins(account, userUnsafePlugins) {
           );
         } catch (e) {
           logger.error('Error injecting plugin', e);
-          this.emit('error', e, {
+          self.emit('error', e, {
             type: 'plugin',
             pluginType: 'plugin',
             pluginName: plugin.name,
