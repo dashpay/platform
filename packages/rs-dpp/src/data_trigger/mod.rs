6654af502fc2663cad5dfc7b77987b0bed635506
Pawel Iwan <pawel.iwan@dash.org>
2022-07-07 10:10:35 +0200

    remove function pointers from Data Trigger implementation

--- packages/rs-dpp/src/data_trigger/mod.rs
+++ packages/rs-dpp/src/data_trigger/mod.rs
@@ -20,29 +20,29 @@ use crate::{
     state_repository::StateRepositoryLike,
 };
 
-pub type BoxedTrigger<'a, SR> = Box<Trigger<'a, SR>>;
-pub type Trigger<'a, SR> =
-    dyn Fn(
-        &'a DocumentTransition,
-        &'a DataTriggerExecutionContext<SR>,
-        Option<&'a Identifier>,
-    ) -> LocalBoxFuture<'a, Result<DataTriggerExecutionResult, anyhow::Error>>;
+use self::dashpay_data_triggers::create_contract_request_data_trigger;
+use self::dpns_triggers::create_domain_data_trigger;
+use self::feature_flags_data_triggers::create_feature_flag_data_trigger;
+use self::reward_share_data_triggers::create_masternode_reward_shares_data_trigger;
 
-pub struct DataTrigger<'a, SR>
-where
-    SR: StateRepositoryLike,
-{
+#[derive(Debug, Clone, Copy)]
+pub enum DataTriggerCode {
+    CreateDataContractRequest,
+    DataTriggerCreateDomain,
+    DataTriggerRewardShare,
+    DataTriggerReject,
+    CrateFeatureFlag,
+}
+
+pub struct DataTrigger {
     pub data_contract_id: Identifier,
     pub document_type: String,
-    pub trigger: BoxedTrigger<'a, SR>,
+    pub data_trigger_code: DataTriggerCode,
     pub transition_action: Action,
     pub top_level_identity: Option<Identifier>,
 }
 
-impl<'a, SR> DataTrigger<'a, SR>
-where
-    SR: StateRepositoryLike,
-{
+impl DataTrigger {
     /// Check this trigger is matching for specified data
     pub fn is_matching_trigger_for_data(
         &self,
@@ -55,16 +55,17 @@ where
             && self.transition_action == transition_action
     }
 
-    pub async fn execute(
-        &'a self,
-        document_transition: &'a DocumentTransition,
-        context: &'a DataTriggerExecutionContext<SR>,
+    pub async fn execute<SR>(
+        &self,
+        document_transition: &DocumentTransition,
+        context: &DataTriggerExecutionContext<SR>,
     ) -> DataTriggerExecutionResult
     where
         SR: StateRepositoryLike,
     {
         let mut result = DataTriggerExecutionResult::default();
-        let execution_result = (self.trigger)(
+        let execution_result = trigger_executor(
+            self.data_trigger_code,
             document_transition,
             context,
             self.top_level_identity.as_ref(),
@@ -102,5 +103,35 @@ where
         message: msg,
         owner_id: Some(context.owner_id.clone()),
         document_transition: Some(DocumentTransition::Create(dt_create.clone())),
+    }
+}
+
+pub async fn trigger_executor<SR>(
+    trigger_code: DataTriggerCode,
+    document_transition: &DocumentTransition,
+    context: &DataTriggerExecutionContext<SR>,
+    identifier: Option<&Identifier>,
+) -> Result<DataTriggerExecutionResult, anyhow::Error>
+where
+    SR: StateRepositoryLike,
+{
+    match trigger_code {
+        DataTriggerCode::CreateDataContractRequest => {
+            create_contract_request_data_trigger(document_transition, context, identifier).await
+        }
+        DataTriggerCode::DataTriggerCreateDomain => {
+            create_domain_data_trigger(document_transition, context, identifier).await
+        }
+
+        DataTriggerCode::CrateFeatureFlag => {
+            create_feature_flag_data_trigger(document_transition, context, identifier).await
+        }
+        DataTriggerCode::DataTriggerReject => {
+            reject_data_trigger(document_transition, context, identifier).await
+        }
+        DataTriggerCode::DataTriggerRewardShare => {
+            create_masternode_reward_shares_data_trigger(document_transition, context, identifier)
+                .await
+        }
     }
 }
--- packages/rs-dpp/src/document/state_transition/documents_batch_transition/validation/state/mod.rs
+++ packages/rs-dpp/src/document/state_transition/documents_batch_transition/validation/state/mod.rs
@@ -1,2 +1,64 @@
+use std::borrow::Borrow;
+
+use crate::{
+    data_trigger::{
+        self, get_data_triggers_factory::get_data_triggers, DataTrigger,
+        DataTriggerExecutionContext, DataTriggerExecutionResult,
+    },
+    document::document_transition::DocumentTransition,
+    state_repository::StateRepositoryLike,
+    ProtocolError,
+};
+
+pub mod execute_data_triggers;
 pub mod fetch_documents_factory;
+pub mod validate_documents_batch_transition_state;
 pub mod validate_documents_uniqueness_by_indices;
+
+async fn execute_data_triggers<SR>(
+    document_transitions: impl IntoIterator<Item = impl Borrow<DocumentTransition>>,
+    context: DataTriggerExecutionContext<SR>,
+) -> Result<Vec<DataTriggerExecutionResult>, ProtocolError>
+where
+    SR: StateRepositoryLike,
+{
+    let data_contract_id = &context.data_contract.id;
+    let mut execution_results: Vec<DataTriggerExecutionResult> = vec![];
+
+    for document_transition in document_transitions {
+        let dt = document_transition.borrow();
+        let document_type = &dt.base().document_type;
+        let transition_action = dt.base().action;
+
+        let data_triggers_for_transition =
+            get_data_triggers::<SR>(data_contract_id, document_type, transition_action)?;
+
+        if data_triggers_for_transition.is_empty() {
+            continue;
+        }
+
+        execute_data_triggers_sequentially(
+            dt,
+            data_triggers_for_transition,
+            &context,
+            &mut execution_results,
+        )
+        .await;
+    }
+
+    Ok(execution_results)
+}
+
+async fn execute_data_triggers_sequentially<'a, 'b, SR>(
+    document_transition: &'a DocumentTransition,
+    data_triggers: Vec<DataTrigger>,
+    context: &'a DataTriggerExecutionContext<SR>,
+    results: &'a mut Vec<DataTriggerExecutionResult>,
+) where
+    'b: 'a,
+    SR: StateRepositoryLike,
+{
+    for data_trigger in data_triggers.into_iter() {
+        results.push(data_trigger.execute(document_transition, context).await);
+    }
+}
