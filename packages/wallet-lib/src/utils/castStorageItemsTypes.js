7835bbf21bcbea33bf907bdb5b8c8f47768c224a
markin.io <markin.io210@gmail.com>
2022-07-28 13:59:05 +0400

    feat(wallet-lib): castStorageItemsTypes.js support arrays

--- packages/wallet-lib/src/utils/castStorageItemsTypes.js
+++ packages/wallet-lib/src/utils/castStorageItemsTypes.js
@@ -1,6 +1,46 @@
+const createCastFunction = (schemaValue) => {
+  let castFunction;
+  if (typeof schemaValue === 'string') {
+    castFunction = (itemToCast) => {
+      // eslint-disable-next-line valid-typeof
+      if (typeof itemToCast !== schemaValue) {
+        throw new Error(`Value "${itemToCast}" is not of type "${schemaValue}"`);
+      }
+      return itemToCast;
+    };
+  } else if (typeof schemaValue === 'function') {
+    castFunction = schemaValue;
+  } else {
+    castFunction = (itemToCast) => {
+      const Clazz = schemaValue;
+      return new Clazz(itemToCast);
+    };
+  }
+
+  return castFunction;
+};
+
+/**
+ * Performs casting of items to specified schema.
+ * Supports nested objects, but does not support nested arrays or arrays of objects.
+ * @param originalItem
+ * @param schema
+ * @returns {{}|*}
+ */
 const castStorageItemsTypes = (originalItem, schema) => {
   if (!schema) {
     throw new Error('Schema is undefined');
+  }
+
+  if (Array.isArray(schema)) {
+    const schemaValue = schema[0];
+
+    const schemaType = schemaValue.constructor.name;
+    if (schemaType !== 'Object' && schemaType !== 'Array') {
+      const castItem = createCastFunction(schemaValue);
+      return originalItem.map((element) => castItem(element));
+    }
+    throw new Error('Casting of nested arrays and arrays of objects is not supported.');
   }
 
   return Object.entries(schema).reduce((acc, next) => {
@@ -8,28 +48,15 @@ const castStorageItemsTypes = (originalItem, schema) => {
     const result = {};
 
     if (schemaKey !== '*' && originalItem[schemaKey] === undefined) {
-      throw new Error(`No item found for schema key "${schemaKey}" in item ${JSON.stringify(Object.keys(originalItem))}`);
+      const output = typeof originalItem === 'object'
+        ? JSON.stringify(Object.keys(originalItem)) : originalItem;
+
+      throw new Error(`No item found for schema key "${schemaKey}" in item "${output}"`);
     }
 
-    if (schemaValue.constructor.name !== 'Object') {
-      let castItem;
-
-      if (typeof schemaValue === 'string') {
-        castItem = (itemToCast) => {
-          // eslint-disable-next-line valid-typeof
-          if (typeof itemToCast !== schemaValue) {
-            throw new Error(`Value "${itemToCast}" is not of type "${schemaValue}"`);
-          }
-          return itemToCast;
-        };
-      } else if (typeof schemaValue === 'function') {
-        castItem = schemaValue;
-      } else {
-        castItem = (itemToCast) => {
-          const Clazz = schemaValue;
-          return new Clazz(itemToCast);
-        };
-      }
+    const schemaType = schemaValue.constructor.name;
+    if (schemaType !== 'Object' && schemaType !== 'Array') {
+      const castItem = createCastFunction(schemaValue);
 
       if (schemaKey === '*') {
         Object.keys(originalItem).forEach((itemKey) => {
--- packages/wallet-lib/src/utils/castStorageItemsTypes.spec.js
+++ packages/wallet-lib/src/utils/castStorageItemsTypes.spec.js
@@ -6,9 +6,16 @@ const {BlockHeader, Transaction} = require('@dashevo/dashcore-lib')
 const WalletStore = require("../types/WalletStore/WalletStore");
 
 const mockChainStorage = {
-  "blockHeaders": {
-    "fakeBlockHash": "000000206ff6709b4816a98a7601bc9626a597191fe4f788228a037de3bd839e811f4913c49de57ac9553f0cd2529c94eaa9781273f315a792c4148eb7e3756c9cd7e1ce40285562ffff7f2000000000"
+  "headersMetadata": {
+     "fakeBlockHash": {
+       height: 1,
+       time: 99999999
+     }
   },
+  "lastSyncedHeaderHeight": 1,
+  "blockHeaders": [
+    "000000206ff6709b4816a98a7601bc9626a597191fe4f788228a037de3bd839e811f4913c49de57ac9553f0cd2529c94eaa9781273f315a792c4148eb7e3756c9cd7e1ce40285562ffff7f2000000000"
+  ],
   "transactions": {
     "fakeTxHash": "0300000001e569f827418be2e49f5ae4a34d30ff3bc5abc723f72b0e10712598dff1e70689010000006b483045022100f9aebe9bcfaa8208f1486ad4d15f65730b5adc0cb02c1d2bd8836a4582e2ea7f02200250bbfe524f70324417237549f421c2c3584d6b532194dbac8043e46900753701210387f3d1ff9e6a06db60bd61d0757002836407e8a3b1094b446690d13392c3fb9affffffff02e8030000000000001976a9148d0ba6247ad4988ba70fdcb56bb5f45b6e49423d88aca351c253040000001976a91471a97f71915e7c1d4460ad520511761b06534d8088ac00000000"
   },
@@ -36,7 +43,7 @@ describe('Utils - castStorageItemsTypes', function suite() {
   it('should proceed with valid schema', function () {
     const chainStore = castItemTypes(mockChainStorage, ChainStore.prototype.SCHEMA)
 
-    expect(chainStore.blockHeaders.fakeBlockHash instanceof BlockHeader).to.be.true
+    expect(chainStore.blockHeaders[0] instanceof BlockHeader).to.be.true
     expect(chainStore.transactions.fakeTxHash instanceof Transaction).to.be.true
     expect(chainStore.txMetadata.fakeTxHash.isInstantLocked).to.be.true
     expect(chainStore.txMetadata.fakeTxHash.isChainLocked).to.be.true
@@ -64,7 +71,7 @@ describe('Utils - castStorageItemsTypes', function suite() {
     mockChainStorageWithUnknownKeys.txMetadata.unknownKey = true
 
     expect(() => castItemTypes(mockChainStorageWithUnknownKeys, ChainStore.prototype.SCHEMA))
-      .to.throw('No item found for schema key "blockHash" in item true')
+      .to.throw('No item found for schema key "blockHash" in item "true"')
   });
 
   it('should throw if invalid uniform object with primitives passed', function () {
