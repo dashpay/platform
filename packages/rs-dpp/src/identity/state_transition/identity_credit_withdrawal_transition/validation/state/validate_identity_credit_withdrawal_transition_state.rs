ca6452bb4aa48d3638cbffac1552ca4c5cf7c274
Djavid Gabibiyan <djavidoff@gmail.com>
2022-08-13 22:34:38 +0500

    feat: identity credit withdrawal transition (dashpay/rs-platform#25)
    * add new validators
    
    * add tests for identity_create_transition_basic_validator
    
    * add some code for handling is locks
    
    * finished is lock fixture
    
    * add simple test consensus error for test cases
    
    * implement the rest of asset lock proof fixture
    
    * add mock for public key validator
    
    * make asset lock test compile
    
    * reformat
    
    * add lazy_static for schema
    
    * make previously failing tests pass
    
    * add asset lock transaction validation
    
    * add data type to the validation result
    
    * add error codes for new errors
    
    * reimplemented the rest of instant asset lock proof validation
    
    * make everything compile and resolve all cargo warnings
    
    * implement base chain lock proof class
    
    * add asset lock public key fetcher and asset lock transaction output fetcher
    
    * add AssetLockTransactionIsNotFoundError error
    
    * add UnknownAssetLockProofTypeError error
    
    * add missing methods for IS asset lock proof and added identity id generation function
    
    * implement part of the AL output fetcher
    
    * reorg imports
    
    * add DPP error and some more asset lock functions
    
    * implement public key fetcher
    
    * make AssetLockTransactionOutputFetcher work
    
    * fix some is lock types
    
    * merge dev
    
    * reformat
    
    * implement Se and De for AssetLock
    
    * fix IS asset lock serialization
    
    * fix formatting errors
    
    * implement `TryFrom<RawInstantLock>` for `InstantAssetLockProof`
    
    * reformat
    
    * make asset lock validation function async and fixed all tests
    
    * merge with dev
    
    * fix remaining validator tests
    
    * chore: initial work
    
    * chore: basic validator
    
    * chore: use `reduce_balance`
    
    * implement serializers and parsers for identity_create_transition and asset_lock_proof
    
    * fix json serializer for asset lock proof
    
    * refactor state transtion method names
    
    * remove identity create transition mock, made json converter to work properly
    
    * remove Option from AssetLock
    
    * fix identity st apply function
    
    * fix warnings
    
    * chore: small fixes
    
    * chore: remove unfinished
    
    * chore: remove schemas
    
    * chore: more schemas
    
    * chore: fix paths
    
    * chore: remove unused
    
    * fix asset lock validation tests
    
    * chore: typo
    
    * fix last basic test
    
    * chore: basic validation tests
    
    * chore: initial state test
    
    * chore: rest of the state tests
    
    * chore: apply tests
    
    * chore: do not suppress error
    
    * chore: tests
    
    * chore: newline
    
    * chore: set default pooling to `Never`
    
    * resolve all errors after the dev merge
    
    * chore: allow only `Never` pooling for now
    
    * chore: throw error if identity not found on `apply`
    
    * chore: serializtion, deserialization
    
    * chore: fix tests
    
    * chore: use `Arc` for `SR`
    
    * chore: newline
    
    Co-authored-by: Anton Suprunchuk <anton.suprunchuk@gmail.com>

---
+++ packages/rs-dpp/src/identity/state_transition/identity_credit_withdrawal_transition/validation/state/validate_identity_credit_withdrawal_transition_state.rs
@@ -0,0 +1,67 @@
+use std::sync::Arc;
+
+use anyhow::Result;
+
+use crate::{
+    consensus::basic::{identity::IdentityInsufficientBalanceError, BasicError},
+    identity::state_transition::identity_credit_withdrawal_transition::IdentityCreditWithdrawalTransition,
+    prelude::Identity,
+    state_repository::StateRepositoryLike,
+    validation::ValidationResult,
+    NonConsensusError,
+};
+
+pub struct IdentityCreditWithdrawalTransitionValidator<SR>
+where
+    SR: StateRepositoryLike,
+{
+    state_repository: Arc<SR>,
+}
+
+impl<SR> IdentityCreditWithdrawalTransitionValidator<SR>
+where
+    SR: StateRepositoryLike,
+{
+    pub fn new(state_repository: Arc<SR>) -> Self {
+        IdentityCreditWithdrawalTransitionValidator { state_repository }
+    }
+
+    pub async fn validate_identity_credit_withdrawal_transition_state(
+        &self,
+        state_transition: &IdentityCreditWithdrawalTransition,
+    ) -> Result<ValidationResult<()>, NonConsensusError> {
+        let mut result: ValidationResult<()> = ValidationResult::default();
+
+        let maybe_existing_identity: Option<Identity> = self
+            .state_repository
+            .fetch_identity(&state_transition.identity_id)
+            .await
+            .map_err(|err| NonConsensusError::StateRepositoryFetchError(err.to_string()))?;
+
+        let existing_identity = match maybe_existing_identity {
+            None => {
+                let err = BasicError::IdentityNotFoundError {
+                    identity_id: state_transition.identity_id.clone(),
+                };
+
+                result.add_error(err);
+
+                return Ok(result);
+            }
+            Some(identity) => identity,
+        };
+
+        if existing_identity.get_balance() < state_transition.amount {
+            let err = IdentityInsufficientBalanceError {
+                identity_id: state_transition.identity_id.clone(),
+                balance: existing_identity.balance,
+            };
+
+            result.add_error(err);
+
+            return Ok(result);
+        }
+
+        Ok(result)
+    }
+}
