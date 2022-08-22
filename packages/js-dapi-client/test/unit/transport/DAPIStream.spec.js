e08089ef8d8a47f16f7556dcafe78a3c7e324e2b
markin.io <markin.io210@gmail.com>
2022-08-22 20:46:01 +0400

    refactor(dapi-client): fix lint

--- packages/js-dapi-client/test/unit/transport/DAPIStream.spec.js
+++ packages/js-dapi-client/test/unit/transport/DAPIStream.spec.js
@@ -2,7 +2,6 @@ const EventEmitter = require('events');
 const { expect } = require('chai');
 
 const DAPIStream = require('../../../lib/transport/DAPIStream');
-const wait = require('../../../lib/test/utils/wait');
 
 describe('DAPIStream', () => {
   let dapiStream;
