e2c4cb2a6fc45de6891a68eae8cb2a0eb147c611
pshenmic <pshenmic@gmail.com>
2022-09-28 18:03:28 +0700

    fix(dashmate): add missing core command

--- packages/dashmate/src/core/startCoreFactory.js
+++ packages/dashmate/src/core/startCoreFactory.js
@@ -49,6 +49,8 @@ function startCoreFactory(
     if (options.wallet) {
       config.set('core.masternode.enable', 0)
       config.set('core.masternode.operator.privateKey', null)
+
+      coreCommand.push('--disablewallet=0');
     }
 
     const coreContainer = await dockerCompose.runService(
