13bd0387ebe284d2b974df43cfb14eefaa002b22
Konstantin Shuplenkov <konstantin.shuplenkov@dash.org>
2022-10-07 18:17:29 +0300

    WIP

--- scripts/release/bump_version.js
+++ scripts/release/bump_version.js
@@ -4,13 +4,13 @@ const semver = require('semver');
 const packagesIterator = require('../utils/packagesIterator');
 const rootPackageJson = require('../../package.json');
 
-const convertReleaseToPrerelease = (version, prereleaseType = 'dev') => {
+const convertReleaseToPrerelease = (version, prereleaseType) => {
   const bumpedVersion = semver.inc(version, 'minor');
 
   return `${semver.major(bumpedVersion)}.${semver.minor(bumpedVersion)}.0-${prereleaseType}.1`;
 };
 
-const convertPrereleaseType = (version, prereleaseType = 'dev') => {
+const convertPrereleaseType = (version, prereleaseType) => {
   return `${semver.major(version)}.${semver.minor(version)}.0-${prereleaseType}.1`;
 };
 
@@ -55,7 +55,7 @@ const convertPrereleaseType = (version, prereleaseType = 'dev') => {
     }
 
     // root version
-    rootPackageJson.version = convertReleaseToPrerelease(rootPackageJson.version);
+    rootPackageJson.version = convertReleaseToPrerelease(rootPackageJson.version, releaseType);
     fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), `${JSON.stringify(rootPackageJson, null, 2)}\n`);
   } else if (rootVersionType !== 'release' && releaseType === 'release') {
     // prerelease to release
