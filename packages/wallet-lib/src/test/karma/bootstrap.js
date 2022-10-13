5c1ac02d4489ed192b8a8282fba1a2567b6dbcf4
markin.io <markin.io210@gmail.com>
2022-10-13 20:59:20 +0400

    test(wallet-lib): fix karma tests

--- packages/wallet-lib/src/test/karma/bootstrap.js
+++ packages/wallet-lib/src/test/karma/bootstrap.js
@@ -2,10 +2,12 @@ const { expect, use } = require('chai');
 const sinon = require('sinon');
 const chaiAsPromised = require('chai-as-promised');
 const sinonChai = require('sinon-chai');
+const dirtyChai = require('dirty-chai');
 require('setimmediate');
 
 use(chaiAsPromised);
 use(sinonChai);
+use(dirtyChai);
 
 beforeEach(function beforeEach() {
   if (!this.sinon) {
