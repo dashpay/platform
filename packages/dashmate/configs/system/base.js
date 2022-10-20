7cce2070778e70046a1eaba9db4314f3de1e1192
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2022-10-20 16:24:11 +0400

    change docker image

--- packages/dashmate/configs/system/base.js
+++ packages/dashmate/configs/system/base.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   core: {
     docker: {
-      image: 'dashpay/dashd:18.1.0-rc.1',
+      image: 'dashpay/dashd:19.0.0-alpha.bls.4',
     },
     p2p: {
       port: 20001,
