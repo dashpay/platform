5353a827ce6c722eb0138c7e4c60fdf55fb4748d
Evgeny Fomin <fominok@hotmail.com>
2022-11-30 15:44:16 +0100

    wip

--- packages/wasm-dpp/src/data_contract/state_transition/data_contract_create_transition/mod.rs
+++ packages/wasm-dpp/src/data_contract/state_transition/data_contract_create_transition/mod.rs
@@ -1,7 +1,15 @@
-use dpp::data_contract::state_transition::DataContractCreateTransition;
+use dpp::{
+    data_contract::state_transition::DataContractCreateTransition,
+    state_transition::StateTransitionType,
+};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-use crate::{buffer::Buffer, DataContractWasm};
+use crate::{
+    buffer::Buffer,
+    errors::{from_dpp_err, RustConversionError},
+    with_js_error, DataContractParameters, DataContractWasm,
+};
 
 #[wasm_bindgen(js_name=DataContractCreateTransition)]
 pub struct DataContractCreateTransitionWasm(DataContractCreateTransition);
@@ -18,8 +26,33 @@ impl Into<DataContractCreateTransition> for DataContractCreateTransitionWasm {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct DataContractCreateTransitionParameters {
+    protocol_version: u32,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    data_contract: Option<DataContractParameters>,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    entropy: Option<Vec<u8>>,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    signature_public_key_id: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    signature: Option<Vec<u8>>,
+}
+
 #[wasm_bindgen(js_class=DataContractCreateTransition)]
 impl DataContractCreateTransitionWasm {
+    #[wasm_bindgen(constructor)]
+    pub fn new(raw_parameters: JsValue) -> Result<DataContractCreateTransitionWasm, JsValue> {
+        let parameters: DataContractCreateTransitionParameters =
+            with_js_error!(serde_wasm_bindgen::from_value(raw_parameters))?;
+        DataContractCreateTransition::from_raw_object(
+            serde_json::to_value(parameters).expect("the struct will be a valid json"),
+        )
+        .map(Into::into)
+        .map_err(from_dpp_err)
+    }
+
     #[wasm_bindgen(js_name=getDataContract)]
     pub fn get_data_contract(&self) -> DataContractWasm {
         self.0.data_contract.clone().into()
