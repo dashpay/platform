7bc882b820917e865933dabd0c8516ae99b07260
markin.io <markin.io210@gmail.com>
2022-12-05 20:35:28 +0400

    feat(wasm-dpp): instant_asset_lock_proof.rs finish bindings

--- packages/wasm-dpp/src/identity/state_transition/asset_lock_proof/instant/instant_asset_lock_proof.rs
+++ packages/wasm-dpp/src/identity/state_transition/asset_lock_proof/instant/instant_asset_lock_proof.rs
@@ -1,4 +1,12 @@
-use dpp::dashcore::blockdata::{script::Script, transaction::txout::TxOut};
+use dpp::{
+    dashcore::{
+        blockdata::{script::Script, transaction::txout::TxOut},
+        consensus::encode::serialize,
+    },
+    util::string_encoding,
+    util::string_encoding::Encoding,
+};
+
 use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 use wasm_bindgen::prelude::*;
@@ -6,6 +14,7 @@ use wasm_bindgen::prelude::*;
 use crate::{
     buffer::Buffer,
     errors::{from_dpp_err, RustConversionError},
+    identifier::IdentifierWrapper,
     with_js_error,
 };
 use dpp::identity::state_transition::asset_lock_proof::instant::{
@@ -73,6 +82,89 @@ impl InstantAssetLockProofWasm {
             serde_json::to_string(&TxOutSerdeHelper(output)).map_err(|e| from_dpp_err(e.into()))?;
 
         let js_object = js_sys::JSON::parse(&output_json_string)?;
+        Ok(js_object)
+    }
+
+    #[wasm_bindgen(js_name=createIdentifier)]
+    pub fn create_identifier(&self) -> Result<IdentifierWrapper, JsValue> {
+        let identifier = self
+            .0
+            .create_identifier()
+            .map_err(|e| from_dpp_err(e.into()))?;
+        Ok(identifier.into())
+    }
+
+    #[wasm_bindgen(js_name=getInstantLock)]
+    pub fn get_instant_lock(&self) -> Buffer {
+        let instant_lock = self.0.instant_lock();
+        let serialized_instant_lock = serialize(instant_lock);
+        Buffer::from_bytes(serialized_instant_lock.as_slice())
+    }
+
+    #[wasm_bindgen(js_name=getTransaction)]
+    pub fn get_transaction(&self) -> Buffer {
+        let transaction = self.0.transaction();
+        let serialized_transaction = serialize(transaction);
+        Buffer::from_bytes(serialized_transaction.as_slice())
+    }
+
+    #[wasm_bindgen(js_name=toObject)]
+    pub fn to_object(&self) -> Result<JsValue, JsValue> {
+        let asset_lock_json =
+            serde_json::to_value(self.0.clone()).map_err(|e| from_dpp_err(e.into()))?;
+
+        let asset_lock_json_string =
+            serde_json::to_string(&asset_lock_json).map_err(|e| from_dpp_err(e.into()))?;
+        let js_object = js_sys::JSON::parse(&asset_lock_json_string)?;
+
+        let transaction = self.get_transaction();
+        let instant_lock = self.get_instant_lock();
+
+        js_sys::Reflect::set(
+            &js_object,
+            &"transaction".to_owned().into(),
+            &JsValue::from(transaction),
+        )?;
+
+        js_sys::Reflect::set(
+            &js_object,
+            &"instantLock".to_owned().into(),
+            &JsValue::from(instant_lock),
+        )?;
+
+        Ok(js_object)
+    }
+
+    #[wasm_bindgen(js_name=toJSON)]
+    pub fn to_json(&self) -> Result<JsValue, JsValue> {
+        let js_object = self.to_object()?;
+
+        let transaction = self.0.transaction();
+        let serialized_transaction = serialize(transaction);
+
+        let instant_lock = self.0.instant_lock();
+        let serialized_instant_lock = serialize(instant_lock);
+
+        let instant_lock_base64 =
+            string_encoding::encode(serialized_instant_lock.as_slice(), Encoding::Base64);
+
+        let mut transaction_hex = String::new();
+        for &byte in serialized_transaction.as_slice() {
+            transaction_hex.push_str(&format!("{:02x}", byte));
+        }
+
+        js_sys::Reflect::set(
+            &js_object,
+            &"transaction".to_owned().into(),
+            &JsValue::from(transaction_hex),
+        )?;
+
+        js_sys::Reflect::set(
+            &js_object,
+            &"instantLock".to_owned().into(),
+            &JsValue::from(instant_lock_base64),
+        )?;
+
         Ok(js_object)
     }
 }
