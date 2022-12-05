d780efb56f05888bd20cd0b8950d8fd8ef4ae066
markin.io <markin.io210@gmail.com>
2022-12-05 19:01:07 +0400

    feat(wasm-dpp): instant_asset_lock_proof.rs bindings for getOutPoint and getOutput

--- packages/wasm-dpp/src/identity/state_transition/asset_lock_proof/instant/instant_asset_lock_proof.rs
+++ packages/wasm-dpp/src/identity/state_transition/asset_lock_proof/instant/instant_asset_lock_proof.rs
@@ -1,11 +1,28 @@
+use dpp::dashcore::blockdata::{script::Script, transaction::txout::TxOut};
 use serde::{Deserialize, Serialize};
 use std::convert::TryInto;
 use wasm_bindgen::prelude::*;
 
-use crate::{buffer::Buffer, errors::RustConversionError, with_js_error};
+use crate::{
+    buffer::Buffer,
+    errors::{from_dpp_err, RustConversionError},
+    with_js_error,
+};
 use dpp::identity::state_transition::asset_lock_proof::instant::{
     InstantAssetLockProof, RawInstantLock,
 };
+
+#[derive(Serialize, Deserialize)]
+#[serde(remote = "TxOut")]
+struct TxOutJS {
+    #[serde(rename = "satoshis")]
+    value: u64,
+    #[serde(rename = "script")]
+    script_pubkey: Script,
+}
+
+#[derive(Serialize)]
+struct TxOutSerdeHelper<'a>(#[serde(with = "TxOutJS")] &'a TxOut);
 
 #[wasm_bindgen(js_name=InstantAssetLockProof)]
 pub struct InstantAssetLockProofWasm(InstantAssetLockProof);
@@ -47,5 +64,15 @@ impl InstantAssetLockProofWasm {
         self.0
             .out_point()
             .map(|out_point| Buffer::from_bytes(out_point.as_slice()))
+    }
+
+    #[wasm_bindgen(js_name=getOutput)]
+    pub fn get_output(&self) -> Result<JsValue, JsValue> {
+        let output = self.0.output().unwrap();
+        let output_json_string =
+            serde_json::to_string(&TxOutSerdeHelper(output)).map_err(|e| from_dpp_err(e.into()))?;
+
+        let js_object = js_sys::JSON::parse(&output_json_string)?;
+        Ok(js_object)
     }
 }
