643aea4dd8e6480e9ebb704ef2315c4c9efc54ee
markin.io <markin.io210@gmail.com>
2022-12-05 20:35:28 +0400

    feat(wasm-dpp): instant_asset_lock_proof.rs finish bindings

--- packages/wasm-dpp/test/unit/identity/stateTransition/assetLockProof/InstantAssetLockProof.spec.js
+++ packages/wasm-dpp/test/unit/identity/stateTransition/assetLockProof/InstantAssetLockProof.spec.js
@@ -58,9 +58,48 @@ describe('InstantAssetLockProof', () => {
         .to.deep.equal(instantAssetLockProofJS.getOutput().toObject());
     });
   });
-  // describe('#createIdentifier')
-  // describe('#getInstantLock')
-  // describe('#getTransaction')
-  // describe('#toObject')
-  // describe('#toJSON')
+
+  describe('#createIdentifier', () => {
+    it('should return correct identifier', () => {
+      const identifier = instantAssetLockProof.createIdentifier();
+      const identifierJS = instantAssetLockProofJS.createIdentifier();
+
+      expect(identifier.toBuffer())
+        .to.deep.equal(identifierJS.toBuffer());
+    });
+  });
+
+  describe('#getInstantLock', () => {
+    it('should return correct instant lock', () => {
+      const instantLock = instantAssetLockProof.getInstantLock();
+      const instantLockJS = instantAssetLockProofJS.getInstantLock();
+
+      expect(instantLock)
+        .to.deep.equal(instantLockJS.toBuffer());
+    });
+  });
+
+  describe('#getTransaction', () => {
+    it('should return correct transaction', () => {
+      const transaction = instantAssetLockProof.getTransaction();
+      const transactionJS = instantAssetLockProofJS.getTransaction();
+
+      expect(transaction)
+        .to.deep.equal(transactionJS.toBuffer());
+    });
+  });
+
+  describe('#toObject', () => {
+    it('should return correct object', () => {
+      expect(instantAssetLockProof.toObject())
+        .to.deep.equal(instantAssetLockProofJS.toObject());
+    });
+  });
+
+  describe('#toJSON', () => {
+    it('should return correct JSON', () => {
+      expect(instantAssetLockProof.toJSON())
+        .to.deep.equal(instantAssetLockProofJS.toJSON());
+    });
+  });
 });
