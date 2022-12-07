2cc7e649664a11d40c7e62ebb45d75e1fce3443e
Ivan Shumkov <ivan@shumkov.ru>
2022-12-07 22:44:17 +0800

    Update finalizeBlockHandlerFactory.js

--- packages/js-drive/lib/abci/handlers/finalizeBlockHandlerFactory.js
+++ packages/js-drive/lib/abci/handlers/finalizeBlockHandlerFactory.js
@@ -135,7 +135,7 @@ function finalizeBlockHandlerFactory(
       {
         timings: blockExecutionTimings,
       },
-      `Block #${height} finalized in ${round + 1} rounds and ${blockExecutionTimings} seconds`,
+      `Block #${height} finalized in ${round + 1} round(s) and ${blockExecutionTimings} seconds`,
     );
 
     return new ResponseFinalizeBlock();
