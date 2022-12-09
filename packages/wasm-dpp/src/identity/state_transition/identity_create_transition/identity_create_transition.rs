8a777650f80ae63ca8699e72a360aec28fd5692a
markin.io <markin.io210@gmail.com>
2022-12-09 16:10:03 +0400

    feat(wasm-dpp): IdentityCreateTransition setPublicKeys, getPublicKeys, addPublicKeys, getIdentityId, getOwnerId

--- packages/wasm-dpp/src/identity/state_transition/identity_create_transition/identity_create_transition.rs
+++ packages/wasm-dpp/src/identity/state_transition/identity_create_transition/identity_create_transition.rs
@@ -47,18 +47,6 @@ impl IdentityCreateTransitionWasm {
         Ok(identity_create_transition.into())
     }
 
-    #[wasm_bindgen(getter)]
-    #[wasm_bindgen(js_name=publicKeys)]
-    pub fn public_keys(&self) -> Vec<JsValue> {
-        self.0
-            .get_public_keys()
-            .iter()
-            .map(IdentityPublicKey::to_owned)
-            .map(IdentityPublicKeyWasm::from)
-            .map(JsValue::from)
-            .collect()
-    }
-
     #[wasm_bindgen(js_name=setAssetLockProof)]
     pub fn set_asset_lock_proof(&mut self, asset_lock_proof: JsValue) -> Result<(), JsValue> {
         let asset_lock_proof = AssetLockProofWasm::new(asset_lock_proof)?;
@@ -70,8 +58,7 @@ impl IdentityCreateTransitionWasm {
         Ok(())
     }
 
-    #[wasm_bindgen(getter)]
-    #[wasm_bindgen(js_name=assetLockProof)]
+    #[wasm_bindgen(getter, js_name=assetLockProof)]
     pub fn asset_lock_proof(&self) -> JsValue {
         self.get_asset_lock_proof()
     }
@@ -89,14 +76,76 @@ impl IdentityCreateTransitionWasm {
         }
     }
 
+    #[wasm_bindgen(js_name=setPublicKeys)]
+    pub fn set_public_keys(&mut self, public_keys: Vec<JsValue>) -> Result<(), JsValue> {
+        let public_keys = public_keys
+            .into_iter()
+            .map(|key| IdentityPublicKeyWasm::new(key))
+            .collect::<Result<Vec<IdentityPublicKeyWasm>, _>>()?;
+
+        self.0
+            .set_public_keys(public_keys.into_iter().map(|key| key.into()).collect());
+
+        // TODO: consider returning self as it's done in the internal set_public_keys method
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name=addPublicKeys)]
+    pub fn add_public_keys(&mut self, public_keys: Vec<JsValue>) -> Result<(), JsValue> {
+        let public_keys_wasm: Vec<IdentityPublicKeyWasm> = public_keys
+            .into_iter()
+            .map(|key| IdentityPublicKeyWasm::new(key))
+            .collect::<Result<Vec<IdentityPublicKeyWasm>, _>>()?;
+
+        let mut public_keys = public_keys_wasm
+            .into_iter()
+            .map(|key| key.into())
+            .collect::<Vec<IdentityPublicKey>>();
+
+        self.0.add_public_keys(&mut public_keys);
+
+        // TODO: consider returning self as it's done in the internal add_public_keys method
+        Ok(())
+    }
+
+    #[wasm_bindgen(js_name=getPublicKeys)]
+    pub fn get_public_keys(&self) -> Vec<JsValue> {
+        self.0
+            .get_public_keys()
+            .iter()
+            .map(IdentityPublicKey::to_owned)
+            .map(IdentityPublicKeyWasm::from)
+            .map(JsValue::from)
+            .collect()
+    }
+
+    #[wasm_bindgen(getter, js_name=publicKeys)]
+    pub fn public_keys(&self) -> Vec<JsValue> {
+        self.get_public_keys()
+    }
+
     #[wasm_bindgen(js_name=getType)]
     pub fn get_type(&self) -> u8 {
         IdentityCreateTransition::get_type() as u8
     }
 
-    #[wasm_bindgen(getter)]
-    #[wasm_bindgen(js_name=identityId)]
+    #[wasm_bindgen(getter, js_name=identityId)]
     pub fn identity_id(&self) -> IdentifierWrapper {
+        self.get_identity_id()
+    }
+
+    #[wasm_bindgen(js_name=getIdentityId)]
+    pub fn get_identity_id(&self) -> IdentifierWrapper {
         self.0.get_identity_id().clone().into()
     }
+
+    #[wasm_bindgen(js_name=getOwnerId)]
+    pub fn get_owner_id(&self) -> IdentifierWrapper {
+        self.0.get_owner_id().clone().into()
+    }
+
+    // pub fn to_object(&self) -> JsValue {
+    // let json_object = self.0.to_json_object()
+    // serde_wasm_bindgen::to_value(&self.0).unwrap()
+    // }
 }
