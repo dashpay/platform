c04d940c8e20c5c2a9c127135214cb9900c7aa4f
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2022-12-13 16:19:22 +0400

    fix formatting issue

--- packages/wasm-dpp/src/data_contract/errors/mod.rs
+++ packages/wasm-dpp/src/data_contract/errors/mod.rs
@@ -22,8 +22,11 @@ pub fn from_data_contract_to_js_error(e: DataContractError) -> JsValue {
         DataContractError::InvalidDocumentTypeError {
             doc_type,
             data_contract,
-        } => invalid_document_type::InvalidDocumentTypeInDataContractError::new(doc_type, data_contract.into())
-            .into(),
+        } => invalid_document_type::InvalidDocumentTypeInDataContractError::new(
+            doc_type,
+            data_contract.into(),
+        )
+        .into(),
         _ => todo!(),
     }
 }
