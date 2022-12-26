da682aaef39d1256240ad91e4e5e66bffc83a4cb
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2022-12-26 13:25:29 +0400

    remove unused code

--- packages/wasm-dpp/lib/AbstractConsensusError.ts
+++ packages/wasm-dpp/lib/AbstractConsensusError.ts
@@ -1,7 +1,5 @@
 import { DPPError } from './DPPError'
 
-//const CONSTRUCTOR_ARGUMENTS_SYMBOL = Symbol.for('constructorArguments');
-
 /**
  * @abstract
  */
@@ -11,46 +9,5 @@ export class AbstractConsensusError extends DPPError {
      */
     constructor(message: string) {
         super(message);
-
-        // this[CONSTRUCTOR_ARGUMENTS_SYMBOL] = [];
     }
-
-    // /**
-    //  * @return {number}
-    //  */
-    // getCode() {
-    //     // Mitigate recursive dependency
-    //
-    //     // eslint-disable-next-line global-require
-    //     const codes = require('./codes');
-    //
-    //     const code = Object.keys(codes)
-    //         .find((c) => this.constructor === codes[c]);
-    //
-    //     if (!code) {
-    //         throw new Error('Error code is not defined');
-    //     }
-    //
-    //     return parseInt(code, 10);
-    // }
-    //
-    // /**
-    //  * Get array of the error's arguments
-    //  *
-    //  * @returns {*[]}
-    //  */
-    // getConstructorArguments() {
-    //     return this[CONSTRUCTOR_ARGUMENTS_SYMBOL];
-    // }
-    //
-    // /**
-    //  * Set the error's arguments.
-    //  * Must be called from the constructor
-    //  *
-    //  * @protected
-    //  * @param {Object|Array} args
-    //  */
-    // setConstructorArguments(args) {
-    //     this[CONSTRUCTOR_ARGUMENTS_SYMBOL] = Array.from(args);
-    // }
 }
