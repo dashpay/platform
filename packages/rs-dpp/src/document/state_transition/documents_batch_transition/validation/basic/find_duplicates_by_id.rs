9dbf47c207dd2a3ede30faf89fb33af57a60c7bf
Djavid Gabibyan <djavidoff@gmail.com>
2023-01-06 17:30:30 +0500

    chore: wip

--- packages/rs-dpp/src/document/state_transition/documents_batch_transition/validation/basic/find_duplicates_by_id.rs
+++ packages/rs-dpp/src/document/state_transition/documents_batch_transition/validation/basic/find_duplicates_by_id.rs
@@ -35,12 +35,13 @@ fn create_fingerprint(document_transition: &JsonValue) -> Option<String> {
     Some(format!(
         "{}:{}",
         document_transition.as_object()?.get("$type")?,
-        document_transition.as_object()?.get("id")?,
+        document_transition.as_object()?.get("$id")?,
     ))
 }
 
 #[cfg(test)]
 mod test {
+    use crate::document::document_transition::{DocumentTransitionObjectLike, JsonValue};
     use crate::{
         document::document_transition::{
             DocumentCreateTransition, DocumentDeleteTransition, DocumentReplaceTransition,
@@ -48,7 +49,6 @@ mod test {
         },
         tests::utils::generate_random_identifier_struct,
     };
-    use crate::document::document_transition::{DocumentTransitionObjectLike, JsonValue};
 
     use super::find_duplicates_by_id;
 
