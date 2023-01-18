c3bd3a1b35071c2349a2b335b33f7b4189acc107
Quantum Explorer <quantum@dash.org>
2023-01-18 13:09:07 +0700

    added in test for 10 proved full identities

--- packages/rs-drive/src/drive/identity/fetch/full_identity.rs
+++ packages/rs-drive/src/drive/identity/fetch/full_identity.rs
@@ -363,6 +363,109 @@ mod tests {
             let path_query = Drive::full_identities_query(identities.keys().copied().collect())
                 .expect("expected to get query");
 
+            let query = path_query.query.query.clone();
+            assert_eq!(path_query.path, Vec::<Vec<u8>>::new()); // it splits at the root
+            assert_eq!(query.items.len(), 2); // 32 (Identities) and 96 (Balances)
+
+            let conditional_subqueries = query
+                .conditional_subquery_branches
+                .expect("expected to have conditional subqueries");
+
+            assert_eq!(conditional_subqueries.len(), 2); // 32 (Identities) and 96 (Balances)
+
+            let (_, identity_conditional_subquery) = conditional_subqueries.last().unwrap();
+            let (_, balance_conditional_subquery) = conditional_subqueries.first().unwrap();
+
+            // Lets start with balance
+            // There should be no subquery path
+
+            assert_eq!(balance_conditional_subquery.subquery_path, None);
+
+            // There should be a subquery pointing to the identity id
+
+            let balance_subquery = *balance_conditional_subquery
+                .subquery
+                .clone()
+                .expect("expected balances to have a subquery");
+
+            assert_eq!(balance_subquery.conditional_subquery_branches, None);
+
+            assert_eq!(balance_subquery.items.len(), 10);
+            assert_eq!(
+                balance_subquery
+                    .items
+                    .into_iter()
+                    .map(|query_item| {
+                        query_item
+                            .keys()
+                            .expect("expected to get keys of query item")
+                            .first()
+                            .unwrap()
+                            .clone()
+                            .try_into()
+                            .unwrap()
+                    })
+                    .collect::<Vec<[u8; 32]>>(),
+                identities.keys().copied().collect::<Vec<[u8; 32]>>()
+            );
+
+            // Moving on to Identity subquery
+
+            // The subquery path is our identity
+            assert_eq!(identity_conditional_subquery.subquery_path, None,);
+
+            // We should have 10 conditional subqueries
+
+            let identities_subquery = *identity_conditional_subquery
+                .subquery
+                .clone()
+                .expect("expected identities to have a subquery");
+
+            assert_eq!(identities_subquery.items.len(), 10); // This query is for the 10 identities
+
+            let identities_conditional_subqueries = identities_subquery
+                .conditional_subquery_branches
+                .expect("expected to have conditional subqueries");
+
+            // We only subquery the keys
+            assert_eq!(identities_conditional_subqueries.len(), 10);
+
+            // Let's check one out
+            let (_, identity_conditional_subquery) =
+                identities_conditional_subqueries.first().unwrap();
+
+            let identity_subquery = *identity_conditional_subquery
+                .subquery
+                .clone()
+                .expect("expected identities to have a subquery");
+
+            assert_eq!(identity_subquery.items.len(), 2); // This query is for the Revision 0 and Keys 1
+
+            let identity_conditional_subqueries = identity_subquery
+                .conditional_subquery_branches
+                .expect("expected to have conditional subqueries");
+
+            // We only subquery the keys
+            assert_eq!(identity_conditional_subqueries.len(), 1);
+
+            let (_, identity_keys_conditional_subquery) =
+                identity_conditional_subqueries.first().unwrap();
+
+            assert_eq!(identity_keys_conditional_subquery.subquery_path, None);
+
+            // We are requesting all keys
+            //todo: maybe we shouldn't be
+
+            let identity_keys_subquery = *identity_keys_conditional_subquery
+                .subquery
+                .clone()
+                .expect("expected identities to have a subquery");
+
+            assert_eq!(
+                identity_keys_subquery.items.first().unwrap(),
+                &QueryItem::RangeFull(RangeFull)
+            );
+
             let (elements, _) = drive
                 .grove
                 .query_raw(
@@ -373,7 +476,7 @@ mod tests {
                 )
                 .unwrap()
                 .expect("expected to run the path query");
-            assert_eq!(elements.len(), 14);
+            assert_eq!(elements.len(), 70);
 
             let fetched_identities = drive
                 .fetch_proved_full_identities(identities.keys().copied().collect(), None)
@@ -384,7 +487,7 @@ mod tests {
                 .expect("expected to verify query");
 
             // We want to get a proof on the balance, the revision and 5 keys
-            assert_eq!(proof.len(), 14);
+            assert_eq!(proof.len(), 70);
         }
     }
 
