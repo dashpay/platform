90aae5a6de5a8513e54066a39db7069c43bb6630
markin.io <markin.io210@gmail.com>
2023-01-18 13:30:06 +0000

    feat(wasm-dpp): apply_identity_topup_transition.rs and applyIdentityTopUpTransitionFactory.spec.js

---
+++ packages/wasm-dpp/src/identity/state_transition/identity_topup_transition/apply_identity_topup_transition.rs
@@ -0,0 +1,31 @@
+use dpp::identity::state_transition::asset_lock_proof::AssetLockTransactionOutputFetcher;
+use dpp::identity::state_transition::identity_topup_transition::ApplyIdentityTopUpTransition;
+use std::sync::Arc;
+use wasm_bindgen::prelude::*;
+
+use crate::{
+    state_repository::{ExternalStateRepositoryLike, ExternalStateRepositoryLikeWrapper},
+    IdentityTopUpTransitionWasm,
+};
+
+#[wasm_bindgen(js_name=applyIdentityTopUpTransition)]
+pub async fn apply_identity_topup_transition_wasm(
+    state_repository: ExternalStateRepositoryLike,
+    state_transition: &IdentityTopUpTransitionWasm,
+) -> Result<(), JsValue> {
+    let wrapped_state_repository =
+        Arc::new(ExternalStateRepositoryLikeWrapper::new(state_repository));
+
+    let tx_output_fetcher =
+        AssetLockTransactionOutputFetcher::new(wrapped_state_repository.clone());
+
+    let instance =
+        ApplyIdentityTopUpTransition::new(wrapped_state_repository, Arc::new(tx_output_fetcher));
+
+    instance
+        .apply(&state_transition.to_owned().into())
+        .await
+        .map_err(|e| JsValue::from_str(&e.to_string()))?;
+
+    Ok(())
+}
--- packages/wasm-dpp/src/identity/state_transition/identity_topup_transition/mod.rs
+++ packages/wasm-dpp/src/identity/state_transition/identity_topup_transition/mod.rs
@@ -1,7 +1,7 @@
 pub use identity_topup_transition::*;
 // pub use validation::*;
 
-// pub mod apply_identity_create_transition;
+pub mod apply_identity_topup_transition;
 pub mod identity_topup_transition;
 // pub mod validation;
 
