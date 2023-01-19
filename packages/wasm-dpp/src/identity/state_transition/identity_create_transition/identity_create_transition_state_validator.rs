af5c81c6bfd093df89139525f2f31b1426ac8bb6
markin.io <markin.io210@gmail.com>
2023-01-19 16:19:24 +0000

    fix(wasm-dpp): state_transition.to_owned().clone()

--- packages/wasm-dpp/src/identity/state_transition/identity_create_transition/identity_create_transition_state_validator.rs
+++ packages/wasm-dpp/src/identity/state_transition/identity_create_transition/identity_create_transition_state_validator.rs
@@ -27,7 +27,7 @@ impl IdentityCreateTransitionStateValidator {
     ) -> Result<ValidationResultWasm, JsValue> {
         let validation_result = validate_identity_create_transition_state(
             &self.state_repository,
-            state_transition.to_owned().clone().into(),
+            state_transition.to_owned().into(),
         )
         .await
         .map_err(|e| from_dpp_err(e.into()))?;
