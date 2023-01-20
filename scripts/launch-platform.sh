1646815dae35a369b6c758e2c66af02dcb5826f4
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-01-20 11:57:42 +0100

    build(scripts): adjust launch-platform.sh to work with platform 0.24

--- scripts/launch-platform.sh
+++ scripts/launch-platform.sh
@@ -11,12 +11,13 @@ $0
 This is a one-liner script to set up your test instance of Dash Platform inside a VM.
 It will start a new Ubuntu 22.04 VM using multipass, and provision local Platform instance.
 
-You will need:
+It uses:
 
 - ubuntu 22.04 (recommended)
 - multipass
-- 4 GB RAM
-- 20 GB HDD
+- 4 CPU cores
+- 8 GB RAM
+- 50 GB HDD
 
 Usage:
 
@@ -25,6 +26,8 @@ $0 [--branch BRANCHNAME] [--debug] [--mode MODE] [--name VMNAME] [--repo /path/t
 where:
 
 * --branch, -b - name of Platform branch to use; defaults to 'master'
+* --cpus N - use N CPUs (defaults to 2)
+* --cache DIR - use DIR to cache some files
 * --debug, -d - more verbose output
 * --mode - mode of operation:
    * multipass - start Dash Platform using multipass
@@ -36,21 +39,46 @@ where:
 EOF
 }
 
+
+YARN_CACHE=/cache/yarn
+DOWNLOADS_CACHE=/cache/downloads
+CPUS=4
+DISK=50G
+MEM=8G
+
 function start_multipass {
     if ! which multipass >/dev/null; then
         echo 'Please install multipass; see https://multipass.run/ for more details.'
         exit 1
     fi
 
-    multipass launch --cpus 2 --disk 20G --mem 4G --name "${VMNAME}" 22.04
+    multipass launch --cpus "${CPUS}" --disk "${DISK}" --mem "${MEM}" --name "${VMNAME}" 22.04
+
+    if [[ -n "${CACHE}" ]]; then
+        mkdir -p "${CACHE}"
+        multipass mount "${CACHE}" "${VMNAME}:/cache"
+    fi
 
     if [[ -n "${REPO}" ]]; then
         multipass mount "${REPO}" "${VMNAME}:/home/ubuntu/platform"
     fi
 
     multipass transfer "$0" "${VMNAME}:/home/ubuntu/setup-multipass.sh"
-    multipass exec "${VMNAME}" -- /bin/bash /home/ubuntu/setup-multipass.sh --mode 'local' --name "${VMNAME}"
+    multipass exec "${VMNAME}" -- /bin/bash /home/ubuntu/setup-multipass.sh --mode 'local' --name "${VMNAME}" --branch "${BRANCH}"
 }
+
+
+function setup_cache {
+    sudo mkdir -p /cache/downloads /cache/yarn /cache/apt-archives
+    sudo chown ${UID}:0 /cache/{downloads,yarn}
+    chmod 770  /cache/{downloads,yarn}
+
+    sudo chown root:root /cache/apt-archives
+    sudo chmod 755 /cache/apt-archives
+    sudo rm -r /var/cache/apt/archives
+    sudo ln -s  /cache/apt-archives /var/cache/apt/archives || true
+}
+
 
 function cleanup_multipass {
     if ! which multipass >/dev/null; then
@@ -69,6 +97,16 @@ function parse_args {
         -b | --branch)
             shift
             BRANCH="$1"
+            shift
+            ;;
+        --cache)
+            shift
+            CACHE="$(realpath "$1")"
+            shift
+            ;;
+        --cpus)
+            shift
+            CPUS="$1"
             shift
             ;;
         -d | --debug)
@@ -112,6 +150,20 @@ function parse_args {
 
 }
 
+# Download with caching
+# Usage: download SRC_URL FILENAME
+function download {
+    FILENAME="$2"
+    if [[ -z "${FILENAME}" ]] ; then
+        FILENAME="$(basename "$1")"
+    fi
+    if [[ ! -f "${DOWNLOADS_CACHE}/${FILENAME}" ]] ; then
+        wget --continue -O "${DOWNLOADS_CACHE}/${FILENAME}" "$1"
+    fi
+}
+
+
+
 function install_deps {
     export DEBIAN_FRONTEND=noninteractive
     export TZ=UTC
@@ -128,7 +180,17 @@ function install_deps {
         ca-certificates \
         curl \
         gnupg \
-        lsb-release
+        lsb-release \
+        libssl-dev \
+        clang
+}
+
+function install_rust {
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain none -y
+    bash -l -c "rustup install stable" # nightly
+    bash -l -c "rustup target add wasm32-unknown-unknown"
+#    sudo apt-get --yes install rustc cargo
+#    sudo apt-get --yes install --no-install-recommends librust-cargo+openssl-dev
 }
 
 function install_docker {
@@ -136,24 +198,17 @@ function install_docker {
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-    sudo apt-get update && sudo apt-get install --yes docker-ce docker-ce-cli containerd.io
+    sudo apt-get update && sudo apt-get install --yes docker-ce docker-ce-cli containerd.io docker-compose-plugin
     sudo usermod -aG docker "$USER"
 }
 
-function install_compose {
-    # Docker Compose
-    wget -O docker-compose https://github.com/docker/compose/releases/download/1.29.2/docker-compose-Linux-x86_64
-    sudo mv docker-compose /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-}
-
 function install_nodejs {
-    wget https://nodejs.org/dist/v16.13.1/node-v16.13.1-linux-x64.tar.xz
-    tar xJf node-v16.13.1-linux-x64.tar.xz
-    rm -f node-v16.13.1-linux-x64.tar.xz
+    download https://nodejs.org/dist/v16.13.1/node-v16.13.1-linux-x64.tar.xz node.tar.xz
+    tar xJf ${DOWNLOADS_CACHE}/node.tar.xz
     sudo mv node-v16.13.1-linux-x64 /opt/node
     sudo ln -s /opt/node/bin/* /usr/local/bin/
     sudo corepack enable
+    yarn config set cache-folder "${YARN_CACHE}"
 }
 
 function install_platform {
@@ -162,18 +217,21 @@ function install_platform {
     fi
     cd /home/ubuntu/platform
     # sudo to ensure our group membership is updated to include `docker` group
-    sudo -u "$USER" yarn install
-    sudo -u "$USER" yarn build
+    sudo -u "$USER" bash -l -c "yarn install"
+    sudo -u "$USER" bash -l -c "yarn build"
 }
 
 function start_platform {
-    sudo -u "$USER" yarn setup
-    sudo -u "$USER" yarn start
+    sudo -u "$USER" bash -l -c "yarn setup"
+    sudo -u "$USER" bash -l -c "yarn start"
 }
 
 function debug {
     [[ -n "$DEBUG" ]] && echo "$@"
 }
+
+
+### MAIN CODE ###
 
 parse_args "$@"
 
@@ -186,9 +244,10 @@ cleanup)
     cleanup_multipass
     ;;
 local)
+    setup_cache
     install_deps
+    install_rust
     install_docker
-    install_compose
     install_nodejs
     install_platform
     start_platform
