e02e29e44721d9f4ac66addd02f57ddaafb92d0c
Pawel Iwan <pawel.iwan@dash.org>
2023-02-10 06:49:06 +0100

    feat(dpp): integration tests for wasm-dpp document transitions <!--- Provide a general summary of your changes in the Title above --> <!--- Pull request titles must use the [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#summary) format -->
    ## Issue being fixed or feature implemented
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    
    ## What was done?
    <!--- Describe your changes in detail -->
    
    ## How Has This Been Tested?
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Breaking Changes
    <!--- Please describe any breaking changes your code introduces and verify that -->
    <!--- the title includes "!" following the conventional commit type (e.g. "feat!: ..."-->
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have added or updated relevant unit/integration/functional/e2e tests
    - [ ] I have made corresponding changes to the documentation
    
    **For repository code-owners and collaborators only**
    - [ ] I have assigned this pull request to a milestone

---
+++ packages/wasm-dpp/src/document/generate_document_id.rs
@@ -0,0 +1,23 @@
+use std::convert::TryInto;
+
+use dpp::document::generate_document_id::generate_document_id;
+use js_sys::Array;
+use wasm_bindgen::prelude::*;
+
+use crate::{
+    console_log,
+    identifier::{identifier_from_js_value, IdentifierWrapper},
+};
+
+#[wasm_bindgen(js_name=generateDocumentId)]
+pub fn generate_document_id_wasm(
+    contract_id: &JsValue,
+    owner_id: &JsValue,
+    document_type: String,
+    entropy: Vec<u8>,
+) -> Result<IdentifierWrapper, JsValue> {
+    let contract_id = identifier_from_js_value(contract_id)?;
+    let owner_id = identifier_from_js_value(owner_id)?;
+    let id = generate_document_id(&contract_id, &owner_id, &document_type, &entropy);
+    Ok(id.into())
+}
