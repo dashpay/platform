0c2e5b6ec6814424745f62cab6c9ca98c6e1f718
Ivan Shumkov <ivan@shumkov.ru>
2023-02-21 21:10:14 +0800

    chore: implement generateTenderdashNodeKeyAndId

--- packages/dashmate/src/tenderdash/generateTenderdashNodeKeyAndId.js
+++ packages/dashmate/src/tenderdash/generateTenderdashNodeKeyAndId.js
@@ -1,10 +1,37 @@
+const { derivePath, getPublicKey } = require('ed25519-hd-key');
+
+const crypto = require('crypto');
+
 /**
  * @typedef generateTenderdashNodeKeyAndId
- * @returns {{}}
+ * @returns {{
+ *   privateKey: Buffer,
+ *   publicKey: Buffer,
+ *   key: string,
+ *   id: string,
+ * }}
  */
 function generateTenderdashNodeKeyAndId() {
-  return {
+  const derivationPath = "m/9'/5'/3'/4'/0'";
 
+  const seed = crypto.randomBytes(25);
+
+  const { key: privateKey } = derivePath(derivationPath, seed.toString('hex'));
+
+  const publicKey = getPublicKey(privateKey).slice(1);
+
+  const key = Buffer.concat([privateKey, publicKey]);
+
+  const id = crypto.createHash('sha256')
+    .update(publicKey)
+    .digest('hex')
+    .slice(0, 40);
+
+  return {
+    privateKey,
+    publicKey,
+    key: key.toString('base64'),
+    id,
   };
 }
 
