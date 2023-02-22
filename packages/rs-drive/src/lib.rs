16229eb9aa9568ea0ff050f872ddcf73bbd56364
Wisdom Ogwu <iammadab@gmail.com>
2023-02-22 09:41:09 +0100

    move all code logic to the full feature

--- packages/rs-drive/src/lib.rs
+++ packages/rs-drive/src/lib.rs
@@ -8,25 +8,35 @@
 #![forbid(unsafe_code)]
 #![deny(missing_docs)]
 
+#[cfg(feature = "full")]
 extern crate core;
 
+#[cfg(feature = "full")]
 pub mod common;
 /// Contract module
+#[cfg(feature = "full")]
 pub mod contract;
 /// Drive module
+#[cfg(feature = "full")]
 pub mod drive;
 /// Error module
+#[cfg(feature = "full")]
 pub mod error;
 /// Fee module
+#[cfg(feature = "full")]
 pub mod fee;
 /// Fee pools module
+#[cfg(feature = "full")]
 pub mod fee_pools;
 /// Query module
+#[cfg(feature = "full")]
 pub mod query;
 
 /// DPP module
+#[cfg(feature = "full")]
 pub use dpp;
 /// GroveDB module
+#[cfg(feature = "full")]
 pub use grovedb;
 
 /// Test helpers
