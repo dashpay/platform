a18b1de685a2363569166b9e8dce5ff2b380cb7e
Evgeny Fomin <fominok@hotmail.com>
2023-02-23 19:45:23 +0100

    cleanup

--- packages/wasm-dpp/test/integration/dataContract/stateTransition/DataContractCreateTransition/validation/basic/validateDataContractCreateTransitionBasicFactory.spec.js
+++ packages/wasm-dpp/test/integration/dataContract/stateTransition/DataContractCreateTransition/validation/basic/validateDataContractCreateTransitionBasicFactory.spec.js
@@ -1,45 +1,18 @@
-const dataContractMetaSchema = require('@dashevo/dpp/schema/dataContract/dataContractMeta.json');
-
 const crypto = require('crypto');
-
-// const { getRE2Class } = require('@dashevo/wasm-re2');
-
-// const createAjv = require('@dashevo/dpp/lib/ajv/createAjv');
-
-//const JsonSchemaValidator = require('@dashevo/dpp/lib/validation/JsonSchemaValidator');
-
 const protocolVersion = require('@dashevo/dpp/lib/version/protocolVersion');
 
-const { expectJsonSchemaError, expectValidationError } = require('../../../../../../../lib/test/expect/expectError');
-
-const validateDataContractCreateTransitionBasicFactory = require('@dashevo/dpp/lib/dataContract/stateTransition/DataContractCreateTransition/validation/basic/validateDataContractCreateTransitionBasicFactory');
-
-//const DataContractCreateTransition = require('@dashevo/dpp/lib/dataContract/stateTransition/DataContractCreateTransition/DataContractCreateTransition');
-
 const getDataContractFixture = require('@dashevo/dpp/lib/test/fixtures/getDataContractFixture');
-
-// const {
-//   expectValidationError,
-//   expectJsonSchemaError,
-// } = require('@dashevo/dpp/lib/test/expect/expectError');
-
-// const ValidationResult = require('@dashevo/dpp/lib/validation/ValidationResult');
-
-// const InvalidDataContractIdError = require('@dashevo/dpp/lib/errors/consensus/basic/dataContract/InvalidDataContractIdError');
-// const SomeConsensusError = require('@dashevo/dpp/lib/test/mocks/SomeConsensusError');
+const { expectJsonSchemaError, expectValidationError } = require('../../../../../../../lib/test/expect/expectError');
 
 const { default: loadWasmDpp } = require('../../../../../../../dist');
 
 describe('validateDataContractCreateTransitionBasicFactory', () => {
-  // let validateDataContractMock;
   let stateTransition;
   let rawStateTransition;
   let dataContract;
   let rawDataContract;
-  // let validateProtocolVersionMock;
 
   let DataContractCreateTransition;
-  let ProtocolVersionValidator;
   let validateDataContractCreateTransitionBasic;
   let ValidationResult;
   let ProtocolVersionParsingError;
@@ -55,10 +28,7 @@ describe('validateDataContractCreateTransitionBasicFactory', () => {
     } = await loadWasmDpp());
   });
 
-  beforeEach(async function beforeEach() {
-    // validateDataContractMock = this.sinonSandbox.stub().returns(new ValidationResult());
-    // validateProtocolVersionMock = this.sinonSandbox.stub().returns(new ValidationResult());
-
+  beforeEach(async () => {
     dataContract = getDataContractFixture();
     rawDataContract = dataContract.toObject();
 
@@ -71,18 +41,6 @@ describe('validateDataContractCreateTransitionBasicFactory', () => {
     });
 
     rawStateTransition = stateTransition.toObject();
-
-    // const RE2 = await getRE2Class();
-    // const ajv = createAjv(RE2);
-    // const protocolVersionValidator = new ProtocolVersionValidator();
-    // const jsonSchemaValidator = new JsonSchemaValidator(dataContractMetaSchema);
-
-    // eslint-disable-next-line max-len
-    // validateDataContractCreateTransitionBasic = validateDataContractCreateTransitionBasicFactory(
-    //   jsonSchemaValidator,
-    //   validateDataContractMock,
-    //   validateProtocolVersionMock,
-    // );
   });
 
   describe('protocolVersion', () => {
@@ -116,16 +74,7 @@ describe('validateDataContractCreateTransitionBasicFactory', () => {
     it('should be valid', async () => {
       rawStateTransition.protocolVersion = -1;
 
-      // const protocolVersionError = new SomeConsensusError('test');
-      // const protocolVersionResult = new ValidationResult([
-      //   protocolVersionError,
-      // ]);
-
-      // validateProtocolVersionMock.returns(protocolVersionResult);
-
       const result = await validateDataContractCreateTransitionBasic(rawStateTransition);
-
-      // expectValidationError(result, SomeConsensusError);
 
       const [error] = result.getErrors();
       expect(error).to.be.an.instanceOf(ProtocolVersionParsingError);
@@ -178,34 +127,13 @@ describe('validateDataContractCreateTransitionBasicFactory', () => {
     });
 
     it('should be valid', async () => {
-      // const dataContractError = new SomeConsensusError('test');
-      // const dataContractResult = new ValidationResult([
-      //   dataContractError,
-      // ]);
-
-      // validateDataContractMock.returns(dataContractResult);
-
       const result = await validateDataContractCreateTransitionBasic(rawStateTransition);
-      
+
       expect(result).to.be.an.instanceOf(ValidationResult);
       expect(result.isValid()).to.be.true();
-
-      // await expectValidationError(result);
-
-      // const [error] = result.getErrors();
-
-      // console.log(error.getCode());
-
-      // expect(error).to.equal(dataContractError);
-
-      // expect(validateDataContractMock.getCall(0).args).to.have.deep.members([rawDataContract]);
     });
 
     it('should return invalid result on invalid Data Contract id', async () => {
-      // const dataContractResult = new ValidationResult();
-
-      // validateDataContractMock.returns(dataContractResult);
-
       const expectedId = Buffer.from(rawStateTransition.dataContract.$id);
       rawStateTransition.dataContract.$id = Buffer.from(crypto.randomBytes(32));
 
@@ -219,7 +147,6 @@ describe('validateDataContractCreateTransitionBasicFactory', () => {
       expect(error.getExpectedId()).to.deep.equal(expectedId);
       expect(error.getInvalidId()).to.deep.equal(rawStateTransition.dataContract.$id);
       expect(error).to.be.an.instanceOf(InvalidDataContractIdError);
-
     });
   });
 
