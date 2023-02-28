f160b3c540931e29edf9ba9bf4d352cd86c49c84
Pawel Iwan <pawel.iwan@dash.org>
2023-02-28 07:40:09 +0100

    use system data contract

--- packages/rs-dpp/src/tests/fixtures/get_dashpay_contract_fixture.rs
+++ packages/rs-dpp/src/tests/fixtures/get_dashpay_contract_fixture.rs
@@ -1,8 +1,5 @@
 use std::sync::Arc;
 
-use lazy_static::lazy_static;
-use serde_json::Value;
-
 use crate::{
     data_contract::{
         validation::data_contract_validator::DataContractValidator, DataContract,
@@ -13,21 +10,20 @@ use crate::{
     version::{ProtocolVersionValidator, COMPATIBILITY_MAP, LATEST_VERSION},
 };
 
-lazy_static! {
-    static ref DASHPAY_SCHEMA: Value = serde_json::from_str(include_str!(
-        "./../../../../dashpay-contract/schema/dashpay.schema.json"
-    ))
-    .unwrap();
-}
+use data_contracts::SystemDataContract;
 
 pub fn get_dashpay_contract_fixture(owner_id: Option<Identifier>) -> DataContract {
     let protocol_version_validator =
         ProtocolVersionValidator::new(LATEST_VERSION, LATEST_VERSION, COMPATIBILITY_MAP.clone());
     let data_contract_validator = DataContractValidator::new(Arc::new(protocol_version_validator));
     let factory = DataContractFactory::new(1, Arc::new(data_contract_validator));
-
+    let dpns_schema = SystemDataContract::Dashpay
+        .source()
+        .expect("DPNS contract must be defined")
+        .document_schemas;
     let owner_id = owner_id.unwrap_or_else(generate_random_identifier_struct);
+
     factory
-        .create(owner_id, DASHPAY_SCHEMA.clone(), None)
+        .create(owner_id, dpns_schema, None)
         .expect("data in fixture should be correct")
 }
