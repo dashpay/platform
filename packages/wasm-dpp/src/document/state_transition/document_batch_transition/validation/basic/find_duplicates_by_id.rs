4dce677307684ebe3ebdb1d57d79598066500c67
Quantum Explorer <quantum@dash.org>
2023-03-06 20:34:17 +0700

    fix

--- packages/wasm-dpp/src/document/state_transition/document_batch_transition/validation/basic/find_duplicates_by_id.rs
+++ packages/wasm-dpp/src/document/state_transition/document_batch_transition/validation/basic/find_duplicates_by_id.rs
@@ -1,8 +1,12 @@
 use dpp::document::document_transition::{document_base_transition, document_create_transition};
 use dpp::document::validation::basic::find_duplicates_by_id::find_duplicates_by_id;
+use dpp::platform_value::btreemap_field_replacement::BTreeValueMapReplacementPathHelper;
+use dpp::platform_value::{ReplacementType, Value};
+use dpp::ProtocolError;
 use itertools::Itertools;
 use js_sys::Array;
-use serde_json::Value;
+use serde_json::Value as JsonValue;
+use std::convert::TryInto;
 use wasm_bindgen::prelude::*;
 
 use crate::document_batch_transition::document_transition::to_object;
@@ -10,18 +14,21 @@ use crate::utils::{replace_identifiers_with_bytes_without_failing, ToSerdeJSONEx
 
 #[wasm_bindgen(js_name=findDuplicatesById)]
 pub fn find_duplicates_by_id_wasm(js_raw_transitions: Array) -> Result<Vec<JsValue>, JsValue> {
-    let raw_transitions: Vec<Value> = js_raw_transitions
+    let raw_transitions: Vec<JsonValue> = js_raw_transitions
         .iter()
-        .map(|t| {
-            t.with_serde_to_json_value().map(|mut v| {
-                replace_identifiers_with_bytes_without_failing(
-                    &mut v,
-                    document_base_transition::IDENTIFIER_FIELDS,
-                );
-                v
-            })
+        .map(|transition| {
+            let mut map = transition.with_serde_to_platform_value_map()?;
+            map.replace_at_paths(
+                document_base_transition::IDENTIFIER_FIELDS.map(|a| a.to_string()),
+                ReplacementType::Identifier,
+            );
+            let value: Value = map.into();
+            value
+                .try_into()
+                .map_err(ProtocolError::ValueError)
+                .with_js_error()
         })
-        .try_collect()?;
+        .collect::<Result<Vec<JsonValue>, JsValue>>()?;
 
     let result: Vec<JsValue> = find_duplicates_by_id(&raw_transitions)
         .with_js_error()?
