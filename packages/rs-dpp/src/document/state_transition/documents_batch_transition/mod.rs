dbca2eed1d181c302a2343d4ba12faea4be902bd
Pawel Iwan <pawel.iwan@dash.org>
2023-03-08 09:25:56 +0100

    fix invalid merge

--- packages/rs-dpp/src/document/state_transition/documents_batch_transition/mod.rs
+++ packages/rs-dpp/src/document/state_transition/documents_batch_transition/mod.rs
@@ -278,10 +278,6 @@ impl StateTransitionConvert for DocumentsBatchTransition {
 
     fn to_object(&self, skip_signature: bool) -> Result<JsonValue, ProtocolError> {
         let mut json_object: JsonValue = serde_json::to_value(self)?;
-        if self.signature.is_none() {
-            json_object.insert(property_names::SIGNATURE.to_string(), JsonValue::Null);
-        }
-
         json_object
             .replace_identifier_paths(Self::identifiers_property_paths(), ReplaceWith::Bytes)?;
 
