591d5cd3660697bd68775468e29d434f5c34574f
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2023-03-13 16:25:48 +0800

    feat(wasm-dpp): fix `Metadata` and its tests

--- packages/wasm-dpp/test/unit/Metadata.spec.js
+++ packages/wasm-dpp/test/unit/Metadata.spec.js
@@ -1,15 +1,26 @@
-const Metadata = require('@dashevo/dpp/lib/Metadata');
+// const Metadata = require('@dashevo/dpp/lib/Metadata');
+const { default: loadWasmDpp } = require('../..');
+
+let Metadata;
 
 describe('Metadata', () => {
+  beforeEach(async () => {
+    ({ Metadata } = await loadWasmDpp());
+  });
+
   describe('#constructor', () => {
     it('should set height and core chain-locked height', () => {
       const result = new Metadata({
         blockHeight: 42,
         coreChainLockedHeight: 1,
+        timeMs: 100,
+        protocolVersion: 2,
       });
 
-      expect(result.blockHeight).to.equal(42);
-      expect(result.coreChainLockedHeight).to.equal(1);
+      expect(result.getBlockHeight()).to.equal(42);
+      expect(result.getCoreChainLockedHeight()).to.equal(1);
+      expect(result.getTimeMs()).to.equal(100);
+      expect(result.getProtocolVersion()).to.equal(2);
     });
   });
 
@@ -18,6 +29,8 @@ describe('Metadata', () => {
       const result = new Metadata({
         blockHeight: 42,
         coreChainLockedHeight: 1,
+        timeMs: 100,
+        protocolVersion: 2,
       });
 
       expect(result.getBlockHeight()).to.equal(42);
@@ -29,6 +42,8 @@ describe('Metadata', () => {
       const result = new Metadata({
         blockHeight: 1,
         coreChainLockedHeight: 42,
+        timeMs: 100,
+        protocolVersion: 2,
       });
 
       expect(result.getCoreChainLockedHeight()).to.equal(42);
