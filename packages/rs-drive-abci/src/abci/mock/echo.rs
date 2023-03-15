e6bf9e86e07a39168d07b4dce19889f5ff7ca739
Quantum Explorer <quantum@dash.org>
2023-03-15 17:50:58 +0700

    removed unnecessary files

--- packages/rs-drive-abci/src/abci/mock/echo.rs
+++
@@ -1,9 +0,0 @@
-//! Trivial ABCI echo application
-
-use crate::Application;
-
-/// Trivial echo application, mainly for testing purposes.
-#[derive(Clone, Default)]
-pub struct EchoApp;
-
-impl Application for EchoApp {}
--- packages/rs-drive-abci/src/abci/mod.rs
+++ packages/rs-drive-abci/src/abci/mod.rs
@@ -1,7 +1,2 @@
 pub mod handlers;
 pub mod messages;
-pub mod server;
-pub mod codec;
-pub mod client;
-pub mod error;
-pub mod application;
