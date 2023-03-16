fd610ef6390cf4033ac8543fd5d7cab23109952a
markin.io <markin.io210@gmail.com>
2023-03-16 21:24:16 +0000

    feat(wasm-dpp): make queueable wasm dpp loader (don't trigger bundle loading on multiple invocations)

--- packages/wasm-dpp/lib/index.ts
+++ packages/wasm-dpp/lib/index.ts
@@ -5,27 +5,35 @@ import wasmBase from '../wasm/wasm_dpp_bg.js';
 import patchIdentifier from "./identifier/patchIdentifier";
 
 let isInitialized = false;
+let loadingPromise: Promise<void> | null = null;
 
 export default async function loadDpp() {
   if (isInitialized) {
-    return dpp_module;
-  } else {
-    // @ts-ignore
-    let bytes = Buffer.from(wasmBase, 'base64');
-
-    if (typeof fetch !== 'undefined') {
-      let blob = new Blob([bytes], { type: "application/wasm" });
-      let wasmUrl = URL.createObjectURL(blob);
-      await init(wasmUrl);
-      isInitialized = true;
-    } else {
-      dpp_module.initSync(bytes);
-      isInitialized = true;
-    }
-
-    patchIdentifier(dpp_module);
-
-    return dpp_module;
+    return dpp_module
   }
+
+  if (!loadingPromise) {
+    loadingPromise = loadDppModule()
+  }
+
+  await loadingPromise;
+  isInitialized = true;
+  loadingPromise = null;
+  return dpp_module;
 };
+
+const loadDppModule = async () => {
+  // @ts-ignore
+  let bytes = Buffer.from(wasmBase, 'base64');
+
+  if (typeof fetch !== 'undefined') {
+    let blob = new Blob([bytes], { type: "application/wasm" });
+    let wasmUrl = URL.createObjectURL(blob);
+    await init(wasmUrl);
+  } else {
+    dpp_module.initSync(bytes);
+  }
+
+  patchIdentifier(dpp_module);
+}
 
