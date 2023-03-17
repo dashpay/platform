53e912ff7e04f7d91c68aec90afe51c25251ba8b
markin.io <markin.io210@gmail.com>
2023-03-17 11:19:53 +0000

    chore(sdk): add eslint and config

---
+++ packages/js-dash-sdk/.eslintrc.js
@@ -0,0 +1,83 @@
+// A set of rules to easen misuse in the existing codebase
+// We should fix these warnigns when possible
+const warnRules = {
+  'import/prefer-default-export': 'warn',
+  'no-param-reassign': 'warn',
+  'import/no-cycle': 'warn',
+  'import/no-named-default': 'warn',
+  'import/no-named-as-default': 'warn',
+};
+
+// A set of common rules applicable to both JS and TS files
+const commonRules = {
+  'no-await-in-loop': 'off',
+};
+
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  root: true,
+  env: {
+    node: true,
+    mocha: true,
+  },
+  rules: {
+    ...warnRules,
+    ...commonRules,
+  },
+  overrides: [
+    // TypeScript
+    {
+      files: [
+        '**/*.ts',
+      ],
+      extends: [
+        'airbnb-base',
+        'airbnb-typescript/base',
+      ],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      rules: {
+        '@typescript-eslint/return-await': 'warn',
+        ...warnRules,
+        ...commonRules,
+      },
+    },
+    // TypeScript tests
+    {
+      files: [
+        'src/**/*.spec.ts',
+        'src/test/**/*.ts',
+        'tests/**/*.ts',
+      ],
+      rules: {
+        // Ignore dirty-chai errors
+        '@typescript-eslint/no-unused-expressions': 0,
+        // Ignore require('dev-dependency') errors for tests and mocks
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+    // JS tests
+    {
+      files: [
+        'src/test/**/*.js',
+        'tests/**/*.js',
+      ],
+      rules: {
+        // Ignore dirty-chai errors
+        'no-unused-expressions': 0,
+        // Ignore require('dev-dependency') errors for tests and mocks
+        'import/no-extraneous-dependencies': 0,
+      },
+    },
+  ],
+  ignorePatterns: [
+    // TODO: why do we have d.ts files in typescript project at all?
+    '*.d.ts',
+    'build',
+    'dist',
+    'webpack.*.js',
+  ],
+};
