278f2b87c1323eccdb7e72df6d77bd0709a26abf
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2023-03-24 12:28:28 +0800

    useless commit

--- packages/wasm-dpp/src/state_transition/state_transition_facade.rs
+++ packages/wasm-dpp/src/state_transition/state_transition_facade.rs
@@ -14,6 +14,7 @@ use std::sync::Arc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
 
+
 #[derive(Clone)]
 #[wasm_bindgen(js_name = StateTransitionFacade)]
 pub struct StateTransitionFacadeWasm(
