a63646f6b06e80e7d237260ac5466d230760454e
Pawel Iwan <pawel.iwan@dash.org>
2023-03-24 12:27:59 +0100

    fix types in balance fee error

--- packages/rs-dpp/src/errors/consensus/fee.rs
+++ packages/rs-dpp/src/errors/consensus/fee.rs
@@ -5,5 +5,5 @@ use crate::state_transition::fee::Credits;
 #[derive(Error, Debug)]
 pub enum FeeError {
     #[error("Current credits balance {balance} is not enough to pay {fee} fee")]
-    BalanceIsNotEnoughError { balance: u64, fee: Credits },
+    BalanceIsNotEnoughError { balance: Credits, fee: Credits },
 }
