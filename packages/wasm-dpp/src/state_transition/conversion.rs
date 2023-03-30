cd884142c3077eddd8ebcb8977c3d3d0e5119b2d
Djavid Gabibiyan <djavidoff@gmail.com>
2023-03-30 21:37:47 +0500

    chore: `rs-dpp` and `wasm-dpp` updates for integration (#875)
    Co-authored-by: markin.io <markin.io210@gmail.com>

--- packages/wasm-dpp/src/state_transition/conversion.rs
+++ packages/wasm-dpp/src/state_transition/conversion.rs
@@ -3,15 +3,49 @@ use crate::utils::generic_of_js_val;
 use crate::{
     DataContractCreateTransitionWasm, DataContractUpdateTransitionWasm,
     DocumentsBatchTransitionWasm, IdentityCreateTransitionWasm, IdentityTopUpTransitionWasm,
-    IdentityUpdateTransitionWasm,
+    IdentityUpdateTransitionWasm, PreCalculatedOperationWasm, ReadOperationWasm,
+    SignatureVerificationOperationWasm,
 };
 use dpp::consensus::basic::state_transition::InvalidStateTransitionTypeError;
 use dpp::consensus::basic::BasicError;
 use dpp::consensus::ConsensusError;
+use dpp::state_transition::fee::operations::Operation;
 use dpp::state_transition::{StateTransition, StateTransitionType};
 use std::convert::TryInto;
 use wasm_bindgen::__rt::Ref;
 use wasm_bindgen::{JsCast, JsError, JsValue};
+
+pub fn create_operation_from_wasm_instance(js_value: &JsValue) -> Result<Operation, JsValue> {
+    let maybe_signature_verification_operation: Result<
+        Ref<SignatureVerificationOperationWasm>,
+        JsValue,
+    > = generic_of_js_val::<SignatureVerificationOperationWasm>(
+        js_value,
+        "SignatureVerificationOperation",
+    );
+
+    if let Ok(operation) = maybe_signature_verification_operation {
+        return Ok(Operation::SignatureVerification(
+            operation.to_owned().into(),
+        ));
+    }
+
+    let maybe_read_operation: Result<Ref<ReadOperationWasm>, JsValue> =
+        generic_of_js_val::<ReadOperationWasm>(js_value, "ReadOperation");
+
+    if let Ok(operation) = maybe_read_operation {
+        return Ok(Operation::Read(operation.to_owned().into()));
+    }
+
+    let maybe_precalculated_operation: Result<Ref<PreCalculatedOperationWasm>, JsValue> =
+        generic_of_js_val::<PreCalculatedOperationWasm>(js_value, "PreCalculatedOperation");
+
+    if let Ok(operation) = maybe_precalculated_operation {
+        return Ok(Operation::PreCalculated(operation.to_owned().into()));
+    }
+
+    Err(JsError::new("Unknown operation").into())
+}
 
 pub fn create_state_transition_from_wasm_instance(
     js_value: &JsValue,
