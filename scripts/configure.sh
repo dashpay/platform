101a1c8d0d24c0df434ff2e4d46b7be2c9b6c65d
Djavid Gabibiyan <djavidoff@gmail.com>
2023-03-30 20:49:25 +0500

    build: operations in configure.sh script were in a wrong order (#876)

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-source ./scripts/setup_local_network.sh
 source ./scripts/configure_dashmate.sh
+source ./scripts/setup_local_network.sh
 source ./scripts/configure_test_suite.sh
 source ./scripts/configure_dotenv.sh
