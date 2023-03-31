d28b06b39048156bf3544fd5b4a68de9c69c8b5f
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2023-03-31 18:49:06 +0800

    expose apply method through the binding

--- packages/wasm-dpp/src/state_transition/state_transition_facade.rs
+++ packages/wasm-dpp/src/state_transition/state_transition_facade.rs
@@ -15,6 +15,7 @@ use serde::Deserialize;
 use std::sync::Arc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsValue;
+use crate::errors::protocol_error::from_protocol_error;
 
 #[derive(Clone)]
 #[wasm_bindgen(js_name = StateTransitionFacade)]
@@ -213,6 +214,16 @@ impl StateTransitionFacadeWasm {
             .with_js_error()?;
 
         Ok(validation_result.map(|_| JsValue::undefined()).into())
+    }
+
+    #[wasm_bindgen]
+    pub async fn apply(&self, state_transition_js: JsValue) -> Result<JsValue, JsValue> {
+        let state_transition =
+            super::super::conversion::create_state_transition_from_wasm_instance(
+                &state_transition_js,
+            )?;
+
+        self.0.apply(&state_transition).await.map(|_| JsValue::undefined()).map_err(from_protocol_error)
     }
 }
 
