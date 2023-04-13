c43b7c3b29a4eacc5e96718c418e60809b42ecef
Wisdom Ogwu <iammadab@gmail.com>
2023-04-13 22:15:27 +0100

    more work

--- packages/rs-drive-abci/src/execution/masternode_identities.rs
+++ packages/rs-drive-abci/src/execution/masternode_identities.rs
@@ -1,10 +1,15 @@
 use crate::error::Error;
 use crate::platform::Platform;
 use crate::rpc::core::CoreRPCLike;
-use dashcore_rpc::json::{Masternode, MasternodeListItem, ProTxHash, QuorumMasternodeListItem};
+use chrono::Utc;
+use dashcore_rpc::json::{
+    Masternode, MasternodeListItem, ProTxHash, QuorumMasternodeListItem, UpdatedMasternodeItem,
+};
 use dpp::identifier::Identifier;
 use dpp::identity::factory::IDENTITY_PROTOCOL_VERSION;
-use dpp::identity::{Identity, IdentityPublicKey, KeyType, Purpose, SecurityLevel};
+use dpp::identity::{
+    Identity, IdentityPublicKey, KeyID, KeyType, Purpose, SecurityLevel, TimestampMillis,
+};
 use dpp::platform_value::BinaryData;
 use drive::drive::block_info::BlockInfo;
 use drive::grovedb::Transaction;
@@ -21,14 +26,11 @@ where
     fn create_owner_identity(&self, masternode: &MasternodeListItem) -> Result<Identity, Error> {
         let owner_identifier = Self::get_owner_identifier(&masternode)?;
         let mut identity = Self::create_basic_identity(owner_identifier);
-        identity.add_public_keys([self.get_owner_identity_key(&masternode)?]);
+        identity.add_public_keys([Self::get_owner_identity_key(&masternode)?]);
         Ok(identity)
     }
 
-    fn get_owner_identity_key(
-        &self,
-        masternode: &MasternodeListItem,
-    ) -> Result<IdentityPublicKey, Error> {
+    fn get_owner_identity_key(masternode: &MasternodeListItem) -> Result<IdentityPublicKey, Error> {
         Ok(IdentityPublicKey {
             id: 0,
             key_type: KeyType::ECDSA_HASH160,
@@ -40,12 +42,9 @@ where
         })
     }
 
-    fn create_voter_identity(
-        &self,
-        protx_hash: &[u8],
-        masternode: &MasternodeListItem,
-    ) -> Result<Identity, Error> {
-        let voting_identifier = Self::get_voter_identifier(protx_hash, &masternode)?;
+    fn create_voter_identity(&self, masternode: &MasternodeListItem) -> Result<Identity, Error> {
+        let protx_hash = &masternode.protx_hash.0;
+        let voting_identifier = Self::get_voter_identifier(&protx_hash, &masternode)?;
         let mut identity = Self::create_basic_identity(voting_identifier);
         identity.add_public_keys([self.get_voter_identity_key(&masternode)?]);
         Ok(identity)
@@ -66,12 +65,9 @@ where
         })
     }
 
-    fn create_operator_identity(
-        &self,
-        protx_hash: &[u8],
-        masternode: &MasternodeListItem,
-    ) -> Result<Identity, Error> {
-        let operator_identifier = Self::get_operator_identifier(protx_hash, &masternode)?;
+    fn create_operator_identity(&self, masternode: &MasternodeListItem) -> Result<Identity, Error> {
+        let protx_hash = &masternode.protx_hash.0;
+        let operator_identifier = Self::get_operator_identifier(&protx_hash, &masternode)?;
         let mut identity = Self::create_basic_identity(operator_identifier);
         identity.add_public_keys(self.get_operator_identity_keys(&masternode)?);
 
@@ -167,6 +163,162 @@ where
         }
     }
 
+    fn update_owner_identity(
+        &self,
+        masternode: &UpdatedMasternodeItem,
+        block_info: &BlockInfo,
+        transaction: Option<&Transaction>,
+    ) -> Result<(), Error> {
+        // what would cause an error here???
+        // need to check if we need to update the owner identity
+        if masternode.state_diff.payout_address.is_none() {
+            // need better feedback, this is not enough
+            return Ok(());
+        }
+
+        // there is an update
+        // we need to get the public keys to disable
+        // sadly can't pass the updated master node item directly, so have to generate
+        // the owner identifier here again
+        // TODO: fix this!!!!
+        let owner_identifier: [u8; 32] = masternode.protx_hash.clone().0.try_into().unwrap();
+        // we need to get the full identity
+        // TODO: return an actual error if the identity is None, as it should be Some
+        let owner_identity = self
+            .drive
+            .fetch_full_identity(owner_identifier, transaction)?
+            .unwrap();
+        // TODO: extract the diff function
+        // now we need to figure out which of the keys to disable
+        let new_key_id: KeyID = owner_identity
+            .public_keys
+            .last_key_value()
+            .map(|(last_key_id, _)| last_key_id + 1)
+            .unwrap_or(0);
+        let to_disable = owner_identity
+            .public_keys
+            .iter()
+            .filter(|(_, pk)| pk.disabled_at.is_none())
+            .map(|(id, _)| id.clone())
+            .collect::<Vec<KeyID>>();
+        // we need to build the new key
+        // TODO: make generic over the masternode type
+        let new_owner_key = IdentityPublicKey {
+            id: new_key_id,
+            key_type: KeyType::ECDSA_HASH160,
+            purpose: Purpose::WITHDRAW,
+            security_level: SecurityLevel::MASTER,
+            read_only: true,
+            data: BinaryData::new(
+                masternode
+                    .state_diff
+                    .payout_address
+                    .clone()
+                    .expect("confirmed not none"),
+            ),
+            disabled_at: None,
+        };
+        let current_time = Utc::now().timestamp_millis() as TimestampMillis;
+        self.drive.disable_identity_keys(
+            owner_identifier,
+            to_disable,
+            current_time,
+            block_info,
+            true,
+            transaction,
+        );
+        // add the new key
+        self.drive.add_new_non_unique_keys_to_identity(
+            owner_identifier,
+            vec![new_owner_key],
+            block_info,
+            true,
+            transaction,
+        );
+        Ok(())
+    }
+
+    // TODO: factor out duplication
+    //  there is a common thread going on here
+    //  get the next key id and the next public key
+    //  figure out what you want to disable
+    //  to make this generic, you need to pass an optional filter function
+    //  specifically for the opreator identity
+    fn update_voter_identity(
+        &self,
+        masternode: &UpdatedMasternodeItem,
+        block_info: &BlockInfo,
+        transaction: Option<&Transaction>,
+    ) -> Result<(), Error> {
+        // what would cause an error here???
+        // need to check if we need to update the owner identity
+        if masternode.state_diff.voting_address.is_none() {
+            // need better feedback, this is not enough
+            return Ok(());
+        }
+
+        // there is an update
+        // we need to get the public keys to disable
+        // sadly can't pass the updated master node item directly, so have to generate
+        // the owner identifier here again
+        // TODO: fix this!!!!
+        let owner_identifier: [u8; 32] = masternode.protx_hash.clone().0.try_into().unwrap();
+        // we need to get the full identity
+        // TODO: return an actual error if the identity is None, as it should be Some
+        let owner_identity = self
+            .drive
+            .fetch_full_identity(owner_identifier, transaction)?
+            .unwrap();
+        // TODO: extract the diff function
+        // now we need to figure out which of the keys to disable
+        let new_key_id: KeyID = owner_identity
+            .public_keys
+            .last_key_value()
+            .map(|(last_key_id, _)| last_key_id + 1)
+            .unwrap_or(0);
+        let to_disable = owner_identity
+            .public_keys
+            .iter()
+            .filter(|(_, pk)| pk.disabled_at.is_none())
+            .map(|(id, _)| id.clone())
+            .collect::<Vec<KeyID>>();
+        // we need to build the new key
+        // TODO: make generic over the masternode type
+        let new_owner_key = IdentityPublicKey {
+            id: new_key_id,
+            key_type: KeyType::ECDSA_HASH160,
+            purpose: Purpose::WITHDRAW,
+            security_level: SecurityLevel::MASTER,
+            read_only: true,
+            data: BinaryData::new(
+                masternode
+                    .state_diff
+                    .payout_address
+                    .clone()
+                    .expect("confirmed not none"),
+            ),
+            disabled_at: None,
+        };
+        let current_time = Utc::now().timestamp_millis() as TimestampMillis;
+        self.drive.disable_identity_keys(
+            owner_identifier,
+            to_disable,
+            current_time,
+            block_info,
+            true,
+            transaction,
+        );
+        // add the new key
+        self.drive.add_new_non_unique_keys_to_identity(
+            owner_identifier,
+            vec![new_owner_key],
+            block_info,
+            true,
+            transaction,
+        );
+        Ok(())
+    }
+
     /// Update of the masternode identities
     pub fn update_masternode_identities(
         &self,
@@ -187,18 +339,13 @@ where
                 let protx_hash = hex::decode(&masternode.protx_hash.0).unwrap();
 
                 let owner_identity = self.create_owner_identity(&masternode)?;
-                let voter_identity = self.create_voter_identity(&protx_hash, &masternode)?;
-                let operator_identity = self.create_operator_identity(&protx_hash, &masternode)?;
+                let voter_identity = self.create_voter_identity(&masternode)?;
+                let operator_identity = self.create_operator_identity(&masternode)?;
             }
 
-            // how do we handle updated masternodes?
-            // well from the state diff, we can know which identity changed
-            // if an identity has changed, we retrieve the full identity
-            // next, we need to disable the part that changed, which means we need to identity that part
-            // need to get the next key id, need to disable the keys that should not be there e.t.c.
-            // would be better off if I can get this building and actually write some tests
-            // just test to make sure that the identities are created the way we want
-            // and that we are doing the updates correctly.
+            // to update, we need to pass in an identity and a state diff,
+            // based on the state diff, we need to
+            // TODO: can the owner address ever be updated?
         }
         Ok(())
     }
@@ -206,9 +353,14 @@ where
 
 #[cfg(test)]
 mod tests {
+    use crate::config::PlatformConfig;
+    use crate::platform;
+    use crate::platform::Platform;
+    use crate::rpc::core::CoreRPCLike;
+    use crate::test::helpers::setup::{TempPlatform, TestPlatformBuilder};
     use dashcore_rpc::dashcore_rpc_json::MasternodeListDiffWithMasternodes;
     use dashcore_rpc::json::MasternodeType::Regular;
-    use dashcore_rpc::json::{DMNState, MasternodeListItem, ProTxHash};
+    use dashcore_rpc::json::{DMNState, MasternodeListItem, ProTxHash, UpdatedMasternodeItem};
     use std::net::SocketAddr;
     use std::str::FromStr;
 
@@ -235,35 +387,107 @@ mod tests {
         MasternodeListDiffWithMasternodes {
             base_height: 850000,
             block_height: 867165,
-            added_mns: vec![
-                MasternodeListItem{
-                    node_type: Regular,
-                    protx_hash: ProTxHash::from("1628e387a7badd30fd4ee391ae0cab7e3bc84e792126c6b7cccd99257dad741d"),
-                    collateral_hash: hex::decode("4fde102b0c14c50d58d01cc7a53f9a73ae8283dcfe3f13685682ac6dd93f6210").unwrap().try_into().unwrap(),
-                    collateral_index: 1,
-                    operator_reward: 0,
-                    state: DMNState {
-                        service: SocketAddr::from_str("1.2.3.4:1234").unwrap(),
-                        registered_height: 0,
-                        last_paid_height: 0,
-                        consecutive_payments: 0,
-                        pose_penalty: 519,
-                        pose_revived_height: -1,
-                        pose_ban_height: 850091,
-                        revocation_reason: 0,
-                        owner_address: hex::decode("yT5qnTnzEe6a3qAaiaC6eG5XZgoyYErppd").unwrap(),
-                        voting_address: hex::decode("yccz9JhWMQsdeNAJ8TQ5JtNFXqUxk7dAS2").unwrap(),
-                        payout_address: hex::decode("yeBF5m81EKyr44JXgzFzj8ppSkS1R9vdz9").unwrap(),
-                        pub_key_operator: hex::decode("b9ba4890073eda0df9987857a9ecc4a47e25d0ca475e33586fae44684ef1d703d64a33f52bb93ba0fe5e81f832469fb4").unwrap()
-                        operator_payout_address: None,
-                    }
-                }
+            added_mns: vec![MasternodeListItem {
+                node_type: Regular,
+                protx_hash: ProTxHash::from(
+                    "1628e387a7badd30fd4ee391ae0cab7e3bc84e792126c6b7cccd99257dad741d",
+                ),
+                collateral_hash: hex::decode(
+                    "4fde102b0c14c50d58d01cc7a53f9a73ae8283dcfe3f13685682ac6dd93f6210",
+                )
+                .unwrap()
+                .try_into()
+                .unwrap(),
+                collateral_index: 1,
+                operator_reward: 0,
+                state: DMNState {
+                    service: SocketAddr::from_str("1.2.3.4:1234").unwrap(),
+                    registered_height: 0,
+                    last_paid_height: 0,
+                    consecutive_payments: 0,
+                    pose_penalty: 519,
+                    pose_revived_height: -1,
+                    pose_ban_height: 850091,
+                    revocation_reason: 0,
+                    owner_address: vec![1],
+                    voting_address: vec![2],
+                    payout_address: vec![3],
+                    pub_key_operator: vec![4],
+                    operator_payout_address: None,
+                },
+            }],
+            updated_mns: vec![
+                // only updates the owner identifier
+                UpdatedMasternodeItem {},
             ],
-            updated_mns: vec![],
             removed_mns: vec![],
         }
     }
 
     #[test]
-    fn test_owner_identity() {}
+    fn test_owner_identity() {
+        // todo: get rid of the multiple configs
+        let config = PlatformConfig {
+            verify_sum_trees: true,
+            quorum_size: 100,
+            validator_set_quorum_rotation_block_count: 25,
+            block_spacing_ms: 3000,
+            ..Default::default()
+        };
+        let mut platform = TestPlatformBuilder::new()
+            .with_config(config.clone())
+            .build_with_mock_rpc();
+
+        let mn_diff = get_masternode_list_diff();
+        let added_mn_one = &mn_diff.added_mns[0];
+        let owner_identity = platform.create_owner_identity(added_mn_one).unwrap();
+
+        dbg!(owner_identity);
+        // TODO: perform proper assertions when you have correct data
+        //  just adding this test to guide development and make sure things
+        //  are semi working
+    }
+
+    #[test]
+    fn test_voting_identity() {
+        let config = PlatformConfig {
+            verify_sum_trees: true,
+            quorum_size: 100,
+            validator_set_quorum_rotation_block_count: 25,
+            block_spacing_ms: 3000,
+            ..Default::default()
+        };
+        let mut platform = TestPlatformBuilder::new()
+            .with_config(config.clone())
+            .build_with_mock_rpc();
+
+        let mn_diff = get_masternode_list_diff();
+        let added_mn_one = &mn_diff.added_mns[0];
+        let voter_identity = platform.create_voter_identity(added_mn_one).unwrap();
+
+        dbg!(voter_identity);
+    }
+
+    #[test]
+    fn test_operator_identity() {
+        let config = PlatformConfig {
+            verify_sum_trees: true,
+            quorum_size: 100,
+            validator_set_quorum_rotation_block_count: 25,
+            block_spacing_ms: 3000,
+            ..Default::default()
+        };
+        let mut platform = TestPlatformBuilder::new()
+            .with_config(config.clone())
+            .build_with_mock_rpc();
+
+        let mn_diff = get_masternode_list_diff();
+        let added_mn_one = &mn_diff.added_mns[0];
+        let operator_identity = platform.create_operator_identity(added_mn_one).unwrap();
+
+        dbg!(operator_identity);
+    }
+
+    #[test]
+    fn test_update_owner_identity() {}
 }
