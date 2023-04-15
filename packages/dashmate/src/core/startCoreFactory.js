b41bb2936421fea47d8c6398126963bb9dfbec94
pshenmic <pshenmic@gmail.com>
2023-04-15 08:20:53 +0300

    chore(dashmate): lint fix

--- packages/dashmate/src/core/startCoreFactory.js
+++ packages/dashmate/src/core/startCoreFactory.js
@@ -1,6 +1,6 @@
+const fs = require('fs');
+const path = require('path');
 const CoreService = require('./CoreService');
-const fs = require("fs");
-const path = require("path");
 
 /**
  * @param {createRpcClient} createRpcClient
