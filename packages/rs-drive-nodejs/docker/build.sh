f1d74237fb1fc88ad9c86327d1b0863431ad11f3
Ivan Shumkov <ivan@shumkov.ru>
2023-04-19 15:04:46 +0800

    ci: disable sccache

--- packages/rs-drive-nodejs/docker/build.sh
+++ packages/rs-drive-nodejs/docker/build.sh
@@ -19,9 +19,6 @@ then
     apt install -y gcc-aarch64-linux-gnu libstdc++-11-dev-arm64-cross
 fi
 
-# sccache requirements
-#apt install libssl-dev pkg-config
-
 ## Update toolchain
 rustup update stable
 
@@ -36,15 +33,10 @@ chmod 777 -R /github/workspace/target
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 apt install -y nodejs
 
-#cargo install sccache@0.4.1
-
 corepack enable
 
 yarn install
 
-#SCCACHE_GHA_ENABLED=true \
-#RUSTC_WRAPPER=sccache \
-#CARGO_INCREMENTAL=false \
 CARGO_BUILD_TARGET=$target \
 CARGO_BUILD_PROFILE=release \
 ARCH=$arch \
