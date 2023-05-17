8b6c4e825759c7b1b45c2970aef8fcb984c663b8
shuplenkov <konstantin.shuplenkov@dash.org>
2023-05-17 17:32:54 +0300

    fix(dashmate): e2e tests

--- packages/dashmate/src/test/areServicesRunningFactory.js
+++ packages/dashmate/src/test/areServicesRunningFactory.js
@@ -1,27 +1,27 @@
 /**
- * @param {Config[]} configGroup 
- * @param {DockerCompose} dockerCompose 
+ * @param {Config[]} configGroup
+ * @param {DockerCompose} dockerCompose
  * @param {Object} services
- *  
+ *
  * @returns {areServicesRunning}
  */
 function areServicesRunningFactory(configGroup, dockerCompose, services) {
   /**
    * Check all node services are up and running
-   * 
+   *
    * @returns {Promise<void>}
    */
   async function areServicesRunning() {
     for (const config of configGroup) {
       if (config.name === 'local_seed') {
-        result = await dockerCompose.isServiceRunning(config.toEnvs(), 'core');
+        const result = await dockerCompose.isServiceRunning(config.toEnvs(), 'core');
         if (!result) {
           throw new Error('Core in local_seed is not running');
         }
       } else {
         for (const serviceName of Object.keys(services)) {
-          result = await dockerCompose.isServiceRunning(config.toEnvs(), serviceName);
-          if (result !== isRunning) {
+          const result = await dockerCompose.isServiceRunning(config.toEnvs(), serviceName);
+          if (!result) {
             throw new Error(`Service ${serviceName} in ${config.name} is not running`);
           }
         }
