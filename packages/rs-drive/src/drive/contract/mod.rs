e2006bf9ebb6bcea8551c3804e6e25adcafdf61e
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2023-05-26 18:16:30 +0800

    remove unused history fetch info

--- packages/rs-drive/src/drive/contract/mod.rs
+++ packages/rs-drive/src/drive/contract/mod.rs
@@ -156,22 +156,6 @@ pub struct ContractFetchInfo {
     pub fee: Option<FeeResult>,
 }
 
-#[cfg(any(feature = "full", feature = "verify"))]
-/// Contract history and fetch information
-#[derive(Default, PartialEq, Debug, Clone)]
-pub struct ContractHistoryFetchInfo {
-    /// Contracts history
-    pub history: Vec<Contract>,
-    /// The contract's potential storage flags
-    pub storage_flags: Option<StorageFlags>,
-    /// These are the operations that are used to fetch a contract
-    /// This is only used on epoch change
-    pub(crate) cost: OperationCost,
-    /// The fee is updated every epoch based on operation costs
-    /// Except if protocol version has changed in which case all the cache is cleared
-    pub fee: Option<FeeResult>,
-}
-
 #[cfg(feature = "full")]
 impl Drive {
     /// Adds a contract to storage.
