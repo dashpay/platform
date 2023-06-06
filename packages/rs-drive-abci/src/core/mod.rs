2f67bedbca47c596e64e4016331cf2c700ec398b
Ivan Shumkov <ivan@shumkov.ru>
2023-06-07 00:09:35 +0800

    chore: reduce timeout to 5 seconds

--- packages/rs-drive-abci/src/core/mod.rs
+++ packages/rs-drive-abci/src/core/mod.rs
@@ -2,13 +2,15 @@ use crate::error::Error;
 use crate::rpc::core::CoreRPCLike;
 use std::time::Duration;
 
+const CORE_SYNC_STATUS_CHECK_TIMEOUT: Duration = Duration::from_secs(5);
+
 /// Blocks execution until Core is synced
 pub fn wait_for_core_to_sync<C: CoreRPCLike>(core_rpc: &C) -> Result<(), Error> {
     loop {
         let mn_sync_status = core_rpc.masternode_sync_status()?;
 
         if !mn_sync_status.is_synced || !mn_sync_status.is_blockchain_synced {
-            std::thread::sleep(Duration::from_secs(10));
+            std::thread::sleep(CORE_SYNC_STATUS_CHECK_TIMEOUT);
 
             tracing::info!("waiting for core to sync...");
         } else {
