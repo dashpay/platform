7172c684b1d56916879925bf47be37d2f744179c
pshenmic <pshenmic@gmail.com>
2023-06-06 17:21:27 +0700

    feat(dashmate): add type mismatch case

--- packages/dashmate/test/unit/commands/config/set.spec.js
+++ packages/dashmate/test/unit/commands/config/set.spec.js
@@ -14,17 +14,23 @@ describe('Set Command Status', () => {
     it('should allow setting strings', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'core.docker.image', value: 'fake_image' }, flags, config);
+      await command.runWithDependencies({
+        option: 'core.docker.image', value: 'fake_image',
+      }, flags, config);
     });
 
     it('should allow setting null', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'description', value: null }, flags, config);
+      await command.runWithDependencies({
+        option: 'description', value: null,
+      }, flags, config);
 
       expect(config.get('description')).to.equal(null);
 
-      await command.runWithDependencies({ option: 'description', value: 'null' }, flags, config);
+      await command.runWithDependencies({
+        option: 'description', value: 'null',
+      }, flags, config);
 
       expect(config.get('description')).to.equal(null);
     });
@@ -32,11 +38,17 @@ describe('Set Command Status', () => {
     it('should allow setting numbers', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'platform.drive.abci.validatorSet.llmqType', value: 107 }, flags, config);
+      await command.runWithDependencies({
+        option: 'platform.drive.abci.validatorSet.llmqType',
+        value: 107,
+      }, flags, config);
 
       expect(config.get('platform.drive.abci.validatorSet.llmqType')).to.equal(107);
 
-      await command.runWithDependencies({ option: 'platform.drive.abci.validatorSet.llmqType', value: '107' }, flags, config);
+      await command.runWithDependencies({
+        option: 'platform.drive.abci.validatorSet.llmqType',
+        value: '107',
+      }, flags, config);
 
       expect(config.get('platform.drive.abci.validatorSet.llmqType')).to.equal(107);
     });
@@ -44,11 +56,15 @@ describe('Set Command Status', () => {
     it('should allow setting booleans', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'dashmate.helper.api.enable', value: 'true' }, flags, config);
+      await command.runWithDependencies({
+        option: 'dashmate.helper.api.enable', value: 'true',
+      }, flags, config);
 
       expect(config.get('dashmate.helper.api.enable')).to.equal(true);
 
-      await command.runWithDependencies({ option: 'dashmate.helper.api.enable', value: true }, flags, config);
+      await command.runWithDependencies({
+        option: 'dashmate.helper.api.enable', value: true,
+      }, flags, config);
 
       expect(config.get('dashmate.helper.api.enable')).to.equal(true);
     });
@@ -56,7 +72,9 @@ describe('Set Command Status', () => {
     it('should allow setting array of values', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'core.rpc.allowIps', value: '["1337", "36484"]' }, flags, config);
+      await command.runWithDependencies({
+        option: 'core.rpc.allowIps', value: '["1337", "36484"]',
+      }, flags, config);
 
       expect(config.get('core.rpc.allowIps')).to.deep.equal(['1337', '36484']);
     });
@@ -64,7 +82,10 @@ describe('Set Command Status', () => {
     it('should allow replacing part of the json', async () => {
       const command = new ConfigSetCommand();
 
-      await command.runWithDependencies({ option: 'docker', value: '{"network":{"subnet":"127.0.0.1/24"}}' }, flags, config);
+      await command.runWithDependencies({
+        option: 'docker',
+        value: '{"network":{"subnet":"127.0.0.1/24"}}',
+      }, flags, config);
     });
 
     it('should throw on unknown path', async () => {
@@ -72,7 +93,9 @@ describe('Set Command Status', () => {
 
       // invalid path
       try {
-        await command.runWithDependencies({ option: 'fakePath', value: 'fake' }, flags, config);
+        await command.runWithDependencies({
+          option: 'fakePath', value: 'fake',
+        }, flags, config);
 
         expect.fail('should throw error');
       } catch (e) {
@@ -85,7 +108,24 @@ describe('Set Command Status', () => {
 
       // invalid json
       try {
-        await command.runWithDependencies({ option: 'core.rpc.allowIps', value: 'fake_image' }, flags, config);
+        await command.runWithDependencies({
+          option: 'core.rpc.allowIps', value: 'fake_image',
+        }, flags, config);
+
+        expect.fail('should throw error');
+      } catch (e) {
+        expect(e.name).to.equal('InvalidOptionError');
+      }
+    });
+
+    it('should throw on type mismatch', async () => {
+      const command = new ConfigSetCommand();
+
+      // invalid json
+      try {
+        await command.runWithDependencies({
+          option: 'dashmate.helper.api.enable', value: 120,
+        }, flags, config);
 
         expect.fail('should throw error');
       } catch (e) {
