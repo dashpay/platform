baa8d3db91d8684ff40334f4ad7e0709c8b5811b
QuantumExplorer <quantum@dash.org>
2023-06-17 05:47:20 +0700

    docs: better drive verify docs (#1171)

--- packages/rs-drive/src/drive/verify/contract.rs
+++ packages/rs-drive/src/drive/verify/contract.rs
@@ -14,7 +14,26 @@ use crate::error::drive::DriveError;
 use grovedb::GroveDb;
 
 impl Drive {
-    /// Verifies that the contract is in the Proof
+    /// Verifies that the contract is included in the proof.
+    ///
+    /// # Parameters
+    ///
+    /// - `proof`: A byte slice representing the proof to be verified.
+    /// - `contract_known_keeps_history`: An optional boolean indicating whether the contract keeps a history.
+    /// - `is_proof_subset`: A boolean indicating whether to verify a subset of a larger proof.
+    /// - `contract_id`: The contract's unique identifier.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` with a tuple of `RootHash` and `Option<DataContract>`. The `Option<DataContract>`
+    /// represents the verified contract if it exists.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Error` if:
+    ///
+    /// - The proof is corrupted.
+    /// - The GroveDb query fails.
     pub fn verify_contract(
         proof: &[u8],
         contract_known_keeps_history: Option<bool>,
@@ -80,7 +99,28 @@ impl Drive {
         }
     }
 
-    /// Verifies that the contract history is in the Proof
+    /// Verifies that the contract's history is included in the proof.
+    ///
+    /// # Parameters
+    ///
+    /// - `proof`: A byte slice representing the proof to be verified.
+    /// - `contract_id`: The contract's unique identifier.
+    /// - `start_at_date`: The start date for the contract's history.
+    /// - `limit`: An optional limit for the number of items to be retrieved.
+    /// - `offset`: An optional offset for the items to be retrieved.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` with a tuple of `RootHash` and `Option<BTreeMap<u64, DataContract>>`. The `Option<BTreeMap<u64, DataContract>>`
+    /// represents a mapping from dates to contracts if it exists.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Error` if:
+    ///
+    /// - The proof is corrupted.
+    /// - The GroveDb query fails.
+    /// - The contract serialization fails.
     pub fn verify_contract_history(
         proof: &[u8],
         contract_id: [u8; 32],
