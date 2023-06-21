8cc557fe72ee3981ead53a6be66c095fe74b0fbe
Ivan Shumkov <ivan@shumkov.ru>
2023-06-21 21:33:38 +0800

    chore: remove unsafe retries from download certificate

--- packages/dashmate/src/ssl/zerossl/downloadCertificate.js
+++ packages/dashmate/src/ssl/zerossl/downloadCertificate.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch');
-const wait = require('../../util/wait');
 const errorDescriptions = require('./errors/errorDescriptions');
 
 /**
@@ -11,9 +10,6 @@ const errorDescriptions = require('./errors/errorDescriptions');
  * @returns {Promise<string>}
  */
 async function downloadCertificate(id, apiKey) {
-  const maxTime = 10 * 60 * 1000; // 10 minutes
-  const startedAt = Date.now();
-
   const url = `https://api.zerossl.com/certificates/${id}/download/return?access_key=${apiKey}`;
 
   const requestOptions = {
@@ -21,24 +17,8 @@ async function downloadCertificate(id, apiKey) {
     headers: { },
   };
 
-  let data;
-  let success = false;
-
-  do {
-    try {
-      await wait(2000);
-      const response = await fetch(url, requestOptions);
-      data = await response.json();
-
-      ({ success } = data);
-    } catch (e) {
-      // do nothing
-    }
-  } while (success === false && Date.now() - startedAt < maxTime);
-
-  if (!data) {
-    throw new Error('Can\'t download certificate: max time limit has been reached');
-  }
+  const response = await fetch(url, requestOptions);
+  const data = await response.json();
 
   if (data.error) {
     const errorMessage = errorDescriptions[data.error.code];
