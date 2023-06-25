d0829461da68875fcc02b0d4036b71d3243f4512
Quantum Explorer <quantum@dash.org>
2023-06-25 09:08:17 +0700

    more work

---
+++ packages/rs-drive/src/drive/verify/identity/verify_full_identities_by_public_key_hashes/v0/mod.rs
@@ -0,0 +1,81 @@
+
+use crate::drive::balances::balance_path;
+use crate::drive::defaults::PROTOCOL_VERSION;
+use crate::drive::identity::IdentityRootStructure::IdentityTreeRevision;
+use crate::drive::identity::{identity_key_tree_path, identity_path};
+use crate::drive::{unique_key_hashes_tree_path_vec, Drive};
+
+use crate::error::proof::ProofError;
+use crate::error::Error;
+use crate::fee::credits::Credits;
+
+use crate::drive::identity::key::fetch::IdentityKeysRequest;
+use crate::drive::verify::RootHash;
+use dpp::identifier::Identifier;
+use dpp::identity::{IdentityPublicKey, KeyID, PartialIdentity};
+pub use dpp::prelude::{Identity, Revision};
+use dpp::serialization_traits::PlatformDeserializable;
+use grovedb::GroveDb;
+use std::collections::BTreeMap;
+
+impl Drive {
+    /// Verifies the full identities of multiple users by their public key hashes.
+    ///
+    /// This function is a generalization of `verify_full_identity_by_public_key_hash`,
+    /// which works with a slice of public key hashes instead of a single hash.
+    ///
+    /// # Parameters
+    ///
+    /// - `proof`: A byte slice representing the proof of authentication from the users.
+    /// - `public_key_hashes`: A reference to a slice of 20-byte arrays, each representing
+    ///    a hash of a public key of a user.
+    ///
+    /// # Generic Parameters
+    ///
+    /// - `T`: The type of the collection to hold the results, which must be constructible
+    ///    from an iterator of tuples of a 20-byte array and an optional `Identity`.
+    ///
+    /// # Returns
+    ///
+    /// If the verification is successful, it returns a `Result` with a tuple of `RootHash` and `T`.
+    /// The `RootHash` represents the root hash of GroveDB, and `T` represents
+    /// the collection of the public key hash and associated identity (if it exists) for each user.
+    ///
+    /// # Errors
+    ///
+    /// Returns an `Error` if:
+    ///
+    /// - The proof of authentication is not valid.
+    /// - Any of the public key hashes do not correspond to a valid identity ID.
+    /// - Any of the identity IDs do not correspond to a valid full identity.
+    ///
+    pub(super) fn verify_full_identities_by_public_key_hashes_v0<
+        T: FromIterator<([u8; 20], Option<Identity>)>,
+    >(
+        proof: &[u8],
+        public_key_hashes: &[[u8; 20]],
+    ) -> Result<(RootHash, T), Error> {
+        let (root_hash, identity_ids_by_key_hashes) =
+            Self::verify_identity_ids_by_public_key_hashes::<Vec<(_, _)>>(
+                proof,
+                true,
+                public_key_hashes,
+            )?;
+        let maybe_identity = identity_ids_by_key_hashes
+            .into_iter()
+            .map(|(key_hash, identity_id)| match identity_id {
+                None => Ok((key_hash, None)),
+                Some(identity_id) => {
+                    let identity =
+                        Self::verify_full_identity_by_identity_id(proof, true, identity_id)
+                            .map(|(_, maybe_identity)| maybe_identity)?;
+                    let identity = identity.ok_or(Error::Proof(ProofError::IncompleteProof(
+                        "proof returned an identity id without identity information",
+                    )))?;
+                    Ok((key_hash, Some(identity)))
+                }
+            })
+            .collect::<Result<T, Error>>()?;
+        Ok((root_hash, maybe_identity))
+    }
+}
---
+++ packages/rs-drive/src/drive/verify/identity/verify_full_identity_by_public_key_hash/v0/mod.rs
@@ -0,0 +1,65 @@
+
+use crate::drive::balances::balance_path;
+use crate::drive::defaults::PROTOCOL_VERSION;
+use crate::drive::identity::IdentityRootStructure::IdentityTreeRevision;
+use crate::drive::identity::{identity_key_tree_path, identity_path};
+use crate::drive::{unique_key_hashes_tree_path_vec, Drive};
+
+use crate::error::proof::ProofError;
+use crate::error::Error;
+use crate::fee::credits::Credits;
+
+use crate::drive::identity::key::fetch::IdentityKeysRequest;
+use crate::drive::verify::RootHash;
+use dpp::identifier::Identifier;
+use dpp::identity::{IdentityPublicKey, KeyID, PartialIdentity};
+pub use dpp::prelude::{Identity, Revision};
+use dpp::serialization_traits::PlatformDeserializable;
+use grovedb::GroveDb;
+use std::collections::BTreeMap;
+
+impl Drive {
+    /// Verifies the full identity of a user by their public key hash.
+    ///
+    /// This function takes a byte slice `proof` and a 20-byte array `public_key_hash` as arguments,
+    /// then it verifies the identity of the user with the given public key hash.
+    ///
+    /// The `proof` should contain the proof of authentication from the user.
+    /// The `public_key_hash` should contain the hash of the public key of the user.
+    ///
+    /// The function first verifies the identity ID associated with the given public key hash
+    /// by calling `verify_identity_id_by_public_key_hash()`. It then uses this identity ID to verify
+    /// the full identity by calling `verify_full_identity_by_identity_id()`.
+    ///
+    /// # Returns
+    ///
+    /// If the verification is successful, it returns a `Result` with a tuple of `RootHash` and
+    /// an `Option` of `Identity`. The `RootHash` represents the root hash of GroveDB, and the
+    /// `Option<Identity>` represents the full identity of the user if it exists.
+    ///
+    /// If the verification fails at any point, it will return an `Error`.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an `Error` if:
+    ///
+    /// * The proof of authentication is not valid.
+    /// * The public key hash does not correspond to a valid identity ID.
+    /// * The identity ID does not correspond to a valid full identity.
+    ///
+    pub(super) fn verify_full_identity_by_public_key_hash_v0(
+        proof: &[u8],
+        public_key_hash: [u8; 20],
+    ) -> Result<(RootHash, Option<Identity>), Error> {
+        let (root_hash, identity_id) =
+            Self::verify_identity_id_by_public_key_hash(proof, true, public_key_hash)?;
+        let maybe_identity = identity_id
+            .map(|identity_id| {
+                Self::verify_full_identity_by_identity_id(proof, true, identity_id)
+                    .map(|(_, maybe_identity)| maybe_identity)
+            })
+            .transpose()?
+            .flatten();
+        Ok((root_hash, maybe_identity))
+    }
+}
