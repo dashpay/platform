2b9e9137eb2cf45f4a59a61b8e4f9b44ada5838c
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-06-29 10:47:03 +0200

    Revert "test(grpc): test client against testnet"
    This reverts commit e6fcbd7c6e59efc6f35c601473f1b6e529455a54.

--- packages/dapi-grpc/src/lib.rs
+++ packages/dapi-grpc/src/lib.rs
@@ -13,33 +13,3 @@ pub mod platform {
         include!("platform/proto/org.dash.platform.dapi.v0.rs");
     }
 }
-
-#[cfg(test)]
-mod test {
-    use crate::platform::v0::platform_client::PlatformClient;
-
-    #[cfg(feature = "testnet")]
-    #[tokio::test]
-    async fn testnet_request() {
-        use crate::platform::v0::get_identity_response::Result as ResponseResult;
-        use crate::platform::v0::GetIdentityRequest;
-        use tonic::transport::{Channel, ClientTlsConfig};
-
-        let endpoint = Channel::from_static("https://seed-1.testnet.networks.dash.org:1443")
-            .tls_config(ClientTlsConfig::new())
-            .unwrap();
-
-        // FIXME: fails with H2NotNegotiated - it needs http/2 support
-        let mut c = PlatformClient::connect(endpoint).await.unwrap();
-        let request = GetIdentityRequest {
-            id: vec![1u8; 32],
-            prove: true,
-        };
-        let response = c.get_identity(request).await.unwrap();
-        let response = response.get_ref();
-        assert!(matches!(
-            response.result.as_ref().unwrap(),
-            ResponseResult::Proof(_)
-        ));
-    }
-}
