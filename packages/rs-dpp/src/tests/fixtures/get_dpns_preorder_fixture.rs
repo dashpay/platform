315e5926d01ae67add32496a9435c87010d4bcfd
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2023-07-20 15:40:21 +0800

    return salt from the preorder fixture

--- packages/rs-dpp/src/tests/fixtures/get_dpns_preorder_fixture.rs
+++ packages/rs-dpp/src/tests/fixtures/get_dpns_preorder_fixture.rs
@@ -5,36 +5,22 @@ use getrandom::getrandom;
 use platform_value::Value;
 
 use crate::document::ExtendedDocument;
+use crate::tests::fixtures::ParentDocumentOptions;
+use crate::util::hash::hash;
 use crate::{
     document::{
         document_factory::DocumentFactory,
         fetch_and_validate_data_contract::DataContractFetcherAndValidator,
     },
-    prelude::Identifier,
     state_repository::MockStateRepositoryLike,
-    tests::utils::generate_random_identifier_struct,
     version::LATEST_VERSION,
 };
 
 use super::{get_document_validator_fixture, get_dpns_data_contract_fixture};
 
-pub struct ParentDocumentOptions {
-    pub label: String,
-    pub normalized_label: String,
-    pub owner_id: Identifier,
-}
-
-impl Default for ParentDocumentOptions {
-    fn default() -> Self {
-        Self {
-            label: String::from("Parent"),
-            normalized_label: String::from("parent"),
-            owner_id: generate_random_identifier_struct(),
-        }
-    }
-}
-
-pub fn get_dpns_preorder_document_fixture(options: ParentDocumentOptions) -> ExtendedDocument {
+pub fn get_dpns_preorder_document_fixture(
+    options: ParentDocumentOptions,
+) -> (ExtendedDocument, [u8; 32]) {
     let document_factory = DocumentFactory::new(
         LATEST_VERSION,
         get_document_validator_fixture(),
@@ -44,38 +30,23 @@ pub fn get_dpns_preorder_document_fixture(options: ParentDocumentOptions) -> Ext
     let mut pre_order_salt = [0u8; 32];
     let _ = getrandom(&mut pre_order_salt);
 
+    let salted_domain_hash = hash(&[&options.label, &pre_order_salt].concat());
+
     let mut map = BTreeMap::new();
-    map.insert("label".to_string(), Value::Text(options.label));
     map.insert(
-        "normalizedLabel".to_string(),
-        Value::Text(options.normalized_label),
-    );
-    map.insert(
-        "normalizedParentDomainName".to_string(),
-        Value::Text(String::new()),
-    );
-    map.insert("preorderSalt".to_string(), Value::Bytes32(pre_order_salt));
-    map.insert(
-        "records".to_string(),
-        Value::Map(vec![(
-            Value::Text("dashUniqueIdentityId".to_string()),
-            Value::Identifier(options.owner_id.to_buffer()),
-        )]),
-    );
-    map.insert(
-        "subdomainRules".to_string(),
-        Value::Map(vec![(
-            Value::Text("allowSubdomains".to_string()),
-            Value::Bool(true),
-        )]),
+        "saltedDomainHash".to_string(),
+        Value::Bytes32(salted_domain_hash),
     );
 
-    document_factory
-        .create_extended_document_for_state_transition(
-            data_contract.data_contract,
-            options.owner_id,
-            String::from("domain"),
-            map.into(),
-        )
-        .expect("DPNS document should be created")
+    (
+        document_factory
+            .create_extended_document_for_state_transition(
+                data_contract.data_contract,
+                options.owner_id,
+                String::from("preorder"),
+                map.into(),
+            )
+            .expect("DPNS preorder document should be created"),
+        pre_order_salt,
+    )
 }
