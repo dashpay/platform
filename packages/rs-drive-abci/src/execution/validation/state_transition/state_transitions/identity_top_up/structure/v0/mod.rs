c8bb8f8c009fc9832f1daf3cbb28fb8097df7d58
markin.io <markin.io210@gmail.com>
2023-07-31 10:52:13 +0100

    refactor(rs-drive-abci): remove schema validationf from identity create and identity top up

--- packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/identity_top_up/structure/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/identity_top_up/structure/v0/mod.rs
@@ -1,7 +1,6 @@
 use crate::error::Error;
 
 use crate::execution::validation::state_transition::common::validate_protocol_version::v0::validate_protocol_version_v0;
-use crate::execution::validation::state_transition::common::validate_schema::v0::validate_schema_v0;
 use dpp::state_transition::identity_topup_transition::IdentityTopUpTransition;
 use dpp::validation::SimpleConsensusValidationResult;
 
@@ -11,11 +10,6 @@ pub(crate) trait StateTransitionStructureValidationV0 {
 
 impl StateTransitionStructureValidationV0 for IdentityTopUpTransition {
     fn validate_structure_v0(&self) -> Result<SimpleConsensusValidationResult, Error> {
-        let result = validate_schema_v0(&IDENTITY_TOP_UP_TRANSITION_SCHEMA_VALIDATOR, self);
-        if !result.is_valid() {
-            return Ok(result);
-        }
-
         Ok(validate_protocol_version_v0(self.protocol_version))
     }
 }
