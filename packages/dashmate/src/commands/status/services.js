1712fd829da2df4041258b4e9c37854ffb1c230c
pshenmic <pshenmic@gmail.com>
2023-08-02 18:08:14 +0700

    fix(dashmate): lint fix

--- packages/dashmate/src/commands/status/services.js
+++ packages/dashmate/src/commands/status/services.js
@@ -28,7 +28,9 @@ class ServicesStatusCommand extends ConfigBaseCommand {
     const outputRows = [];
 
     for (const [serviceName, service] of Object.entries(scope)) {
-      const { title, containerId, image, status } = service;
+      const {
+        title, containerId, image, status,
+      } = service;
       if (flags.format === OUTPUT_FORMATS.PLAIN) {
         let statusText;
         if (status) {
