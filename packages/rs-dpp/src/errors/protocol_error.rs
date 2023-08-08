c20f32c7eea11ebfc71b27ad565c739a70e84f76
Ivan Shumkov <ivan@shumkov.ru>
2023-08-08 14:55:39 +0300

    chore: merged v3

--- packages/rs-dpp/src/errors/protocol_error.rs
+++ packages/rs-dpp/src/errors/protocol_error.rs
@@ -7,7 +7,7 @@ use crate::consensus::signature::{
 use crate::consensus::ConsensusError;
 use crate::data_contract::errors::*;
 use crate::document::errors::*;
-#[cfg(feature = "state-transitions-validation")]
+#[cfg(feature = "validation")]
 use crate::state_transition::errors::{
     InvalidIdentityPublicKeyTypeError, InvalidSignaturePublicKeyError, PublicKeyMismatchError,
     PublicKeySecurityLevelNotMetError, StateTransitionError, StateTransitionIsNotSignedError,
