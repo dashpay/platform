f5b9933152118da4c5a17cb1dd3a699c8d904e83
pshenmic <pshenmic@gmail.com>
2023-08-09 23:35:25 +0700

    fix(js-dapi-client): fix polyfill condition

--- packages/js-dapi-client/polyfills/fetch-polyfill.js
+++ packages/js-dapi-client/polyfills/fetch-polyfill.js
@@ -2,7 +2,7 @@
 
 const { default: fetch, Headers, Request, Response } = require('node-fetch');
 
-if (typeof window === undefined) {
+if (typeof window === 'undefined') {
   globalThis.fetch = fetch;
   globalThis.Headers = Headers;
   globalThis.Request = Request;
