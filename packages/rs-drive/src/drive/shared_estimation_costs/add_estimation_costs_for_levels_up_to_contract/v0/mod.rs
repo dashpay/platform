4276dc7deaa4f5eccf5554c916ea6be67e332d95
pauldelucia <pauldelucia2@gmail.com>
2023-08-29 16:00:12 +0800

    feat: add docs

--- packages/rs-drive/src/drive/shared_estimation_costs/add_estimation_costs_for_levels_up_to_contract/v0/mod.rs
+++ packages/rs-drive/src/drive/shared_estimation_costs/add_estimation_costs_for_levels_up_to_contract/v0/mod.rs
@@ -14,6 +14,36 @@ use grovedb::EstimatedSumTrees::NoSumTrees;
 use std::collections::HashMap;
 
 impl Drive {
+    /// Adds estimated costs for layers up to the contract level.
+    ///
+    /// This function populates the `estimated_costs_only_with_layer_info` hashmap with estimated layer information for the top level and the contract layer. 
+    /// These estimates are useful for optimizing GroveDB operations.
+    ///
+    /// # Parameters
+    ///
+    /// - `estimated_costs_only_with_layer_info`: A mutable reference to a hashmap that will be populated with estimated layer information for the top level and the contract layer.
+    ///
+    /// # Estimated Layer Information
+    ///
+    /// The function estimates two layers:
+    ///
+    /// 1. The top layer, which is an empty layer.
+    /// 2. The contract layer, which contains all global contracts.
+    ///
+    /// These estimates are useful for optimizing batch insertions, deletions, and other operations in GroveDB.
+    ///
+    /// # Usage
+    ///
+    /// This function is intended to be used internally within the Drive implementation.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// // Assuming all types and constants are defined
+    /// let mut estimated_costs_only_with_layer_info: HashMap<KeyInfoPath, EstimatedLayerInformation> = HashMap::new();
+    ///
+    /// Drive::add_estimation_costs_for_levels_up_to_contract_v0(&mut estimated_costs_only_with_layer_info);
+    /// ```
     pub(super) fn add_estimation_costs_for_levels_up_to_contract_v0(
         estimated_costs_only_with_layer_info: &mut HashMap<KeyInfoPath, EstimatedLayerInformation>,
     ) {
--- packages/rs-drive/src/state_transition_action/identity/identity_credit_transfer/mod.rs
+++ packages/rs-drive/src/state_transition_action/identity/identity_credit_transfer/mod.rs
@@ -1,4 +1,6 @@
+/// transformer
 pub mod transformer;
+/// v0
 pub mod v0;
 
 use crate::state_transition_action::identity::identity_credit_transfer::v0::IdentityCreditTransferTransitionActionV0;
@@ -6,27 +8,29 @@ use derive_more::From;
 use dpp::fee::Credits;
 use dpp::platform_value::Identifier;
 
+/// action
 #[derive(Debug, Clone, From)]
 pub enum IdentityCreditTransferTransitionAction {
+    /// v0
     V0(IdentityCreditTransferTransitionActionV0),
 }
 
 impl IdentityCreditTransferTransitionAction {
-    // Transfer amount
+    /// Transfer amount
     pub fn transfer_amount(&self) -> Credits {
         match self {
             IdentityCreditTransferTransitionAction::V0(transition) => transition.transfer_amount,
         }
     }
 
-    // Identity Id
+    /// Identity Id
     pub fn identity_id(&self) -> Identifier {
         match self {
             IdentityCreditTransferTransitionAction::V0(transition) => transition.identity_id,
         }
     }
 
-    // Recipient Id
+    /// Recipient Id
     pub fn recipient_id(&self) -> Identifier {
         match self {
             IdentityCreditTransferTransitionAction::V0(transition) => transition.recipient_id,
