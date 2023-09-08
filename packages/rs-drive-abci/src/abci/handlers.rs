f25f79f0244ddc0937428f5655cc7cf27b3c3be8
Ivan Shumkov <ivan@shumkov.ru>
2023-09-08 17:11:29 +0300

    fix(drive): -32603 error code on broadcast (#1381)

--- packages/rs-drive-abci/src/abci/handlers.rs
+++ packages/rs-drive-abci/src/abci/handlers.rs
@@ -37,6 +37,7 @@
 
 use crate::abci::server::AbciApplication;
 use crate::error::execution::ExecutionError;
+use ciborium::cbor;
 
 use crate::error::Error;
 use crate::rpc::core::CoreRPCLike;
@@ -56,6 +57,7 @@ use super::AbciError;
 
 use dpp::platform_value::string_encoding::{encode, Encoding};
 
+use crate::error::serialization::SerializationError;
 use crate::execution::types::block_execution_context::v0::{
     BlockExecutionContextV0Getters, BlockExecutionContextV0MutableGetters,
     BlockExecutionContextV0Setters,
@@ -598,11 +600,16 @@ where
                         .serialize_to_bytes_with_platform_version(platform_version)
                         .map_err(|e| ResponseException::from(Error::Protocol(e)))?;
 
-                    let error_data = json!({
-                        "data": {
-                            "serializedError": consensus_error_bytes
+                    let error_data = cbor!({
+                        "data" => {
+                            "serializedError" => consensus_error_bytes
                         }
-                    });
+                    })
+                    .map_err(|err| {
+                        Error::Serialization(SerializationError::CorruptedSerialization(format!(
+                            "can't create cbor: {err}"
+                        )))
+                    })?;
 
                     let mut error_data_buffer: Vec<u8> = Vec::new();
                     ciborium::ser::into_writer(&error_data, &mut error_data_buffer)
@@ -610,7 +617,7 @@ where
 
                     (
                         consensus_error.code(),
-                        encode(&consensus_error_bytes, Encoding::Base64),
+                        encode(&error_data_buffer, Encoding::Base64),
                     )
                 } else {
                     // If there are no execution errors the code will be 0
@@ -633,9 +640,14 @@ where
                 })
             }
             Err(error) => {
-                let error_data = json!({
-                    "message": "Internal error",
-                });
+                let error_data = cbor!({
+                    "message" => "Internal error",
+                })
+                .map_err(|err| {
+                    Error::Serialization(SerializationError::CorruptedSerialization(format!(
+                        "can't create cbor: {err}"
+                    )))
+                })?;
 
                 let mut error_data_buffer: Vec<u8> = Vec::new();
                 ciborium::ser::into_writer(&error_data, &mut error_data_buffer)
