f0d1f4942136fbbaa4b6cc961e7466279d5381d0
Quantum Explorer <quantum@dash.org>
2023-09-19 15:56:26 +0700

    removed comment

--- packages/rs-drive-abci/src/execution/engine/run_block_proposal/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/engine/run_block_proposal/v0/mod.rs
@@ -143,16 +143,6 @@ where
             proposer_results: None,
         };
 
-        // If last synced Core block height is not set instead of scanning
-        // number of blocks for asset unlock transactions scan only one
-        // on Core chain locked height by setting last_synced_core_height to the same value
-        // FIXME: re-enable and implement
-        // let last_synced_core_height = if request.last_synced_core_height == 0 {
-        //     block_execution_context.block_info.core_chain_locked_height
-        // } else {
-        //     request.last_synced_core_height
-        // };
-
         // Determine a new protocol version if enough proposers voted
         if block_execution_context
             .epoch_info
