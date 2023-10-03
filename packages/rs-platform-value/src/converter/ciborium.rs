0d99331c80238103ad85767926c020300bfe95e1
markin.io <markin.io210@gmail.com>
2023-10-03 09:21:19 +0100

    Revert "wip: test rs-platform-value buffer"
    This reverts commit 7bd8b4625e2b823ed06522d31744a59ad1a806b1.

--- packages/rs-platform-value/src/converter/ciborium.rs
+++ packages/rs-platform-value/src/converter/ciborium.rs
@@ -25,11 +25,11 @@ impl Value {
     }
 
     pub fn to_cbor_buffer(&self) -> Result<Vec<u8>, Error> {
-        let mut test_buffer: Vec<u8> = Vec::new();
-        ciborium::ser::into_writer(self, &mut test_buffer)
+        let mut buffer: Vec<u8> = Vec::new();
+        ciborium::ser::into_writer(self, &mut buffer)
             .map_err(|e| Error::CborSerializationError(e.to_string()))?;
 
-        Ok(test_buffer)
+        Ok(buffer)
     }
 }
 
