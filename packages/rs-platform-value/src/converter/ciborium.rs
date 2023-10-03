8ec05bea9389879ecf663ed4894330d9d0ed82a6
markin.io <markin.io210@gmail.com>
2023-10-03 10:59:58 +0100

    Revert "wip: test mtime hack"
    This reverts commit 304830d1a88bd5fcee7f94af6c3cd7bac6956add.

--- packages/rs-platform-value/src/converter/ciborium.rs
+++ packages/rs-platform-value/src/converter/ciborium.rs
@@ -25,7 +25,6 @@ impl Value {
     }
 
     pub fn to_cbor_buffer(&self) -> Result<Vec<u8>, Error> {
-        // test
         let mut buffer: Vec<u8> = Vec::new();
         ciborium::ser::into_writer(self, &mut buffer)
             .map_err(|e| Error::CborSerializationError(e.to_string()))?;
