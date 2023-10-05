811dfd4aef2e4113899d223e4c7f4d26b2f5171a
Ivan Shumkov <ivan@shumkov.ru>
2023-10-05 16:20:34 +0700

    pref: micro optimization for convertToBase58chars

--- packages/js-dpp/lib/util/convertToBase58chars.js
+++ packages/js-dpp/lib/util/convertToBase58chars.js
@@ -1,11 +1,19 @@
 /**
- * @param {string} string
+ * @param {string} input
  * @return {string}
  */
-function convertToBase58chars(string) {
-  return string
-    .replace('0', 'o')
-    .replace('l', '1');
+function convertToBase58chars(input) {
+  return input.replace(/[oi]/g, (match) => {
+    if (match === 'o') {
+      return '0';
+    }
+
+    if (match === 'i') {
+      return '1';
+    }
+
+    return match;
+  });
 }
 
 module.exports = convertToBase58chars;
