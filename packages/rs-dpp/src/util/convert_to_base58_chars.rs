560f9281dc35d3b9224eddbb534af2fdfb22f3f6
Ivan Shumkov <ivan@shumkov.ru>
2023-10-05 16:06:22 +0700

    pref: micro optimization for convert_to_base58_chars

--- packages/rs-dpp/src/util/convert_to_base58_chars.rs
+++ packages/rs-dpp/src/util/convert_to_base58_chars.rs
@@ -1,3 +1,13 @@
 pub fn convert_to_base58_chars(input: &str) -> String {
-    input.replace("o", "0").replace("i", "1")
+    let mut replaced = String::with_capacity(input.len());
+
+    for c in input.chars() {
+        match c {
+            'o' => replaced.push('0'),
+            'i' => replaced.push('1'),
+            _ => replaced.push(c),
+        }
+    }
+
+    replaced
 }
