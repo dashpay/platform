81f80c9f29351b5df9d714c33621840848b215e1
markin.io <markin.io210@gmail.com>
2023-10-12 12:31:55 +0100

    test: query handler

--- packages/rs-drive-abci/src/query/mod.rs
+++ packages/rs-drive-abci/src/query/mod.rs
@@ -22,3 +22,40 @@ impl<C> Platform<C> {
         self.query_v0(query_path, query_data, platform_version)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    mod query {
+        use crate::config::PlatformConfig;
+        use crate::error::query::QueryError;
+        use crate::rpc::core::MockCoreRPCLike;
+        use crate::test::helpers::setup::{TempPlatform, TestPlatformBuilder};
+        use platform_version::version::PlatformVersion;
+
+        fn setup_platform<'a>() -> (TempPlatform<MockCoreRPCLike>, &'a PlatformVersion) {
+            (
+                TestPlatformBuilder::new()
+                    .with_config(PlatformConfig {
+                        ..Default::default()
+                    })
+                    .build_with_mock_rpc(),
+                PlatformVersion::latest(),
+            )
+        }
+        #[test]
+        /// Tests for query handler
+        fn test_invalid_path() {
+            let (platform, platform_version) = setup_platform();
+
+            let data = vec![0; 32];
+            let result = platform.query("/invalid_path", &data, &platform_version);
+            assert!(result.is_ok());
+
+            let validation_result = result.unwrap();
+            assert!(matches!(
+                validation_result.first_error().unwrap(),
+                QueryError::InvalidArgument(msg) if msg == "query path '/invalid_path' is not supported"
+            ));
+        }
+    }
+}
