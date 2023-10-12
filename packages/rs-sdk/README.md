0206ced3b74d9b06a297e4c6f12c64d5ddeb38f8
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-10-12 11:02:53 +0200

    chore: rs-sdk example

--- packages/rs-sdk/README.md
+++ packages/rs-sdk/README.md
@@ -3,3 +3,7 @@
 This is the official Rust SDK for the Dash Platform. Dash Platform is a Layer 2 cryptocurrency technology that builds upon the Dash layer 1 network. This SDK provides an abstraction layer to simplify usage of the Dash Platform along with data models based on the Dash Platform Protocol (DPP), a CRUD interface, and bindings for other technologies such as C.
 
 See Rust documentation of this crate for more details.
+
+## Examples
+
+You can find quick start examples in `examples/` folder.
--- packages/rs-sdk/examples/read_contract.rs
+++ packages/rs-sdk/examples/read_contract.rs
@@ -1,21 +1,34 @@
 use dpp::prelude::{DataContract, Identifier};
+use rs_dapi_client::AddressList;
 use rs_sdk::platform::Fetch;
 
 // Some constants we need to connect to the platform
 include!("../tests/common.rs");
 
+/// Read data contract.
+///
+/// This example demonstrates how to connect to running platform and try to read a data contract.
 #[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() {
+    // Replace const below with data contract identifier of data contract, 32 bytes
     const DATA_CONTRACT_ID_BYTES: [u8; 32] = [1; 32];
-    let mut api = setup_api();
 
+    // Create an instance of the SDK; we use constants defined in `common.rs` for convenience.
+    // Note that in future versions of the SDK, core user and password will not be needed.
+    let uri = http::Uri::from_maybe_shared(format!("http://{}:{}", PLATFORM_IP, PLATFORM_PORT))
+        .expect("platform address uri");
+    let mut sdk = rs_sdk::SdkBuilder::new(AddressList::from(vec![uri]))
+        .with_core(PLATFORM_IP, CORE_PORT, CORE_USER, CORE_PASSWORD)
+        .build()
+        .expect("cannot initialize api");
+
+    // Convert bytes to identifier object that can be used as a Query
     let id = Identifier::from_bytes(&DATA_CONTRACT_ID_BYTES).expect("parse identity id");
-    let result = DataContract::fetch(&mut api, id).await;
 
-    assert!(matches!(
-        result,
-        Err(rs_sdk::error::Error::Proof(
-            drive_proof_verifier::Error::NotFound
-        ))
-    ));
+    // Execute the fetch operation
+    let result = DataContract::fetch(&mut sdk, id).await;
+
+    // Check the result; note that in our case, we expect to not find the data contract, as the
+    // identifier is not valid.
+    assert!(matches!(result, Ok(None)), "result: {:?}", result);
 }
