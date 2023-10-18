41fba30636820cb55bb7bd88256a6a07a4b1278e
pshenmic <pshenmic@gmail.com>
2023-10-18 12:59:08 +0700

    feat(dashmate): add cli command for core containers

---
+++ packages/dashmate/src/commands/core/cli.js
@@ -0,0 +1,45 @@
+const { Flags } = require('@oclif/core');
+
+const ConfigBaseCommand = require('../../oclif/command/ConfigBaseCommand');
+
+class DashCliCommand extends ConfigBaseCommand {
+  /**
+   * @param {Object} args
+   * @param {Object} flags
+   * @param {Config} config
+   *
+   * @param dockerCompose
+   * @return {Promise<void>}
+   */
+  async runWithDependencies(
+    args,
+    {
+      verbose: isVerbose,
+    },
+    config,
+    dockerCompose
+  ) {
+    const {command} = args
+    const {exitCode, out} = await dockerCompose.execCommand(config, 'core', `dash-cli ${command}`);
+
+    console.log(out.trim())
+
+    return out
+  }
+}
+
+DashCliCommand.description = 'Dash Core CLI`';
+
+DashCliCommand.args = [{
+  name: 'command',
+  required: true,
+  description: 'Execute a dash-cli command on the core container of the given node config',
+}];
+
+
+DashCliCommand.flags = {
+  ...ConfigBaseCommand.flags,
+  verbose: Flags.boolean({ char: 'v', description: 'use verbose mode for output', default: false }),
+};
+
+module.exports = DashCliCommand;
