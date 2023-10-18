7a1e5dacac6405f7c01bd4d833c2fe5bca14556d
Ivan Shumkov <ivan@shumkov.ru>
2023-10-18 23:22:58 +0700

    chore(drive): add more logs (#1506)

--- packages/rs-drive-abci/src/execution/platform_events/core_based_updates/update_masternode_list/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/platform_events/core_based_updates/update_masternode_list/v0/mod.rs
@@ -90,7 +90,9 @@ where
             if tracing::enabled!(tracing::Level::TRACE) {
                 let block_platform_state_fingerprint =
                     hex::encode(block_platform_state.fingerprint());
+
                 tracing::trace!(
+                    block_platform_state = ?block_platform_state,
                     block_platform_state_fingerprint,
                     method = "update_masternode_list_v0",
                     "masternode list updated",
