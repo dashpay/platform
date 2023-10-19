e7dae45db5879e459ddf22f81d42fc6e20480d37
Ivan Shumkov <ivan@shumkov.ru>
2023-10-19 15:25:48 +0700

    style: fix formatting

--- packages/rs-drive-abci/src/execution/engine/run_block_proposal/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/engine/run_block_proposal/v0/mod.rs
@@ -332,7 +332,8 @@ where
             tracing::trace!(
                 method = "run_block_proposal_v0",
                 app_hash = hex::encode(root_hash),
-                block_platform_state_fingerprint = hex::encode(block_execution_context.block_platform_state().fingerprint()),
+                block_platform_state_fingerprint =
+                    hex::encode(block_execution_context.block_platform_state().fingerprint()),
                 "block proposal executed successfully",
             );
         }
