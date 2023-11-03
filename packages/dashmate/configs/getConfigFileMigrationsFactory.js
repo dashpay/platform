e88a125d413e7193d636dd73120072ca17fa81a4
Ivan Shumkov <ivan@shumkov.ru>
2023-11-03 11:25:37 +0700

    fix: migration

--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -304,7 +304,7 @@ function getConfigFileMigrationsFactory(homeDir, defaultConfigs) {
 
               if (name !== base.getName()) {
                 options.platform.drive.tenderdash.genesis.chain_id = testnet.get('platform.drive.tenderdash.genesis.chain_id');
-                options.platform.drive.tenderdash.genesis.chain_id = testnet.get('platform.drive.tenderdash.genesis.chain_id');
+                options.platform.drive.tenderdash.genesis.initial_core_chain_locked_height = testnet.get('platform.drive.tenderdash.genesis.initial_core_chain_locked_height');
                 options.platform.drive.tenderdash.genesis.genesis_time = testnet.get('platform.drive.tenderdash.genesis.genesis_time');
               }
             }
