89204a9a25b59db7f4500671921cb250b3810b6b
Ivan Shumkov <ivan@shumkov.ru>
2023-11-06 22:03:07 +0700

    ci: run dashmate e2e tests without building

--- packages/dashmate/test/e2e/localNetwork.js
+++ packages/dashmate/test/e2e/localNetwork.js
@@ -31,8 +31,13 @@ describe('Local Network', function main() {
 
     // Update local config template that will be used to setup nodes
     const localConfig = configFile.getConfig(groupName);
-    localConfig.set('dashmate.helper.docker.build.enabled', true);
-    localConfig.set('platform.drive.abci.docker.build.enabled', true);
+
+    if (process.env.DASHMATE_E2E_TESTS_SKIP_BUILD !== 'true') {
+      localConfig.set('dashmate.helper.docker.build.enabled', true);
+      localConfig.set('platform.drive.abci.docker.build.enabled', true);
+      localConfig.set('platform.dapi.api.docker.build.enabled', true);
+    }
+
     localConfig.set('docker.network.subnet', '172.30.0.0/24');
     localConfig.set('dashmate.helper.api.port', 40000);
     localConfig.set('core.p2p.port', 40001);
