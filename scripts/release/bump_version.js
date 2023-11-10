ccc36f9f03930d917bed1c7bc8513ebe3811ed17
pshenmic <pshenmic@gmail.com>
2023-11-10 18:46:05 +0700

    feat(package): follow style

--- scripts/release/bump_version.js
+++ scripts/release/bump_version.js
@@ -18,21 +18,21 @@ const handlePackages = (versionFunc, releaseType) => {
   for (const { filename, json, toml } of packagesIterator())  {
     if (json) {
       const { version } = json;
-      
+
       json.version = versionFunc(version, releaseType);
 
       fs.writeFileSync(filename, `${JSON.stringify(json, null, 2)}\n`);
     }
 
     if (toml) {
-      const {version} = toml.package
+      const {version} = toml.package;
 
       const tomlVersion = versionFunc(version, releaseType);
 
-      const cargoFile = fs.readFileSync(filename, 'utf-8')
+      const cargoFile = fs.readFileSync(filename, 'utf-8');
 
-      const replaceFrom = `version = "${version}"`
-      const replaceTo = `version = "${tomlVersion}"`
+      const replaceFrom = `version = "${version}"`;
+      const replaceTo = `version = "${tomlVersion}"`;
 
       fs.writeFileSync(filename, cargoFile.replace(replaceFrom, replaceTo));
     }
@@ -59,35 +59,35 @@ const handlePackages = (versionFunc, releaseType) => {
 
   if (rootVersionType === releaseType && releaseType === 'release') {
     // release to release
-    handlePackages(semver.inc, 'patch')
+    handlePackages(semver.inc, 'patch');
 
     // root version
     rootPackageJson.version = semver.inc(rootPackageJson.version, 'patch');
     fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), `${JSON.stringify(rootPackageJson, null, 2)}\n`);
   } else if (rootVersionType === 'release' && releaseType !== 'release') {
     // release to prerelease
-    handlePackages(convertReleaseToPrerelease, releaseType)
+    handlePackages(convertReleaseToPrerelease, releaseType);
 
     // root version
     rootPackageJson.version = convertReleaseToPrerelease(rootPackageJson.version, releaseType);
     fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), `${JSON.stringify(rootPackageJson, null, 2)}\n`);
   } else if (rootVersionType !== 'release' && releaseType === 'release') {
     // prerelease to release
-    handlePackages(semver.inc, 'minor')
+    handlePackages(semver.inc, 'minor');
 
     // root version
     rootPackageJson.version = semver.inc(rootPackageJson.version, 'minor');
     fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), `${JSON.stringify(rootPackageJson, null, 2)}\n`);
   } else if (rootVersionType !== releaseType) {
     // dev to alpha or vice versa
-    handlePackages(convertPrereleaseType, releaseType)
+    handlePackages(convertPrereleaseType, releaseType);
 
     // root version
     rootPackageJson.version = convertPrereleaseType(rootPackageJson.version, releaseType);
     fs.writeFileSync(path.join(__dirname, '..', '..', 'package.json'), `${JSON.stringify(rootPackageJson, null, 2)}\n`);
   } else {
     // prerelease to prerelease (the same type)
-    handlePackages(semver.inc, 'prerelease')
+    handlePackages(semver.inc, 'prerelease');
 
     // root version
     rootPackageJson.version = semver.inc(rootPackageJson.version, 'prerelease');
