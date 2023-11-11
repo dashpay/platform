7d4d569add6f25eb6f3256f541dfa442fd85c922
pshenmic <pshenmic@gmail.com>
2023-11-11 23:08:48 +0700

    feat(build): fix core-js security audit

--- .pnp.cjs
+++ .pnp.cjs
@@ -2618,7 +2618,7 @@ const RAW_RUNTIME_STATE =
           ["chai", "npm:4.3.10"],\
           ["chai-as-promised", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:7.1.1"],\
           ["comment-parser", "npm:0.7.6"],\
-          ["core-js", "npm:3.19.1"],\
+          ["core-js", "npm:3.33.2"],\
           ["crypto-browserify", "npm:3.12.0"],\
           ["dirty-chai", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:2.0.1"],\
           ["eslint", "npm:7.32.0"],\
@@ -8159,10 +8159,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["core-js", [\
-      ["npm:3.19.1", {\
-        "packageLocation": "./.yarn/unplugged/core-js-npm-3.19.1-772a85cbf5/node_modules/core-js/",\
+      ["npm:3.33.2", {\
+        "packageLocation": "./.yarn/unplugged/core-js-npm-3.33.2-08a6fba407/node_modules/core-js/",\
         "packageDependencies": [\
-          ["core-js", "npm:3.19.1"]\
+          ["core-js", "npm:3.33.2"]\
         ],\
         "linkType": "HARD"\
       }]\
