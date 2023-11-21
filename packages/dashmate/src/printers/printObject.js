1f4ffea5809264adb0cec9e249bfcd1831a368a4
pshenmic <pshenmic@gmail.com>
2023-11-21 14:45:55 +0700

    fix(dashmate): publicIp.v4 function is undefined (#1587)

--- packages/dashmate/src/printers/printObject.js
+++ packages/dashmate/src/printers/printObject.js
@@ -1,4 +1,4 @@
-import table from 'table';
+import { table } from 'table';
 import { OUTPUT_FORMATS } from '../constants.js';
 import UnsupportedFormatError from './errors/UnsupportedFormatError.js';
 
