83a9ed69d6124e1558895f412a8efbd2266ee46d
pshenmic <pshenmic@gmail.com>
2023-11-28 14:42:08 +0700

    feat(dashmate): lint fix

--- packages/dashmate/src/helper/waitForDashmateHelperAPI.js
+++ packages/dashmate/src/helper/waitForDashmateHelperAPI.js
@@ -8,33 +8,34 @@ import wait from '../util/wait.js';
  * @param {{maxRetries: number, delay: number}} options, default 120s
  * @return {Promise<void>}
  */
-export default async function waitForDashmateHelperAPI(config, options = {maxRetries : 120, delay: 1000}) {
+export default async function waitForDashmateHelperAPI(config, options =
+{ maxRetries: 120, delay: 1000 }) {
   if (!config.get('dashmate.helper.api.enable')) {
-    throw new Error('Dashmate helper HTTP API is not enabled for ' + config.getName())
+    throw new Error(`Dashmate helper HTTP API is not enabled for ${config.getName()}`);
   }
 
   let retries = 0;
   let isReady = false;
 
-  const {maxRetries, delay} = options
+  const { maxRetries, delay } = options;
 
   do {
     try {
       const response = await fetch(`http://127.0.0.1:${config.get('dashmate.helper.api.port')}`, {
-        headers: {'Content-Type': 'application/json'},
-        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
         body: JSON.stringify({
-          "jsonrpc": "2.0",
-          "id": "id",
-          "method": "status",
-          "params": {
-            "format": "json",
-            "config": config.getName(),
-          }
-        })
-      })
+          jsonrpc: '2.0',
+          id: 'id',
+          method: 'status',
+          params: {
+            format: 'json',
+            config: config.getName(),
+          },
+        }),
+      });
 
-      isReady = response.status === 200
+      isReady = response.status === 200;
     } catch (e) {
       ++retries;
     } finally {
