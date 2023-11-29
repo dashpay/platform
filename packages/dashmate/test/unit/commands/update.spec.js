e5fe1f5d1e00402f328fa73fabc48028d1e0095f
pshenmic <pshenmic@gmail.com>
2023-11-29 23:04:47 +0700

    chore(dashmate): refactor update node unit test

--- packages/dashmate/test/unit/commands/update.spec.js
+++ packages/dashmate/test/unit/commands/update.spec.js
@@ -1,37 +1,47 @@
-import UpdateCommand from "../../../src/commands/update.js";
-import HomeDir from "../../../src/config/HomeDir.js";
-import getBaseConfigFactory from "../../../configs/defaults/getBaseConfigFactory.js";
-import updateNodeFactory from "../../../src/update/updateNodeFactory.js";
-import getConfigMock from "../../../src/test/mock/getConfigMock.js";
+import UpdateCommand from '../../../src/commands/update.js';
+import HomeDir from '../../../src/config/HomeDir.js';
+import getBaseConfigFactory from '../../../configs/defaults/getBaseConfigFactory.js';
+import updateNodeFactory from '../../../src/update/updateNodeFactory.js';
+import getConfigMock from '../../../src/test/mock/getConfigMock.js';
 
 describe('Update command', () => {
   let config;
+  let mockServicesList;
+  let mockGetServicesList;
+  let mockDocker;
+  let mockDockerStream;
+  let mockDockerResponse;
 
   beforeEach(async function it() {
     config = getConfigMock(this.sinon);
   });
 
-  beforeEach(async () => {
+  beforeEach(async function it() {
     const getBaseConfig = getBaseConfigFactory(HomeDir.createTemp());
 
     config = getBaseConfig();
+
+    mockGetServicesList = this.sinon.stub().callsFake(() => mockServicesList);
+    mockDockerStream = {
+      on: this.sinon.stub().callsFake((channel, cb) => (channel !== 'error'
+        ? cb(Buffer.from(`${JSON.stringify(mockDockerResponse)}\r\n`)) : null)),
+    };
+    mockDocker = { pull: this.sinon.stub().callsFake((image, cb) => cb(false, mockDockerStream)) };
   });
 
   describe('Update dashmate command', () => {
-    it('should just update', async function it () {
+    it('should just update', async () => {
+      mockDockerResponse = { status: 'Status: Image is up to date for' };
+      mockServicesList = [{ name: 'fake', image: 'fake', title: 'FAKE' }];
+
       const command = new UpdateCommand();
 
-      const mockObj = {status: 'Status: Image is up to date for'}
-      const mockGetServicesList = this.sinon.stub();
-      const mockDocker = {pull : this.sinon.stub()}
-      const mockStream = {on: (channel, cb) => channel !== 'error' ? cb(Buffer.from(`${JSON.stringify(mockObj)}\r\n`)) : null}
+      const updateNode = updateNodeFactory(mockGetServicesList, mockDocker);
 
-      mockGetServicesList.returns([{name: 'fake', image: 'fake', title: 'FAKE'}])
-      mockDocker.pull = (image, cb) => cb(false, mockStream)
+      await command.runWithDependencies({}, { format: 'json' }, mockDocker, config, updateNode);
 
-      const updateNode = updateNodeFactory(mockGetServicesList, mockDocker)
-
-      await command.runWithDependencies({}, {format: 'json'}, mockDocker, config, updateNode);
+      expect(mockGetServicesList).to.have.been.calledOnceWithExactly(config);
+      expect(mockDocker.pull.firstCall.firstArg).to.be.equal(mockServicesList[0].image);
     });
   });
 });
