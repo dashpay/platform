41e723c9d888f53a91fef297e1a111be7c052c84
pshenmic <pshenmic@gmail.com>
2023-11-29 23:34:02 +0700

    fix(dashmate): remove redundant imports

--- packages/dashmate/test/bootstrap.js
+++ packages/dashmate/test/bootstrap.js
@@ -3,7 +3,6 @@ import { expect, use } from 'chai';
 import sinonChai from 'sinon-chai';
 import dirtyChai from 'dirty-chai';
 import chaiAsPromised from 'chai-as-promised';
-import WasmDPP from '@dashevo/wasm-dpp';
 
 use(sinonChai);
 use(chaiAsPromised);
