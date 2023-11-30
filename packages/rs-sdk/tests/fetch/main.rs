a2b7c1edcfbc75904b147a65808bbf9f92dfc3a1
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2023-11-30 11:24:22 +0100

    test(rs-sdk): timeout on broadcast

--- packages/rs-sdk/tests/fetch/main.rs
+++ packages/rs-sdk/tests/fetch/main.rs
@@ -1,9 +1,11 @@
+// TODO: Rename this test package from `fetch` to `sdk`,  'integration` or sth similar
 #[cfg(not(feature = "mocks"))]
 compile_error!("tests require `mocks` feature to be enabled");
 
 #[cfg(not(any(feature = "network-testing", feature = "offline-testing")))]
 compile_error!("network-testing or offline-testing must be enabled for tests");
 
+mod broadcast;
 mod common;
 mod config;
 mod data_contract;
