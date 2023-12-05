4d78e8481a8776d8e164f19c3d9b91e9c212d608
Quantum Explorer <quantum@dash.org>
2023-12-05 17:51:58 +0700

    small fix

--- packages/rs-sdk/tests/fetch/protocol_version_vote_count.rs
+++ packages/rs-sdk/tests/fetch/protocol_version_vote_count.rs
@@ -1,6 +1,6 @@
 use crate::{common::setup_logs, config::Config};
 use dash_platform_sdk::platform::FetchMany;
-use drive_proof_verifier::types::ProtocolVersionVoteCount;
+use dpp::version::ProtocolVersionVoteCount;
 
 /// Given some existing identity ID, when I fetch the identity keys, I get some of them indexed by key ID.
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
