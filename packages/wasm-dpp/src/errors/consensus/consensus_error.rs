0e26221c22072ba199fb7ec0a1f0aa289bae74ea
markin.io <markin.io210@gmail.com>
2023-12-06 14:05:28 +0000

    fix(wasm-dpp): missing data trigger errors

--- packages/wasm-dpp/src/errors/consensus/consensus_error.rs
+++ packages/wasm-dpp/src/errors/consensus/consensus_error.rs
@@ -3,6 +3,7 @@ use crate::errors::consensus::basic::{
     UnsupportedProtocolVersionErrorWasm, UnsupportedVersionErrorWasm,
 };
 use dpp::consensus::ConsensusError as DPPConsensusError;
+use std::ops::Deref;
 
 use crate::errors::consensus::basic::identity::{
     DuplicatedIdentityPublicKeyErrorWasm, DuplicatedIdentityPublicKeyIdErrorWasm,
@@ -52,6 +53,9 @@ use dpp::consensus::signature::SignatureError;
 // use dpp::consensus::state::data_trigger::data_trigger_error::DataTriggerError;
 use dpp::consensus::state::state_error::StateError;
 
+use dpp::consensus::state::data_trigger::DataTriggerError::{
+    DataTriggerConditionError, DataTriggerExecutionError, DataTriggerInvalidResultError,
+};
 use wasm_bindgen::{JsError, JsValue};
 
 use crate::errors::consensus::basic::data_contract::{
@@ -72,9 +76,9 @@ use crate::errors::consensus::signature::{
     BasicBLSErrorWasm, BasicECDSAErrorWasm, IdentityNotFoundErrorWasm,
     SignatureShouldNotBePresentErrorWasm,
 };
-// use crate::errors::consensus::state::data_contract::data_trigger::{
-//     DataTriggerConditionErrorWasm, DataTriggerExecutionErrorWasm,
-// };
+use crate::errors::consensus::state::data_contract::data_trigger::{
+    DataTriggerConditionErrorWasm, DataTriggerExecutionErrorWasm, DataTriggerInvalidResultErrorWasm,
+};
 use crate::errors::consensus::state::data_contract::{
     DataContractAlreadyPresentErrorWasm, DataContractConfigUpdateErrorWasm,
     DataContractIsReadonlyErrorWasm,
@@ -183,20 +187,11 @@ pub fn from_state_error(state_error: &StateError) -> JsValue {
         StateError::MissingIdentityPublicKeyIdsError(e) => {
             MissingIdentityPublicKeyIdsErrorWasm::from(e).into()
         }
-        // TODO(versioning): restore
-        // StateError::DataTriggerError(data_trigger_error) => match data_trigger_error.deref() {
-        //     DataTriggerError::DataTriggerConditionError(e) => {
-        //         DataTriggerConditionErrorWasm::from(e).into()
-        //     }
-        //     DataTriggerError::DataTriggerExecutionError(e) => {
-        //         DataTriggerExecutionErrorWasm::from(e).into()
-        //     }
-        //     DataTriggerError::DataTriggerInvalidResultError(e) => {
-        //         DataTriggerInvalidResultErrorWasm::from(e).into()
-        //     }
-        // },
-        // TODO(versioning): restore
-        // StateError::DataTriggerActionError(_) => JsError::new("Data Trigger action error").into(),
+        StateError::DataTriggerError(data_trigger_error) => match data_trigger_error.deref() {
+            DataTriggerConditionError(e) => DataTriggerConditionErrorWasm::from(e).into(),
+            DataTriggerExecutionError(e) => DataTriggerExecutionErrorWasm::from(e).into(),
+            DataTriggerInvalidResultError(e) => DataTriggerInvalidResultErrorWasm::from(e).into(),
+        },
         StateError::IdentityAlreadyExistsError(e) => {
             let wasm_error: IdentityAlreadyExistsErrorWasm = e.into();
             wasm_error.into()
