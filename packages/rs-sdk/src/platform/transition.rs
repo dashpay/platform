672dc1ff45bb4b6f79a22f6d223a439b9ebbdf46
pauldelucia <pauldelucia2@gmail.com>
2023-12-13 11:47:39 +0700

    make broadcast_request public

--- packages/rs-sdk/src/platform/transition.rs
+++ packages/rs-sdk/src/platform/transition.rs
@@ -1,7 +1,7 @@
 //! State transitions used to put changed objects to the Dash Platform.
 pub mod broadcast;
 pub(crate) mod broadcast_identity;
-pub(crate) mod broadcast_request;
+pub mod broadcast_request;
 pub(crate) mod context;
 pub mod put_document;
 pub mod put_identity;
