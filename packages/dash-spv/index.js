68510d608bd6f92172d6e2523e02dd4f0e2848f9
Ivan Shumkov <ivan@shumkov.ru>
2024-02-06 11:11:32 +0700

    revert: double quoutes for dash spv

--- packages/dash-spv/index.js
+++ packages/dash-spv/index.js
@@ -1,7 +1,7 @@
-const spvChain = require("./lib/spvchain");
-const merkleProof = require("./lib/merkleproofs");
-const genesis = require("./config/config");
-const SPVError = require("./lib/errors/SPVError");
+const spvChain = require('./lib/spvchain');
+const merkleProof = require('./lib/merkleproofs');
+const genesis = require('./config/config');
+const SPVError = require('./lib/errors/SPVError');
 
 module.exports = {
   SpvChain: spvChain,
