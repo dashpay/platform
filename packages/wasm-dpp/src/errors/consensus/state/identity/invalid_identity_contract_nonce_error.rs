fc04d6174a1eee6b197bb48c1d3a450da09a10d6
markin.io <markin.io210@gmail.com>
2024-02-15 11:31:54 +0000

    feat(wasm-dpp): add bindings for nonce errors

---
+++ packages/wasm-dpp/src/errors/consensus/state/identity/invalid_identity_contract_nonce_error.rs
@@ -0,0 +1,57 @@
+use crate::buffer::Buffer;
+
+use dpp::consensus::state::identity::invalid_identity_contract_nonce_error::InvalidIdentityContractNonceError;
+use dpp::identity::identity_contract_nonce::MergeIdentityContractNonceResult;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(js_name=InvalidIdentityContractNonceError)]
+pub struct InvalidIdentityContractNonceErrorWasm {
+    inner: InvalidIdentityContractNonceError,
+}
+
+impl From<&InvalidIdentityContractNonceError> for InvalidIdentityContractNonceErrorWasm {
+    fn from(e: &InvalidIdentityContractNonceError) -> Self {
+        Self { inner: e.clone() }
+    }
+}
+
+#[wasm_bindgen(js_class=InvalidIdentityContractNonceError)]
+impl InvalidIdentityContractNonceErrorWasm {
+    #[wasm_bindgen(js_name=getIdentityId)]
+    pub fn identity_id(&self) -> Buffer {
+        Buffer::from_bytes(self.inner.identity_id().as_bytes())
+    }
+
+    #[wasm_bindgen(js_name=getCurrentIdentityContractNonce)]
+    pub fn current_identity_contract_nonce(&self) -> Option<u64> {
+        self.inner
+            .current_identity_contract_nonce()
+            .map(|nonce| *nonce as u64)
+    }
+
+    #[wasm_bindgen(js_name=getSettingIdentityContractNonce)]
+    pub fn setting_identity_contract_nonce(&self) -> u64 {
+        *self.inner.setting_identity_contract_nonce() as u64
+    }
+
+    #[wasm_bindgen(js_name=getError)]
+    pub fn error(&self) -> js_sys::Error {
+        match self.inner.error() {
+            MergeIdentityContractNonceResult::NonceTooFarInFuture => {
+                js_sys::Error::new("nonce too far in future")
+            }
+            MergeIdentityContractNonceResult::NonceTooFarInPast => {
+                js_sys::Error::new("nonce too far in past")
+            }
+            MergeIdentityContractNonceResult::NonceAlreadyPresentAtTip => {
+                js_sys::Error::new("nonce already present at tip")
+            }
+            MergeIdentityContractNonceResult::NonceAlreadyPresentInPast(nonce) => {
+                js_sys::Error::new(&format!("nonce already present in past: {}", nonce))
+            }
+            MergeIdentityContractNonceResult::MergeIdentityContractNonceSuccess(_) => {
+                js_sys::Error::new("no error")
+            }
+        }
+    }
+}
