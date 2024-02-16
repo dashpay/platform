953f9517d6996fc7856b1b96d3e3b5d15de31817
markin.io <markin.io210@gmail.com>
2024-02-16 17:24:01 +0000

    fix(wasm): nonce error message

--- packages/wasm-dpp/src/errors/consensus/state/identity/invalid_identity_contract_nonce_error.rs
+++ packages/wasm-dpp/src/errors/consensus/state/identity/invalid_identity_contract_nonce_error.rs
@@ -1,24 +1,24 @@
 use crate::buffer::Buffer;
 
 use dpp::consensus::codes::ErrorWithCode;
-use dpp::consensus::state::identity::invalid_identity_contract_nonce_error::InvalidIdentityContractNonceError;
+use dpp::consensus::state::identity::invalid_identity_contract_nonce_error::InvalidIdentityNonceError;
 use dpp::consensus::ConsensusError;
-use dpp::identity::identity_contract_nonce::MergeIdentityContractNonceResult;
+use dpp::identity::identity_nonce::MergeIdentityNonceResult;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen(js_name=InvalidIdentityContractNonceError)]
-pub struct InvalidIdentityContractNonceErrorWasm {
-    inner: InvalidIdentityContractNonceError,
+#[wasm_bindgen(js_name=InvalidIdentityNonceError)]
+pub struct InvalidIdentityNonceErrorWasm {
+    inner: InvalidIdentityNonceError,
 }
 
-impl From<&InvalidIdentityContractNonceError> for InvalidIdentityContractNonceErrorWasm {
-    fn from(e: &InvalidIdentityContractNonceError) -> Self {
+impl From<&InvalidIdentityNonceError> for InvalidIdentityNonceErrorWasm {
+    fn from(e: &InvalidIdentityNonceError) -> Self {
         Self { inner: e.clone() }
     }
 }
 
-#[wasm_bindgen(js_class=InvalidIdentityContractNonceError)]
-impl InvalidIdentityContractNonceErrorWasm {
+#[wasm_bindgen(js_class=InvalidIdentityNonceError)]
+impl InvalidIdentityNonceErrorWasm {
     #[wasm_bindgen(js_name=getIdentityId)]
     pub fn identity_id(&self) -> Buffer {
         Buffer::from_bytes(self.inner.identity_id().as_bytes())
@@ -39,19 +39,19 @@ impl InvalidIdentityContractNonceErrorWasm {
     #[wasm_bindgen(js_name=getError)]
     pub fn error(&self) -> js_sys::Error {
         match self.inner.error() {
-            MergeIdentityContractNonceResult::NonceTooFarInFuture => {
+            MergeIdentityNonceResult::NonceTooFarInFuture => {
                 js_sys::Error::new("nonce too far in future")
             }
-            MergeIdentityContractNonceResult::NonceTooFarInPast => {
+            MergeIdentityNonceResult::NonceTooFarInPast => {
                 js_sys::Error::new("nonce too far in past")
             }
-            MergeIdentityContractNonceResult::NonceAlreadyPresentAtTip => {
+            MergeIdentityNonceResult::NonceAlreadyPresentAtTip => {
                 js_sys::Error::new("nonce already present at tip")
             }
-            MergeIdentityContractNonceResult::NonceAlreadyPresentInPast(nonce) => {
+            MergeIdentityNonceResult::NonceAlreadyPresentInPast(nonce) => {
                 js_sys::Error::new(&format!("nonce already present in past: {}", nonce))
             }
-            MergeIdentityContractNonceResult::MergeIdentityContractNonceSuccess(_) => {
+            MergeIdentityNonceResult::MergeIdentityNonceSuccess(_) => {
                 js_sys::Error::new("no error")
             }
         }
@@ -60,5 +60,10 @@ impl InvalidIdentityContractNonceErrorWasm {
     #[wasm_bindgen(js_name=getCode)]
     pub fn get_code(&self) -> u32 {
         ConsensusError::from(self.inner.clone()).code()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn message(&self) -> String {
+        self.inner.to_string()
     }
 }
