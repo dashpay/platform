75108ed0aabe6b537d54c7cf83630a9d4f3fae9f
Quantum Explorer <quantum@dash.org>
2024-03-05 19:31:30 +0700

    fixed feature

--- packages/rs-drive-abci/src/abci/mod.rs
+++ packages/rs-drive-abci/src/abci/mod.rs
@@ -3,10 +3,9 @@ mod error;
 // server configuration
 pub mod config;
 
-#[cfg(any(feature = "server", test))]
 /// ABCI applications
 pub mod app;
-#[cfg(any(feature = "server", test))]
+
 mod handler;
 
 pub use error::AbciError;
