5bfae103976cabc8316bc78683c9026f4a50a6c6
Quantum Explorer <quantum@dash.org>
2024-03-14 04:03:45 +0700

    adding a identity data contract nonce set to 0 for data contract create

--- packages/rs-drive/src/drive/batch/transitions/contract/data_contract_create_transition.rs
+++ packages/rs-drive/src/drive/batch/transitions/contract/data_contract_create_transition.rs
@@ -5,6 +5,7 @@ use crate::error::Error;
 use crate::state_transition_action::contract::data_contract_create::DataContractCreateTransitionAction;
 use dpp::block::epoch::Epoch;
 use dpp::data_contract::accessors::v0::DataContractV0Getters;
+use dpp::data_contract::config::v0::DataContractConfigGettersV0;
 use dpp::version::PlatformVersion;
 use std::borrow::Cow;
 
@@ -14,16 +15,35 @@ impl DriveHighLevelOperationConverter for DataContractCreateTransitionAction {
         _epoch: &Epoch,
         _platform_version: &PlatformVersion,
     ) -> Result<Vec<DriveOperation<'a>>, Error> {
-        Ok(vec![
-            IdentityOperation(IdentityOperationType::UpdateIdentityNonce {
+        let mut operations = vec![];
+
+        operations.push(IdentityOperation(
+            IdentityOperationType::UpdateIdentityNonce {
                 identity_id: self.data_contract_ref().owner_id().into_buffer(),
                 nonce: self.identity_nonce(),
-            }),
-            // We must create the contract
-            DataContractOperation(DataContractOperationType::ApplyContract {
+            },
+        ));
+
+        if !self.data_contract_ref().config().readonly() {
+            // This means we can update the data contract, so we should add an identity contract
+            // nonce now to make it so there are no additional bytes used later for bumping the
+            // identity data contract nonce
+            operations.push(IdentityOperation(
+                IdentityOperationType::UpdateIdentityContractNonce {
+                    identity_id: self.data_contract_ref().owner_id().into_buffer(),
+                    contract_id: self.data_contract_ref().id().into_buffer(),
+                    nonce: 0,
+                },
+            ));
+        }
+
+        operations.push(DataContractOperation(
+            DataContractOperationType::ApplyContract {
                 contract: Cow::Owned(self.data_contract()),
                 storage_flags: None,
-            }),
-        ])
+            },
+        ));
+
+        Ok(operations)
     }
 }
