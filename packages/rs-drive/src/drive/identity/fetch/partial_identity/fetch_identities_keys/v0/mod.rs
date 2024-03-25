dd49a81065f0a68bae02ccfec3bb559eae6e5739
markin.io <markin.io210@gmail.com>
2024-03-25 16:50:48 +0000

    fix(drive): fetch all keys query

--- packages/rs-drive/src/drive/identity/fetch/partial_identity/fetch_identities_keys/v0/mod.rs
+++ packages/rs-drive/src/drive/identity/fetch/partial_identity/fetch_identities_keys/v0/mod.rs
@@ -1,18 +1,9 @@
-use crate::drive::identity::key::fetch::{
-    IdentityKeysRequest, KeyIDIdentityPublicKeyPairBTreeMap,
-    KeyIDOptionalIdentityPublicKeyPairBTreeMap,
-};
 use crate::drive::Drive;
 use crate::error::Error;
 
-use dpp::block::epoch::Epoch;
-use dpp::fee::default_costs::KnownCostItem::FetchIdentityBalanceProcessingCost;
-use dpp::fee::fee_result::FeeResult;
 use dpp::identifier::Identifier;
-use dpp::identity::{IdentityPublicKey, PartialIdentity};
+use dpp::identity::{PartialIdentity};
 use grovedb::TransactionArg;
-
-use dpp::fee::default_costs::EpochCosts;
 
 use dpp::version::PlatformVersion;
 use std::collections::{BTreeMap, BTreeSet};
@@ -29,40 +20,19 @@ impl Drive {
         identity_ids
             .iter()
             .map(|identity_id| {
-                let keys_request = IdentityKeysRequest::new_all_keys_query(
-                    identity_id,
-                    None, // TODO: set some limit?
-                );
-
-                let public_keys_with_optionals = self
-                    .fetch_identity_keys::<BTreeMap::<[u8; 32], Vec<IdentityPublicKey>>>(
-                        keys_request,
+                let public_keys = self
+                    .fetch_all_identity_keys(
+                        *identity_id,
                         transaction,
-                        platform_version,
+                        platform_version
                     )?;
-
-                let mut loaded_public_keys = BTreeMap::new();
-                let mut not_found_public_keys = BTreeSet::new();
-
-                public_keys_with_optionals
-                    .into_iter()
-                    .for_each(|(keyId, key)| {
-                        match key {
-                            None => {
-                                not_found_public_keys.insert(keyId);
-                            }
-                            Some(key) => {
-                                loaded_public_keys.insert(keyId, key);
-                            }
-                        }
-                    });
-
+                
                 let identity = PartialIdentity {
                     id: Identifier::new(*identity_id),
-                    loaded_public_keys,
+                    loaded_public_keys: public_keys,
                     balance: None,
                     revision: None,
-                    not_found_public_keys,
+                    not_found_public_keys: BTreeSet::new(),
                 };
 
                 Ok((
