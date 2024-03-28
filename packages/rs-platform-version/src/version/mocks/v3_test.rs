e973127e3a15afaf3bcc1d2a06caac56b8b39edf
Quantum Explorer <quantum@dash.org>
2024-03-28 16:54:47 +0700

    lowered required funding balance

--- packages/rs-platform-version/src/version/mocks/v3_test.rs
+++ packages/rs-platform-version/src/version/mocks/v3_test.rs
@@ -919,9 +919,9 @@ pub(crate) const TEST_PLATFORM_V3: PlatformVersion = PlatformVersion {
                 max_public_keys_in_creation: 6,
                 asset_locks: IdentityTransitionAssetLockVersions {
                     required_asset_lock_duff_balance_for_processing_start_for_identity_create:
-                        200000,
-                    required_asset_lock_duff_balance_for_processing_start_for_identity_top_up:
                         100000,
+                    required_asset_lock_duff_balance_for_processing_start_for_identity_top_up:
+                        50000,
                     validate_asset_lock_transaction_structure: 0,
                     validate_instant_asset_lock_proof_structure: 0,
                 },
--- packages/rs-platform-version/src/version/v1.rs
+++ packages/rs-platform-version/src/version/v1.rs
@@ -916,9 +916,9 @@ pub(super) const PLATFORM_V1: PlatformVersion = PlatformVersion {
                 max_public_keys_in_creation: 6,
                 asset_locks: IdentityTransitionAssetLockVersions {
                     required_asset_lock_duff_balance_for_processing_start_for_identity_create:
-                        200000,
-                    required_asset_lock_duff_balance_for_processing_start_for_identity_top_up:
                         100000,
+                    required_asset_lock_duff_balance_for_processing_start_for_identity_top_up:
+                        50000,
                     validate_asset_lock_transaction_structure: 0,
                     validate_instant_asset_lock_proof_structure: 0,
                 },
