cb1b7323d2b943238afdbd4681db696f9726bd9d
markin.io <markin.io210@gmail.com>
2024-04-09 09:38:55 +0100

    update macro

--- packages/wasm-dpp/src/errors/generic_consensus_error.rs
+++ packages/wasm-dpp/src/errors/generic_consensus_error.rs
@@ -1,42 +1,54 @@
 #[macro_export]
 macro_rules! generic_consensus_error {
-    ($error_type:ident, $error_instance:expr) => {
-        {
-            use wasm_bindgen::prelude::wasm_bindgen;
-            use dpp::consensus::ConsensusError;
-            use dpp::consensus::codes::ErrorWithCode;
-            use paste::paste;
+    ($error_type:ident, $error_instance:expr) => {{
+        use {
+            $crate::buffer::Buffer,
+            dpp::{
+                consensus::{codes::ErrorWithCode, ConsensusError},
+                serialization::PlatformSerializableWithPlatformVersion,
+                version::PlatformVersion,
+            },
+            paste::paste,
+            wasm_bindgen::prelude::wasm_bindgen,
+        };
 
-            paste! {
-                #[derive(Debug)]
-                #[wasm_bindgen(js_name=$error_type)]
-                pub struct [<$error_type Wasm>] {
-                    inner: $error_type
-                }
-
-                impl From<&$error_type> for [<$error_type Wasm>] {
-                    fn from(e: &$error_type) -> Self {
-                        Self {
-                            inner: e.clone()
-                        }
-                    }
-                }
-
-                #[wasm_bindgen(js_class=$error_type)]
-                impl [<$error_type Wasm>] {
-                    #[wasm_bindgen(js_name=getCode)]
-                    pub fn get_code(&self) -> u32 {
-                        ConsensusError::from(self.inner.clone()).code()
-                    }
-
-                    #[wasm_bindgen(getter)]
-                    pub fn message(&self) -> String {
-                        self.inner.to_string()
-                    }
-                }
-
-                [<$error_type Wasm>]::from($error_instance)
+        paste! {
+            #[derive(Debug)]
+            #[wasm_bindgen(js_name=$error_type)]
+            pub struct [<$error_type Wasm>] {
+                inner: $error_type
             }
+
+            impl From<&$error_type> for [<$error_type Wasm>] {
+                fn from(e: &$error_type) -> Self {
+                    Self {
+                        inner: e.clone()
+                    }
+                }
+            }
+
+            #[wasm_bindgen(js_class=$error_type)]
+            impl [<$error_type Wasm>] {
+                #[wasm_bindgen(js_name=getCode)]
+                pub fn get_code(&self) -> u32 {
+                    ConsensusError::from(self.inner.clone()).code()
+                }
+
+                #[wasm_bindgen(getter)]
+                pub fn message(&self) -> String {
+                    self.inner.to_string()
+                }
+
+                pub fn serialize(&self) -> Result<Buffer, JsError> {
+                    let bytes = ConsensusError::from(self.inner.clone())
+                        .serialize_to_bytes_with_platform_version(PlatformVersion::first())
+                        .map_err(JsError::from)?;
+
+                    Ok(Buffer::from_bytes(bytes.as_slice()))
+                }
+            }
+
+            [<$error_type Wasm>]::from($error_instance)
         }
-    }
+    }};
 }
