ba402480ae43a4bd9aa96eba0f643f95aba31798
Ivan Shumkov <ivan@shumkov.ru>
2024-04-21 00:04:48 +0700

    chore(dashmate): limit concurrent state transition pre-validations

--- packages/dashmate/configs/defaults/getBaseConfigFactory.js
+++ packages/dashmate/configs/defaults/getBaseConfigFactory.js
@@ -206,7 +206,7 @@ export default function getBaseConfigFactory(homeDir) {
           tenderdash: {
             mode: 'full',
             docker: {
-              image: 'dashpay/tenderdash:0.14.0-dev.5',
+              image: 'dashpay/tenderdash:feat-grpc-client-conn-limit',
             },
             p2p: {
               host: '0.0.0.0',
--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -521,6 +521,14 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
 
         return configFile;
       },
+      '1.0.0-dev.12': (configFile) => {
+        Object.entries(configFile.configs)
+          .forEach(([, options]) => {
+            options.platform.drive.tenderdash.docker.image = base.get('platform.drive.tenderdash.docker.image');
+          });
+
+        return configFile;
+      },
     };
   }
 
