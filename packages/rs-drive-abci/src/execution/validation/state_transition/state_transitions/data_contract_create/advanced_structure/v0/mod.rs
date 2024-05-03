b38dd114399664e0015831afb27049f90be254b0
Ivan Shumkov <ivan@shumkov.ru>
2024-05-03 21:22:05 +0700

    fix: missing initial data contract version validation

--- packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/data_contract_create/advanced_structure/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/data_contract_create/advanced_structure/v0/mod.rs
@@ -3,8 +3,11 @@ use crate::execution::types::execution_operation::ValidationOperation;
 use crate::execution::types::state_transition_execution_context::{
     StateTransitionExecutionContext, StateTransitionExecutionContextMethodsV0,
 };
-use dpp::consensus::basic::data_contract::InvalidDataContractIdError;
+use dpp::consensus::basic::data_contract::{
+    InvalidDataContractIdError, InvalidDataContractVersionError,
+};
 use dpp::consensus::basic::BasicError;
+use dpp::data_contract::INITIAL_DATA_CONTRACT_VERSION;
 use dpp::prelude::DataContract;
 use dpp::state_transition::data_contract_create_transition::accessors::DataContractCreateTransitionAccessorsV0;
 use dpp::state_transition::data_contract_create_transition::DataContractCreateTransition;
@@ -23,6 +26,23 @@ impl DataContractCreatedStateTransitionAdvancedStructureValidationV0
         &self,
         execution_context: &mut StateTransitionExecutionContext,
     ) -> Result<ConsensusValidationResult<StateTransitionAction>, Error> {
+        if self.data_contract().version() != INITIAL_DATA_CONTRACT_VERSION {
+            let bump_action = StateTransitionAction::BumpIdentityNonceAction(
+                BumpIdentityNonceAction::from_borrowed_data_contract_create_transition(self),
+            );
+
+            return Ok(ConsensusValidationResult::new_with_data_and_errors(
+                bump_action,
+                vec![BasicError::InvalidDataContractVersionError(
+                    InvalidDataContractVersionError::new(
+                        self.data_contract().version(),
+                        INITIAL_DATA_CONTRACT_VERSION,
+                    ),
+                )
+                .into()],
+            ));
+        }
+
         // Validate data contract id
         let generated_id = DataContract::generate_data_contract_id_v0(
             self.data_contract().owner_id(),
