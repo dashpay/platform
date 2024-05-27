9e09c5ac86d503272fda3bcf81e0fd4728b1a8dc
Quantum Explorer <quantum@dash.org>
2024-05-27 10:13:17 +0200

    small fixes

--- packages/rs-drive-abci/src/execution/platform_events/voting/clean_up_after_vote_poll_end/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/platform_events/voting/clean_up_after_vote_poll_end/v0/mod.rs
@@ -4,6 +4,8 @@ use crate::rpc::core::CoreRPCLike;
 use dpp::block::block_info::BlockInfo;
 use dpp::document::DocumentV0Getters;
 use dpp::version::PlatformVersion;
+use dpp::voting::vote_polls::VotePoll;
+use drive::drive::votes::resolved::vote_polls::contested_document_resource_vote_poll::ContestedDocumentResourceVotePollWithContractInfo;
 use drive::drive::votes::resolved::vote_polls::contested_document_resource_vote_poll::resolve::ContestedDocumentResourceVotePollResolver;
 use drive::grovedb::TransactionArg;
 use drive::query::vote_poll_vote_state_query::FinalizedContender;
@@ -15,11 +17,13 @@ where
 {
     /// Checks for ended vote polls
     #[inline(always)]
-    pub(super) fn clean_up_after_vote_poll_end_v0(
+    pub(super) fn clean_up_after_vote_polls_end_v0(
         &self,
         block_info: &BlockInfo,
+        vote_poll: &[&VotePoll],
         transaction: TransactionArg,
         platform_version: &PlatformVersion,
     ) -> Result<(), Error> {
+        self.drive.remove_vote_poll_end_date_query_operations(vote_poll)
     }
 }
