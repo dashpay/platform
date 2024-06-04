e716feffd91dbcd3c1e29b04c89f74759e2c2b46
Ivan Shumkov <ivan@shumkov.ru>
2024-06-04 20:07:05 +0700

    fix: masternode list stream is not stopping when wallet is stopped

--- packages/js-dapi-client/lib/SimplifiedMasternodeListProvider/SimplifiedMasternodeListProvider.js
+++ packages/js-dapi-client/lib/SimplifiedMasternodeListProvider/SimplifiedMasternodeListProvider.js
@@ -22,6 +22,7 @@ class SimplifiedMasternodeListProvider {
      * @type {ReconnectableStream}
      */
     this.stream = undefined;
+    this.removeStreamListeners = () => {}
 
     /**
      * @type {SimplifiedMNList}
@@ -47,6 +48,11 @@ class SimplifiedMasternodeListProvider {
    * @returns {Promise<void>}
    */
   async subscribeToMasternodeList() {
+    if (this.stream) {
+      this.logger.debug('Masternode list stream already started');
+      return;
+    }
+
     this.logger.debug('Starting masternode list stream');
 
     this.stream = await this.createStream();
@@ -172,6 +178,7 @@ class SimplifiedMasternodeListProvider {
           { diffCount },
         );
 
+        this.removeStreamListeners();
         this.stream = null;
       };
 
@@ -179,6 +186,13 @@ class SimplifiedMasternodeListProvider {
       this.stream.on('beforeReconnect', beforeReconnectHandler);
       this.stream.on('error', errorHandler);
       this.stream.on('end', endHandler);
+
+      this.removeStreamListeners = () => {
+        this.stream.removeListener('data', dataHandler);
+        this.stream.removeListener('beforeReconnect', beforeReconnectHandler);
+        this.stream.removeListener('error', errorHandler);
+        this.stream.removeListener('end', endHandler);
+      };
     });
   }
 
@@ -187,6 +201,7 @@ class SimplifiedMasternodeListProvider {
    */
   unsubscribe() {
     if (this.stream) {
+      this.removeStreamListeners();
       this.stream.cancel();
       this.stream = null;
     }
