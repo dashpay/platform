9cd2b69f4aaeae4e3f7683a03ca3d76d9735e4bf
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-06-17 12:31:59 +0200

    test(sdk): update test contested_resource_vote_states_not_found

--- packages/rs-sdk/tests/fetch/contested_resource_vote_state.rs
+++ packages/rs-sdk/tests/fetch/contested_resource_vote_state.rs
@@ -1,18 +1,13 @@
 //! Tests for SDK requests that return one or more [Contender] objects.
 use crate::fetch::{common::setup_logs, config::Config};
-use dash_sdk::platform::{DocumentQuery, Fetch, FetchMany};
-use dpp::{
-    data_contract::DataContract,
-    document::Document,
-    voting::{
-        contender_structs::ContenderWithSerializedDocument,
-        vote_polls::contested_document_resource_vote_poll::ContestedDocumentResourceVotePoll,
-    },
+use dash_sdk::platform::FetchMany;
+use dpp::voting::{
+    contender_structs::ContenderWithSerializedDocument,
+    vote_polls::contested_document_resource_vote_poll::ContestedDocumentResourceVotePoll,
 };
 use drive::query::vote_poll_vote_state_query::{
     ContestedDocumentVotePollDriveQuery, ContestedDocumentVotePollDriveQueryResultType,
 };
-use std::sync::Arc;
 
 /// Given some data contract ID, document type and document ID, when I fetch it, then I get it.
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
@@ -26,26 +21,6 @@ async fn contested_resource_vote_states_not_found() {
 
     let data_contract_id = cfg.existing_data_contract_id;
 
-    let contract = Arc::new(
-        DataContract::fetch(&sdk, data_contract_id)
-            .await
-            .expect("fetch data contract")
-            .expect("data contract not found"),
-    );
-
-    // Fetch multiple documents so that we get document ID
-    let all_docs_query =
-        DocumentQuery::new(Arc::clone(&contract), &cfg.existing_document_type_name)
-            .expect("create SdkDocumentQuery");
-    let first_doc = Document::fetch_many(&sdk, all_docs_query)
-        .await
-        .expect("fetch many documents")
-        .pop_first()
-        .expect("first item must exist")
-        .1
-        .expect("document must exist");
-
-    tracing::info!("first_doc: {}", first_doc.to_string());
     // Now query for individual document
     let query = ContestedDocumentVotePollDriveQuery {
         limit: None,
@@ -53,7 +28,7 @@ async fn contested_resource_vote_states_not_found() {
         start_at: None,
         vote_poll: ContestedDocumentResourceVotePoll {
             index_name: "parentNameAndLabel".to_string(),
-            index_values: vec!["dash".into()],
+            index_values: vec!["non existing name".into()],
             document_type_name: cfg.existing_document_type_name,
             contract_id: data_contract_id,
         },
