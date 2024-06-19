17a41f7099092fca509d54ba58dde76adca9973b
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2024-06-19 13:25:00 +0200

    refactor(sdk): refactor contested_resource tests

--- packages/rs-sdk/tests/fetch/contested_resource.rs
+++ packages/rs-sdk/tests/fetch/contested_resource.rs
@@ -1,19 +1,32 @@
 //! Tests of ContestedResource object
 use crate::fetch::{common::setup_logs, config::Config};
 use dash_sdk::platform::FetchMany;
-use dpp::platform_value::Value;
-use drive::query::vote_polls_by_document_type_query::VotePollsByDocumentTypeQuery;
+use dpp::{
+    platform_value::Value,
+    voting::{
+        contender_structs::ContenderWithSerializedDocument,
+        vote_polls::contested_document_resource_vote_poll::ContestedDocumentResourceVotePoll,
+    },
+};
+use drive::query::{
+    vote_poll_vote_state_query::{
+        ContestedDocumentVotePollDriveQuery, ContestedDocumentVotePollDriveQueryResultType,
+    },
+    vote_polls_by_document_type_query::VotePollsByDocumentTypeQuery,
+};
 use drive_proof_verifier::types::ContestedResource;
+
+pub(crate) const INDEX_VALUE: &str = "dada";
 
 /// Test that we can fetch contested resources
 ///
 /// ## Preconditions
 ///
-/// 1. At least one contested resource exists
+/// 1. At least one contested resource (DPNS name) exists
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 #[cfg_attr(
-    feature = "network-testing1",
-    ignore = "requires a DPNS name to be registered"
+    feature = "network-testing",
+    ignore = "requires manual DPNS names setup for masternode voting tests; see fn check_mn_voting_prerequisities()"
 )]
 async fn test_contested_resources_ok() {
     setup_logs();
@@ -21,6 +34,9 @@ async fn test_contested_resources_ok() {
     let cfg = Config::new();
 
     let sdk = cfg.setup_api("test_contested_resources_ok").await;
+    check_mn_voting_prerequisities(&sdk, &cfg)
+        .await
+        .expect("prerequisities");
 
     let index_name = "parentNameAndLabel";
 
@@ -44,20 +60,25 @@ async fn test_contested_resources_ok() {
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 #[cfg_attr(
-    feature = "network-testing1",
-    ignore = "requires a DPNS name to be registered"
+    feature = "network-testing",
+    ignore = "requires manual DPNS names setup for masternode voting tests; see fn check_mn_voting_prerequisities()"
 )]
-/// Test pagination
+
+/// Test [ContestedResource] start index (`start_at_value`)
 ///
 /// ## Preconditions
 ///
-/// 1. Multiple contested resources exists
-async fn contested_resources_paginate() {
+/// 1. At least 2 contested resources (eg. different DPNS names) exist
+async fn contested_resources_start_at_value() {
     setup_logs();
 
     let cfg = Config::new();
 
-    let sdk = cfg.setup_api("contested_resources_paginate").await;
+    let sdk = cfg.setup_api("contested_resources_start_at_value").await;
+    check_mn_voting_prerequisities(&sdk, &cfg)
+        .await
+        .expect("prerequisities");
+
     // Given all contested resources sorted ascending
     let index_name = "parentNameAndLabel";
 
@@ -68,7 +89,7 @@ async fn contested_resources_paginate() {
         start_at_value: None,
         start_index_values: vec![Value::Text("dash".into())],
         end_index_values: vec![],
-        limit: None,
+        limit: Some(50),
         order_ascending: true,
     };
 
@@ -77,49 +98,58 @@ async fn contested_resources_paginate() {
         .expect("fetch contested resources");
 
     tracing::debug!(contested_resources=?all, "All contested resources");
-    // when I set start_at_value to some value,
-    for (i, start) in all.0.iter().enumerate() {
-        let ContestedResource::Value(start_value) = start.clone();
+    for inclusive in [true, false] {
+        // when I set start_at_value to some value,
+        for (i, start) in all.0.iter().enumerate() {
+            let ContestedResource::Value(start_value) = start.clone();
 
-        let query = VotePollsByDocumentTypeQuery {
-            start_at_value: Some((start_value, false)),
-            ..query_all.clone()
-        };
+            let query = VotePollsByDocumentTypeQuery {
+                start_at_value: Some((start_value, inclusive)),
+                ..query_all.clone()
+            };
 
-        let rss = ContestedResource::fetch_many(&sdk, query)
-            .await
-            .expect("fetch contested resources");
-        tracing::debug!(?start, contested_resources=?rss, "Contested resources");
+            let rss = ContestedResource::fetch_many(&sdk, query)
+                .await
+                .expect("fetch contested resources");
+            tracing::debug!(?start, contested_resources=?rss, "Contested resources");
 
-        for (j, fetched) in rss.0.into_iter().enumerate() {
-            let all_index = i + j + 1; // we fetch exclusive
+            for (j, fetched) in rss.0.into_iter().enumerate() {
+                let all_index = if inclusive { i + j } else { i + j + 1 };
 
-            assert_eq!(
+                assert_eq!(
                 fetched,
                 (all.0[all_index]),
-                "when starting with {:?}, fetched element {} ({:?}) must equal all element {} ({:?})",
+                "when starting with {:?} with inclusive {}, fetched element {} ({:?}) must equal all element {} ({:?})",
                 start,
+                inclusive,
                 j,
                 fetched,
                 all_index,
                 all.0[all_index]
             );
+            }
         }
     }
 }
 
+/// Test that we can fetch contested resources with a limit
+///
+/// ## Preconditions
+///
+/// 1. At least 3 contested resources (eg. different DPNS names) exist
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 #[cfg_attr(
-    feature = "network-testing1",
-    ignore = "requires at least 3 DPNS name contests"
+    feature = "network-testing",
+    ignore = "requires manual DPNS names setup for masternode voting tests; see fn check_mn_voting_prerequisities()"
 )]
-
 async fn contested_resources_limit() {
     setup_logs();
 
     let cfg = Config::new();
-
     let sdk = cfg.setup_api("contested_resources_limit").await;
+    check_mn_voting_prerequisities(&sdk, &cfg)
+        .await
+        .expect("prerequisities");
 
     // Given index with more than 2 contested resources
     const LIMIT: u16 = 2;
@@ -179,11 +209,14 @@ async fn contested_resources_limit() {
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 
 /// Empty string in start_index_values should not cause an error
+///
+/// ## Preconditions
+///
+/// None
 async fn test_contested_resources_idx_value_empty_string() {
     setup_logs();
 
     let cfg = Config::new();
-
     let sdk = cfg
         .setup_api("test_contested_resources_idx_value_empty_string")
         .await;
@@ -209,4 +242,72 @@ async fn test_contested_resources_idx_value_empty_string() {
 
     // Then I don't get an error
     assert!(result.is_ok(), "empty index value shall not cause an error");
+}
+
+/// Ensure prerequsities for masternode voting tests are met
+pub async fn check_mn_voting_prerequisities(
+    sdk: &dash_sdk::Sdk,
+    cfg: &Config,
+) -> Result<(), Vec<String>> {
+    let mut errors = Vec::new();
+
+    let index_name = "parentNameAndLabel".to_string();
+
+    let query_contested_resources = VotePollsByDocumentTypeQuery {
+        contract_id: cfg.existing_data_contract_id,
+        document_type_name: cfg.existing_document_type_name.clone(),
+        index_name: index_name.to_string(),
+        start_at_value: None,
+        start_index_values: vec![Value::Text("dash".into())],
+        end_index_values: vec![],
+        limit: None,
+        order_ascending: true,
+    };
+
+    // Check if we have enough contested resources; this implies that we have at least 1 vote poll
+    let contested_resources = ContestedResource::fetch_many(sdk, query_contested_resources)
+        .await
+        .expect("fetch contested resources");
+    if contested_resources.0.len() < 3 {
+        errors.push(format!(
+            "Please create at least 3 different DPNS names for masternode voting tests, found {}",
+            contested_resources.0.len()
+        ));
+    }
+
+    // ensure we have enough contenders
+    let query_all = ContestedDocumentVotePollDriveQuery {
+        limit: None,
+        offset: None,
+        start_at: None,
+        vote_poll: ContestedDocumentResourceVotePoll {
+            index_name: "parentNameAndLabel".to_string(),
+            index_values: vec![
+                Value::Text("dash".into()),
+                Value::Text(INDEX_VALUE.to_string()),
+            ],
+            document_type_name: cfg.existing_document_type_name.clone(),
+            contract_id: cfg.existing_data_contract_id,
+        },
+        allow_include_locked_and_abstaining_vote_tally: true,
+        result_type: ContestedDocumentVotePollDriveQueryResultType::DocumentsAndVoteTally,
+    };
+
+    let all_contenders = ContenderWithSerializedDocument::fetch_many(sdk, query_all.clone())
+        .await
+        .expect("fetch many contenders");
+    if all_contenders.contenders.len() < 3 {
+        errors.push(format!(
+            "Please create 3 identities and create DPNS name `{}` for each of them, found {}",
+            INDEX_VALUE,
+            all_contenders.contenders.len()
+        ));
+    }
+
+    if errors.is_empty() {
+        Ok(())
+    } else {
+        tracing::error!(?errors, "Prerequisities for masternode voting tests not met, please configure the network accordingly");
+        Err(errors)
+    }
 }
