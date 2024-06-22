7443cb5235be070f069908c3bc41cc7f3db4de4f
Ivan Shumkov <ivan@shumkov.ru>
2024-06-22 16:29:08 +0700

    chore: update Karma to recent version (#1901)

--- .pnp.cjs
+++ .pnp.cjs
@@ -2655,7 +2655,7 @@ const RAW_RUNTIME_STATE =
           ["eslint-plugin-jsdoc", "virtual:8f25fc90e0fb5fd89843707863857591fa8c52f9f33eadced4bf404b1871d91959f7bb86948ae0e1b53ee94d491ef8fde9c0b58b39c9490c0d0fa6c931945f97#npm:46.9.0"],\
           ["events", "npm:3.3.0"],\
           ["google-protobuf", "npm:3.19.1"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["karma-chai", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:0.1.0"],\
           ["karma-chrome-launcher", "npm:3.1.0"],\
           ["karma-firefox-launcher", "npm:2.1.2"],\
@@ -2965,7 +2965,7 @@ const RAW_RUNTIME_STATE =
           ["glob", "npm:10.4.1"],\
           ["https-browserify", "npm:1.0.0"],\
           ["js-merkle", "npm:0.1.5"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["karma-chai", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:0.1.0"],\
           ["karma-chrome-launcher", "npm:3.1.0"],\
           ["karma-firefox-launcher", "npm:2.1.2"],\
@@ -3053,7 +3053,7 @@ const RAW_RUNTIME_STATE =
           ["eslint-plugin-import", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:2.29.0"],\
           ["events", "npm:3.3.0"],\
           ["https-browserify", "npm:1.0.0"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["karma-chai", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:0.1.0"],\
           ["karma-chrome-launcher", "npm:3.1.0"],\
           ["karma-firefox-launcher", "npm:2.1.2"],\
@@ -3118,7 +3118,7 @@ const RAW_RUNTIME_STATE =
           ["fast-json-patch", "npm:3.1.1"],\
           ["https-browserify", "npm:1.0.0"],\
           ["json-schema-diff-validator", "npm:0.4.1"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["karma-chai", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:0.1.0"],\
           ["karma-chrome-launcher", "npm:3.1.0"],\
           ["karma-firefox-launcher", "npm:2.1.2"],\
@@ -8411,7 +8411,7 @@ const RAW_RUNTIME_STATE =
           ["eslint-plugin-import", "virtual:ad53cff31b1dbd4927a99e71702e3b8b10338636eaff010987c27c9ccea2d52af36900a9e36a4231cbb6e5464248ccc9c1da5d1d24d9b0f4f95660296b1060a6#npm:2.29.0"],\
           ["events", "npm:3.3.0"],\
           ["https-browserify", "npm:1.0.0"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["karma-chai", "virtual:ad53cff31b1dbd4927a99e71702e3b8b10338636eaff010987c27c9ccea2d52af36900a9e36a4231cbb6e5464248ccc9c1da5d1d24d9b0f4f95660296b1060a6#npm:0.1.0"],\
           ["karma-chrome-launcher", "npm:3.1.0"],\
           ["karma-firefox-launcher", "npm:2.1.2"],\
@@ -8567,6 +8567,13 @@ const RAW_RUNTIME_STATE =
         ],\
         "linkType": "SOFT"\
       }],\
+      ["npm:4.3.5", {\
+        "packageLocation": "./.yarn/cache/debug-npm-4.3.5-b5001f59b7-cb6eab424c.zip/node_modules/debug/",\
+        "packageDependencies": [\
+          ["debug", "npm:4.3.5"]\
+        ],\
+        "linkType": "SOFT"\
+      }],\
       ["virtual:2a426afc4b2eef43db12a540d29c2b5476640459bfcd5c24f86bb401cf8cce97e63bd81794d206a5643057e7f662643afd5ce3dfc4d4bfd8e706006c6309c5fa#npm:3.2.7", {\
         "packageLocation": "./.yarn/__virtual__/debug-virtual-d2345003b7/0/cache/debug-npm-3.2.7-754e818c7a-d86fd7be2b.zip/node_modules/debug/",\
         "packageDependencies": [\
@@ -8615,6 +8622,20 @@ const RAW_RUNTIME_STATE =
           ["debug", "virtual:6e177cabfad012f413f9c41366539c04d8701f0567119998690ab02224012faa99ec3a16b9f74f4d7920ab472c12b3e70f47f8f143239c06d0e2569e60ed9f62#npm:2.6.9"],\
           ["@types/supports-color", null],\
           ["ms", "npm:2.0.0"],\
+          ["supports-color", null]\
+        ],\
+        "packagePeers": [\
+          "@types/supports-color",\
+          "supports-color"\
+        ],\
+        "linkType": "HARD"\
+      }],\
+      ["virtual:7a0505537f63825f62aaaf982168c2b7c1e816756656d44af98202b8d07990e163024e7dadf5587aa11d691887401ca8792ff06467da4d479c747705c9e87544#npm:4.3.5", {\
+        "packageLocation": "./.yarn/__virtual__/debug-virtual-e443f3d004/0/cache/debug-npm-4.3.5-b5001f59b7-cb6eab424c.zip/node_modules/debug/",\
+        "packageDependencies": [\
+          ["debug", "virtual:7a0505537f63825f62aaaf982168c2b7c1e816756656d44af98202b8d07990e163024e7dadf5587aa11d691887401ca8792ff06467da4d479c747705c9e87544#npm:4.3.5"],\
+          ["@types/supports-color", null],\
+          ["ms", "npm:2.1.2"],\
           ["supports-color", null]\
         ],\
         "packagePeers": [\
@@ -12663,10 +12684,10 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["karma", [\
-      ["npm:6.4.1", {\
-        "packageLocation": "./.yarn/cache/karma-npm-6.4.1-5443d36ee9-24faabfaf0.zip/node_modules/karma/",\
+      ["npm:6.4.3", {\
+        "packageLocation": "./.yarn/cache/karma-npm-6.4.3-c1db2b322c-7fc194f5d4.zip/node_modules/karma/",\
         "packageDependencies": [\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["@colors/colors", "npm:1.5.0"],\
           ["body-parser", "npm:1.19.0"],\
           ["braces", "npm:3.0.2"],\
@@ -12686,7 +12707,7 @@ const RAW_RUNTIME_STATE =
           ["qjobs", "npm:1.2.0"],\
           ["range-parser", "npm:1.2.1"],\
           ["rimraf", "npm:3.0.2"],\
-          ["socket.io", "npm:4.5.2"],\
+          ["socket.io", "npm:4.7.5"],\
           ["source-map", "npm:0.6.1"],\
           ["tmp", "npm:0.2.1"],\
           ["ua-parser-js", "npm:1.0.33"],\
@@ -12710,7 +12731,7 @@ const RAW_RUNTIME_STATE =
           ["@types/chai", "npm:4.2.22"],\
           ["@types/karma", null],\
           ["chai", "npm:4.3.10"],\
-          ["karma", "npm:6.4.1"]\
+          ["karma", "npm:6.4.3"]\
         ],\
         "packagePeers": [\
           "@types/chai",\
@@ -12727,7 +12748,7 @@ const RAW_RUNTIME_STATE =
           ["@types/chai", null],\
           ["@types/karma", null],\
           ["chai", "npm:4.3.10"],\
-          ["karma", "npm:6.4.1"]\
+          ["karma", "npm:6.4.3"]\
         ],\
         "packagePeers": [\
           "@types/chai",\
@@ -12784,7 +12805,7 @@ const RAW_RUNTIME_STATE =
           ["karma-mocha-reporter", "virtual:e2d057e7cc143d3cb9bec864f4a2d862441b5a09f81f8e6c46e7a098cbc89e4d07017cc6e2e2142d5704bb55da853cbec2d025ebc0b30e8696c31380c00f2c7d#npm:2.2.5"],\
           ["@types/karma", null],\
           ["chalk", "npm:2.4.2"],\
-          ["karma", "npm:6.4.1"],\
+          ["karma", "npm:6.4.3"],\
           ["log-symbols", "npm:2.2.0"],\
           ["strip-ansi", "npm:4.0.0"]\
         ],\
@@ -17273,25 +17294,28 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["socket.io", [\
-      ["npm:4.5.2", {\
-        "packageLocation": "./.yarn/cache/socket.io-npm-4.5.2-11f9913f37-872605db0f.zip/node_modules/socket.io/",\
+      ["npm:4.7.5", {\
+        "packageLocation": "./.yarn/cache/socket.io-npm-4.7.5-7db6120b90-911528f5bf.zip/node_modules/socket.io/",\
         "packageDependencies": [\
-          ["socket.io", "npm:4.5.2"],\
+          ["socket.io", "npm:4.7.5"],\
           ["accepts", "npm:1.3.7"],\
           ["base64id", "npm:2.0.0"],\
+          ["cors", "npm:2.8.5"],\
           ["debug", "virtual:4b12ba5111caf7e8338099bdbc7cb046a9f8e079a44e74d0c03dca469876e3071ebbe671c5e90ae6b78ae33e22c205fa5ed32169a4aabd1404b13c56d09986e1#npm:4.3.4"],\
           ["engine.io", "npm:6.4.2"],\
-          ["socket.io-adapter", "npm:2.4.0"],\
+          ["socket.io-adapter", "npm:2.5.5"],\
           ["socket.io-parser", "npm:4.2.3"]\
         ],\
         "linkType": "HARD"\
       }]\
     ]],\
     ["socket.io-adapter", [\
-      ["npm:2.4.0", {\
-        "packageLocation": "./.yarn/cache/socket.io-adapter-npm-2.4.0-36a74a6ea1-e10c8c36a1.zip/node_modules/socket.io-adapter/",\
+      ["npm:2.5.5", {\
+        "packageLocation": "./.yarn/cache/socket.io-adapter-npm-2.5.5-7a0505537f-e364733a4c.zip/node_modules/socket.io-adapter/",\
         "packageDependencies": [\
-          ["socket.io-adapter", "npm:2.4.0"]\
+          ["socket.io-adapter", "npm:2.5.5"],\
+          ["debug", "virtual:7a0505537f63825f62aaaf982168c2b7c1e816756656d44af98202b8d07990e163024e7dadf5587aa11d691887401ca8792ff06467da4d479c747705c9e87544#npm:4.3.5"],\
+          ["ws", "virtual:b375dcefccef90d9158d5f197a75395cffedb61772e66f2efcf31c6c8e30c82a6423e0d52b091b15b4fa72cda43a09256ed00b6ce89b9cfb14074f087b9c8496#npm:8.17.1"]\
         ],\
         "linkType": "HARD"\
       }]\
