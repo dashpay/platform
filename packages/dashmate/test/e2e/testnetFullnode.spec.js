cd6f7c07e0a081ddaead476e7566fcb9407f34aa
Ivan Shumkov <ivan@shumkov.ru>
2024-07-09 19:56:47 +0700

    test: fix dependencies in tests

--- packages/dashmate/test/e2e/testnetFullnode.spec.js
+++ packages/dashmate/test/e2e/testnetFullnode.spec.js
@@ -126,7 +126,7 @@ describe('Testnet Fullnode', function main() {
         pass: config.get('core.rpc.password'),
       });
 
-      waitForCoreData = waitForCoreDataFactory({ coreRpcClient });
+      waitForCoreData = waitForCoreDataFactory({ rpcClient: coreRpcClient });
 
       lastBlockHeight = await waitForCoreData(0, (currentValue) => currentValue > 0);
     });
