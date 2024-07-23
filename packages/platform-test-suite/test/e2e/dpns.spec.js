4b74d69bcea90263a6c84240deb596bd7100b80c
Quantum Explorer <quantum@dash.org>
2024-07-23 12:04:28 +0700

    fix

--- packages/platform-test-suite/test/e2e/dpns.spec.js
+++ packages/platform-test-suite/test/e2e/dpns.spec.js
@@ -80,7 +80,7 @@ describe('DPNS', () => {
     // skip if DPNS owner private key is not passed and use `dash` in tests above
     it('should be able to register a TLD', async () => {
       createdTLD = await ownerClient.platform.names.register(newTopLevelDomain, {
-        dashAliasIdentityId: identity.getId(),
+        identity: identity.getId(),
       }, identity);
 
       // Additional wait time to mitigate testnet latency
@@ -148,7 +148,7 @@ describe('DPNS', () => {
 
       try {
         await client.platform.names.register(getRandomDomain(), {
-          dashAliasIdentityId: identity.getId(),
+          identity: identity.getId(),
         }, identity);
       } catch (e) {
         broadcastError = e;
@@ -179,7 +179,7 @@ describe('DPNS', () => {
         const domain = `${secondLevelDomain}O.${topLevelDomain}`;
 
         await client.platform.names.register(domain, {
-          dashAliasIdentityId: identity.getId(),
+          identity: identity.getId(),
         }, identity);
 
         expect.fail('should throw error');
@@ -199,7 +199,7 @@ describe('DPNS', () => {
         const domain = `${getRandomDomain()}.${getRandomDomain()}`;
 
         await client.platform.names.register(domain, {
-          dashAliasIdentityId: identity.getId(),
+          identity: identity.getId(),
         }, identity);
 
         expect.fail('should throw error');
@@ -310,9 +310,9 @@ describe('DPNS', () => {
       expect(broadcastError.code).to.equal(40500);
     });
 
-    it('should not be able to register two domains with same `dashAliasIdentityId` record');
+    it('should not be able to register two domains with same `identity` record');
 
-    it('should be able to register many domains with same `dashAliasIdentityId` record');
+    it('should be able to register many domains with same `identity` record');
 
     it('should not be able to update preorder');
 
