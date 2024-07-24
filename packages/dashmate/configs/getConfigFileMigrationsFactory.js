990bc355b2b01fb9a463ec5fa7ff2f90b40f3e63
lklimek <842586+lklimek@users.noreply.github.com>
2024-07-24 08:30:36 +0200

    build: update tenderdash to 1.1.0-dev.1 (#1985)
    Co-authored-by: Ivan Shumkov <ivan@shumkov.ru>

--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-param-reassign */
 import fs from 'fs';
-import path from 'path';
 import lodash from 'lodash';
+import path from 'path';
 
 import {
   NETWORK_LOCAL,
-  NETWORK_TESTNET,
   NETWORK_MAINNET,
+  NETWORK_TESTNET,
   SSL_PROVIDERS,
 } from '../src/constants.js';
 
@@ -700,6 +700,16 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
             if (options.network === NETWORK_TESTNET) {
               options.platform.drive.tenderdash.genesis = testnet.get('platform.drive.tenderdash.genesis');
             }
+          });
+        return configFile;
+      },
+      '1.0.0-rc.1': (configFile) => {
+        Object.entries(configFile.configs)
+          .forEach(([, options]) => {
+            options.platform.drive.tenderdash.docker.image = base.get('platform.drive.tenderdash.docker.image');
+
+            // Replace quorumsign with qurumplatformsign in Core RPC Tenderdash auth whitelist
+            options.core.rpc.users.tenderdash.whitelist = base.get('core.rpc.users.tenderdash.whitelist');
           });
         return configFile;
       },
