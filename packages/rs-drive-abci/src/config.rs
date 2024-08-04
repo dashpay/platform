1f10ed585f2201e4398e692acd7ce2d73bccd324
Quantum Explorer <quantum@dash.org>
2024-08-04 10:40:23 +0700

    fixes

--- packages/rs-drive-abci/src/config.rs
+++ packages/rs-drive-abci/src/config.rs
@@ -1,31 +1,3 @@
-// MIT LICENSE
-//
-// Copyright (c) 2021 Dash Core Group
-//
-// Permission is hereby granted, free of charge, to any
-// person obtaining a copy of this software and associated
-// documentation files (the "Software"), to deal in the
-// Software without restriction, including without
-// limitation the rights to use, copy, modify, merge,
-// publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software
-// is furnished to do so, subject to the following
-// conditions:
-//
-// The above copyright notice and this permission notice
-// shall be included in all copies or substantial portions
-// of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
-// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
-// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-
 use bincode::{Decode, Encode};
 use dashcore_rpc::json::QuorumType;
 use std::path::PathBuf;
@@ -173,6 +145,7 @@ where
 // https://github.com/softprops/envy/issues/61 and https://github.com/softprops/envy/pull/69
 pub struct PlatformConfig {
     /// The network type
+    #[serde(default = "PlatformConfig::default_network")]
     pub network: Network,
     /// Drive configuration
     #[serde(flatten)]
@@ -579,6 +552,10 @@ impl ExecutionConfig {
 impl PlatformConfig {
     fn default_initial_protocol_version() -> ProtocolVersion {
         INITIAL_PROTOCOL_VERSION
+    }
+
+    fn default_network() -> Network {
+        Network::Regtest //todo: Do not leave this as regtest
     }
 
     fn default_tokio_console_address() -> String {
