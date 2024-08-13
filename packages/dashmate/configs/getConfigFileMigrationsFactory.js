faf6497c2837ffaae2559d0f282c96fb81ea6dcb
Ivan Shumkov <ivan@shumkov.ru>
2024-08-13 15:41:20 +0700

    chore: update timeouts

--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -768,7 +768,7 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
       '1.1.0-dev.1': (configFile) => {
         Object.entries(configFile.configs)
           .forEach(([, options]) => {
-            options.platform.gateway.listeners.dapiAndDrive.waitForStResultTimeout = '120s';
+            options.platform.gateway.listeners.dapiAndDrive.waitForStResultTimeout = '125s';
             options.platform.dapi.api.waitForStResultTimeout = 120000;
           });
         return configFile;
