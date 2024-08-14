36ad576b79462a1e076247a2c15173e6b12f93bf
owl352 <kokosinca123@gmail.com>
2024-08-14 13:54:48 +0500

    names fix

--- packages/js-dapi-client/lib/methods/platform/getTotalCreditsInPlatform/GetTotalCreditsInPlatformResponse.js
+++ packages/js-dapi-client/lib/methods/platform/getTotalCreditsInPlatform/GetTotalCreditsInPlatformResponse.js
@@ -3,21 +3,21 @@ const InvalidResponseError = require('../response/errors/InvalidResponseError');
 
 class GetTotalCreditsInPlatformResponse extends AbstractResponse {
   /**
-   * @param {number} totalCreditsOnPlatform
+   * @param {number} totalCreditsInPlatform
    * @param {Metadata} metadata
    * @param {Proof} [proof]
    */
-  constructor(totalCreditsOnPlatform, metadata, proof = undefined) {
+  constructor(totalCreditsInPlatform, metadata, proof = undefined) {
     super(metadata, proof);
 
-    this.totalCreditsOnPlatform = totalCreditsOnPlatform;
+    this.totalCreditsInPlatform = totalCreditsInPlatform;
   }
 
   /**
    * @returns {number}
    */
   getTotalCreditsInPlatform() {
-    return this.totalCreditsOnPlatform;
+    return this.totalCreditsInPlatform;
   }
 
   /**
@@ -26,18 +26,18 @@ class GetTotalCreditsInPlatformResponse extends AbstractResponse {
    */
   static createFromProto(proto) {
     // eslint-disable-next-line
-    const totalCreditsOnPlatform = proto.getV0().getCredits();
+    const totalCreditsInPlatform = proto.getV0().getCredits();
 
     const { metadata, proof } = AbstractResponse.createMetadataAndProofFromProto(
       proto,
     );
 
-    if ((typeof totalCreditsOnPlatform === 'undefined' || totalCreditsOnPlatform === null) && !proof) {
+    if ((typeof totalCreditsInPlatform === 'undefined' || totalCreditsInPlatform === null) && !proof) {
       throw new InvalidResponseError('Total Credits on Platform data is not defined');
     }
 
     return new GetTotalCreditsInPlatformResponse(
-      totalCreditsOnPlatform,
+      totalCreditsInPlatform,
       metadata,
       proof,
     );
