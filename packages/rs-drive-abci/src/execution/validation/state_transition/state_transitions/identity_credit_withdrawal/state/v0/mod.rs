3404f5958ccfb4e9f9098b9ea1024e745a25d8ea
Ivan Shumkov <ivan@shumkov.ru>
2024-10-03 13:53:43 +0300

    refactor(drive): remove duplicated withdrawal amount validation (#2191)

--- packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/identity_credit_withdrawal/state/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/identity_credit_withdrawal/state/v0/mod.rs
@@ -3,10 +3,7 @@ use crate::platform_types::platform::PlatformRef;
 use crate::rpc::core::CoreRPCLike;
 use dpp::block::block_info::BlockInfo;
 
-use dpp::consensus::signature::IdentityNotFoundError;
-use dpp::consensus::state::identity::IdentityInsufficientBalanceError;
 use dpp::prelude::ConsensusValidationResult;
-use dpp::state_transition::identity_credit_withdrawal_transition::accessors::IdentityCreditWithdrawalTransitionAccessorsV0;
 use dpp::state_transition::identity_credit_withdrawal_transition::IdentityCreditWithdrawalTransition;
 
 use crate::execution::types::execution_operation::ValidationOperation;
@@ -49,29 +46,6 @@ impl IdentityCreditWithdrawalStateTransitionStateValidationV0
         tx: TransactionArg,
         platform_version: &PlatformVersion,
     ) -> Result<ConsensusValidationResult<StateTransitionAction>, Error> {
-        let maybe_existing_identity_balance = platform.drive.fetch_identity_balance(
-            self.identity_id().to_buffer(),
-            tx,
-            platform_version,
-        )?;
-
-        let Some(existing_identity_balance) = maybe_existing_identity_balance else {
-            return Ok(ConsensusValidationResult::new_with_error(
-                IdentityNotFoundError::new(self.identity_id()).into(),
-            ));
-        };
-
-        if existing_identity_balance < self.amount() {
-            return Ok(ConsensusValidationResult::new_with_error(
-                IdentityInsufficientBalanceError::new(
-                    self.identity_id(),
-                    existing_identity_balance,
-                    self.amount(),
-                )
-                .into(),
-            ));
-        }
-
         self.transform_into_action_v0(
             platform,
             block_info,
@@ -91,7 +65,7 @@ impl IdentityCreditWithdrawalStateTransitionStateValidationV0
     ) -> Result<ConsensusValidationResult<StateTransitionAction>, Error> {
         let consensus_validation_result =
             IdentityCreditWithdrawalTransitionAction::try_from_identity_credit_withdrawal(
-                &platform.drive,
+                platform.drive,
                 tx,
                 self,
                 block_info,
