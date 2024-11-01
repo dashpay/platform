61f3ce7836857f4b8e259920c8846161aceb401c
Ivan Shumkov <ivan@shumkov.ru>
2024-11-01 17:08:37 +0700

    chore: introduce one more version

--- packages/rs-platform-version/src/version/v6.rs
+++ packages/rs-platform-version/src/version/v6.rs
@@ -5,7 +5,6 @@ use crate::version::dpp_versions::dpp_costs_versions::v1::DPP_COSTS_VERSIONS_V1;
 use crate::version::dpp_versions::dpp_document_versions::v1::DOCUMENT_VERSIONS_V1;
 use crate::version::dpp_versions::dpp_factory_versions::v1::DPP_FACTORY_VERSIONS_V1;
 use crate::version::dpp_versions::dpp_identity_versions::v1::IDENTITY_VERSIONS_V1;
-use crate::version::dpp_versions::dpp_method_versions::v1::DPP_METHOD_VERSIONS_V1;
 use crate::version::dpp_versions::dpp_method_versions::v2::DPP_METHOD_VERSIONS_V2;
 use crate::version::dpp_versions::dpp_state_transition_conversion_versions::v2::STATE_TRANSITION_CONVERSION_VERSIONS_V2;
 use crate::version::dpp_versions::dpp_state_transition_method_versions::v1::STATE_TRANSITION_METHOD_VERSIONS_V1;
@@ -14,8 +13,7 @@ use crate::version::dpp_versions::dpp_state_transition_versions::v2::STATE_TRANS
 use crate::version::dpp_versions::dpp_validation_versions::v2::DPP_VALIDATION_VERSIONS_V2;
 use crate::version::dpp_versions::dpp_voting_versions::v2::VOTING_VERSION_V2;
 use crate::version::dpp_versions::DPPVersion;
-use crate::version::drive_abci_versions::drive_abci_method_versions::v3::DRIVE_ABCI_METHOD_VERSIONS_V3;
-use crate::version::drive_abci_versions::drive_abci_method_versions::v4::DRIVE_ABCI_METHOD_VERSIONS_V4;
+use crate::version::drive_abci_versions::drive_abci_method_versions::v5::DRIVE_ABCI_METHOD_VERSIONS_V5;
 use crate::version::drive_abci_versions::drive_abci_query_versions::v1::DRIVE_ABCI_QUERY_VERSIONS_V1;
 use crate::version::drive_abci_versions::drive_abci_structure_versions::v1::DRIVE_ABCI_STRUCTURE_VERSIONS_V1;
 use crate::version::drive_abci_versions::drive_abci_validation_versions::v3::DRIVE_ABCI_VALIDATION_VERSIONS_V3;
@@ -28,15 +26,18 @@ use crate::version::system_data_contract_versions::v1::SYSTEM_DATA_CONTRACT_VERS
 use crate::version::system_limits::v1::SYSTEM_LIMITS_V1;
 use crate::version::ProtocolVersion;
 
-pub const PROTOCOL_VERSION_5: ProtocolVersion = 5;
+pub const PROTOCOL_VERSION_6: ProtocolVersion = 6;
 
 /// This version contains some fixes for withdrawals.
 pub const PLATFORM_V5: PlatformVersion = PlatformVersion {
-    protocol_version: PROTOCOL_VERSION_5,
+    protocol_version: PROTOCOL_VERSION_6,
     drive: DRIVE_VERSION_V2,
     drive_abci: DriveAbciVersion {
         structs: DRIVE_ABCI_STRUCTURE_VERSIONS_V1,
-        methods: DRIVE_ABCI_METHOD_VERSIONS_V4, // changed to v4
+        // We changed `pool_withdrawals_into_transactions_queue` to v1 in order to add pool
+        // withdrawals on any validator quorums. v0 allowed us to pool only on the first two
+        // quorums as workaround for Core v21 bug.
+        methods: DRIVE_ABCI_METHOD_VERSIONS_V5,
         validation_and_processing: DRIVE_ABCI_VALIDATION_VERSIONS_V3,
         withdrawal_constants: DRIVE_ABCI_WITHDRAWAL_CONSTANTS_V2,
         query: DRIVE_ABCI_QUERY_VERSIONS_V1,
@@ -53,7 +54,9 @@ pub const PLATFORM_V5: PlatformVersion = PlatformVersion {
         identity_versions: IDENTITY_VERSIONS_V1,
         voting_versions: VOTING_VERSION_V2,
         asset_lock_versions: DPP_ASSET_LOCK_VERSIONS_V1,
-        methods: DPP_METHOD_VERSIONS_V1,
+        // We changed `daily_withdrawal_limit` to v1 to increase daily withdrawal limit
+        // to 2000 Dash.
+        methods: DPP_METHOD_VERSIONS_V2,
         factory_versions: DPP_FACTORY_VERSIONS_V1,
     },
     system_data_contracts: SYSTEM_DATA_CONTRACT_VERSIONS_V1,
