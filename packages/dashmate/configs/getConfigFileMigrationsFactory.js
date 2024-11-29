f3e34b86860a2f0f6ef731359bcf3228390f012a
ivanshumkov <ivanshumkov@gmail.com>
2024-11-29 15:02:29 +0700

    chore: update migration

--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -1035,8 +1035,8 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
       '1.6.0': (configFile) => {
         Object.entries(configFile.configs)
           .forEach(([, options]) => {
-            options.platform.drive.abci.docker.image = 'dashpay/drive:1-dev';
-            options.platform.dapi.api.docker.image = 'dashpay/dapi:1-dev';
+            options.platform.drive.abci.docker.image = 'dashpay/drive:1';
+            options.platform.dapi.api.docker.image = 'dashpay/dapi:1';
           });
         return configFile;
       },
