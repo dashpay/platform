b980183d9cbef9bf0062f59aa1e7f04e25192ecc
pankcuf <rastacake@gmail.com>
2024-12-17 18:20:07 +0700

    chore: remove bad merge artefacts

--- packages/rs-platform-version/src/version/protocol_version.rs
+++ packages/rs-platform-version/src/version/protocol_version.rs
@@ -24,32 +24,6 @@ use crate::version::v6::PLATFORM_V6;
 use crate::version::v7::PLATFORM_V7;
 use crate::version::ProtocolVersion;
 
-#[ferment_macro::export]
-pub type FeatureVersion = u16;
-#[ferment_macro::export]
-pub type OptionalFeatureVersion = Option<u16>; //This is a feature that didn't always exist
-
-#[derive(Clone, Debug, Default)]
-#[ferment_macro::export]
-pub struct FeatureVersionBounds {
-    pub min_version: FeatureVersion,
-    pub max_version: FeatureVersion,
-    pub default_current_version: FeatureVersion,
-}
-
-impl FeatureVersionBounds {
-    /// Will get a protocol error if the version is unknown
-    pub fn check_version(&self, version: FeatureVersion) -> bool {
-        version >= self.min_version && version <= self.max_version
-    }
-}
-
-#[derive(Clone, Debug, Default)]
-#[ferment_macro::export]
-pub struct AbciStructureVersion {
-    pub extended_block_info: FeatureVersionBounds,
-}
-
 #[derive(Clone, Debug)]
 pub struct PlatformVersion {
     pub protocol_version: ProtocolVersion,
