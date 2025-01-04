83796655f407aafa03e2655cb15fe663a20c68cc
Quantum Explorer <quantum@dash.org>
2025-01-05 02:38:22 +0700

    transfer tests

--- packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/batch/action_validation/token_transfer_transition_action/state_v0/mod.rs
+++ packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/batch/action_validation/token_transfer_transition_action/state_v0/mod.rs
@@ -1,4 +1,7 @@
 use dpp::block::block_info::BlockInfo;
+use dpp::consensus::ConsensusError;
+use dpp::consensus::state::state_error::StateError;
+use dpp::consensus::state::token::IdentityDoesNotHaveEnoughTokenBalanceError;
 use dpp::prelude::{ConsensusValidationResult, Identifier};
 use dpp::validation::SimpleConsensusValidationResult;
 use drive::state_transition_action::document::documents_batch::document_transition::document_base_transition_action::DocumentBaseTransitionActionAccessorsV0;
@@ -8,8 +11,10 @@ use dpp::voting::vote_info_storage::contested_document_vote_poll_stored_info::{C
 use drive::error::drive::DriveError;
 use drive::query::TransactionArg;
 use drive::state_transition_action::document::documents_batch::document_transition::token_base_transition_action::TokenBaseTransitionActionAccessorsV0;
+use drive::state_transition_action::document::documents_batch::document_transition::token_transfer_transition_action::v0::TokenTransferTransitionActionAccessorsV0;
 use crate::error::Error;
-use crate::execution::types::state_transition_execution_context::StateTransitionExecutionContext;
+use crate::execution::types::execution_operation::ValidationOperation;
+use crate::execution::types::state_transition_execution_context::{StateTransitionExecutionContext, StateTransitionExecutionContextMethodsV0};
 use crate::platform_types::platform::PlatformStateRef;
 
 pub(super) trait TokenTransferTransitionActionStateValidationV0 {
@@ -33,7 +38,30 @@ impl TokenTransferTransitionActionStateValidationV0 for TokenTransferTransitionA
         transaction: TransactionArg,
         platform_version: &PlatformVersion,
     ) -> Result<SimpleConsensusValidationResult, Error> {
-        // todo verify that minting would not break max supply
+        // We need to verify that we have enough of the token
+        let balance = platform
+            .drive
+            .fetch_identity_token_balance(
+                self.token_id().to_buffer(),
+                owner_id.to_buffer(),
+                transaction,
+                platform_version,
+            )?
+            .unwrap_or_default();
+        execution_context.add_operation(ValidationOperation::RetrieveIdentityTokenBalance);
+        if balance < self.amount() {
+            // The identity does not exist
+            return Ok(SimpleConsensusValidationResult::new_with_error(
+                ConsensusError::StateError(StateError::IdentityDoesNotHaveEnoughTokenBalanceError(
+                    IdentityDoesNotHaveEnoughTokenBalanceError::new(
+                        owner_id,
+                        self.amount(),
+                        balance,
+                        "transfer".to_string(),
+                    ),
+                )),
+            ));
+        }
 
         Ok(SimpleConsensusValidationResult::new())
     }
--- packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/batch/mod.rs
+++ packages/rs-drive-abci/src/execution/validation/state_transition/state_transitions/batch/mod.rs
@@ -2882,7 +2882,7 @@ mod tests {
 
             assert_eq!(processing_result.valid_count(), 1);
 
-            assert_eq!(processing_result.aggregated_fees().processing_fee, 1341740);
+            assert_eq!(processing_result.aggregated_fees().processing_fee, 1443820);
 
             let issues = platform
                 .drive
@@ -4818,7 +4818,7 @@ mod tests {
 
             assert_eq!(processing_result.valid_count(), 1);
 
-            assert_eq!(processing_result.aggregated_fees().processing_fee, 1609340);
+            assert_eq!(processing_result.aggregated_fees().processing_fee, 1711420);
 
             let issues = platform
                 .drive
@@ -10561,7 +10561,6 @@ mod tests {
 
         mod token_burn_tests {
             use super::*;
-            use platform_version::version::PlatformVersion;
 
             #[test]
             fn test_token_burn() {
@@ -10841,6 +10840,223 @@ mod tests {
                         platform_version,
                     )
                     .expect("expected to fetch token balance");
+                assert_eq!(token_balance, None);
+            }
+        }
+
+        mod token_transfer_tests {
+            use super::*;
+
+            #[test]
+            fn test_token_transfer() {
+                let platform_version = PlatformVersion::latest();
+                let mut platform = TestPlatformBuilder::new()
+                    .with_latest_protocol_version()
+                    .build_with_mock_rpc()
+                    .set_genesis_state();
+
+                let mut rng = StdRng::seed_from_u64(49853);
+
+                let platform_state = platform.state.load();
+
+                let (identity, signer, key) =
+                    setup_identity(&mut platform, rng.gen(), dash_to_credits!(0.5));
+
+                let (recipient, _, _) =
+                    setup_identity(&mut platform, rng.gen(), dash_to_credits!(0.5));
+
+                let (contract, token_id) = create_token_contract_with_owner_identity(
+                    &mut platform,
+                    identity.id(),
+                    None::<fn(&mut TokenConfiguration)>,
+                    None,
+                    platform_version,
+                );
+
+                let token_transfer_transition = BatchTransition::new_token_transfer_transition(
+                    token_id,
+                    identity.id(),
+                    contract.id(),
+                    0,
+                    1337,
+                    recipient.id(),
+                    None,
+                    None,
+                    None,
+                    None,
+                    &key,
+                    2,
+                    0,
+                    &signer,
+                    platform_version,
+                    None,
+                    None,
+                    None,
+                )
+                .expect("expect to create documents batch transition");
+
+                let token_transfer_serialized_transition = token_transfer_transition
+                    .serialize_to_bytes()
+                    .expect("expected documents batch serialized state transition");
+
+                let transaction = platform.drive.grove.start_transaction();
+
+                let processing_result = platform
+                    .platform
+                    .process_raw_state_transitions(
+                        &vec![token_transfer_serialized_transition.clone()],
+                        &platform_state,
+                        &BlockInfo::default(),
+                        &transaction,
+                        platform_version,
+                        false,
+                        None,
+                    )
+                    .expect("expected to process state transition");
+
+                assert_matches!(
+                    processing_result.execution_results().as_slice(),
+                    [StateTransitionExecutionResult::SuccessfulExecution(_, _)]
+                );
+
+                platform
+                    .drive
+                    .grove
+                    .commit_transaction(transaction)
+                    .unwrap()
+                    .expect("expected to commit transaction");
+
+                let token_balance = platform
+                    .drive
+                    .fetch_identity_token_balance(
+                        token_id.to_buffer(),
+                        identity.id().to_buffer(),
+                        None,
+                        platform_version,
+                    )
+                    .expect("expected to fetch token balance");
+                let expected_amount = 100000 - 1337;
+                assert_eq!(token_balance, Some(expected_amount));
+
+                let token_balance = platform
+                    .drive
+                    .fetch_identity_token_balance(
+                        token_id.to_buffer(),
+                        recipient.id().to_buffer(),
+                        None,
+                        platform_version,
+                    )
+                    .expect("expected to fetch token balance");
+                let expected_amount = 1337;
+                assert_eq!(token_balance, Some(expected_amount));
+            }
+
+            #[test]
+            fn test_token_transfer_trying_to_send_more_than_we_have() {
+                let platform_version = PlatformVersion::latest();
+                let mut platform = TestPlatformBuilder::new()
+                    .with_latest_protocol_version()
+                    .build_with_mock_rpc()
+                    .set_genesis_state();
+
+                let mut rng = StdRng::seed_from_u64(49853);
+
+                let platform_state = platform.state.load();
+
+                let (identity, signer, key) =
+                    setup_identity(&mut platform, rng.gen(), dash_to_credits!(0.5));
+
+                let (recipient, _, _) =
+                    setup_identity(&mut platform, rng.gen(), dash_to_credits!(0.5));
+
+                let (contract, token_id) = create_token_contract_with_owner_identity(
+                    &mut platform,
+                    identity.id(),
+                    None::<fn(&mut TokenConfiguration)>,
+                    None,
+                    platform_version,
+                );
+
+                let token_transfer_transition = BatchTransition::new_token_transfer_transition(
+                    token_id,
+                    identity.id(),
+                    contract.id(),
+                    0,
+                    200000,
+                    recipient.id(),
+                    None,
+                    None,
+                    None,
+                    None,
+                    &key,
+                    2,
+                    0,
+                    &signer,
+                    platform_version,
+                    None,
+                    None,
+                    None,
+                )
+                .expect("expect to create documents batch transition");
+
+                let token_transfer_serialized_transition = token_transfer_transition
+                    .serialize_to_bytes()
+                    .expect("expected documents batch serialized state transition");
+
+                let transaction = platform.drive.grove.start_transaction();
+
+                let processing_result = platform
+                    .platform
+                    .process_raw_state_transitions(
+                        &vec![token_transfer_serialized_transition.clone()],
+                        &platform_state,
+                        &BlockInfo::default(),
+                        &transaction,
+                        platform_version,
+                        false,
+                        None,
+                    )
+                    .expect("expected to process state transition");
+
+                assert_matches!(
+                    processing_result.execution_results().as_slice(),
+                    [StateTransitionExecutionResult::PaidConsensusError(
+                        ConsensusError::StateError(
+                            StateError::IdentityDoesNotHaveEnoughTokenBalanceError(_)
+                        ),
+                        _
+                    )]
+                );
+
+                platform
+                    .drive
+                    .grove
+                    .commit_transaction(transaction)
+                    .unwrap()
+                    .expect("expected to commit transaction");
+
+                let token_balance = platform
+                    .drive
+                    .fetch_identity_token_balance(
+                        token_id.to_buffer(),
+                        identity.id().to_buffer(),
+                        None,
+                        platform_version,
+                    )
+                    .expect("expected to fetch token balance");
+                let expected_amount = 100000;
+                assert_eq!(token_balance, Some(expected_amount));
+
+                let token_balance = platform
+                    .drive
+                    .fetch_identity_token_balance(
+                        token_id.to_buffer(),
+                        recipient.id().to_buffer(),
+                        None,
+                        platform_version,
+                    )
+                    .expect("expected to fetch token balance");
+                let expected_amount = 1337;
                 assert_eq!(token_balance, None);
             }
         }
