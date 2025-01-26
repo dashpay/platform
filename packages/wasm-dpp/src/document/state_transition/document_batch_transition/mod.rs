0743be234d4008a5260d6ec41db6736a48b35798
pshenmic <pshenmic@gmail.com>
2025-01-26 22:00:40 +0700

    feat(dpp): extra methods for state transitions in wasm (#2401)

--- packages/wasm-dpp/src/document/state_transition/document_batch_transition/mod.rs
+++ packages/wasm-dpp/src/document/state_transition/document_batch_transition/mod.rs
@@ -98,6 +98,16 @@ impl DocumentsBatchTransitionWasm {
         self.0.owner_id().to_owned().into()
     }
 
+    #[wasm_bindgen(js_name=getUserFeeIncrease)]
+    pub fn get_user_fee_increase(&self) -> u16 {
+        self.0.user_fee_increase() as u16
+    }
+
+    #[wasm_bindgen(js_name=setUserFeeIncrease)]
+    pub fn set_user_fee_increase(&mut self, user_fee_increase: u16) {
+        self.0.set_user_fee_increase(user_fee_increase);
+    }
+
     #[wasm_bindgen(js_name=getTransitions)]
     pub fn get_transitions(&self) -> js_sys::Array {
         let array = js_sys::Array::new();
