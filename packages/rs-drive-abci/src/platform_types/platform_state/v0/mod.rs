390847ad2cdb72a20714d619e2a651d036f4681b
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2025-02-10 13:12:21 +0200

    chore: log field previous_fee_versions

--- packages/rs-drive-abci/src/platform_types/platform_state/v0/mod.rs
+++ packages/rs-drive-abci/src/platform_types/platform_state/v0/mod.rs
@@ -114,6 +114,10 @@ impl Debug for PlatformStateV0 {
                 "instant_lock_validating_quorums",
                 &self.instant_lock_validating_quorums,
             )
+            .field(
+                "previous_fee_versions",
+                &self.previous_fee_versions,
+            )
             .finish()
     }
 }
