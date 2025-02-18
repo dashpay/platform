2277e0207dc35ea10b4b1aa40ac0c10e4313a15d
thephez <thephez@users.noreply.github.com>
2025-02-18 13:00:49 -0500

    chore: cargo fmt

--- packages/rs-dpp/src/identity/credits_converter.rs
+++ packages/rs-dpp/src/identity/credits_converter.rs
@@ -1,17 +1,21 @@
+use crate::balances::credits::CREDITS_PER_DUFF;
 use crate::fee::Credits;
 use crate::ProtocolError;
-use crate::balances::credits::CREDITS_PER_DUFF;
 
 pub fn convert_duffs_to_credits(amount: u64) -> Result<Credits, ProtocolError> {
-    amount.checked_mul(CREDITS_PER_DUFF).ok_or(ProtocolError::Overflow(
-        "converting duffs to credits failed",
-    ))
+    amount
+        .checked_mul(CREDITS_PER_DUFF)
+        .ok_or(ProtocolError::Overflow(
+            "converting duffs to credits failed",
+        ))
 }
 
 pub fn convert_credits_to_duffs(amount: Credits) -> Result<u64, ProtocolError> {
-    amount.checked_div(CREDITS_PER_DUFF).ok_or(ProtocolError::Overflow(
-        "converting credits to duffs failed",
-    ))
+    amount
+        .checked_div(CREDITS_PER_DUFF)
+        .ok_or(ProtocolError::Overflow(
+            "converting credits to duffs failed",
+        ))
 }
 
 #[cfg(test)]
