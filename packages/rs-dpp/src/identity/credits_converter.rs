9c322bacd498f9925389bcd28f764b4297385cb7
thephez <thephez@users.noreply.github.com>
2025-02-18 12:51:32 -0500

    refactor: import CREDITS_PER_DUFF for use in conversion converter
    Eliminates a second definition for the same constant (credits/duff ratio)

--- packages/rs-dpp/src/identity/credits_converter.rs
+++ packages/rs-dpp/src/identity/credits_converter.rs
@@ -1,16 +1,15 @@
 use crate::fee::Credits;
 use crate::ProtocolError;
-
-pub const RATIO: u64 = 1000;
+use crate::balances::credits::CREDITS_PER_DUFF;
 
 pub fn convert_duffs_to_credits(amount: u64) -> Result<Credits, ProtocolError> {
-    amount.checked_mul(RATIO).ok_or(ProtocolError::Overflow(
+    amount.checked_mul(CREDITS_PER_DUFF).ok_or(ProtocolError::Overflow(
         "converting duffs to credits failed",
     ))
 }
 
 pub fn convert_credits_to_duffs(amount: Credits) -> Result<u64, ProtocolError> {
-    amount.checked_div(RATIO).ok_or(ProtocolError::Overflow(
+    amount.checked_div(CREDITS_PER_DUFF).ok_or(ProtocolError::Overflow(
         "converting credits to duffs failed",
     ))
 }
@@ -23,20 +22,19 @@ mod test {
     fn test_should_convert_satoshi_to_credits() {
         let amount = 42;
         let converted = convert_duffs_to_credits(amount).unwrap();
-
-        assert_eq!(converted, amount * RATIO);
+        assert_eq!(converted, amount * CREDITS_PER_DUFF);
     }
 
     #[test]
     fn test_should_convert_credits_to_satoshi() {
         let amount = 10000;
         let converted = convert_credits_to_duffs(amount).unwrap();
-        assert_eq!(converted, amount / RATIO);
+        assert_eq!(converted, amount / CREDITS_PER_DUFF);
     }
 
     #[test]
     fn test_convert_to_0_satoshi_if_amount_lower_than_ratio() {
-        let amount = RATIO - 1;
+        let amount = CREDITS_PER_DUFF - 1;
         let converted = convert_credits_to_duffs(amount).unwrap();
         assert_eq!(converted, 0);
     }
