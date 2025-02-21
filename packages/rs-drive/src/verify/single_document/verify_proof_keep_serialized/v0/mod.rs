fa63231d7c866716d83c3b2f17a8c5005263bd90
Ivan Shumkov <ivan@shumkov.ru>
2025-02-21 18:15:16 +0700

    revert: proof verification changes

--- packages/rs-drive/src/verify/single_document/verify_proof_keep_serialized/v0/mod.rs
+++ packages/rs-drive/src/verify/single_document/verify_proof_keep_serialized/v0/mod.rs
@@ -38,15 +38,24 @@ impl SingleDocumentDriveQuery {
     ) -> Result<(RootHash, Option<Vec<u8>>), Error> {
         let path_query = self.construct_path_query(platform_version)?;
         let (root_hash, mut proved_key_values) = if is_subset {
-            GroveDb::verify_subset_query(proof, &path_query, &platform_version.drive.grove_version)?
+            GroveDb::verify_subset_query_with_absence_proof(
+                proof,
+                &path_query,
+                &platform_version.drive.grove_version,
+            )?
         } else {
-            GroveDb::verify_query(proof, &path_query, &platform_version.drive.grove_version)?
+            GroveDb::verify_query_with_absence_proof(
+                proof,
+                &path_query,
+                &platform_version.drive.grove_version,
+            )?
         };
 
         if proved_key_values.len() != 1 {
-            return Err(Error::Proof(ProofError::CorruptedProof(
-                "we should always get back one element".to_string(),
-            )));
+            return Err(Error::Proof(ProofError::CorruptedProof(format!(
+                "we should always get back one element, we got {}",
+                proved_key_values.len()
+            ))));
         }
 
         let element = proved_key_values.remove(0).2;
