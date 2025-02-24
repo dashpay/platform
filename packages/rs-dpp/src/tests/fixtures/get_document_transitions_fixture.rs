e17ddda1629992a04d7f7eab377c5b8c585905c1
pshenmic <pshenmic@gmail.com>
2025-02-25 03:32:48 +0700

    fix(rs-dpp): clippy

--- packages/rs-dpp/src/tests/fixtures/get_document_transitions_fixture.rs
+++ packages/rs-dpp/src/tests/fixtures/get_document_transitions_fixture.rs
@@ -4,7 +4,7 @@ use platform_value::{Bytes32, Identifier};
 use platform_version::version::PlatformVersion;
 use std::collections::BTreeMap;
 
-use crate::document::Document;
+use crate::document::{Document, DocumentTransitionParams};
 use crate::state_transition::documents_batch_transition::document_transition::action_type::DocumentTransitionActionType;
 use crate::state_transition::documents_batch_transition::document_transition::DocumentTransition;
 
@@ -14,7 +14,7 @@ pub fn get_document_transitions_fixture<'a>(
     documents: impl IntoIterator<
         Item = (
             DocumentTransitionActionType,
-            Vec<(Document, DocumentTypeRef<'a>, Bytes32)>,
+            Vec<(Document, Option<DocumentTransitionParams>, DocumentTypeRef<'a>, Bytes32)>,
         ),
     >,
     nonce_counter: &mut BTreeMap<(Identifier, Identifier), u64>, //IdentityID/ContractID -> nonce
@@ -24,7 +24,7 @@ pub fn get_document_transitions_fixture<'a>(
         DocumentFactory::new(protocol_version).expect("expected to get document factory");
 
     document_factory
-        .create_state_transition(documents, nonce_counter, None, None)
+        .create_state_transition(documents, nonce_counter)
         .expect("the transitions should be created")
         .transitions()
         .to_owned()
