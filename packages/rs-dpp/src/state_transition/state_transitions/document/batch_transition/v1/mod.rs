bdbf2ae8d8c1d74833951e7c35fe91102395b3a6
pauldelucia <pauldelucia2@gmail.com>
2025-03-06 12:27:06 +0700

    fix: rename some v0_methods to v1_methods

--- packages/rs-dpp/src/state_transition/state_transitions/document/batch_transition/v1/mod.rs
+++ packages/rs-dpp/src/state_transition/state_transitions/document/batch_transition/v1/mod.rs
@@ -3,7 +3,7 @@ mod identity_signed;
 mod json_conversion;
 mod state_transition_like;
 mod types;
-mod v0_methods;
+mod v1_methods;
 #[cfg(feature = "state-transition-value-conversion")]
 mod value_conversion;
 mod version;
