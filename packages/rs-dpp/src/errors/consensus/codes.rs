03cdc36e239efc38664d64eda4e1dabfeef7f220
Ivan Shumkov <ivan@shumkov.ru>
2025-03-19 15:33:16 +0700

    fix: token transfer to non-existing identity (#2505)

--- packages/rs-dpp/src/errors/consensus/codes.rs
+++ packages/rs-dpp/src/errors/consensus/codes.rs
@@ -304,6 +304,7 @@ impl ErrorWithCode for StateError {
             Self::InvalidTokenClaimPropertyMismatch(_) => 40715,
             Self::InvalidTokenClaimNoCurrentRewards(_) => 40716,
             Self::InvalidTokenClaimWrongClaimant(_) => 40717,
+            Self::TokenTransferRecipientIdentityNotExistError(_) => 40718,
 
             // Group errors: 40800-40899
             Self::IdentityNotMemberOfGroupError(_) => 40800,
