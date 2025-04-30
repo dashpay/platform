1ea78b9338ffe793dd5da473806c054e31ccbd49
QuantumExplorer <quantum@dash.org>
2025-05-01 01:43:44 +0700

    fix(dpp): do not allow mint of 0 tokens (#2581)

--- packages/rs-dpp/src/state_transition/state_transitions/document/batch_transition/batched_transition/token_mint_transition/validate_structure/v0/mod.rs
+++ packages/rs-dpp/src/state_transition/state_transitions/document/batch_transition/batched_transition/token_mint_transition/validate_structure/v0/mod.rs
@@ -1,6 +1,7 @@
 use crate::consensus::basic::token::{InvalidTokenAmountError, InvalidTokenNoteTooBigError};
 use crate::consensus::basic::BasicError;
 use crate::consensus::ConsensusError;
+use crate::data_contract::associated_token::token_perpetual_distribution::distribution_function::MAX_DISTRIBUTION_PARAM;
 use crate::state_transition::batch_transition::token_mint_transition::v0::v0_methods::TokenMintTransitionV0Methods;
 use crate::state_transition::batch_transition::TokenMintTransition;
 use crate::tokens::MAX_TOKEN_NOTE_LEN;
@@ -12,10 +13,10 @@ pub(super) trait TokenMintTransitionActionStructureValidationV0 {
 }
 impl TokenMintTransitionActionStructureValidationV0 for TokenMintTransition {
     fn validate_structure_v0(&self) -> Result<SimpleConsensusValidationResult, ProtocolError> {
-        if self.amount() > i64::MAX as u64 {
+        if self.amount() > MAX_DISTRIBUTION_PARAM || self.amount() == 0 {
             return Ok(SimpleConsensusValidationResult::new_with_error(
                 ConsensusError::BasicError(BasicError::InvalidTokenAmountError(
-                    InvalidTokenAmountError::new(i64::MAX as u64, self.amount()),
+                    InvalidTokenAmountError::new(MAX_DISTRIBUTION_PARAM, self.amount()),
                 )),
             ));
         }
