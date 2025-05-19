790162c6722f51ca494c8fef07cae33b1702cbda
Quantum Explorer <quantum@dash.org>
2025-05-19 10:29:26 +0200

    fix

--- packages/rs-drive/src/verify/state_transition/verify_state_transition_was_executed_with_proof/v0/mod.rs
+++ packages/rs-drive/src/verify/state_transition/verify_state_transition_was_executed_with_proof/v0/mod.rs
@@ -369,8 +369,9 @@ impl Drive {
                                     TokenTransition::DirectPurchase(_) => {
                                         let purchase_cost: Credits =
                                             document.properties().get_integer("purchaseCost")?;
-                                        let agreed_to_purchase_cost: Credits =
-                                            document.properties().get_integer("purchaseCost")?;
+                                        let agreed_to_purchase_cost: Credits = expected_document
+                                            .properties()
+                                            .get_integer("purchaseCost")?;
                                         if purchase_cost > agreed_to_purchase_cost {
                                             return Err(Error::Proof(ProofError::UnexpectedResultProof(format!("proof of state transition execution showed a purchase price of {}, whereas we only agreed to {}, state transition is [{}]", purchase_cost, agreed_to_purchase_cost, token_transition))));
                                         }
