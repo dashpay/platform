fe032c0c0ae70a91ed93df3a7b77aa7b4877c139
thephez <thephez@users.noreply.github.com>
2025-06-19 10:21:37 -0400

    chore: remove unused token meta schema and references

--- packages/rs-dpp/src/data_contract/document_type/schema/enrich_with_base_schema/v0/mod.rs
+++ packages/rs-dpp/src/data_contract/document_type/schema/enrich_with_base_schema/v0/mod.rs
@@ -6,8 +6,6 @@ use platform_value::{Value, ValueMapHelper};
 pub const DATA_CONTRACT_SCHEMA_URI_V0: &str =
     "https://github.com/dashpay/platform/blob/master/packages/rs-dpp/schema/meta_schemas/document/v0/document-meta.json";
 
-pub const TOKEN_SCHEMA_URI_V0: &str =
-    "https://github.com/dashpay/platform/blob/master/packages/rs-dpp/schema/meta_schemas/document/v0/token-meta.json";
 
 pub const PROPERTY_SCHEMA: &str = "$schema";
 
