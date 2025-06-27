c7a2b395284796f947ddeb58078c5cfb153b5431
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2025-06-27 11:53:46 +0200

    fix(dashmate): migrations broken

--- packages/dashmate/configs/getConfigFileMigrationsFactory.js
+++ packages/dashmate/configs/getConfigFileMigrationsFactory.js
@@ -554,7 +554,7 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
         Object.entries(configFile.configs)
           .forEach(([name, options]) => {
             if (options.network === NETWORK_TESTNET && name !== 'base') {
-              options.platform.drive.tenderdash.genesis = testnet.get('platform.drive.tenderdash.genesis');
+              options.platform.drive.tenderdash.genesis = lodash.cloneDeep(testnet.get('platform.drive.tenderdash.genesis'));
             }
 
             const defaultConfig = getDefaultConfigByNameOrGroup(name, options.group);
@@ -758,7 +758,7 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
             options.core.devnet.llmq = base.get('core.devnet.llmq');
 
             if (options.network === NETWORK_TESTNET) {
-              options.platform.drive.tenderdash.genesis = testnet.get('platform.drive.tenderdash.genesis');
+              options.platform.drive.tenderdash.genesis = lodash.cloneDeep(testnet.get('platform.drive.tenderdash.genesis'));
             }
           });
         return configFile;
@@ -784,7 +784,7 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
         Object.entries(configFile.configs)
           .forEach(([, options]) => {
             if (options.network === NETWORK_TESTNET) {
-              options.platform.drive.tenderdash.genesis = testnet.get('platform.drive.tenderdash.genesis');
+              options.platform.drive.tenderdash.genesis = lodash.cloneDeep(testnet.get('platform.drive.tenderdash.genesis'));
             }
 
             // Update tenderdash image
@@ -1096,11 +1096,13 @@ export default function getConfigFileMigrationsFactory(homeDir, defaultConfigs) 
           });
         return configFile;
       },
-      '2.0.1': (configFile) => {
+      '2.0.1-rc.1': (configFile) => {
         Object.entries(configFile.configs)
           .forEach(([name, options]) => {
             if (options.network === NETWORK_TESTNET && name !== 'base') {
-              options.platform.drive.tenderdash.genesis = testnet.get('platform.drive.tenderdash.genesis');
+              options.platform.drive.tenderdash.genesis.consensus_params = lodash.cloneDeep(
+                testnet.get('platform.drive.tenderdash.genesis.consensus_params')
+              );
             }
           });
         return configFile;
