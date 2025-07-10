68fbb7fe5d8ae115104a79971851ca8734a42711
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2025-07-10 12:25:56 +0200

    test:ensure keyword search contract has correct base58

--- packages/keyword-search-contract/src/lib.rs
+++ packages/keyword-search-contract/src/lib.rs
@@ -7,8 +7,8 @@ use platform_version::version::PlatformVersion;
 use serde_json::Value;
 
 pub const ID_BYTES: [u8; 32] = [
-    161, 147, 167, 153, 40, 225, 219, 101, 50, 156, 28, 146, 150, 52, 114, 213,
-    56, 154, 106, 15, 79, 66, 18, 156, 94, 146, 216, 104, 140, 93, 170, 215,
+    161, 147, 167, 153, 40, 225, 219, 101, 50, 156, 28, 146, 150, 52, 114, 213, 56, 154, 106, 15,
+    79, 66, 18, 156, 94, 146, 216, 104, 140, 93, 170, 215,
 ];
 
 pub const OWNER_ID_BYTES: [u8; 32] = [0; 32];
@@ -33,5 +33,31 @@ pub fn load_documents_schemas(platform_version: &PlatformVersion) -> Result<Valu
             known_versions: vec![1],
             received: version,
         }),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use base58::FromBase58;
+
+    use super::*;
+
+    #[test]
+    /// Ensure that the ID constant matches the expected value
+    /// and that it can be encoded to base58 correctly.
+    fn test_id() {
+        assert_eq!(
+            ID,
+            Identifier(IdentifierBytes32(ID_BYTES)),
+            "ID should match the expected value"
+        );
+
+        let base58_decoded = "BsjE6tQxG47wffZCRQCovFx5rYrAYYC3rTVRWKro27LA"
+            .from_base58()
+            .unwrap();
+        assert_eq!(
+            base58_decoded, ID_BYTES,
+            "ID should match the base58 decoded value"
+        );
     }
 }
