77bd55a5eab088721a534a360064dbbdd4c7fedb
Quantum Explorer <quantum@dash.org>
2025-09-08 07:55:36 +0700

    fixes

--- packages/strategy-tests/src/operations.rs
+++ packages/strategy-tests/src/operations.rs
@@ -505,14 +505,14 @@ impl TryFromPlatformVersioned<ContestedDocumentResourceVotePollWithSerializableC
             index_values,
         } = value;
         Ok(ContestedDocumentResourceVotePollWithContractInfo {
-            contract: DataContractOwnedResolvedInfo::OwnedDataContract(
+            contract: DataContractOwnedResolvedInfo::OwnedDataContract(Box::new(
                 DataContract::try_from_platform_versioned(
                     contract,
                     false,
                     &mut vec![],
                     platform_version,
                 )?,
-            ),
+            )),
             document_type_name,
             index_name,
             index_values,
