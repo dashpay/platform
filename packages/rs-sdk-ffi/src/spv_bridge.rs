f73203a64a52b6bddcc46a00e40ee45eb304bba8
Quantum Explorer <quantum@dash.org>
2025-09-11 18:37:37 +0700

    wanrnings

--- packages/rs-sdk-ffi/src/spv_bridge.rs
+++ packages/rs-sdk-ffi/src/spv_bridge.rs
@@ -1,9 +1,9 @@
 #![allow(non_camel_case_types)]
 
-#[cfg(feature = "local-dashcore")]
+#[cfg(feature = "dash_spv")]
 use std::os::raw::{c_char, c_int};
 
-#[cfg(feature = "local-dashcore")]
+#[cfg(feature = "dash_spv")]
 #[no_mangle]
 pub extern "C" fn dash_spv_ffi_config_add_peer(
     config: *mut dash_spv_ffi::FFIClientConfig,
@@ -13,7 +13,7 @@ pub extern "C" fn dash_spv_ffi_config_add_peer(
     unsafe { dash_spv_ffi::dash_spv_ffi_config_add_peer(config, addr) }
 }
 
-#[cfg(feature = "local-dashcore")]
+#[cfg(feature = "dash_spv")]
 #[no_mangle]
 pub extern "C" fn dash_spv_ffi_config_set_restrict_to_configured_peers(
     config: *mut dash_spv_ffi::FFIClientConfig,
@@ -22,4 +22,3 @@ pub extern "C" fn dash_spv_ffi_config_set_restrict_to_configured_peers(
     // Safety: Forward call to underlying FFI; pointers are opaque
     unsafe { dash_spv_ffi::dash_spv_ffi_config_set_restrict_to_configured_peers(config, restrict_peers) }
 }
-
