fdda58faffd676582a8799938ad18282a00bb86f
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2025-09-12 08:44:56 +0200

    chore: add subscribePlatformEvents to envoy config

--- packages/rs-dash-notify/src/grpc_producer.rs
+++ packages/rs-dash-notify/src/grpc_producer.rs
@@ -26,13 +26,16 @@ impl GrpcPlatformEventsProducer {
             Into<dapi_grpc::tonic::codegen::StdError> + Send,
     {
         let (cmd_tx, cmd_rx) = mpsc::unbounded_channel::<PlatformEventsCommand>();
+        tracing::debug!("connecting gRPC producer to upstream");
         let resp_stream = client
             .subscribe_platform_events(UnboundedReceiverStream::new(cmd_rx))
             .await?;
         let cmd_sink = unbounded_sender_sink(cmd_tx);
         let resp_rx = resp_stream.into_inner();
 
+        tracing::debug!("registering gRPC producer with mux");
         let producer = mux.add_producer().await;
+        tracing::debug!("gRPC producer connected to mux, starting forward loop");
         producer.forward(cmd_sink, resp_rx).await;
         Ok(())
     }
