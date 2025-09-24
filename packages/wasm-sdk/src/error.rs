fcb3d97a3bf206df30f039521bedd96d121366ff
pauldelucia <pauldelucia2@gmail.com>
2025-09-25 01:06:57 +0700

    chore(dpp): update dashcore revision

--- packages/wasm-sdk/src/error.rs
+++ packages/wasm-sdk/src/error.rs
@@ -169,6 +169,12 @@ impl From<SdkError> for WasmSdkError {
             Cancelled(msg) => Self::new(WasmSdkErrorKind::Cancelled, msg, None, retriable),
             StaleNode(e) => Self::new(WasmSdkErrorKind::StaleNode, e.to_string(), None, retriable),
             StateTransitionBroadcastError(e) => WasmSdkError::from(e),
+            DapiMocksError(e) => Self::new(
+                WasmSdkErrorKind::DapiMocksError,
+                e.to_string(),
+                None,
+                retriable,
+            ),
         }
     }
 }
