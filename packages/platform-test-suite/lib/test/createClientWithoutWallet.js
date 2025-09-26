834f8d9926207265352e2e7d78a2d02976dab4be
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2025-09-26 17:53:42 +0200

    feat(platform-test-suite): disable peer lookup when DAPI_ADDRESSES is provided

--- packages/platform-test-suite/lib/test/createClientWithoutWallet.js
+++ packages/platform-test-suite/lib/test/createClientWithoutWallet.js
@@ -5,8 +5,15 @@ const { contractId } = require('@dashevo/dpns-contract/lib/systemIds');
 const getDAPISeeds = require('./getDAPISeeds');
 
 function createClientWithoutWallet() {
+  const dapiAddresses = (process.env.DAPI_ADDRESSES || '')
+    .split(',')
+    .map((address) => address.trim())
+    .filter(Boolean);
+
   return new Dash.Client({
-    seeds: getDAPISeeds(),
+    ...(dapiAddresses.length > 0
+      ? { dapiAddresses }
+      : { seeds: getDAPISeeds() }),
     network: process.env.NETWORK,
     timeout: 25000,
     apps: {
