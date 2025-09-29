ccd9b40c653e616988db3716480f8e1159ef2537
Lukasz Klimek <842586+lklimek@users.noreply.github.com>
2025-09-29 08:56:29 +0200

    test: add debug info - to revert

--- packages/platform-test-suite/lib/test/bootstrap.js
+++ packages/platform-test-suite/lib/test/bootstrap.js
@@ -6,6 +6,8 @@ const chaiAsPromised = require('chai-as-promised');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
+const wasmDpp = require('@dashevo/wasm-dpp');
+
 use(chaiAsPromised);
 use(dirtyChai);
 use(sinonChai);
@@ -15,6 +17,44 @@ dotenvSafe.config({
 });
 
 process.env.NODE_ENV = 'test';
+
+if (!wasmDpp.deserializeConsensusError.__withSillyDebug) {
+  const originalDeserializeConsensusError = wasmDpp.deserializeConsensusError;
+
+  wasmDpp.deserializeConsensusError = function debugDeserializeConsensusError(bytes, ...args) {
+    const buffer = bytes ? Buffer.from(bytes) : Buffer.alloc(0);
+
+    console.debug('[consensus-error-debug] will deserialize consensus error bytes', {
+      hex: buffer.toString('hex'),
+      base64: buffer.toString('base64'),
+      length: buffer.length,
+      isEmpty: buffer.length === 0,
+    });
+
+    try {
+      const result = originalDeserializeConsensusError.call(this, bytes, ...args);
+
+      const code = typeof result?.getCode === 'function' ? result.getCode() : undefined;
+
+      console.debug('[consensus-error-debug] deserialized consensus error result', {
+        name: result?.constructor?.name,
+        code,
+        message: result?.message,
+      });
+
+      return result;
+    } catch (e) {
+      console.error('[consensus-error-debug] failed to deserialize consensus error', {
+        errorMessage: e?.message,
+        stack: e?.stack,
+      });
+
+      throw e;
+    }
+  };
+
+  wasmDpp.deserializeConsensusError.__withSillyDebug = true;
+}
 
 let faucetIndex = 1;
 if (process.env.MOCHA_WORKER_ID) {
