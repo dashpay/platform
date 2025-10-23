c9226423670f4cf4a2a9f59489773264fa4a1bdd
thephez <thephez@users.noreply.github.com>
2025-10-23 08:51:27 -0400

    chore: update address list
    Based on https://quorums.testnet.networks.dash.org/masternodes with enabled nodes passing the version check

--- packages/js-dapi-client/lib/networkConfigs.js
+++ packages/js-dapi-client/lib/networkConfigs.js
@@ -13,12 +13,13 @@ module.exports = {
     // that breaks test suite and application logic. Temporary solution is to hardcode
     // reliable DCG testnet masternodes to connect. Should be removed when PoSe is introduced.
     // Generate list with:
-    //  dash-cli -testnet masternodelist evo | jq 'to_entries | map(select(.value.status == "ENABLED") | .value.address | sub(":19999"; ":1443"))'
+    //  curl https://quorums.testnet.networks.dash.org/masternodes | jq 'select(.success) | .data | map(select(.status=="ENABLED" and .versionCheck=="success") | .address | sub(":[0-9]+$"; ":1443")) | sort_by( split(":")[0] | split(".") | map(tonumber) )'
     dapiAddressesWhiteList: [
       '34.214.48.68:1443',
       '35.82.197.197:1443',
       '35.85.21.179:1443',
       '35.163.144.230:1443',
+      '35.164.23.245:1443',
       '35.167.145.149:1443',
       '44.227.137.77:1443',
       '44.228.242.181:1443',
@@ -26,17 +27,16 @@ module.exports = {
       '44.240.98.102:1443',
       '52.10.229.11:1443',
       '52.12.176.90:1443',
+      '52.13.132.146:1443',
       '52.24.124.162:1443',
       '52.33.28.47:1443',
       '52.34.144.50:1443',
-      '52.40.219.41:1443',
       '52.43.13.92:1443',
       '52.43.86.231:1443',
-      '52.89.154.48:1443',
       '54.68.235.201:1443',
       '54.149.33.167:1443',
       '54.187.14.232:1443',
-      '54.201.32.131:1443',
+      '54.201.32.131:1443'
     ],
   },
   local: {
