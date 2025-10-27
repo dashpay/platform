fe405192b37aa8a79c70520cf173639b50af3b15
lklimek <842586+lklimek@users.noreply.github.com>
2025-10-27 13:15:38 +0100

    fix(dashmate): rs-dapi not stopped when dashmate reset --platform -f is called (#2824)

--- packages/dashmate/src/config/getConfigProfilesFactory.js
+++ packages/dashmate/src/config/getConfigProfilesFactory.js
@@ -8,13 +8,24 @@ export default function getConfigProfilesFactory() {
    * @param {{ includeAll?: boolean }} [options]
    * @returns {string[]}
    */
-  function getConfigProfiles(config) {
+  function getConfigProfiles(config, { includeAll = false } = {}) {
     const profiles = [];
 
     profiles.push('core');
 
     if (config.get('platform.enable')) {
       profiles.push('platform');
+
+      const deprecatedEnabled = config.get('platform.dapi.deprecated.enabled');
+
+      if (includeAll) {
+        profiles.push('platform-dapi-deprecated');
+        profiles.push('platform-dapi-rs');
+      } else if (deprecatedEnabled) {
+        profiles.push('platform-dapi-deprecated');
+      } else {
+        profiles.push('platform-dapi-rs');
+      }
     }
 
     return Array.from(new Set(profiles));
