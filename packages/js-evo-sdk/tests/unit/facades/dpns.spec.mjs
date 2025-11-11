e7c15c469349444eafeaa967ce6695a575025aa2
Ivan Shumkov <ivanshumkov@gmail.com>
2025-11-11 23:12:29 +0700

    tests: update evo sdk tests for new API

--- packages/js-evo-sdk/tests/unit/facades/dpns.spec.mjs
+++ packages/js-evo-sdk/tests/unit/facades/dpns.spec.mjs
@@ -37,9 +37,9 @@ describe('DPNSFacade', () => {
     await client.dpns.registerName({
       label: 'l', identityId: 'i', publicKeyId: 1, privateKeyWif: 'w',
     });
-    await client.dpns.usernames('i', { limit: 2 });
+    await client.dpns.usernames({ identityId: 'i', limit: 2 });
     await client.dpns.username('i');
-    await client.dpns.usernamesWithProof('i', { limit: 3 });
+    await client.dpns.usernamesWithProof({ identityId: 'i', limit: 3 });
     await client.dpns.usernameWithProof('i');
     await client.dpns.getUsernameByName('u');
     await client.dpns.getUsernameByNameWithProof('u');
