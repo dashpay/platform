34ec88efb73415a9ae6c8ea0b0593d056a7143d1
Paul DeLucia <69597248+pauldelucia@users.noreply.github.com>
2025-12-17 16:44:11 +0700

    refactor(sdk): remove SimpleAddressSigner in favor of Signer<PlatformAddress> (#2904)

--- packages/simple-signer/src/lib.rs
+++ packages/simple-signer/src/lib.rs
@@ -1,11 +1,6 @@
 #[cfg(feature = "state-transitions")]
 pub mod signer;
 
-#[cfg(feature = "state-transitions")]
-pub mod simple_address_signer;
-
 pub mod single_key_signer;
 
-#[cfg(feature = "state-transitions")]
-pub use simple_address_signer::SimpleAddressSigner;
 pub use single_key_signer::SingleKeySigner;
