f9b8b22871ddb19acf0dea3617c883a6a3a294a4
Quantum Explorer <quantum@dash.org>
2026-01-06 10:54:43 +0700

    fixes

--- packages/rs-drive-abci/src/execution/platform_events/protocol_upgrade/perform_events_on_first_block_of_protocol_change/v0/mod.rs
+++ packages/rs-drive-abci/src/execution/platform_events/protocol_upgrade/perform_events_on_first_block_of_protocol_change/v0/mod.rs
@@ -23,7 +23,10 @@ use drive::drive::identity::withdrawals::paths::{
     WITHDRAWAL_TRANSACTIONS_SUM_AMOUNT_TREE_KEY,
 };
 use drive::drive::prefunded_specialized_balances::prefunded_specialized_balances_for_voting_path_vec;
-use drive::drive::saved_block_transactions::ADDRESS_BALANCES_KEY_U8;
+use drive::drive::saved_block_transactions::{
+    ADDRESS_BALANCES_KEY_U8, COMPACTED_ADDRESSES_EXPIRATION_TIME_KEY_U8,
+    COMPACTED_ADDRESS_BALANCES_KEY_U8,
+};
 use drive::drive::system::misc_path;
 use drive::drive::tokens::paths::{
     token_distributions_root_path, token_timed_distributions_path, tokens_root_path,
@@ -569,6 +572,24 @@ impl<C> Platform<C> {
         self.drive.grove_insert_if_not_exists(
             saved_block_path.as_slice().into(),
             &[ADDRESS_BALANCES_KEY_U8],
+            Element::empty_count_sum_tree(),
+            Some(transaction),
+            None,
+            &platform_version.drive,
+        )?;
+
+        self.drive.grove_insert_if_not_exists(
+            saved_block_path.as_slice().into(),
+            &[COMPACTED_ADDRESS_BALANCES_KEY_U8],
+            Element::empty_tree(),
+            Some(transaction),
+            None,
+            &platform_version.drive,
+        )?;
+
+        self.drive.grove_insert_if_not_exists(
+            saved_block_path.as_slice().into(),
+            &[COMPACTED_ADDRESSES_EXPIRATION_TIME_KEY_U8],
             Element::empty_tree(),
             Some(transaction),
             None,
