ca241c4a045319bba3de085dbb5937d8bfd98c4e
Borja Castellano <borja.castellano@dash.org>
2026-02-06 16:41:22 -0600

    dropped withTimeout helper in WalletService

--- packages/swift-sdk/Sources/SwiftDashSDK/Core/Services/WalletService.swift
+++ packages/swift-sdk/Sources/SwiftDashSDK/Core/Services/WalletService.swift
@@ -2,30 +2,6 @@ import Foundation
 import SwiftData
 import Combine
 
-// MARK: - Timeout Helper
-
-struct TimeoutError: Error {}
-
-func withTimeout<T: Sendable>(seconds: TimeInterval, operation: @escaping @Sendable () async throws -> T) async throws -> T {
-    try await withThrowingTaskGroup(of: T.self) { group in
-        // Add the actual operation
-        group.addTask {
-            try await operation()
-        }
-
-        // Add timeout task
-        group.addTask {
-            try await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
-            throw TimeoutError()
-        }
-
-        // Return first result (either completion or timeout)
-        let result = try await group.next()!
-        group.cancelAll()
-        return result
-    }
-}
-
 // MARK: - Logging Preferences
 
 public enum LoggingPreset: String {
