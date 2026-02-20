cd855148684723c065b4b027bacc499a62a3b700
PastaClaw <thepastaclaw@users.noreply.github.com>
2026-02-20 10:30:58 -0600

    test(rs-dpp): restore DataContractUpdateTransition JSON conversion test

--- packages/rs-dpp/src/state_transition/state_transitions/contract/data_contract_update_transition/json_conversion.rs
+++ packages/rs-dpp/src/state_transition/state_transitions/contract/data_contract_update_transition/json_conversion.rs
@@ -25,47 +25,61 @@ impl StateTransitionJsonConvert<'_> for DataContractUpdateTransition {
         }
     }
 }
-//
-// #[cfg(test)]
-// mod test {
-//     use crate::state_transition::data_contract_update_transition::STATE_TRANSITION_PROTOCOL_VERSION;
-//     use crate::state_transition::JsonStateTransitionSerializationOptions;
-//
-//     #[test]
-//     fn should_return_state_transition_in_json_format() {
-//         let data = get_test_data();
-//         let mut json_object = data
-//             .state_transition
-//             .to_json(JsonStateTransitionSerializationOptions {
-//                 skip_signature: false,
-//                 into_validating_json: false,
-//             })
-//             .expect("conversion to JSON shouldn't fail");
-//
-//         assert_eq!(
-//             0,
-//             json_object
-//                 .get_u64(STATE_TRANSITION_PROTOCOL_VERSION)
-//                 .expect("the protocol version should be present") as u32
-//         );
-//
-//         assert_eq!(
-//             4,
-//             json_object
-//                 .get_u64(TRANSITION_TYPE)
-//                 .expect("the transition type should be present") as u8
-//         );
-//         assert_eq!(
-//             0,
-//             json_object
-//                 .get_u64(SIGNATURE_PUBLIC_KEY_ID)
-//                 .expect("default public key id should be defined"),
-//         );
-//         assert_eq!(
-//             "",
-//             json_object
-//                 .remove_into::<String>(SIGNATURE)
-//                 .expect("default string value for signature should be present")
-//         );
-//     }
-// }
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::state_transition::data_contract_update_transition::DataContractUpdateTransition;
+    use crate::state_transition::JsonStateTransitionSerializationOptions;
+    use crate::tests::fixtures::get_data_contract_fixture;
+    use platform_version::version::PlatformVersion;
+    use platform_version::TryIntoPlatformVersioned;
+
+    fn get_test_data() -> DataContractUpdateTransition {
+        let platform_version = PlatformVersion::first();
+        let data_contract = get_data_contract_fixture(None, 0, platform_version.protocol_version)
+            .data_contract_owned();
+
+        (data_contract, 1)
+            .try_into_platform_versioned(platform_version)
+            .expect("expected to get transition")
+    }
+
+    #[test]
+    fn should_return_state_transition_in_json_format() {
+        let mut json_object = get_test_data()
+            .to_json(JsonStateTransitionSerializationOptions {
+                skip_signature: false,
+                into_validating_json: false,
+            })
+            .expect("conversion to JSON shouldn't fail");
+
+        assert_eq!(
+            Some(0),
+            json_object
+                .get(STATE_TRANSITION_PROTOCOL_VERSION)
+                .and_then(JsonValue::as_u64)
+                .map(|v| v as u32),
+            "the protocol version should be present",
+        );
+        assert_eq!(
+            Some(4),
+            json_object
+                .get(TRANSITION_TYPE)
+                .and_then(JsonValue::as_u64)
+                .map(|v| v as u8),
+            "the transition type should be present",
+        );
+        assert_eq!(
+            Some(0),
+            json_object
+                .get(SIGNATURE_PUBLIC_KEY_ID)
+                .and_then(JsonValue::as_u64),
+            "default public key id should be defined",
+        );
+        assert_eq!(
+            Some(""),
+            json_object.get(SIGNATURE).and_then(JsonValue::as_str),
+            "default string value for signature should be present",
+        );
+    }
+}
